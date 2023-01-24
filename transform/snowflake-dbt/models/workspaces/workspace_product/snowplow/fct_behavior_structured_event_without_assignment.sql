ece05a25406d30cc6ba552817c03931f30364c1c
Chris Sharp <csharp@gitlab.com>
2023-01-24 16:37:48 +0000

    add sync_all_columns config

--- transform/snowflake-dbt/models/workspaces/workspace_product/snowplow/fct_behavior_structured_event_without_assignment.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_product/snowplow/fct_behavior_structured_event_without_assignment.sql
@@ -1,6 +1,7 @@
 {{ config(
         materialized = "incremental",
-        unique_key = "behavior_structured_event_pk"
+        unique_key = "behavior_structured_event_pk",
+        on_schema_change='sync_all_columns'
 ) }}
 
 {{ 
