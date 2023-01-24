dd1d5556d38999109740d479a476a11d0cffc860
Lis Vinueza <lvinueza@gitlab.com>
2023-01-24 18:50:20 +1100

    Fix duplicates

--- transform/snowflake-dbt/models/legacy/google_analytics_360/intermediate/ga360_session.sql
+++ transform/snowflake-dbt/models/legacy/google_analytics_360/intermediate/ga360_session.sql
@@ -1,28 +1,41 @@
 WITH source AS (
+
 	SELECT *
 	FROM {{ ref('ga360_session_source') }}
 
 ), custom_dimensions AS (
 
-	SELECT *
+	SELECT visit_id, visitor_id, visit_start_time, dimension_value, dimension_name
 	FROM {{ ref('ga360_session_custom_dimension_xf') }}
 	
 ), joined AS (
 
-	SELECT 
-		source.*,
-		OBJECT_AGG(dims.dimension_name, dims.dimension_value::VARIANT)
-    		OVER (PARTITION BY source.visit_id, source.visitor_id, source.visit_start_time) AS custom_dimensions
-	FROM source
-	LEFT JOIN custom_dimensions AS dims
-	ON dims.visit_id = source.visit_id 
-		AND dims.visitor_id = source.visitor_id 
-		    AND dims.visit_start_time = source.visit_start_time
-	GROUP BY source.visit_id, source.visitor_id, source.visit_start_time,dims.dimension_name, dims.dimension_value
-	
+    SELECT 
+        source.visit_id, 
+        source.visitor_id, 
+        source.visit_start_time,
+        OBJECT_AGG(dims.dimension_name, dims.dimension_value::VARIANT)
+            OVER (PARTITION BY source.visit_id, source.visitor_id, source.visit_start_time) AS custom_dimensions
+    FROM source
+    LEFT JOIN custom_dimensions dims
+    ON dims.visit_id = source.visit_id 
+            AND dims.visitor_id = source.visitor_id 
+                AND dims.visit_start_time = source.visit_start_time
+    GROUP BY source.visit_id, source.visitor_id, source.visit_start_time, dims.dimension_name, dims.dimension_value
+
+), final AS (
+
+    SELECT  
+        source.*,
+        joined.custom_dimensions
+    FROM source
+    LEFT JOIN joined
+    ON joined.visit_id = source.visit_id 
+            AND joined.visitor_id = source.visitor_id 
+                AND joined.visit_start_time = source.visit_start_time
+    {{ dbt_utils.group_by(n=31)}}
+
 )
 
-
-
 SELECT *
-FROM joined
+FROM final
