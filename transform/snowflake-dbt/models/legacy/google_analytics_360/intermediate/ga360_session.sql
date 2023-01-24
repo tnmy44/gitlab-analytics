d3a69c596882b0297802307523efe88bfd346fcd
Lis Vinueza <lvinueza@gitlab.com>
2023-01-24 04:52:34 +0000

    Add object aggregate function

--- transform/snowflake-dbt/models/legacy/google_analytics_360/intermediate/ga360_session.sql
+++ transform/snowflake-dbt/models/legacy/google_analytics_360/intermediate/ga360_session.sql
@@ -11,16 +11,18 @@ WITH source AS (
 
 	SELECT 
 		source.*,
-		dims.dimension_index,
-		dims.dimension_name,
-		dims.dimension_value
+		OBJECT_AGG(dims.dimension_name, dims.dimension_value::VARIANT)
+    		OVER (PARTITION BY source.visit_id, source.visitor_id, source.visit_start_time) AS custom_dimensions
 	FROM source
 	LEFT JOIN custom_dimensions AS dims
 	ON dims.visit_id = source.visit_id 
 		AND dims.visitor_id = source.visitor_id 
 		    AND dims.visit_start_time = source.visit_start_time
-
+	GROUP BY source.visit_id, source.visitor_id, source.visit_start_time,dims.dimension_name, dims.dimension_value
+	
 )
+
+
 
 SELECT *
 FROM joined
