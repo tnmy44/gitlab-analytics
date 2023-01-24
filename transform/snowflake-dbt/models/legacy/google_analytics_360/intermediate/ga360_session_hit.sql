54840a4dc44738e23a47a8d2d663a0b7a03d0dcd
Lis Vinueza <lvinueza@gitlab.com>
2023-01-24 18:50:38 +1100

    Fix duplicates in session_hit

--- transform/snowflake-dbt/models/legacy/google_analytics_360/intermediate/ga360_session_hit.sql
+++ transform/snowflake-dbt/models/legacy/google_analytics_360/intermediate/ga360_session_hit.sql
@@ -10,17 +10,31 @@ WITH source AS (
 ), joined AS (
 
 	SELECT 
-		source.*,
-		dims.dimension_index,
-		dims.dimension_name,
-		dims.dimension_value
+		source.visit_id, 
+        source.visitor_id, 
+        source.hit_number,
+        OBJECT_AGG(dims.dimension_name, dims.dimension_value::VARIANT)
+            OVER (PARTITION BY source.visit_id, source.visitor_id, source.hit_number) AS custom_dimensions
 	FROM source
 	LEFT JOIN custom_dimensions AS dims
 	ON dims.visit_id = source.visit_id 
 		AND dims.visitor_id = source.visitor_id 
 		    AND dims.hit_number = source.hit_number
+    GROUP BY source.visit_id, source.visitor_id, source.hit_number, dims.dimension_name, dims.dimension_value
+
+), final AS (
+
+    SELECT  
+        source.*,
+        joined.custom_dimensions
+    FROM source
+    LEFT JOIN joined
+    ON joined.visit_id = source.visit_id 
+            AND joined.visitor_id = source.visitor_id 
+                AND joined.hit_number = source.hit_number
+    {{ dbt_utils.group_by(n=18)}}
 
 )
 
 SELECT *
-FROM joined
+FROM final
