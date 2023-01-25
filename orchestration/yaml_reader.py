911e6610c7e9429e678bb094572f6e5c63513ebb
Paul Armstrong <parmstrong@gitlab.com>
2023-01-25 10:47:49 +0200

    Switch paths to parameters; debugging lines.

--- orchestration/yaml_reader.py
+++ orchestration/yaml_reader.py
@@ -4,9 +4,9 @@ from fire import Fire
 
 class YamlReader:
 
-    def read_seed_schema(self):
+    def read_seed_schema(self, dbt_project_path):
 
-        with open("../transform/snowflake-dbt/dbt_project.yml", "r") as stream:
+        with open(dbt_project_path, "r") as stream:
             try:
                 seed_schema = yaml.safe_load(stream).get('seeds').get('+schema')
                 return seed_schema
@@ -14,8 +14,8 @@ class YamlReader:
                 print(exc)
 
 
-    def read_seed_name(self):
-        with open("../transform/snowflake-dbt/data/seeds.yml", "r") as stream:
+    def read_seed_name(self, seed_file_path):
+        with open(seed_file_path, "r") as stream:
             try:
                 seed_name = yaml.safe_load(stream).get('seeds')[0]['name']
                 return seed_name
