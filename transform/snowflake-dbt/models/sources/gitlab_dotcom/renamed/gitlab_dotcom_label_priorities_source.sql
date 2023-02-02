86d4162aded6ffbb44d742369fb77fd3d24086ce
Snehansh Chaturvedi <schaturvedi@gitlab.com>
2023-02-02 13:28:57 +0530

    Update label priorities  source

--- transform/snowflake-dbt/models/sources/gitlab_dotcom/renamed/gitlab_dotcom_label_priorities_source.sql
+++ transform/snowflake-dbt/models/sources/gitlab_dotcom/renamed/gitlab_dotcom_label_priorities_source.sql
@@ -10,7 +10,6 @@ WITH source AS (
       id::NUMBER                           AS label_priority_id,
       project_id::NUMBER                   AS project_id,
       label_id::NUMBER                     AS label_id,
-      priority::NUMBER                     AS priority,
       created_at::TIMESTAMP                 AS created_at,
       updated_at::TIMESTAMP                 AS updated_at
 
