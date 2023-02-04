0103ed3f78fbf3e5d4c13ddacd0ff6996f6c7e7a
ved prakash <vprakash@gitlab.com>
2023-02-04 12:15:14 +0100

    upload query

--- extract/zuora_revenue_report/load_zuora_revenue_report.py
+++ extract/zuora_revenue_report/load_zuora_revenue_report.py
@@ -30,12 +30,40 @@ def get_gcs_storage_client():
 
 
 def get_files_for_report(bucket,output_file_name):
+    """
+        This function provide all the file under zuora_revenue_report.
+    """
     source_bucket = get_gcs_storage_client().list_blobs(bucket,prefix="RAW_DB/staging/zuora_revenue_report",delimiter=None)
-    file_name = [file.name for file in source_bucket]
-    print(file_name)
-    
 
+    list_of_files_per_report=[]
+    for file in source_bucket:
+        if output_file_name in file:
+            list_of_files_per_report.append(file)
+
+    return list_of_files_per_report
     
+def get_table_to_load(file_name,output_file_name) -> str:
+    """ This function provide information to which table to load the data"""
+    file_name_without_path=file_name.split("/")[-1]
+    if file_name_without_path.startswith('header'):
+           table_to_load=output_file_name+'_header'
+           type_of_load='header'
+    else:
+            table_to_load=output_file_name+'_body'
+            type_of_load='body'
+
+    print(f"This is the table we load file {file_name}:{table_to_load}")
+    return table_to_load,type_of_load
+
+def load_report_header_snow(schema,file_name,table_to_load,engine):
+
+    copy_header_query=f"COPY INTO {schema}.{table_to_load} from (SELECT REGEXP_REPLACE(t.$1,'( ){1,}','_') AS metadata_column_name, REGEXP_REPLACE(CONCAT(t.$2,COALESCE(t.$3,'')),'(=){1,}','') AS metadata_column_value ,                        GET(SPLIT(METADATA$FILENAME,'/'),4)  AS file_name FROM @raw.zuora_revenue.ZUORA_REVENUE_STAGING/{file_name} t ) file_format = (type = csv,field_DELIMITER=',') ;"
+    print(copy_header_query)
+
+def load_report_body_snow(chema,file_name,table_to_load,engine):
+    copy_body_query=f""
+
+
 
 def zuora_revenue_report_load(
     bucket: str,
@@ -43,17 +71,23 @@ def zuora_revenue_report_load(
     output_file_name: str,
     conn_dict: Dict[str, str] = None,
 ) -> None:
-
     
     # Set some vars
     engine = snowflake_engine_factory(conn_dict or env, "LOADER", schema)
-    
-    if output_file_name.startswith('header'):
-        table_to_load=output_file_name+'_header'
-    else:
-        table_to_load=output_file_name+'_body'
+    # get the filename for the report type from bucket
+    list_of_files=get_files_for_report(bucket,output_file_name)
+    print(f"List of files to download for : {list_of_files}")
+    for file_name in list_of_files:
+        table_to_load,type_of_load=get_table_to_load(file_name,output_file_name)
+        if type_of_load=='header':
+            load_report_header_snow(schema,file_name,table_to_load,engine)
+        else:
+            load_report_body_snow(schema,file_name,table_to_load,engine)
 
-    get_files_for_report(bucket,output_file_name)
+
+        
+
+    
 
 if __name__ == "__main__":
     logging.basicConfig(stream=sys.stdout, level=20)
