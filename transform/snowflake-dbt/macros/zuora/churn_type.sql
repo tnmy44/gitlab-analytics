2b5e04454dbbf1c2ae57292a23e36b69b8110a8a
Paul Armstrong <parmstrong@gitlab.com>
2023-02-06 10:10:36 +0200

    Remove debugging change.

--- transform/snowflake-dbt/macros/zuora/churn_type.sql
+++ transform/snowflake-dbt/macros/zuora/churn_type.sql
@@ -1,5 +1,5 @@
 {%- macro churn_type(original_mrr, new_mrr) -%}
--- test
+
 	CASE WHEN {{ new_mrr }} = 0 and {{ original_mrr }} > 0 THEN 'Cancelled'
 		 WHEN {{ new_mrr }} < {{ original_mrr }} AND {{ new_mrr }} > 0 THEN 'Downgraded'
 		 WHEN {{ new_mrr }} > {{ original_mrr }} THEN 'Upgraded'
