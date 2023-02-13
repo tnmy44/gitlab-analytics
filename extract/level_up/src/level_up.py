5fed8424d149b1ecb71d4b0c0cc8cfc6b956ec89
Justin Wong <juwong@gitlab.com>
2023-02-13 17:10:51 -0500

    level up init

---
+++ extract/level_up/src/level_up.py
@@ -0,0 +1,159 @@
+"""
+ThoughtIndustries is the name of the vendor that provides
+GitLab with Learning Management System internally known as Level Up.
+
+The code will refer to ThoughtIndustries when referring to the API,
+and Level Up when referring to the schemas/tables to save.
+
+There is one parent class- ThoughtIndustries- and for each API endpoint,
+a corresponding child class.
+
+The parent class contains the bulk of the logic as the endpoints are very similiar.
+"""
+import os
+
+from abc import ABC, abstractmethod
+from logging import info, basicConfig, getLogger, error
+from typing import Any, Dict
+
+from level_up_helpers import make_request, iso8601_to_epoch_ts_ms, upload_payload_to_snowflake, is_invalid_ms_timestamp
+
+config_dict = os.environ.copy()
+
+
+class ThoughtIndustries(ABC):
+    BASE_URL = 'https://levelup.gitlab.com/'
+    HEADERS = {
+        'Authorization': f'Bearer {config_dict["THOUGHT_INDUSTRIES_API_KEY"]}'
+    }
+
+    @abstractmethod
+    def get_endpoint_url(self):
+        """ Each child class must implement """
+        pass
+
+    @abstractmethod
+    def get_name(self):
+        """ Each child class must implement """
+        pass
+
+    def __init__(self):
+        """ Instantiate instance vars """
+        self.name = self.get_name()
+        self.endpoint_url = self.get_endpoint_url()
+
+    def fetch_from_endpoint(
+        self, original_epoch_start: int, original_epoch_end: int
+    ) -> Dict[Any, Any]:
+        """
+        Based on the start & end epoch dates, continue calling the API
+        until no more data is returned.
+
+        Note that the API returns data from latest -> earliest, and only returns 100 records per request.
+
+        The sliding window of start/end times looks like this:
+        Start ————————— end  # first request
+        Start ————— prev_min  # 2nd request
+        Start —- prev_min # 3rd request
+        ...
+        """
+        final_events = []
+        events = ['_']  # some placeholder val
+        current_epoch_end = original_epoch_end  # init current epoch end
+
+        # while the response returns events records
+        while len(events) > 0:
+            params = {
+                'startDate': original_epoch_start,
+                'endDate': current_epoch_end
+            }
+            full_url = f'{self.BASE_URL}{self.endpoint_url}'
+            print(f'\nMaking request to {full_url} with params:\n{params}')
+            response = make_request(
+                "GET", full_url, headers=self.HEADERS, params=params
+            )
+
+            events = response.json()['events']
+            if events:
+                events_to_print = [events[0]] + [events[-1]]
+                print(f'\nevents_to_print: {events_to_print}')
+                final_events = final_events + events
+
+                # get the earliest event from latest response
+                # it should be the last one, since api returns from latest to earliest
+                current_epoch_end = iso8601_to_epoch_ts_ms(
+                    events[-1]['timestamp']
+                )
+            else:
+                print('\nThe last response had 0 events, stopping requests\n')
+
+        return final_events
+
+    def upload_events_to_snowflake(self, events: Dict[Any, Any]):
+        """ Upload event payload to Snowflake """
+
+        schema_name = 'level_up'
+        stage_name = 'level_up_load_stage'
+        table_name = self.name
+        json_dump_filename = f"level_up_{self.name}.json"
+        upload_payload_to_snowflake(
+            events, schema_name, stage_name, table_name, json_dump_filename
+        )
+
+    def fetch_and_upload_data(self, epoch_start: int, epoch_end: int) -> Dict[Any, Any]:
+        """ main function, fetch data from API, and upload to snowflake """
+        if is_invalid_ms_timestamp(epoch_start, epoch_end):
+            raise ValueError(
+                'Invalid epoch timestamp(s) passed in. Make sure epoch timestamp is in MILLISECONDS. Aborting now...'
+            )
+        events = self.fetch_from_endpoint(epoch_start, epoch_end)
+        self.upload_events_to_snowflake(events)
+        return events
+
+
+class CourseCompletions(ThoughtIndustries):
+    def get_name(self) -> str:
+        """ implement abstract class """
+        return 'course_completions'
+
+    def get_endpoint_url(self) -> str:
+        """ implement abstract class """
+        return "incoming/v2/events/courseCompletion"
+
+
+class Logins(ThoughtIndustries):
+    def get_name_url(self) -> str:
+        """ implement abstract class """
+        return "logins"
+
+    def get_endpoint_url(self) -> str:
+        """ implement abstract class """
+        return "incoming/v2/events/login"
+
+
+class Visits(ThoughtIndustries):
+    def get_name_url(self) -> str:
+        """ implement abstract class """
+        return "visits"
+
+    def get_endpoint_url(self) -> str:
+        """ implement abstract class """
+        return "incoming/v2/events/visit"
+
+
+class CourseViews(ThoughtIndustries):
+    def get_name_url(self) -> str:
+        """ implement abstract class """
+        return "course_views"
+
+    def get_endpoint_url(self) -> str:
+        """ implement abstract class """
+        return "incoming/v2/events/courseView"
+
+
+if __name__ == '__main__':
+    epoch_start = 1675904400000
+    epoch_end = 1676246400000
+    level_up_obj = CourseCompletions()
+    events = level_up_obj.fetch_and_upload_data(epoch_start, epoch_end)
+    print(f'\nevents: {events[:10]}')
---
+++ extract/level_up/src/level_up_helpers.py
@@ -0,0 +1,124 @@
+"""
+Helpers to the main level_up module
+"""
+import os
+import datetime
+import time
+import json
+
+from typing import Any, Dict, Optional
+from logging import error
+
+import requests
+import dateutil.parser
+
+from gitlabdata.orchestration_utils import (
+    snowflake_stage_load_copy_remove,
+    snowflake_engine_factory,
+)
+
+config_dict = os.environ.copy()
+
+
+def upload_payload_to_snowflake(
+    payload: Dict[Any, Any],
+    schema_name: str,
+    stage_name: str,
+    table_name: str,
+    json_dump_filename: str = 'to_upload.json'
+):
+    """
+    Upload payload to Snowflake using snowflake_stage_load_copy_remove()
+    """
+    loader_engine = snowflake_engine_factory(config_dict, "LOADER")
+    with open(json_dump_filename, "w", encoding="utf8") as upload_file:
+        json.dump(payload, upload_file)
+
+    snowflake_stage_load_copy_remove(
+        json_dump_filename,
+        f"{schema_name}.{stage_name}",
+        f"{schema_name}.{table_name}",
+        loader_engine,
+    )
+    loader_engine.dispose()
+
+
+def iso8601_to_epoch_ts_ms(iso8601_timestamp: str) -> int:
+    """
+    Converts a string representation of a timestamp in the ISO-8601 format
+    to an epoch timestamp in **milliseconds**.
+
+    Args:
+    timestamp (str): The string representation of a timestamp in the ISO-8601 format,
+    i.e "2023-02-10T16:44:45.084Z"
+
+    Returns:
+    int: Epoch timestamp, i.e number of seconds elapsed since 1/1/1970
+    """
+    date_time = dateutil.parser.isoparse(iso8601_timestamp)
+    # 1/1/1970
+    dt_epoch_beginning = datetime.datetime.utcfromtimestamp(0).replace(
+        tzinfo=datetime.timezone.utc
+    )
+
+    delta = date_time - dt_epoch_beginning
+    epoch_ts = delta.total_seconds()
+    epoch_ts_ms = int(epoch_ts * 1000)
+    return epoch_ts_ms
+
+
+def make_request(
+    request_type: str,
+    url: str,
+    headers: Optional[Dict[Any, Any]] = None,
+    params: Optional[Dict[Any, Any]] = None,
+    json_body: Optional[Dict[Any, Any]] = None,
+    timeout: int = 60,
+    current_retry_count: int = 0,
+    max_retry_count: int = 3,
+) -> requests.models.Response:
+    """Generic function that handles making GET and POST requests"""
+    if current_retry_count >= max_retry_count:
+        raise Exception(f"Too many retries when calling the {url}")
+    try:
+        if request_type == "GET":
+            response = requests.get(
+                url, headers=headers, params=params, timeout=timeout
+            )
+        elif request_type == "POST":
+            response = requests.post(
+                url, headers=headers, json=json_body, timeout=timeout
+            )
+        else:
+            raise ValueError("Invalid request type")
+
+        response.raise_for_status()
+        return response
+    except requests.exceptions.RequestException:
+        if response.status_code == 429:
+            retry_after = int(response.headers["Retry-After"])
+            time.sleep(retry_after)
+            current_retry_count += 1
+            # Make the request again
+            return make_request(
+                request_type=request_type,
+                url=url,
+                headers=headers,
+                params=params,
+                json_body=json_body,
+                timeout=timeout,
+                current_retry_count=current_retry_count,
+                max_retry_count=max_retry_count,
+            )
+        error(f"request exception for url {url}, see below")
+        raise
+
+
+def is_invalid_ms_timestamp(epoch_start, epoch_end):
+    """
+    Checks if timestamp in milliseconds > 9/9/2001
+    More info here: https://stackoverflow.com/a/23982005
+    """
+    if len(str(epoch_start)) < 13 or (len(str(epoch_end)) < 13):
+        return True
+    return False
