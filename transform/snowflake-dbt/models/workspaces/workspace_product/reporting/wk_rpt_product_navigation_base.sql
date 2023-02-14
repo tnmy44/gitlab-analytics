3d66b1c2769c06585fa0f09a13b0b6d82ef01d11
Matthew Petersen <mpetersen@gitlab.com>
2023-02-14 00:43:33 +0000

    Adding missing events from audit and increasing date ranges

--- transform/snowflake-dbt/models/workspaces/workspace_product/reporting/wk_rpt_product_navigation_base.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_product/reporting/wk_rpt_product_navigation_base.sql
@@ -17,13 +17,13 @@ WITH filtered_snowplow_events AS (
     THEN 'group_dropdown_frequent_items_list_item'
     WHEN 
     event_label LIKE 'groups_dropdown_frequent_items_list_item_%'
-    THEN 'groups_dropdown_frequent_items_list_item'    
+    THEN 'group_dropdown_frequent_items_list_item'    
     WHEN
     event_label LIKE 'project_dropdown_frequent_items_list_item_%'
     THEN 'project_dropdown_frequent_items_list_item'
     WHEN
     event_label LIKE 'projects_dropdown_frequent_items_list_item_%'
-    THEN 'projects_dropdown_frequent_items_list_item'
+    THEN 'project_dropdown_frequent_items_list_item'
     ELSE event_label 
     END AS event_label,
     event_property,
@@ -33,15 +33,29 @@ WITH filtered_snowplow_events AS (
     app_id,
     gsc_namespace_id,
     session_id
-  FROM {{ ref('snowplow_structured_events_400') }}
+  FROM {{ ref('snowplow_structured_events_all') }}
   WHERE 
+  derived_tstamp >= '2020-01-01'
+  AND
   (
       (
         event_label IN (
           'main_navigation',
           'profile_dropdown',
           'groups_side_navigation',
-          'kubernetes_sections_tabs'
+          'kubernetes_sections_tabs',
+          'hamburger_menu',
+          'menu_view_all_projects',
+          'menu_view_all_groups',
+          'menu_milestones',
+          'menu_snippets',
+          'menu_environments',
+          'menu_operations',
+          'menu_security',
+          'new_dropdown',
+          'plus_menu_dropdown',
+          'main_navigation',
+          'profile_dropdown'
         )
       ) OR
       (
