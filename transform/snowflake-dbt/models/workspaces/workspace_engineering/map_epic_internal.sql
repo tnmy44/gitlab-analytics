4d08924cc587ede8d31f313deb7c1ba9c3ad86b9
Peter Empey <pempey@gitlab.com>
2023-02-15 10:21:59 -0700

    add internal notes

---
+++ transform/snowflake-dbt/models/workspaces/workspace_engineering/map_epic_internal.sql
@@ -0,0 +1,39 @@
+{{ config({
+        "materialized": "view",
+    })
+}}
+
+WITH
+internal_namespace AS (
+  SELECT *
+  FROM {{ ref('map_namespace_internal') }}
+),
+
+lineage AS (
+  SELECT *
+  FROM {{ ref('gitlab_dotcom_namespace_lineage_scd') }}
+  WHERE is_current = TRUE
+),
+
+epics AS (
+  SELECT *
+  FROM {{ ref('gitlab_dotcom_epics_source') }}
+),
+
+final AS (
+
+  SELECT DISTINCT
+    internal_namespace.ultimate_parent_namespace_id,
+    epics.group_id AS namespace_id,
+    epics.epic_id
+  FROM internal_namespace
+  INNER JOIN lineage
+    ON internal_namespace.ultimate_parent_namespace_id = lineage.ultimate_parent_id
+  LEFT JOIN epics
+    ON lineage.namespace_id = epics.group_id
+  WHERE epics.group_id IS NOT NULL
+
+)
+
+SELECT *
+FROM final
