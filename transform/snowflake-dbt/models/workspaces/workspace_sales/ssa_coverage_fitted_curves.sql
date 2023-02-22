45547e7e3d15888a97ec611f5695b4e848f89c43
Noel Figuera <nfiguera@gitlab.com>
2023-02-22 20:22:08 +0000

    Update ssa_coverage_fitted_curves.sql removing direct access to table in sales analytics

--- transform/snowflake-dbt/models/workspaces/workspace_sales/ssa_coverage_fitted_curves.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_sales/ssa_coverage_fitted_curves.sql
@@ -4,8 +4,8 @@
 WITH base AS (
 
     SELECT *
-    FROM raw.sales_analytics.xray_curves_qtd_fitted
-    --FROM {{ ref('driveload_ssa_coverage_fitted_curves_source') }}
+    --FROM raw.sales_analytics.xray_curves_qtd_fitted
+    FROM {{ ref('driveload_ssa_coverage_fitted_curves_source') }}
 
 )
 
