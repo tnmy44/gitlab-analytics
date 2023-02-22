47f5fa3410f831c3a26c9429750ec80f7d834bc7
nfiguera <nfiguera@gitlab.com>
2023-02-22 19:44:17 +0100

    updated report fields to use wk sales user adjusted values

--- transform/snowflake-dbt/models/workspaces/workspace_sales/ssa_coverage_fitted_curves.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_sales/ssa_coverage_fitted_curves.sql
@@ -1,7 +1,11 @@
+{{ config(materialized='view') }}
+
+
 WITH base AS (
 
     SELECT *
-    FROM {{ ref('driveload_ssa_coverage_fitted_curves_source') }}
+    FROM raw.sales_analytics.xray_curves_qtd_fitted
+    --FROM {{ ref('driveload_ssa_coverage_fitted_curves_source') }}
 
 )
 
