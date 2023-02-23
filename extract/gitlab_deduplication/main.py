1bc3aeaa95de7d35fa6ca227bd0b26a3163b47be
ved prakash <vprakash@gitlab.com>
2023-02-23 12:07:40 +0100

    Added remaining logic

--- extract/gitlab_deduplication/main.py
+++ extract/gitlab_deduplication/main.py
@@ -1,32 +1,19 @@
-FROM asyncio.log import logger
+""" This the transformation code for deduplication of data of postgres"""
 import logging
-import yaml
-FROM os import environ as env
-FROM datetime import datetime
-FROM fire import Fire
-FROM typing import Dict
-FROM gitlabdata.orchestration_utils import (
+from os import environ as env
+from datetime import datetime
+from fire import Fire
+from typing import Dict
+from gitlabdata.orchestration_utils import (
     snowflake_engine_factory,
     query_executor,
 )
-
-snowflake_engine = snowflake_engine_factory(env, "LOADER")
-
-
-def manifest_reader(file_path: str) -> Dict[str, Dict]:
-    """
-    Read a yaml manifest file into a dictionary and return it.
-    """
-
-    with open(file_path, "r") as file:
-        manifest_dict = yaml.load(file, Loader=yaml.FullLoader)
-
-    return manifest_dict
 
 
 def build_table_name(
     table_name: str, table_prefix: str = None, table_suffix: str = None
 ) -> str:
+    """The function is responsible for adding prefix and suffix"""
     if table_prefix is None and table_suffix is None:
         return table_name
     elif table_prefix is None and table_suffix is not None:
@@ -36,7 +23,9 @@ def build_table_name(
     else:
         return table_prefix + table_name + table_suffix
 
-def create_table_name(manifest_dict,table_name):
+
+def create_table_name(manifest_dict: Dict, table_name: str) -> list:
+    """Prepare the backup table name and original table name as the table name passed in manifest i postgres table name not the snowflake table name"""
     table_suffix = "_" + datetime.now().strftime("%Y%m%d")
     table_prefix = manifest_dict["generic_info"]["table_prefix"]
 
@@ -46,36 +35,40 @@ def create_table_name(manifest_dict,table_name):
     else:
         bkp_table_name = build_table_name(table_name, table_suffix)
         original_table_name = build_table_name(table_name)
-    return bkp_table_name,original_table_name
+    return bkp_table_name, original_table_name
+
 
 def create_backup_table(
     manifest_dict: dict,
     table_name: str,
 ) -> bool:
+    """This function create clone of the table in the backup schema , if the backup has been taken today it self it will abort here."""
     raw_database = manifest_dict["raw_database"]
     backup_schema_name = manifest_dict["generic_info"]["backup_schema"]
     raw_schema = manifest_dict["generic_info"]["raw_schema"]
-    bkp_table_name,original_table_name =create_table_name(manifest_dict,table_name)
+    bkp_table_name, original_table_name = create_table_name(manifest_dict, table_name)
     create_backup_table = f"CREATE TABLE {raw_database}.{backup_schema_name}.{bkp_table_name} CLONE {raw_database}.{raw_schema}.{original_table_name};"
     logging.info(f"create_backup_table")
     query_executor(snowflake_engine, create_backup_table)
     return True
 
+
 def create_temp_table_ddl(manifest_dict: Dict, table_name: str):
     raw_database = manifest_dict["raw_database"]
     raw_schema = manifest_dict["generic_info"]["raw_schema"]
-    original_table_name = build_table_name(table_name, manifest_dict["generic_info"]["table_prefix"])
-    build_ddl_statement= f"""
-                             SELECT CONCAT('CREATE TABLE {original_table_name}_temp\n AS (' ,'SELECT  \n', 
+    bkp_table_name, original_table_name = create_table_name(manifest_dict, table_name)
+    backup_schema = manifest_dict["generic_info"]["backup_schema"]
+    build_ddl_statement = f"""
+                             SELECT CONCAT('CREATE TRANSIENT TABLE {raw_database}.{raw_schema}.{original_table_name}_temp\n AS (' ,'SELECT  \n', 
                                     LISTAGG(
                                             CASE 
                                             WHEN LOWER(column_name) = '_uploaded_at' THEN 'MIN( _uploaded_at) AS _uploaded_at'
-                                            WHEN LOWER(column_name) = '_task_instance' THEN 'MAX(TO_DATE(RIGHT(_task_instance,8), \\'YYYYMMDD\\'))  AS _task_instance'
+                                            WHEN LOWER(column_name) = '_task_instance' THEN 'MAX(_task_instance)  AS _task_instance'
                                             ELSE column_name 
                                             END ,',\n') WITHIN GROUP (ORDER BY ordinal_position ASC) 
-                                            , ' \n FROM {original_table_name}' ,
+                                            , ' \n FROM {raw_database}.{backup_schema}.{bkp_table_name}' ,
                              '\n GROUP BY  \n', 
-                                    (SELECT LISTAGG(column_name,',\n') FROM {raw_database}.information_schema.columns 
+                                    (SELECT LISTAGG(column_name,',\n') WITHIN GROUP (ORDER BY ordinal_position ASC) FROM {raw_database}.information_schema.columns 
                                     WHERE LOWER(table_name) = ('{original_table_name}')   
                                     AND LOWER(column_name) NOT IN ('_uploaded_at','_task_instance')),
                              ' \r\n UNION \n',
@@ -83,43 +76,45 @@ def create_temp_table_ddl(manifest_dict: Dict, table_name: str):
                                     LISTAGG(
                                             CASE 
                                             WHEN LOWER(column_name) = '_uploaded_at' THEN 'MAX( _uploaded_at) AS _uploaded_at'
-                                            WHEN LOWER(column_name) = '_task_instance' THEN 'MAX(TO_DATE(RIGHT(_task_instance,8), \\'YYYYMMDD\\'))  AS _task_instance'
+                                            WHEN LOWER(column_name) = '_task_instance' THEN 'MAX(_task_instance)  AS _task_instance'
                                             ELSE column_name 
                                             END ,',\n') WITHIN GROUP (ORDER BY ordinal_position ASC),
-                             '\n FROM {original_table_name}' ,
+                             '\n FROM {raw_database}.{backup_schema}.{bkp_table_name}' ,
                              '\n GROUP BY  ', 
-                                   (SELECT LISTAGG(column_name,',\n') FROM {raw_database}.information_schema.columns 
+                                   (SELECT LISTAGG(column_name,',\n') WITHIN GROUP (ORDER BY ordinal_position ASC) FROM {raw_database}.information_schema.columns 
                                     WHERE LOWER(table_name) = ('{original_table_name}')   
                                     AND LOWER(column_name) NOT IN ('_uploaded_at','_task_instance')),');')
                                     from {raw_database}.information_schema.columns 
                                     where LOWER(table_name) = '{original_table_name}';"""
-        
-    return build_ddl_statement
 
-def create_temp_table(
-    manifest_dict: dict,
-    table_name: str,
-):
-    create_temp_table_ddl(manifest_dict,table_name)
+    # Create the temporary table definition
+    table_definition_to_create_table = query_executor(
+        snowflake_engine, build_ddl_statement
+    )
+    return table_definition_to_create_table
+
+
+def create_temp_table(manifest_dict: Dict, table_name: str):
+    """This function create the table in the schema"""
+    table_definition = create_temp_table_ddl(manifest_dict, table_name)
+    query_executor(snowflake_engine, table_definition)
     return True
 
 
-def deduplicate_scd_tables(manifest_dict: Dict, table_name: str) -> bool:
-    """
-    Extract the values FROM the manifest
-    """
-    table_prefix = manifest_dict["generic_info"]["table_prefix"]
-    raw_schema = manifest_dict["generic_info"]["raw_schema"]
+def swap_and_drop_temp_table(manifest_dict: Dict, table_name: str):
     raw_database = manifest_dict["raw_database"]
-    # Create backup table
-    create_backup_table(
-        manifest_dict,  table_name
-    )
-    if create_backup_table:
-        create_temp_table(manifest_dict,table_name)
+    raw_schema = manifest_dict["generic_info"]["raw_schema"]
+    bkp_table_name, original_table_name = create_table_name(manifest_dict, table_name)
+    # Swap the table name with main table.
+    temp_table_name = f"{original_table_name}_temp"
+    swap_query = f"ALTER TABLE {raw_database}.{raw_schema}.{temp_table_name} SWAP WITH {raw_database}.{raw_schema}.{original_table_name};"
+    # Drop the temp table in tap_postgres schema
+    drop_query = f"DROP TABLE {raw_database}.{raw_schema}.{temp_table_name};"
+    if query_executor(snowflake_engine, swap_query):
+        query_executor(snowflake_engine, drop_query)
 
 
-def main(file_path: str = "t_gitlab_com_scd_advance_metadata_manifest.yml") -> None:
+def main(manifest_dict: Dict, table_name: str) -> None:
     """
     Read table name FROM manifest file and decide if the table exist in the database. Check if the advance metadata column `_task_instance`
     is present in the table.
@@ -130,15 +125,14 @@ def main(file_path: str = "t_gitlab_com_scd_advance_metadata_manifest.yml") -> N
     Drop the swapped temp table.
     """
     # Process the manifest
-    logging.info(f"Reading manifest at location: {file_path}")
-    manifest_dict = manifest_reader(file_path)
-    scd_tables_list = manifest_dict["scd_tables"]
-    # Add raw database name to the manifest
-    manifest_dict.update({"raw_database": env.copy()["SNOWFLAKE_LOAD_DATABASE"]})
-    # iterate through each table and check if it exist
-    for table in scd_tables_list:
-        logging.info(f"Proceeding with table {table} for deduplication")
-        deduplicate_scd_tables(manifest_dict, table)
+    logging.info(f"Proceeding with table {table_name} for deduplication")
+    # Create backup table
+    create_backup_table(manifest_dict, table_name)
+    # validate if backup created successfully then create the temp table.
+    if create_backup_table:
+        table_definition = create_temp_table(manifest_dict, table_name)
+    if table_definition:
+        swap_and_drop_temp_table(manifest_dict, table_name)
 
 
 if __name__ == "__main__":
