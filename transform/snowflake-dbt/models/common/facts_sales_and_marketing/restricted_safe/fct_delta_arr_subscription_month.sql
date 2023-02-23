92d6448389c29babbeb82ae60b69c9f224a3d829
Israel Weeks <iweeks@gitlab.com>
2023-02-23 16:23:59 -0500

    add subscription lineage as grouping

--- transform/snowflake-dbt/models/common/facts_sales_and_marketing/restricted_safe/fct_delta_arr_subscription_month.sql
+++ transform/snowflake-dbt/models/common/facts_sales_and_marketing/restricted_safe/fct_delta_arr_subscription_month.sql
@@ -12,7 +12,8 @@
     SELECT 
       sub.dim_subscription_id,
       sub_w_org_id.dim_subscription_id AS dim_oldest_subscription_in_cohort_id,
-      sub_w_org_id.dim_crm_account_id AS dim_oldest_crm_account_in_cohort_id
+      sub_w_org_id.dim_crm_account_id AS dim_oldest_crm_account_in_cohort_id,
+      sub_w_org_id.subscription_lineage
     FROM dim_subscription sub
     LEFT JOIN dim_subscription sub_w_org_id
       ON sub.oldest_subscription_in_cohort = sub_w_org_id.subscription_name_slugify
@@ -28,8 +29,7 @@
       IFF(is_first_day_of_last_month_of_fiscal_quarter, fiscal_quarter_name_fy, NULL)               AS fiscal_quarter_name_fy,
       IFF(is_first_day_of_last_month_of_fiscal_year, fiscal_year, NULL)                             AS fiscal_year,
       dim_crm_account.dim_parent_crm_account_id,
-      COALESCE(dim_crm_account.merged_to_account_id, dim_crm_account.dim_crm_account_id)            AS dim_crm_account_id,
-      oldest_subscription_in_cohort.dim_oldest_subscription_in_cohort_id,
+      oldest_subscription_in_cohort.subscription_lineage,
       dim_product_detail.product_tier_name,
       dim_product_detail.product_delivery_type,
       dim_product_detail.product_ranking,
@@ -51,20 +51,18 @@
 
     SELECT
       dim_parent_crm_account_id,
-      dim_crm_account_id,
-      dim_oldest_subscription_in_cohort_id,
+      subscription_lineage,
       MIN(arr_month)                      AS date_month_start,
       --add 1 month to generate churn month
       DATEADD('month',1,MAX(arr_month))   AS date_month_end
     FROM mart_arr
-    {{ dbt_utils.group_by(n=3) }}
+    {{ dbt_utils.group_by(n=2) }}
 
 ), base AS (
 
     SELECT
       dim_parent_crm_account_id,
-      dim_crm_account_id,
-      dim_oldest_subscription_in_cohort_id,
+      subscription_lineage,
       dim_date.date_actual         AS arr_month,
       dim_date.fiscal_quarter_name_fy,
       dim_date.fiscal_year
@@ -81,8 +79,7 @@
     SELECT
       base.arr_month,
       base.dim_parent_crm_account_id,
-      base.dim_crm_account_id,
-      base.dim_oldest_subscription_in_cohort_id,
+      base.subscription_lineage,
       ARRAY_AGG(DISTINCT product_tier_name ) WITHIN GROUP (ORDER BY product_tier_name  ASC)   AS product_tier_name,
       ARRAY_AGG(DISTINCT product_delivery_type) WITHIN GROUP (ORDER BY product_delivery_type ASC) AS product_delivery_type,
       MAX(product_ranking)                                                                    AS product_ranking,
@@ -91,19 +88,19 @@
     FROM base
     LEFT JOIN mart_arr
       ON base.arr_month = mart_arr.arr_month
-      AND base.dim_oldest_subscription_in_cohort_id = mart_arr.dim_oldest_subscription_in_cohort_id
-    {{ dbt_utils.group_by(n=4) }}
+      AND base.subscription_lineage = mart_arr.subscription_lineage
+    {{ dbt_utils.group_by(n=3) }}
 
 ), prior_month AS (
 
     SELECT
       monthly_arr_subscription_level.*,
-      LAG(product_tier_name ) OVER (PARTITION BY dim_oldest_subscription_in_cohort_id ORDER BY arr_month) AS previous_product_tier_name ,
-      LAG(product_delivery_type) OVER (PARTITION BY dim_oldest_subscription_in_cohort_id ORDER BY arr_month) AS previous_product_delivery_type,
-      COALESCE(LAG(product_ranking) OVER (PARTITION BY dim_oldest_subscription_in_cohort_id ORDER BY arr_month),0) AS previous_product_ranking,
-      COALESCE(LAG(quantity) OVER (PARTITION BY dim_oldest_subscription_in_cohort_id ORDER BY arr_month),0) AS previous_quantity,
-      COALESCE(LAG(arr) OVER (PARTITION BY dim_oldest_subscription_in_cohort_id ORDER BY arr_month),0) AS previous_arr,
-      ROW_NUMBER() OVER (PARTITION BY dim_oldest_subscription_in_cohort_id ORDER BY arr_month) AS row_number
+      LAG(product_tier_name ) OVER (PARTITION BY subscription_lineage ORDER BY arr_month) AS previous_product_tier_name ,
+      LAG(product_delivery_type) OVER (PARTITION BY subscription_lineage ORDER BY arr_month) AS previous_product_delivery_type,
+      COALESCE(LAG(product_ranking) OVER (PARTITION BY subscription_lineage ORDER BY arr_month),0) AS previous_product_ranking,
+      COALESCE(LAG(quantity) OVER (PARTITION BY subscription_lineage ORDER BY arr_month),0) AS previous_quantity,
+      COALESCE(LAG(arr) OVER (PARTITION BY subscription_lineage ORDER BY arr_month),0) AS previous_arr,
+      ROW_NUMBER() OVER (PARTITION BY subscription_lineage ORDER BY arr_month) AS row_number
     FROM monthly_arr_subscription_level
 
 ), type_of_arr_change AS (
@@ -117,7 +114,7 @@
 
     SELECT
       arr_month,
-      dim_oldest_subscription_in_cohort_id,
+      subscription_lineage,
       previous_arr      AS beg_arr,
       previous_quantity AS beg_quantity
     FROM type_of_arr_change
@@ -126,7 +123,7 @@
 
     SELECT
       arr_month,
-      dim_oldest_subscription_in_cohort_id,
+      subscription_lineage,
       {{ reason_for_arr_change_seat_change('quantity', 'previous_quantity', 'arr', 'previous_arr') }},
       {{ reason_for_quantity_change_seat_change('quantity', 'previous_quantity') }}
     FROM type_of_arr_change
@@ -135,7 +132,7 @@
 
     SELECT
       arr_month,
-      dim_oldest_subscription_in_cohort_id,
+      subscription_lineage,
       {{ reason_for_arr_change_price_change('product_tier_name ', 'previous_product_tier_name ', 'quantity', 'previous_quantity', 'arr', 'previous_arr', 'product_ranking',' previous_product_ranking') }}
     FROM type_of_arr_change
 
@@ -143,7 +140,7 @@
 
     SELECT
       arr_month,
-      dim_oldest_subscription_in_cohort_id,
+      subscription_lineage,
       {{ reason_for_arr_change_tier_change('product_ranking', 'previous_product_ranking', 'quantity', 'previous_quantity', 'arr', 'previous_arr') }}
     FROM type_of_arr_change
 
@@ -151,7 +148,7 @@
 
     SELECT
       arr_month,
-      dim_oldest_subscription_in_cohort_id,
+      subscription_lineage,
       arr                   AS end_arr,
       quantity              AS end_quantity
     FROM type_of_arr_change
@@ -160,19 +157,18 @@
 
     SELECT
       arr_month,
-      dim_oldest_subscription_in_cohort_id,
+      subscription_lineage,
       {{ annual_price_per_seat_change('quantity', 'previous_quantity', 'arr', 'previous_arr') }}
     FROM type_of_arr_change
 
 ), combined AS (
 
     SELECT
-      {{ dbt_utils.surrogate_key(['type_of_arr_change.arr_month', 'type_of_arr_change.dim_oldest_subscription_in_cohort_id']) }}
+      {{ dbt_utils.surrogate_key(['type_of_arr_change.arr_month', 'type_of_arr_change.subscription_lineage']) }}
                                                                     AS delta_arr_subscription_month_pk,
       type_of_arr_change.arr_month,
       type_of_arr_change.dim_parent_crm_account_id,
-      type_of_arr_change.dim_crm_account_id,
-      type_of_arr_change.dim_oldest_subscription_in_cohort_id,
+      type_of_arr_change.subscription_lineage,
       type_of_arr_change.product_tier_name ,
       type_of_arr_change.previous_product_tier_name AS previous_month_product_tier_name ,
       type_of_arr_change.product_delivery_type,
@@ -191,22 +187,22 @@
       annual_price_per_seat_change.annual_price_per_seat_change
     FROM type_of_arr_change
     LEFT JOIN reason_for_arr_change_beg
-      ON type_of_arr_change.dim_oldest_subscription_in_cohort_id = reason_for_arr_change_beg.dim_oldest_subscription_in_cohort_id
+      ON type_of_arr_change.subscription_lineage = reason_for_arr_change_beg.subscription_lineage
       AND type_of_arr_change.arr_month = reason_for_arr_change_beg.arr_month
     LEFT JOIN reason_for_arr_change_seat_change
-      ON type_of_arr_change.dim_oldest_subscription_in_cohort_id = reason_for_arr_change_seat_change.dim_oldest_subscription_in_cohort_id
+      ON type_of_arr_change.subscription_lineage = reason_for_arr_change_seat_change.subscription_lineage
       AND type_of_arr_change.arr_month = reason_for_arr_change_seat_change.arr_month
     LEFT JOIN reason_for_arr_change_price_change
-      ON type_of_arr_change.dim_oldest_subscription_in_cohort_id = reason_for_arr_change_price_change.dim_oldest_subscription_in_cohort_id
+      ON type_of_arr_change.subscription_lineage = reason_for_arr_change_price_change.subscription_lineage
       AND type_of_arr_change.arr_month = reason_for_arr_change_price_change.arr_month
     LEFT JOIN reason_for_arr_change_tier_change
-      ON type_of_arr_change.dim_oldest_subscription_in_cohort_id = reason_for_arr_change_tier_change.dim_oldest_subscription_in_cohort_id
+      ON type_of_arr_change.subscription_lineage = reason_for_arr_change_tier_change.subscription_lineage
       AND type_of_arr_change.arr_month = reason_for_arr_change_tier_change.arr_month
     LEFT JOIN reason_for_arr_change_end
-      ON type_of_arr_change.dim_oldest_subscription_in_cohort_id = reason_for_arr_change_end.dim_oldest_subscription_in_cohort_id
+      ON type_of_arr_change.subscription_lineage = reason_for_arr_change_end.subscription_lineage
       AND type_of_arr_change.arr_month = reason_for_arr_change_end.arr_month
     LEFT JOIN annual_price_per_seat_change
-      ON type_of_arr_change.dim_oldest_subscription_in_cohort_id = annual_price_per_seat_change.dim_oldest_subscription_in_cohort_id
+      ON type_of_arr_change.subscription_lineage = annual_price_per_seat_change.subscription_lineage
       AND type_of_arr_change.arr_month = annual_price_per_seat_change.arr_month
 
 )
