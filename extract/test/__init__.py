b6386327748312e24d797b7550cb680450fba8a7
Justin Wong <juwong@gitlab.com>
2023-02-24 11:23:18 -0500

    comment typos

--- extract/test/__init__.py
+++ extract/test/__init__.py
@@ -9,7 +9,7 @@ abs_path = os.path.dirname(os.path.realpath(__file__))
 abs_path_usage_ping = abs_path[: abs_path.find("extract")] + "extract/saas_usage_ping"
 sys.path.append(abs_path_usage_ping)
 
-abs_path_saas_backfill = (
+abs_path_level_up = (
     abs_path[: abs_path.find("extract")] + "extract/level_up_thought_industries/src"
 )
-sys.path.append(abs_path_saas_backfill)
+sys.path.append(abs_path_level_up)
