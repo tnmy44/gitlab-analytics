bb59382cd81c16b8223bb77cc76cb102c88a1402
Snehansh Chaturvedi <schaturvedi@gitlab.com>
2023-02-26 10:40:20 +0530

    Update dbt from debug mode to normal cli logs

--- dags/transformation/dbt_edm_snapshot.py
+++ dags/transformation/dbt_edm_snapshot.py
@@ -108,8 +108,8 @@ dbt_snapshot_models_command = f"""
     {pull_commit_hash} &&
     {dbt_install_deps_and_seed_cmd} &&
     export SNOWFLAKE_TRANSFORM_WAREHOUSE="TRANSFORMING_L" &&
-    dbt --debug --log-format json run --profiles-dir profile --target prod --models tag:edm_snapshot; ret=$?;
-    montecarlo import dbt-run --manifest target/manifest.json --run-results target/run_results.json --logs logs/dbt.log --project-name gitlab-analysis;
+    dbt run --profiles-dir profile --target prod --models tag:edm_snapshot | tee logs/logs.txt; ret=$?;
+    montecarlo import dbt-run --manifest target/manifest.json --run-results target/run_results.json --logs logs/logs.txt --project-name gitlab-analysis;
     python ../../orchestration/upload_dbt_file_to_snowflake.py results; exit $ret
 """
 
@@ -128,8 +128,8 @@ dbt_snapshot_models_run = KubernetesPodOperator(
 dbt_test_snapshots_cmd = f"""
     {pull_commit_hash} &&
     {dbt_install_deps_cmd} &&
-    dbt --debug --log-format json test --profiles-dir profile --target prod --models tag:edm_snapshot; ret=$?;
-    montecarlo import dbt-run --manifest target/manifest.json --run-results target/run_results.json --logs logs/dbt.log --project-name gitlab-analysis;
+    dbt test --profiles-dir profile --target prod --models tag:edm_snapshot | tee logs/logs.txt; ret=$?;
+    montecarlo import dbt-run --manifest target/manifest.json --run-results target/run_results.json --logs logs/logs.txt --project-name gitlab-analysis;
     python ../../orchestration/upload_dbt_file_to_snowflake.py test; exit $ret
 """
 
