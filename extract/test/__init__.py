b41c676fc5ab1ce6e2e741301536d6f682988f67
Radovan Bacovic <rbacovic@gitlab.com>
2023-02-28 16:50:50 +0000

    Add test paths via loop

--- extract/test/__init__.py
+++ extract/test/__init__.py
@@ -12,4 +12,18 @@ sys.path.append(abs_path_usage_ping)
 abs_path_level_up = (
     abs_path[: abs_path.find("extract")] + "extract/level_up_thought_industries/src"
 )
-sys.path.append(abs_path_level_up)
+"""
+Tweak test_path as due to script execution way in Airflow,
+can't touch the original code
+"""
+import os
+import sys
+
+test_paths = ["extract/saas_usage_ping", "extract/level_up_thought_industries/src"]
+absolute_test_path = os.path.dirname(os.path.realpath(__file__))
+
+for test_path in test_paths:
+    testing_full_path = (
+        absolute_test_path[: absolute_test_path.find("extract")] + test_path
+    )
+    sys.path.append(testing_full_path)
