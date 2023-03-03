4967b498694b989ba210cd0891506f3a4b5f3c1e
David Egan <degan@gitlab.com>
2023-03-03 15:13:07 -0800

    Updated union to include facebook and fixed naming. Added comment at the top explaining the different platform structures

--- transform/snowflake-dbt/models/workspaces/workspace_marketing/digital_ads.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_marketing/digital_ads.sql
@@ -1,7 +1,28 @@
-with union_sources as (
+/*
+    Each Platform hAS different ways they organize campaigns. 
+    The table below is taken from Linkedin Docs that hAS an accurate comparison
+
+    +-------+-------------+----------+----------------+
+    | Level |   Google    | Facebook |    LinkedIn    |
+    +-------+-------------+----------+----------------+
+    |     1 | Campaign    | Campaign | Campaign Group |
+    |     2 | Ad Group    | Ad Set   | Campaign       |
+    |     3 | Ad Keyword* | Ad       | Creative       |
+    +-------+-------------+----------+----------------+
+    |     4 |             | Creative |                |
+
+    Facebook hAS another level to get the destination url called creative.
+    *Fivetran reanmes this layer to `Ad`
+
+    This union query prefers the Facebook naming since its the most generic and 
+    most marketers understand a `campaign` AS the top level.
+
+*/
+
+with union_sources AS (
     SELECT
-    google_ads.ad_stats_date as report_date,
-    'google_ads' as platform,
+    google_ads.ad_stats_date AS report_date,
+    'google_ads' AS platform,
 
     google_ads.campaign_id,
     google_ads.campaign_name,
@@ -10,14 +31,14 @@ with union_sources as (
     google_ads.campaign_start_date,
 
     google_ads.ad_status,
-    NULL as creative_type,
-    LTRIM(RTRIM(google_ads.ad_final_urls,']'),'[') as landing_page_url,
+    NULL                                           AS creative_type,
+    LTRIM(RTRIM(google_ads.ad_final_urls,']'),'[') AS landing_page_url,
 
     
-    sum(impressions),
-    sum(clicks),
-    NULL AS linkedin_leads,
-    sum(cost_micros / 1000000) as spend
+    sum(impressions)                               AS impressions,
+    sum(clicks)                                    AS clicks,
+    NULL                                           AS linkedin_leads,
+    sum(cost_micros / 1000000)                     AS spend
 
   FROM
   PROD.WORKSPACE_MARKETING.GOOGLE_ADS
@@ -26,8 +47,8 @@ with union_sources as (
   union all
 
   SELECT
-  linkedin_ads.day as report_date,
-  'linkedin_ads' as platform,
+  linkedin_ads.day                    AS report_date,
+  'linkedin_ads'                      AS platform,
 
   linkedin_ads.campaign_id,
   linkedin_ads.campaign_name,
@@ -37,18 +58,43 @@ with union_sources as (
 
   linkedin_ads.ad_status,
   linkedin_ads.creative_type,
-  linkedin_ads.click_uri as landing_page_url,
+  linkedin_ads.click_uri              AS landing_page_url,
   
-  sum(impressions),
-  sum(clicks),
-  sum(linkedin_leads),
-  sum(cost_in_usd) as spend
+  sum(impressions)                    AS impressions,
+  sum(clicks)                         AS clicks,
+  sum(linkedin_leads)                 AS linkedin_leads,
+  sum(cost_in_usd)                    AS spend
 
   FROM
   PROD.WORKSPACE_MARKETING.LINKEDIN_ADS
   GROUP BY 1,2,3,4,5,6,7,8,9,10
 
-), parse_utms as (
+  union all
+
+  SELECT
+  facebook_ads.day            AS report_date,
+  'facebook_ads'              AS platform,
+
+  facebook_ads.campaign_id,
+  facebook_ads.campaign_name,
+  NULL                        AS campaign_status,
+  NULL                        AS campaign_end_date,
+  NULL                        AS campaign_start_date,
+
+  facebook_ads.ad_status,
+  facebook_ads.creative_type,
+  facebook_ads.page_link      AS landing_page_url,
+  
+  sum(impressions)            AS impressions,
+  sum(inline_link_clicks)     AS clicks,
+  NULL                        AS linkedin_leads,
+  sum(spend)                  AS spend
+
+  FROM
+  PROD.WORKSPACE_MARKETING.FACEBOOK_ADS
+  GROUP BY 1,2,3,4,5,6,7,8,9,10
+
+), parse_utms AS (
   
   SELECT
   union_sources.*
