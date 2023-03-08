bfa7ef4ac759b389c39e430e5607b032ea17795e
Rigerta Demiri <rdemiri@gitlab.com>
2023-03-08 13:22:50 +0100

    Temporarily disable the devTeamMembers stream and cleanup commenting lines

--- extract/engineering/execute.py
+++ extract/engineering/execute.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     file_dict = dict(
         commit_stats="commitStats",
         # red_master_stats="redMasterStats",
-        development_team_members="developmentTeamMembers",
+        # development_team_members="developmentTeamMembers",
     )
 
     logging.basicConfig(stream=sys.stdout, level=20)
@@ -25,10 +25,6 @@ if __name__ == "__main__":
 
     for key, value in file_dict.items():
         logging.info(f"Downloading {value} to JSON file.")
-        print("----------------------")
-        print("\n")
-        print(f"Key: {key}, Value: {value}")
-
         command = f"curl {base_url}{value} > {key}.json"
         print(command)
         p = subprocess.run(command, shell=True)
