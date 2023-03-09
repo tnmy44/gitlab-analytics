71c58e290428e1275c2662eaa2bc8a614d07163b
David Egan <degan@gitlab.com>
2023-03-09 15:00:45 -0800

    Updated from sqlfluff

--- transform/snowflake-dbt/models/sources/facebook_ads/facebook_ads_creative_history_source.sql
+++ transform/snowflake-dbt/models/sources/facebook_ads/facebook_ads_creative_history_source.sql
@@ -6,19 +6,19 @@ WITH source AS (
 renamed AS (
 
   SELECT
-	account_id::NUMBER AS account_id,
-	id::NUMBER AS creative_id,
-	body::VARCHAR AS body,
-	link_destination_display_url::VARCHAR AS link_destination_display_url,
-	link_url::VARCHAR AS link_url,
-	name::VARCHAR AS name,
-	object_type::VARCHAR AS object_type,
-	object_url::VARCHAR AS object_url,
-	status::VARCHAR AS status,
-	title::VARCHAR AS title,
-	page_link::VARCHAR AS page_link,
-    _fivetran_id::VARCHAR AS _fivetran_id,
-    _fivetran_synced::TIMESTAMP AS _fivetran_synced
+    account_id::NUMBER                    AS account_id,
+    id::NUMBER                            AS creative_id,
+    body::VARCHAR                         AS body,
+    link_destination_display_url::VARCHAR AS link_destination_display_url,
+    link_url::VARCHAR                     AS link_url,
+    name::VARCHAR                         AS creative_name,
+    object_type::VARCHAR                  AS object_type,
+    object_url::VARCHAR                   AS object_url,
+    status::VARCHAR                       AS creative_status,
+    title::VARCHAR                        AS title,
+    page_link::VARCHAR                    AS page_link,
+    _fivetran_id::VARCHAR                 AS _fivetran_id,
+    _fivetran_synced::TIMESTAMP           AS _fivetran_synced
   FROM source
 )
 
