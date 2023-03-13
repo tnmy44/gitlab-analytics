7d1ce856ade2d79142472fed2699b697864f79b3
Rigerta Demiri <rdemiri@gitlab.com>
2023-03-13 20:35:17 +0100

    Add column specification for the source model

--- transform/snowflake-dbt/models/sources/data_science/ptpf_scores_source.sql
+++ transform/snowflake-dbt/models/sources/data_science/ptpf_scores_source.sql
@@ -1,7 +1,16 @@
 WITH source AS (
 
-    SELECT *
+    SELECT
+        score_date,
+        score,
+        decile,
+        score_group,
+        insights,
+        sub_model,
+        days_since_trial_start,
+        uploaded_at::TIMESTAMP AS uploaded_at
     FROM {{ source('data_science', 'ptpf_scores') }}
 )
+
 SELECT *
 FROM source
