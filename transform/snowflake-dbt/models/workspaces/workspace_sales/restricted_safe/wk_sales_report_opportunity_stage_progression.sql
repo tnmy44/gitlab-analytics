ea2cf5209f2577e188ca8490c693400cf427017f
nfiguera <nfiguera@gitlab.com>
2023-03-13 18:35:10 +0100

    updated on progression model

--- transform/snowflake-dbt/models/workspaces/workspace_sales/restricted_safe/wk_sales_report_opportunity_stage_progression.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_sales/restricted_safe/wk_sales_report_opportunity_stage_progression.sql
@@ -4,6 +4,7 @@ WITH sfdc_opportunity_snapshot_history_xf AS (
 
     SELECT snapshot_date,
     	opportunity_id,
+        created_date,
     	stage_name  AS new_stage_name,
     	LAG(stage_name,1) OVER(PARTITION BY opportunity_id ORDER BY snapshot_date) AS old_stage_name,
         net_arr
@@ -20,18 +21,20 @@ WITH sfdc_opportunity_snapshot_history_xf AS (
 ), stage_delta AS (
 
     SELECT snapshot_date,
+        created_date,
     	opportunity_id,
         old_stage_name,
     	new_stage_name,
         net_arr
     FROM sfdc_opportunity_snapshot_history_xf hist
-    WHERE new_stage_name <> old_stage_name
+    -- NF: If old_stage is left Null we miss the creation transaction
+    WHERE new_stage_name <> COALESCE(old_stage_name,'CREATED')
 
 ), stage_delta_final AS (
 
     SELECT *,
-        LAG(snapshot_date,1) OVER(PARTITION BY opportunity_id ORDER BY snapshot_date) 	AS old_stage_date,
-        DATEDIFF(DAY,coalesce(old_stage_date,snapshot_date),snapshot_date)			    AS days_in_old_stage
+        COALESCE(LAG(snapshot_date,1) OVER(PARTITION BY opportunity_id ORDER BY snapshot_date),created_date) AS old_stage_date,
+        DATEDIFF(DAY,old_stage_date,snapshot_date)			    AS days_in_old_stage
     FROM stage_delta
 
 ), final AS (
@@ -41,7 +44,7 @@ WITH sfdc_opportunity_snapshot_history_xf AS (
         fiscal.fiscal_quarter_name_fy      AS old_stage_fiscal_quarter_name,
         fiscal.first_day_of_fiscal_quarter AS old_stage_fiscal_quarter_date
     FROM stage_delta_final delta
-        INNER JOIN date_details fiscal
+        LEFT JOIN date_details fiscal
             ON delta.old_stage_date = fiscal.date_actual
 
 )
