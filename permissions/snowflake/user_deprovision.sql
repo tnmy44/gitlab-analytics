43a48f04de176029ada58bba3e8ae48cfcfbfd3b
Justin Stark <jstark@gitlab.com>
2023-03-14 07:47:59 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- permissions/snowflake/user_deprovision.sql
+++ permissions/snowflake/user_deprovision.sql
@@ -1,6 +1,6 @@
 -- using the SECURITYADMIN role run the following by replacing 'USER_NAME' provided in Offboarding or Deprovisioning issue request.
 
-set username = 'USER_NAME';
+-- set username = 'USER_NAME';
 
 set prep_db = (select $username || '_PREP');
 set prod_db = (select $username || '_PROD');
