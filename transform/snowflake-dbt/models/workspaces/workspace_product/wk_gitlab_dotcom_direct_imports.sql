c74f252b33e699a48e145fffd5a6e1efbf1eb4d2
Matthew Petersen <mpetersen@gitlab.com>
2023-03-15 22:55:10 +0000

    Adding View for Direct imports

---
+++ transform/snowflake-dbt/models/workspaces/workspace_product/wk_gitlab_dotcom_direct_imports.sql
@@ -0,0 +1,30 @@
+{{ config(
+    materialized='view',
+    tags=["mnpi_exception", "product"]
+) }}
+
+WITH final AS (
+    SELECT
+    bi.id AS bulk_import_id,
+    bi.created_at AS bulk_import_created_at,
+    bi.user_id,
+    e.namespace_id,
+    CASE WHEN MAX(e.status) = 2 THEN 'Completed' ELSE 'Failed' END AS status,
+    MAX(e.created_at) AS latest_bulk_import,
+    MIN(e.created_at) AS first_bulk_import
+    FROM
+    {{ ref( 'gitlab_dotcom_bulk_import_entities_dedupe_source') }} e
+    LEFT JOIN 
+    {{ ref( 'gitlab_dotcom_bulk_imports_dedupe_source') }} bi ON e.bulk_import_id = bi.id
+    GROUP BY 
+    1,2,3
+    )
+
+
+{{ dbt_audit(
+    cte_ref="final",
+    created_by="@mpetersen",
+    updated_by="@mpetersen",
+    created_date="2023-03-16",
+    updated_date="2023-03-16"
+) }}
