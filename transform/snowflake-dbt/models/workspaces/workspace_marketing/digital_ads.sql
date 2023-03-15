b88e4a772395e82e4974224ee16288e5b5aab4db
David Egan <degan@gitlab.com>
2023-03-14 18:42:46 -0700

    Working model

--- transform/snowflake-dbt/models/workspaces/workspace_marketing/digital_ads.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_marketing/digital_ads.sql
@@ -1,6 +1,6 @@
 /*
-    Each Platform hAS different ways they organize campaigns. 
-    The table below is taken from Linkedin Docs that hAS an accurate comparison
+    Each Platform has different ways they organize campaigns.
+    The table below is taken from Linkedin Docs that has an accurate comparison
 
     +-------+-------------+----------+----------------+
     | Level |   Google    | Facebook |    LinkedIn    |
@@ -11,18 +11,18 @@
     +-------+-------------+----------+----------------+
     |     4 |             | Creative |                |
 
-    Facebook hAS another level to get the destination url called creative.
+    Facebook has another level to get the destination url called creative.
     *Fivetran reanmes this layer to `Ad`
 
-    This union query prefers the Facebook naming since its the most generic and 
+    This union query prefers the Facebook naming since its the most generic and
     most marketers understand a `campaign` AS the top level.
 
 */
 
 WITH union_sources AS (
-    SELECT
-    google_ads.ad_stats_date AS report_date,
-    'google_ads' AS platform,
+  SELECT
+    google_ads.ad_stats_date                         AS report_date,
+    'google_ads'                                     AS platform,
 
     google_ads.campaign_id,
     google_ads.campaign_name,
@@ -31,24 +31,24 @@ WITH union_sources AS (
     google_ads.campaign_start_date,
 
     google_ads.ad_status,
-    NULL                                           AS creative_type,
-    LTRIM(RTRIM(google_ads.ad_final_urls,']'),'[') AS landing_page_url,
+    NULL                                             AS creative_type,
+    LTRIM(RTRIM(google_ads.ad_final_urls, ']'), '[') AS landing_page_url,
 
-    
-    sum(impressions)                               AS impressions,
-    sum(clicks)                                    AS clicks,
-    NULL                                           AS linkedin_leads,
-    sum(cost_micros / 1000000)                     AS spend
 
-    FROM
+    SUM(impressions)                                 AS impressions,
+    SUM(clicks)                                      AS clicks,
+    NULL                                             AS linkedin_leads,
+    SUM(cost_micros / 1000000)                       AS spend
+
+  FROM
     {{ ref('google_ads') }}
-    GROUP BY 1,2,3,4,5,6,7,8,9,10
+  GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
 
   UNION ALL
 
-    SELECT
-    linkedin_ads.day                    AS report_date,
-    'linkedin_ads'                      AS platform,
+  SELECT
+    linkedin_ads.campaign_day    AS report_date,
+    'linkedin_ads'               AS platform,
 
     linkedin_ads.campaign_id,
     linkedin_ads.campaign_name,
@@ -56,79 +56,73 @@ WITH union_sources AS (
     linkedin_ads.campaign_end_date,
     linkedin_ads.campaign_start_date,
 
-    linkedin_ads.ad_status,
+    linkedin_ads.creative_status AS ad_status,
     linkedin_ads.creative_type,
-    linkedin_ads.click_uri              AS landing_page_url,
-    
-    sum(impressions)                    AS impressions,
-    sum(clicks)                         AS clicks,
-    sum(linkedin_leads)                 AS linkedin_leads,
-    sum(cost_in_usd)                    AS spend
+    linkedin_ads.click_uri       AS landing_page_url,
 
-    FROM
-        {{ ref('linkedin_ads') }}
-    GROUP BY 1,2,3,4,5,6,7,8,9,10
-
-  union all
-
-  SELECT
-  facebook_ads.day            AS report_date,
-  'facebook_ads'              AS platform,
-
-  facebook_ads.campaign_id,
-  facebook_ads.campaign_name,
-  NULL                        AS campaign_status,
-  NULL                        AS campaign_end_date,
-  NULL                        AS campaign_start_date,
-
-  facebook_ads.ad_status,
-  facebook_ads.creative_type,
-  facebook_ads.page_link      AS landing_page_url,
-  
-  sum(impressions)            AS impressions,
-  sum(inline_link_clicks)     AS clicks,
-  NULL                        AS linkedin_leads,
-  sum(spend)                  AS spend
+    SUM(impressions)             AS impressions,
+    SUM(clicks)                  AS clicks,
+    SUM(linkedin_leads)          AS linkedin_leads,
+    SUM(cost_in_usd)             AS spend
 
   FROM
-      {{ ref('facebook_ads') }}
-  GROUP BY 1,2,3,4,5,6,7,8,9,10
+    {{ ref('linkedin_ads') }}
+  GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
 
-), parse_utms AS (
-  
+  UNION ALL
+
   SELECT
-  union_sources.*
+    facebook_ads.campaign_day AS report_date,
+    'facebook_ads'            AS platform,
+
+    facebook_ads.campaign_id,
+    facebook_ads.campaign_name,
+    NULL                      AS campaign_status,
+    NULL                      AS campaign_end_date,
+    NULL                      AS campaign_start_date,
+
+    facebook_ads.ad_status,
+    facebook_ads.creative_type,
+    facebook_ads.page_link    AS landing_page_url,
+
+    SUM(impressions)          AS impressions,
+    SUM(inline_link_clicks)   AS clicks,
+    NULL                      AS linkedin_leads,
+    SUM(spend)                AS spend
+
+  FROM
+    {{ ref('facebook_ads') }}
+  GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+
+),
+
+parse_utms AS (
+
+  SELECT
+    union_sources.*,
     -- keywords,
-  lower(CASE WHEN SPLIT_PART(SPLIT_PART(landing_page_url,'utm_medium=',2),'&',1)= ''
-    THEN SPLIT_PART(SPLIT_PART(rpt_lead_to_revenue.BIZIBLE_LANDING_PAGE_RAW,'utm_medium=',2),'&',1)
-   ELSE SPLIT_PART(SPLIT_PART(landing_page_url,'utm_medium=',2),'&',1) END) AS utm_medium,
-  lower(CASE WHEN SPLIT_PART(SPLIT_PART(landing_page_url,'utm_source=',2),'&',1)= ''
-    THEN SPLIT_PART(SPLIT_PART(rpt_lead_to_revenue.BIZIBLE_LANDING_PAGE_RAW,'utm_source=',2),'&',1)
-   ELSE SPLIT_PART(SPLIT_PART(landing_page_url,'utm_source=',2),'&',1) END) AS utm_source,
-  lower(CASE WHEN SPLIT_PART(SPLIT_PART(landing_page_url,'utm_budget=',2),'&',1)= ''
-    THEN SPLIT_PART(SPLIT_PART(rpt_lead_to_revenue.BIZIBLE_LANDING_PAGE_RAW,'utm_budget=',2),'&',1)
-   ELSE SPLIT_PART(SPLIT_PART(landing_page_url,'utm_budget=',2),'&',1) END) AS utm_budget,
-  lower(CASE WHEN SPLIT_PART(SPLIT_PART(landing_page_url,'utm_campaign=',2),'&',1)= ''
-    THEN SPLIT_PART(SPLIT_PART(rpt_lead_to_revenue.BIZIBLE_LANDING_PAGE_RAW,'utm_campaign=',2),'&',1)
-   ELSE SPLIT_PART(SPLIT_PART(landing_page_url,'utm_campaign=',2),'&',1) END) AS utm_campaign,
-  SPLIT_PART(utm_campaign,'_',1) AS utm_campaigncode,
-  SPLIT_PART(utm_campaign,'_',2) AS utm_geo,
-  SPLIT_PART(utm_campaign,'_',3) AS utm_targeting,
-  SPLIT_PART(utm_campaign,'_',4) AS utm_ad_unit,
-  SPLIT_PART(utm_campaign,'_',5) AS "utm_br/bn",
-  SPLIT_PART(utm_campaign,'_',6) AS utm_matchtype,
-  lower(CASE WHEN SPLIT_PART(SPLIT_PART(landing_page_url,'utm_content=',2),'&',1)= ''
-    THEN SPLIT_PART(SPLIT_PART(rpt_lead_to_revenue.BIZIBLE_LANDING_PAGE_RAW,'utm_content=',2),'&',1)
-   ELSE SPLIT_PART(SPLIT_PART(landing_page_url,'utm_content=',2),'&',1) END) AS utm_content,
-  SPLIT_PART(utm_content,'_',1)  AS utm_contentcode,
-  SPLIT_PART(utm_content,'_',2)  AS utm_team,
-  SPLIT_PART(utm_content,'_',3)  AS utm_segment,
-  SPLIT_PART(utm_content,'_',4)  AS utm_language
-  FROM 
-  union_sources
+    PARSE_URL(landing_page_url) :parameters:utm_medium   AS utm_medium,
+    PARSE_URL(landing_page_url) :parameters:utm_source   AS utm_source,
+    PARSE_URL(landing_page_url) :parameters:utm_budget   AS utm_budget,
+    PARSE_URL(landing_page_url) :parameters:utm_campaign AS utm_campaign,
+
+    SPLIT_PART(utm_campaign, '_', 1)                     AS utm_campaigncode,
+    SPLIT_PART(utm_campaign, '_', 2)                     AS utm_geo,
+    SPLIT_PART(utm_campaign, '_', 3)                     AS utm_targeting,
+    SPLIT_PART(utm_campaign, '_', 4)                     AS utm_ad_unit,
+    SPLIT_PART(utm_campaign, '_', 5)                     AS utm_br_bn,
+    SPLIT_PART(utm_campaign, '_', 6)                     AS utm_matchtype,
+
+    PARSE_URL(landing_page_url) :parameters:utm_content  AS utm_content,
+    SPLIT_PART(utm_content, '_', 1)                      AS utm_contentcode,
+    SPLIT_PART(utm_content, '_', 2)                      AS utm_team,
+    SPLIT_PART(utm_content, '_', 3)                      AS utm_segment,
+    SPLIT_PART(utm_content, '_', 4)                      AS utm_language
+  FROM
+    union_sources
 
 )
-SELECT
-*
+
+SELECT *
 FROM
-parse_utms
+  parse_utms
