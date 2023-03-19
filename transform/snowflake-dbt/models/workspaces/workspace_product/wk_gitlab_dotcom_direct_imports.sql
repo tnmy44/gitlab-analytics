803382b1e75e0a04a38fcab5f0d81bdd0cab52a0
Matthew Petersen <mpetersen@gitlab.com>
2023-03-19 19:01:37 +0000

    Update wk_gitlab_dotcom_direct_imports.sql
    Removing Group by but structuring data.

--- transform/snowflake-dbt/models/workspaces/workspace_product/wk_gitlab_dotcom_direct_imports.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_product/wk_gitlab_dotcom_direct_imports.sql
@@ -4,20 +4,34 @@
 ) }}
 
 WITH final AS (
+WITH final AS (
     SELECT
-    bi.id AS bulk_import_id,
-    bi.created_at AS bulk_import_created_at,
     bi.user_id,
-    e.namespace_id,
-    CASE WHEN MAX(e.status) = 2 THEN 'Completed' ELSE 'Failed' END AS status,
-    MAX(e.created_at) AS latest_bulk_import,
-    MIN(e.created_at) AS first_bulk_import
+    e.namespace_id, -- This attribute is only populated when source_type = 0 (groups). If the status is failed, probably the value will be NULL since the migration failed
+    e.project_id,   -- This attribute is only populated when source_type = 1 (project). If the status is failed, probably the value will be NULL since the migration failed
+    COALESCE(e.namespace_id,e.project_id) AS dim_namespace_id,
+    e.created_at AS entity_created_at,
+    e.updated_at AS entity_updated_at,   -- This date can be used as the date the migration finished, failed, or timed out as we update the date along with the final status
+    bi.created_at AS bulk_import_created_at,
+    e.has_failures,
+    CASE 
+      WHEN e.source_type = 0 THEN 'group' 
+      WHEN e.source_type = 1 THEN 'project'
+      ELSE 'unknown'                        -- in case a different source_type is added
+    END AS entity_type,
+    CASE
+      WHEN e.status = 0 THEN 'created'    -- temporary status
+      WHEN e.status = 1 THEN 'started'    -- temporary status
+      WHEN e.status = 2 THEN 'finished'   -- final status
+      WHEN e.status = 3 THEN 'timeout'    -- final status
+      WHEN e.status = -1 THEN 'failed'    -- final status
+      ELSE 'unknown'                      -- in case a different status is added
+    END AS status
     FROM
     {{ ref( 'gitlab_dotcom_bulk_import_entities_dedupe_source') }} e
     LEFT JOIN 
     {{ ref( 'gitlab_dotcom_bulk_imports_dedupe_source') }} bi ON e.bulk_import_id = bi.id
-    GROUP BY 
-    1,2,3,4
+
     )
 
 
