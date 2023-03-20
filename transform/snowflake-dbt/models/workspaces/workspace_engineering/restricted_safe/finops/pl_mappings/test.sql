eb185104825dbc6b3147cbd3fafe3d520aafac26
Clement Leroux <cleroux@gitlab.com>
2023-03-20 10:34:34 +0100

    remove test

--- transform/snowflake-dbt/models/workspaces/workspace_engineering/restricted_safe/finops/pl_mappings/test.sql
+++
@@ -1,5 +0,0 @@
-SELECT sum(net_cost) FROM {{ ref ('rpt_gcp_billing_pl_day') }} where from_mapping = 'container_registry_pl_daily' and date_trunc('month', date_day) = '2023-02-01' 
-
-
-
-SELECT * FROM {{ ref ('combined_pl_mapping') }} where from_mapping = 'single_sku_pl' and date_trunc('month', date_day) = '2023-02-01' 
