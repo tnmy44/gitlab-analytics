e359f7ba0aef7a8cfcceb472a2cdea0a0e92c71f
snalamaru <snalamaru@gitlab.com>
2023-03-21 12:44:52 -0500

    Added sk and missing columns code

--- transform/snowflake-dbt/models/common/dimensions_local/product_and_engineering/dim_gitlab_versions.sql
+++ transform/snowflake-dbt/models/common/dimensions_local/product_and_engineering/dim_gitlab_versions.sql
@@ -1,3 +1,6 @@
+{{ config(
+    tags=["mnpi_exception"]
+) }}
 
 WITH versions AS (
 
@@ -16,25 +19,17 @@ WITH versions AS (
       updated_at::DATE                      AS updated_date
     FROM versions  
 
-), renamed AS (
+) , final AS (
 
     SELECT
-      id AS version_id,
-      version,
-      major_version,
-      minor_version,
-      patch_number,
-      is_monthly_release,
-      is_vulnerable,
-      created_date,
-      updated_date
-    FROM calculated  
-)
 
-, final AS (
+     --surrogate_key
+      {{ dbt_utils.surrogate_key(['id']) }} AS dim_gitlab_versions_sk,
 
-    SELECT
-      version_id as dim_version_id,
+      --natural key
+      id as dim_version_id,
+
+     --attributes
       version,
       major_version,
       minor_version,
@@ -48,15 +43,37 @@ WITH versions AS (
       ELSE NULL END AS vulnerability_type_desc,
       created_date,
       updated_date
-    FROM renamed  
 
-)
+    FROM calculated  
+
+UNION ALL
+
+SELECT 
+
+     --surrogate_key
+      {{ dbt_utils.surrogate_key(['-1']) }} AS dim_gitlab_versions_sk,
+
+      --natural key
+      -1 as dim_version_id,
+    
+     --attributes
+     'Missing version'                      AS version,
+      -1                                    AS major_version,
+      -1                                    AS minor_version,
+      -1                                    AS patch_number,
+      0                                     AS is_monthly_release,
+      'Missing is_vulnerable'               AS is_vulnerable,
+      'Missing vulnerability_type_desc'     AS vulnerability_type_desc,
+      '9999-12-31'                          AS created_date,
+      '9999-12-31'                          AS updated_date
+
+)    
 
 {{ dbt_audit(
     cte_ref="final",
     created_by="@derekatwood",
     updated_by="@snalamaru",
     created_date="2020-08-06",
-    updated_date="2023-03-15"
+    updated_date="2023-03-21"
 ) }}
 
