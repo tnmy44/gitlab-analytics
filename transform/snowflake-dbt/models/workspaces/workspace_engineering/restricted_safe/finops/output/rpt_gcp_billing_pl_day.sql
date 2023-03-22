dbe7cd2c8bbb522e58e151e068bf7741645c7de0
Clement Leroux <cleroux@gitlab.com>
2023-03-22 09:51:22 +0100

    adjust usage amount

--- transform/snowflake-dbt/models/workspaces/workspace_engineering/restricted_safe/finops/output/rpt_gcp_billing_pl_day.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_engineering/restricted_safe/finops/output/rpt_gcp_billing_pl_day.sql
@@ -20,7 +20,7 @@ combined_pl_mapping AS (
 overlaps AS (
 
   SELECT
-    service_base.day                                                    AS date_day,
+    service_base.day                                                                         AS date_day,
     service_base.gcp_project_id,
     service_base.gcp_service_description,
     service_base.gcp_sku_description,
@@ -28,10 +28,10 @@ overlaps AS (
     combined_pl_mapping.pl_category,
     service_base.usage_unit,
     service_base.pricing_unit,
-    service_base.usage_amount,
-    service_base.usage_amount_in_pricing_units,
-    service_base.cost_before_credits,
-    service_base.net_cost * COALESCE(combined_pl_mapping.pl_percent, 1) AS net_cost,
+    service_base.usage_amount * COALESCE(combined_pl_mapping.pl_percent, 1)                  AS usage_amount,
+    service_base.usage_amount_in_pricing_units * COALESCE(combined_pl_mapping.pl_percent, 1) AS usage_amount_in_pricing_units,
+    service_base.cost_before_credits * COALESCE(combined_pl_mapping.pl_percent, 1)           AS cost_before_credits,
+    service_base.net_cost * COALESCE(combined_pl_mapping.pl_percent, 1)                      AS net_cost,
     combined_pl_mapping.from_mapping,
     DENSE_RANK() OVER (PARTITION BY service_base.day,
       service_base.gcp_project_id,
@@ -43,7 +43,7 @@ overlaps AS (
         (CASE WHEN combined_pl_mapping.gcp_service_description IS NOT NULL THEN 1 ELSE 0 END) DESC,
         (CASE WHEN combined_pl_mapping.gcp_sku_description IS NOT NULL THEN 1 ELSE 0 END) DESC,
         (CASE WHEN combined_pl_mapping.infra_label IS NOT NULL THEN 1 ELSE 0 END) DESC
-    )                                                                   AS priority
+    )                                                                                        AS priority
   FROM
     service_base
   LEFT JOIN combined_pl_mapping ON combined_pl_mapping.date_day = service_base.day
