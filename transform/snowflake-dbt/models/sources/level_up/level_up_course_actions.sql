723cf2115d1b4525c7edfcffea3fb54649422a4b
Justin Wong <juwong@gitlab.com>
2023-03-28 12:28:01 -0400

    sqlfluff

--- transform/snowflake-dbt/models/sources/level_up/level_up_course_actions.sql
+++ transform/snowflake-dbt/models/sources/level_up/level_up_course_actions.sql
@@ -15,36 +15,36 @@ intermediate AS (
 
 parsed AS (
   SELECT
-    value['id']::VARCHAR                               AS id,
-    value['companyId']::VARCHAR                        AS company_id,
-    value['courseSku']::VARCHAR                        AS course_sku,
-    value['courseTitle']::VARCHAR                      AS course_title,
-    value['notifiableId']::VARCHAR                     AS notifiable_id,
-    value['source']::VARCHAR                           AS course_action,
-    value['timestamp']::TIMESTAMP                      AS event_timestamp,
-    value['type']::VARCHAR                             AS transaction_type,
+    value['id']::VARCHAR                        AS id,
+    value['companyId']::VARCHAR                 AS company_id,
+    value['courseSku']::VARCHAR                 AS course_sku,
+    value['courseTitle']::VARCHAR               AS course_title,
+    value['notifiableId']::VARCHAR              AS notifiable_id,
+    value['source']::VARCHAR                    AS course_action,
+    value['timestamp']::TIMESTAMP               AS event_timestamp,
+    value['type']::VARCHAR                      AS transaction_type,
 
     CASE
       WHEN value['user'] ILIKE '%@gitlab.com' THEN value['user']::VARCHAR
-    END                                                AS username,
-    value['userDetail']['id']::VARCHAR                 AS user_id,
+    END                                         AS username,
+    value['userDetail']['id']::VARCHAR          AS user_id,
 
-    value['userDetail']['state']::VARCHAR              AS user_state,
-    value['userDetail']['country']::VARCHAR            AS user_country,
+    value['userDetail']['state']::VARCHAR       AS user_state,
+    value['userDetail']['country']::VARCHAR     AS user_country,
 
-    value['userDetail']['client']::VARCHAR             AS user_client,
+    value['userDetail']['client']::VARCHAR      AS user_client,
 
-    value['userDetail']['ref1']::VARCHAR               AS ref1_user_type,
-    value['userDetail']['ref2']::VARCHAR               AS ref2_user_job,
-    value['userDetail']['ref4']::VARCHAR               AS ref4_user_company,
-    value['userDetail']['ref6']::VARCHAR               AS ref6_user_role_type,
-    value['userDetail']['ref7']::VARCHAR               AS ref7_user_continent,
-    value['userDetail']['ref8']::VARCHAR               AS ref8_user_country,
-    value['userDetail']['ref9']::VARCHAR               AS ref9_user_sub_dept,
-    value['userDetail']['ref10']::VARCHAR              AS ref10_user_dept,
+    value['userDetail']['ref1']::VARCHAR        AS ref1_user_type,
+    value['userDetail']['ref2']::VARCHAR        AS ref2_user_job,
+    value['userDetail']['ref4']::VARCHAR        AS ref4_user_company,
+    value['userDetail']['ref6']::VARCHAR        AS ref6_user_role_type,
+    value['userDetail']['ref7']::VARCHAR        AS ref7_user_continent,
+    value['userDetail']['ref8']::VARCHAR        AS ref8_user_country,
+    value['userDetail']['ref9']::VARCHAR        AS ref9_user_sub_dept,
+    value['userDetail']['ref10']::VARCHAR       AS ref10_user_dept,
 
-    value['userDetail']['sfAccountId']::VARCHAR        AS sf_account_id,
-    value['userDetail']['sfContactId']::VARCHAR        AS sf_contact_id,
+    value['userDetail']['sfAccountId']::VARCHAR AS sf_account_id,
+    value['userDetail']['sfContactId']::VARCHAR AS sf_contact_id,
 
     uploaded_at
   FROM intermediate
