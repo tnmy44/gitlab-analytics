499623b3fb5487ced8b906ea923cf77e65b290fe
Dani Deng <ddeng@gitlab.com>
2023-03-29 18:23:08 +0200

    Generalize existing infradev models into history models for issues with severity

---
+++ transform/snowflake-dbt/models/workspaces/workspace_engineering/issues_history.sql
@@ -0,0 +1,106 @@
+{{ simple_cte([
+    ('assigend_users','infradev_current_issue_users')
+])}}
+
+, issues AS (
+  SELECT *
+  FROM {{ ref('engineering_issues') }} 
+)
+
+, label_groups AS (
+  SELECT *
+  FROM {{ ref('labels_history') }} 
+)
+
+, dates AS (
+
+    SELECT
+      *,
+      MIN(date_actual) OVER () AS min_date_actual
+    FROM {{ ref('dim_date') }}  
+    WHERE date_actual > DATE_TRUNC('month', DATEADD('year', -2, CURRENT_DATE()))
+      AND date_actual < CURRENT_DATE()
+)
+
+, final AS (
+SELECT 
+  {{ dbt_utils.surrogate_key(['dates.date_actual','issues.issue_id']) }} AS daily_issue_id,
+  dates.date_actual,
+  issues.issue_id,
+  issues.issue_iid,
+  issues.project_id,
+  issues.namespace_id,
+  issues.labels,
+  issues.issue_title,
+  issues.full_group_path,
+  issues.url,
+  IFF(dates.date_actual > issues.closed_at, 'closed', 'open')        AS issue_state,
+  issues.created_at                                                        AS issue_created_at,
+  issues.closed_at                                                       AS issue_closed_at,
+  issues.priority_label,
+  IFNULL(severity.severity, 'No Severity')                             AS severity,
+  severity.label_added_at AS severity_label_added_at,
+  severity.label_valid_to as severity_label_valid_to,
+  IFNULL(team.assigned_team, 'Unassigned')                         AS assigned_team,
+  team.label_added_at as team_label_added_at,
+  team.label_valid_to as team_label_valid_to,
+  team.label_title AS team_label,
+  IFF(dates.date_actual > issues.closed_at, NULL,
+      DATEDIFF('day', issues.created_at, dates.date_actual))               AS issue_open_age_in_days,
+  DATEDIFF('day', severity.label_added_at, dates.date_actual) AS severity_label_age_in_days,
+  assigend_users.assigned_usernames,
+  IFF(assigend_users.assigned_usernames IS NULL, TRUE, FALSE)              AS is_issue_unassigned,
+  issues.group_label,
+  issues.section_label,
+  issues.stage_label,
+  issues.subtype_label,
+  IFF(ARRAY_CONTAINS('infradev'::VARIANT, issues.labels), TRUE, FALSE) AS is_infradev,
+  IFF(issues.masked_label_title ilike '%fedramp%', TRUE, FALSE) as fedramp_vulnerability,
+  ROW_NUMBER() OVER (PARTITION BY daily_issue_id 
+  ORDER BY LEAST(COALESCE(severity_label_valid_to, CURRENT_DATE),COALESCE(team_label_valid_to,CURRENT_DATE))) AS rn
+FROM issues 
+INNER JOIN dates 
+  ON issues.created_at <= dates.date_actual
+  AND (issues.created_at > dates.min_date_actual  OR issues.created_at IS NULL) 
+LEFT JOIN assigend_users
+  ON issues.issue_id = assigend_users.dim_issue_id
+LEFT JOIN label_groups as severity
+  ON severity.label_type='severity'
+  AND issues.issue_id = severity.dim_issue_id
+  AND dates.date_actual BETWEEN DATE_TRUNC('day', severity.label_valid_from) AND DATE_TRUNC('day', severity.label_valid_to)
+LEFT JOIN label_groups as team
+  ON team.label_type='team'
+  AND issues.issue_id = team.dim_issue_id
+  AND dates.date_actual BETWEEN DATE_TRUNC('day', team.label_valid_from) AND DATE_TRUNC('day', team.label_valid_to))
+
+  SELECT daily_issue_id,
+       date_actual,
+       issue_id,
+       issue_iid,
+       project_id,
+       namespace_id,
+       labels,
+       issue_title,
+       full_group_path,
+       url,
+       issue_state,
+       issue_created_at,
+       issue_closed_at,
+       priority_label,
+       severity,
+       severity_label_added_at,
+       assigned_team,
+       team_label_added_at,
+       team_label,
+       issue_open_age_in_days,
+       severity_label_age_in_days,
+       assigned_usernames,
+       is_issue_unassigned,
+       group_label,
+       section_label,
+       stage_label,
+       subtype_label,
+       is_infradev,
+       fedramp_vulnerability
+  FROM final
+  WHERE rn=1
--- transform/snowflake-dbt/models/workspaces/workspace_engineering/labels_history.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_engineering/labels_history.sql
@@ -1,7 +1,3 @@
-{{ config(
-    materialized='ephemeral'
-) }}
-
 WITH labels AS (
 
     SELECT *
@@ -31,6 +27,8 @@ WITH labels AS (
       label_links.target_id                                                                                           AS dim_issue_id,
       label_type.label_title,
       label_type.label_type,
+      IFF(label_type.label_type='severity','S' || RIGHT(label_title, 1),NULL) AS severity,
+      IFF(label_type.label_type='team',SPLIT(label_title, '::')[ARRAY_SIZE(SPLIT(label_title, '::')) - 1]::VARCHAR, NULL) AS assigned_team,
       label_links.label_link_created_at                                                                                   AS label_added_at,
       label_links.label_link_created_at                                                                                   AS label_valid_from,
       LEAD(label_links.label_link_created_at, 1, CURRENT_DATE())
@@ -42,25 +40,7 @@ WITH labels AS (
     WHERE label_type.label_type != 'other'
       AND label_links.target_id IS NOT NULL
 
-),  label_groups AS (
-
-    SELECT
-      severity.dim_issue_id,
-      severity.label_title                                                      AS severity_label,
-      team.label_title                                                          AS team_label,
-      'S' || RIGHT(severity_label, 1)                                           AS severity,
-      SPLIT(team_label, '::')[ARRAY_SIZE(SPLIT(team_label, '::')) - 1]::VARCHAR AS assigned_team,
-      severity.label_added_at                                                   AS severity_label_added_at,
-      team.label_added_at                                                       AS team_label_added_at,
-      GREATEST(severity.label_valid_from, team.label_valid_from)                AS label_group_valid_from,
-      LEAST(severity.label_valid_to, team.label_valid_to)                       AS label_group_valid_to
-    FROM base_labels AS severity
-    INNER JOIN base_labels AS team
-      ON severity.dim_issue_id = team.dim_issue_id
-    WHERE severity.label_type = 'severity'
-      AND team.label_type = 'team'
-      
 )
 
 SELECT *
-FROM label_groups
+FROM base_labels
