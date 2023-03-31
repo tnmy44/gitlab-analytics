da2fc36fd9be4fabe5d81bd506343f6848b75df4
Miles Russell <mdrussell@gitlab.com>
2023-03-31 11:08:17 -0500

    initial commit

--- transform/snowflake-dbt/models/common/dimensions_local/product_and_engineering/dim_installation.sql
+++ transform/snowflake-dbt/models/common/dimensions_local/product_and_engineering/dim_installation.sql
@@ -1,24 +1,38 @@
 {{ simple_cte([('prep_host', 'prep_host'),
-('prep_ping_instance', 'prep_ping_instance')])}}
+('prep_ping_instance', 'prep_ping_instance'),
+('prep_installation_creation', 'prep_installation_creation')])}},
 
-, joined AS (
+joined AS (
 
-    SELECT
-    prep_ping_instance.dim_installation_id, 
+  SELECT DISTINCT
+    -- Primary Key
+    prep_ping_instance.dim_installation_id,
+
+    -- Natural Keys 
     prep_ping_instance.dim_instance_id,
-    prep_ping_instance.dim_host_id,    
-    prep_host.host_name
-    FROM prep_ping_instance
-    INNER JOIN prep_host ON prep_ping_instance.dim_host_id = prep_host.dim_host_id
-    {{ dbt_utils.group_by(n=4) }}
+    prep_ping_instance.dim_host_id,
+
+    -- Dimensional contexts  
+    prep_host.host_name,
+    prep_installation_creation.installation_creation_date
+  FROM prep_ping_instance
+  INNER JOIN prep_host ON prep_ping_instance.dim_host_id = prep_host.dim_host_id
+  LEFT JOIN prep_installation_creation ON prep_installation_creation.dim_installation_id = prep_ping_instance.dim_installation_id
+
+  UNION ALL
+
+  SELECT
+    MD5('-1')                 AS dim_installation_id,
+    'Missing dim_instance_id' AS dim_instance_id,
+    NULL                      AS dim_host_id,
+    'Missing host_name'       AS host_name,
+    NULL                      AS installation_creation_date
 )
 
 {{ dbt_audit(
     cte_ref="joined",
     created_by="@mpeychet_",
-    updated_by="@tpoole",
+    updated_by="@mdrussell",
     created_date="2021-05-20",
-    updated_date="2023-01-20"
+    updated_date="2023-03-31"
 ) }}
-
-
