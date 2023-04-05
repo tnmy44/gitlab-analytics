b74e0023e445d5aad8a24646563e721ac56d336a
Dani Deng <ddeng@gitlab.com>
2023-04-05 19:05:35 +0200

    Aggregate multiple pl mapping to ensure no duplication per project-sku-service

--- transform/snowflake-dbt/models/workspaces/workspace_engineering/restricted_safe/finops/pl_mappings/combined_pl_mapping.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_engineering/restricted_safe/finops/pl_mappings/combined_pl_mapping.sql
@@ -142,20 +142,42 @@ single_sku_pl AS (
     'single_sku_pl'              AS from_mapping
   FROM date_spine
   CROSS JOIN {{ ref ('single_sku_pl') }}
-)
+),
 
-SELECT * FROM infralabel_pl
-UNION ALL
-SELECT * FROM projects_pl
-UNION ALL
-SELECT * FROM repo_storage_pl_daily
-UNION ALL
-SELECT * FROM sandbox_projects_pl
-UNION ALL
-SELECT * FROM container_registry_pl_daily
-UNION ALL
-SELECT * FROM build_artifacts_pl_daily
-UNION ALL
-SELECT * FROM build_artifacts_pl_dev_daily
-UNION ALL
-SELECT * FROM single_sku_pl
+cte_append AS
+  (SELECT *
+   FROM infralabel_pl
+   UNION ALL 
+   SELECT *
+   FROM projects_pl
+   UNION ALL 
+   SELECT *
+   FROM repo_storage_pl_daily
+   UNION ALL 
+   SELECT *
+   FROM sandbox_projects_pl
+   UNION ALL 
+   SELECT *
+   FROM container_registry_pl_daily
+   UNION ALL 
+   SELECT *
+   FROM build_artifacts_pl_daily
+   UNION ALL 
+   SELECT *
+   FROM build_artifacts_pl_dev_daily
+   UNION ALL 
+   SELECT *
+   FROM single_sku_pl)
+
+SELECT date_day,
+       gcp_project_id,
+       gcp_service_description,
+       gcp_sku_description,
+       infra_label,
+       env_label,
+       pl_category,
+       pl_percent,
+       LISTAGG(from_mapping, ' || ') WITHIN GROUP (
+       ORDER BY from_mapping ASC) AS from_mapping
+FROM cte_append
+{{ dbt_utils.group_by(n=8) }}
