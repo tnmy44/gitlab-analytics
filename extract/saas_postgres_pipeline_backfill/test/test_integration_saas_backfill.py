da43a8f590c53585a3822f972f276a98f8f9395e
Justin Wong <juwong@gitlab.com>
2023-04-05 15:33:03 -0400

    integration test, conn established from local

--- extract/saas_postgres_pipeline_backfill/test/test_integration_saas_backfill.py
+++ extract/saas_postgres_pipeline_backfill/test/test_integration_saas_backfill.py
@@ -1,7 +1,54 @@
+import os
+import pytest
+import re
+import sys
+from unittest.mock import Mock, MagicMock, patch
 
-import unittest
+abs_path = os.path.dirname(os.path.realpath(__file__))
+abs_path = (
+    abs_path[: abs_path.find("extract")]
+    + "extract/saas_postgres_pipeline_backfill/postgres_pipeline/"
+)
+sys.path.append(abs_path)
 
-class TestBackfillIntegration(unittest.TestCase):
+from utils import (
+    has_new_columns,
+    get_latest_parquet_file,
+    update_import_query_for_delete_export,
+    # get_engines,
+    postgres_engine_factory,
+    manifest_reader
+)
+
+class TestBackfillIntegration():
+    def setup(self):
+        manifest_file_path = 'extract/saas_postgres_pipeline_backfill/manifests_decomposed/el_gitlab_com_new_db_manifest.yaml'
+
+        manifest_dict = manifest_reader(manifest_file_path)
+        env = os.environ.copy()
+        metadata_engine = postgres_engine_factory(
+            manifest_dict["connection_info"]['postgres_metadata_connection'], env
+        )
+        metadata_schema = 'saas_db_metadata'
+        metadata_table = 'backfill_metadata'
+        test_table_full_path = f'{metadata_schema}.test_{metadata_table}'
+
+        delete_query = f""" drop table if exists {test_table_full_path}"""
+
+        create_table_query = f"""
+        create table {test_table_full_path}
+        (like {metadata_schema}.{metadata_table});
+        """
+
+        # TODO: remove once Ved has approved other MR
+        alter_query = f"""alter table {test_table_full_path} rename column is_backfill_completed to is_export_completed;"""
+
+        with metadata_engine.connect() as connection:
+            connection.execute(delete_query)
+            connection.execute(create_table_query)
+            connection.execute(alter_query)
+
+
     def test_if_new_table_backfill(self):
         # Arrange
         # Code to create a new table and populate it with data for testing.
@@ -11,7 +58,9 @@ class TestBackfillIntegration(unittest.TestCase):
 
         # Assert
         # Code to verify that the backfill was successful.
+        pass
 
+    '''
     def test_if_new_column_in_source_backfill(self):
         # Arrange
         # Code to add a new column to the source and populate it with data for testing.
@@ -71,6 +120,7 @@ class TestBackfillIntegration(unittest.TestCase):
 
         # Assert
         # Code to verify that the row counts in the Parquet file and source match.
+        '''
 
 if __name__ == '__main__':
     unittest.main()
