6be3433eb016af50fdafb660e76ce448691e7a4b
Robert Kohnke <rkohnke@gitlab.com>
2023-04-10 15:26:19 +0000

    updating to opportunity_base

--- transform/snowflake-dbt/models/common_mart_sales/restricted_safe/reports/rpt_lead_to_revenue.sql
+++ transform/snowflake-dbt/models/common_mart_sales/restricted_safe/reports/rpt_lead_to_revenue.sql
@@ -580,8 +580,8 @@
         ELSE 0 
       END AS won_linear_net_arr
     FROM mart_crm_opportunity_stamped_hierarchy_hist opp
-    LEFT JOIN mart_crm_opportunity
-      ON opp.dim_crm_opportunity_id=mart_crm_opportunity.dim_crm_opportunity_id
+    LEFT JOIN opportunity_base
+      ON opp.dim_crm_opportunity_id=opportunity_base.dim_crm_opportunity_id
     LEFT JOIN opportunity_base
       ON opp.dim_crm_opportunity_id=opportunity_base.dim_crm_opportunity_id
  	{{dbt_utils.group_by(n=167)}}
