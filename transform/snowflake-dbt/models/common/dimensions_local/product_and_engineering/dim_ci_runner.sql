1893fd7a2198753b366cfbc735f2d5f1dac987cc
Nicole Hervas <ngalang@gitlab.com>
2023-04-13 20:33:11 +0000

    Update file dim_ci_runner.sql

--- transform/snowflake-dbt/models/common/dimensions_local/product_and_engineering/dim_ci_runner.sql
+++ transform/snowflake-dbt/models/common/dimensions_local/product_and_engineering/dim_ci_runner.sql
@@ -2,28 +2,58 @@ WITH prep_ci_runner AS (
 
     SELECT 
       dim_ci_runner_id, 
-      
       -- FOREIGN KEYS
       created_date_id,
-     
       created_at,
       updated_at,
       ci_runner_description,
+      
+      --- CI Runner Manager Mapping
       CASE 
+        --- Private Runners
         WHEN ci_runner_description LIKE '%private%manager%'
           THEN 'private-runner-mgr'
+        --- Linux Runners
         WHEN ci_runner_description LIKE 'shared-runners-manager%'
           THEN 'linux-runner-mgr'
         WHEN ci_runner_description LIKE '%.shared.runners-manager.%' 
           THEN 'linux-runner-mgr'
+        WHEN ci_runner_description LIKE '%saas-linux-%-amd64%'
+            AND ci_runner_description NOT LIKE '%shell%'
+        THEN 'linux-runner-mgr'
+        --- Internal GitLab Runners
         WHEN ci_runner_description LIKE 'gitlab-shared-runners-manager%'
           THEN 'gitlab-internal-runner-mgr'
+        --- Window Runners 
         WHEN ci_runner_description LIKE 'windows-shared-runners-manager%'
           THEN 'windows-runner-mgr'
+        --- Shared Runners
         WHEN ci_runner_description LIKE '%.shared-gitlab-org.runners-manager.%' 
           THEN 'shared-gitlab-org-runner-mgr'
+        --- macOS Runners
+        WHEN ci_runner_description LIKE '%macOS%' 
+          THEN 'macos-runner-mgr'
+        --- Other
         ELSE 'Other'
       END                                           AS ci_runner_manager,
+
+      --- CI Runner Machine Type Mapping
+      CASE
+      --- SaaS Linux Runners
+      WHEN ci_runner_description LIKE '%.shared.runners-manager.%'
+          THEN 'SaaS Runner Linux - Small'
+      WHEN ci_runner_description LIKE '%saas-linux-medium-amd64%'
+          THEN 'SaaS Runner Linux - Medium'
+      WHEN ci_runner_description LIKE '%saas-linux-large-amd64%'
+          THEN 'SaaS Runner Linux - Large'
+      --- MacOS Runners
+      WHEN ci_runner_description LIKE '%macOS%'
+          THEN 'SaaS Runners macOS - Medium - amd64'
+      --- Window Runners 
+      WHEN ci_runner_description LIKE 'windows-shared-runners-manager%'
+          THEN 'SaaS Runners Windows - Medium'
+      ELSE 'Other'
+      END as ci_runner_machine_type,
       contacted_at,
       is_active,
       ci_runner_version,
