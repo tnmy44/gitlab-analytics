20eadfafabe4b587d7c27443c45ff03d67a0773d
Matthew Petersen <mpetersen@gitlab.com>
2023-04-13 05:09:34 +0000

    Fix derived_at to behavior_at

--- transform/snowflake-dbt/models/workspaces/workspace_product/wk_navigation_metrics.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_product/wk_navigation_metrics.sql
@@ -27,7 +27,7 @@ GROUP BY 1
 ), news AS 
 (
 SELECT
-DATE_TRUNC(MONTH,mr.derived_tstamp) AS page_view_month,
+DATE_TRUNC(MONTH,mr.behavior_at) AS page_view_month,
 COUNT(DISTINCT mr.gsc_pseudonymized_user_id) AS users_count,
 COUNT(DISTINCT mr.session_id) AS sessions,
 COUNT(1) AS total_occurences
