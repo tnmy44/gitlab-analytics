4e532264cd6b686ee97037eeb1bd76c1eeb7fae1
dmicovic <dmicovic@gitlab.com>
2023-04-21 16:33:13 +0200

    Revert "removed custom_net_arr"
    This reverts commit e01031c5e740bae888bdc9df166a35922de945c7.

--- transform/snowflake-dbt/models/workspaces/workspace_marketing/attribution_touchpoint_offer_type.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_marketing/attribution_touchpoint_offer_type.sql
@@ -10,7 +10,8 @@ WITH attribution_touchpoint AS (
   bizible_touchpoint_type,
   LOWER(bizible_form_url) AS bizible_form_url,
   REPLACE(bizible_form_url,'.html','') AS bizible_form_url_clean,
-  pathfactory_content_type
+  pathfactory_content_type,
+  custom_net_arr
   FROM {{ ref('mart_crm_attribution_touchpoint') }}
   LEFT JOIN {{ ref('sheetload_bizible_to_pathfactory_mapping') }}  ON bizible_form_url_clean=bizible_url
 ),
