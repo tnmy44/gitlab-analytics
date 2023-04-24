02188a18c275b467ff054691b18e588899434c10
Rigerta Demiri <rdemiri@gitlab.com>
2023-04-24 14:22:44 +0200

    Add composite snapshot key to model

--- transform/snowflake-dbt/snapshots/zuora_revenue/zuora_revenue_approval_detail_snapshots.sql
+++ transform/snowflake-dbt/snapshots/zuora_revenue/zuora_revenue_approval_detail_snapshots.sql
@@ -8,7 +8,8 @@
         )
     }}
 
-    SELECT *
+    SELECT concat_ws('-', rc_id, rc_appr_id, approval_status) as rc_id,
+           *
     FROM {{ source('zuora_revenue', 'zuora_revenue_approval_detail') }}
     QUALIFY RANK() OVER (PARTITION BY rc_appr_id, approver_sequence, approval_rule_id ORDER BY incr_updt_dt DESC) = 1
 
