eb4fdbfe0e2103b20263fbb712f435e8d4b5d884
Clement Leroux <cleroux@gitlab.com>
2023-04-25 16:52:46 +0200

    fix lint, removed ifnulls, renamed vars, added inner joins

--- transform/snowflake-dbt/models/sources/thanos/thanos_daily_slas_source.sql
+++ transform/snowflake-dbt/models/sources/thanos/thanos_daily_slas_source.sql
@@ -1,57 +1,58 @@
 WITH source AS (
 
-SELECT * FROM {{ source('thanos', 'periodic_queries') }}
-WHERE source_file_name like '%daily-slas%'
+  SELECT * FROM {{ source('thanos', 'periodic_queries') }}
+  WHERE source_file_name LIKE '%daily-slas%'
 
-), overall_ratio AS (
+),
 
-    SELECT 
-        'overall_ratio'                                                 AS metric_query,
-        pq_2.value['metric']['env']::VARCHAR                            AS metric_env,
-        pq_2.value['metric']['environment']::VARCHAR                    AS metric_environment,
-        pq_2.value['metric']['monitor']::VARCHAR                        AS metric_monitor,
-        pq_2.value['metric']['ruler_cluster']::VARCHAR                  AS metric_ruler_cluster,
-        pq_2.value['metric']['sla_type']::VARCHAR                       AS metric_sla_type,
-        pq_2.value['metric']['stage']::VARCHAR                          AS metric_stage,
-        parse_json(pq_2.value:value[0]::FLOAT)::NUMBER::TIMESTAMP       AS metric_created_at,
-        NULLIF(pq_2.value:value[1],'NaN')::FLOAT                        AS metric_value,
-        pq_1.value['data']['resultType']::VARCHAR                       AS result_type,
-        pq_1.value['status']:: VARCHAR                                  AS status_type,
-        pq_1.this['message']:: VARCHAR                                  AS message_type,
-        pq_1.this['status_code']:: NUMBER                               AS status_code,
-        pq_1.this['success']:: BOOLEAN                                  AS is_success
-    FROM
-        source pq,
-        lateral flatten(input => pq.jsontext, outer => true) pq_base,
-        lateral flatten(input => pq.jsontext['overall_ratio']) pq_1,
-        lateral flatten(input => pq.jsontext['overall_ratio']['body']['data']['result'],outer => true) pq_2
-    WHERE result_type IS NOT NULL AND status_type IS NOT NULL
-)
+overall_ratio AS (
 
-, overall_target AS (
+  SELECT
+    'overall_ratio'                                                      AS metric_query,
+    jsonpath_result.value['metric']['env']::VARCHAR                      AS metric_env,
+    jsonpath_result.value['metric']['environment']::VARCHAR              AS metric_environment,
+    jsonpath_result.value['metric']['monitor']::VARCHAR                  AS metric_monitor,
+    jsonpath_result.value['metric']['ruler_cluster']::VARCHAR            AS metric_ruler_cluster,
+    jsonpath_result.value['metric']['sla_type']::VARCHAR                 AS metric_sla_type,
+    jsonpath_result.value['metric']['stage']::VARCHAR                    AS metric_stage,
+    PARSE_JSON(jsonpath_result.value:value[0]::FLOAT)::NUMBER::TIMESTAMP AS metric_created_at,
+    jsonpath_result.value:value[1]::FLOAT                                AS metric_value,
+    jsonpath_root.value['data']['resultType']::VARCHAR                   AS result_type,
+    jsonpath_root.value['status']::VARCHAR                               AS status_type,
+    jsonpath_root.this['message']::VARCHAR                               AS message_type,
+    jsonpath_root.this['status_code']::NUMBER                            AS status_code,
+    jsonpath_root.this['success']::BOOLEAN                               AS is_success
+  FROM
+    source INNER JOIN
+    LATERAL FLATTEN(input => json.jsontext['overall_ratio']) jsonpath_root INNER JOIN
+    LATERAL FLATTEN(input => json.jsontext['overall_ratio']['body']['data']['result'], outer => TRUE) jsonpath_result
+  WHERE result_type IS NOT NULL AND status_type IS NOT NULL
+),
 
-    SELECT 
-        'overall_target'                                                AS metric_query,
-        NULL                                                            AS metric_env,
-        NULL                                                            AS metric_environment,
-        pq_2.value['metric']['monitor']::VARCHAR                        AS metric_monitor,
-        pq_2.value['metric']['ruler_cluster']::VARCHAR                  AS metric_ruler_cluster,
-        NULL                                                            AS metric_sla_type,
-        NULL                                                            AS metric_stage,
-        parse_json(pq_2.value:value[0]::FLOAT)::NUMBER::TIMESTAMP       AS metric_created_at,
-        NULLIF(pq_2.value:value[1],'NaN')::FLOAT                        AS metric_value,
-        pq_1.value['data']['resultType']::VARCHAR                       AS result_type,
-        pq_1.value['status']:: VARCHAR                                  AS status_type,
-        pq_1.this['message']:: VARCHAR                                  AS message_type,
-        pq_1.this['status_code']:: NUMBER                               AS status_code,
-        pq_1.this['success']:: BOOLEAN                                  AS is_success
-    FROM
-        source pq,
-        lateral flatten(input => pq.jsontext['overall_target']) pq_1,
-        lateral flatten(input => pq.jsontext['overall_target']['body']['data']['result'],outer => true) pq_2
-    WHERE result_type IS NOT NULL AND status_type IS NOT NULL
+overall_target AS (
+
+  SELECT
+    'overall_target'                                                     AS metric_query,
+    NULL                                                                 AS metric_env,
+    NULL                                                                 AS metric_environment,
+    jsonpath_result.value['metric']['monitor']::VARCHAR                  AS metric_monitor,
+    jsonpath_result.value['metric']['ruler_cluster']::VARCHAR            AS metric_ruler_cluster,
+    NULL                                                                 AS metric_sla_type,
+    NULL                                                                 AS metric_stage,
+    PARSE_JSON(jsonpath_result.value:value[0]::FLOAT)::NUMBER::TIMESTAMP AS metric_created_at,
+    jsonpath_result.value:value[1]::FLOAT                                AS metric_value,
+    jsonpath_root.value['data']['resultType']::VARCHAR                   AS result_type,
+    jsonpath_root.value['status']::VARCHAR                               AS status_type,
+    jsonpath_root.this['message']::VARCHAR                               AS message_type,
+    jsonpath_root.this['status_code']::NUMBER                            AS status_code,
+    jsonpath_root.this['success']::BOOLEAN                               AS is_success
+  FROM
+    source INNER JOIN
+    LATERAL FLATTEN(input => json.jsontext['overall_target']) jsonpath_root INNER JOIN
+    LATERAL FLATTEN(input => json.jsontext['overall_target']['body']['data']['result'], outer => TRUE) jsonpath_result
+  WHERE result_type IS NOT NULL AND status_type IS NOT NULL
 )
 
 SELECT * FROM overall_ratio
-UNION ALL 
+UNION ALL
 SELECT * FROM overall_target
