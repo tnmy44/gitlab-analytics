78fe17e1cf67073562d329ad46311881725193c4
Rigerta Demiri <rdemiri@gitlab.com>
2023-04-25 13:04:20 +0200

    Explicitly state unique key as the other approach did not work

--- transform/snowflake-dbt/snapshots/zuora_revenue/zuora_revenue_approval_detail_snapshots.sql
+++ transform/snowflake-dbt/snapshots/zuora_revenue/zuora_revenue_approval_detail_snapshots.sql
@@ -3,13 +3,12 @@
     {{
         config(
           strategy='timestamp',
-          unique_key='dbt_unique_key',
+          unique_key="rc_id||'-'||rc_appr_id||'-'||approval_status",
           updated_at='incr_updt_dt',
         )
     }}
 
-    SELECT concat_ws('-', rc_id, rc_appr_id, approval_status) as dbt_unique_key,
-           *
+    SELECT *
     FROM {{ source('zuora_revenue', 'zuora_revenue_approval_detail') }}
     QUALIFY RANK() OVER (PARTITION BY rc_appr_id, approver_sequence, approval_rule_id ORDER BY incr_updt_dt DESC) = 1
 
