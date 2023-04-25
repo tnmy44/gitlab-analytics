7d538e16fb3d487242e86aee3d54a62758f6a2cb
Rigerta Demiri <rdemiri@gitlab.com>
2023-04-25 11:52:33 +0200

    Add new dbt unique key instead of reusing existing one

--- transform/snowflake-dbt/snapshots/zuora_revenue/zuora_revenue_approval_detail_snapshots.sql
+++ transform/snowflake-dbt/snapshots/zuora_revenue/zuora_revenue_approval_detail_snapshots.sql
@@ -3,12 +3,12 @@
     {{
         config(
           strategy='timestamp',
-          unique_key='rc_id',
+          unique_key='dbt_unique_key',
           updated_at='incr_updt_dt',
         )
     }}
 
-    SELECT concat_ws('-', rc_id, rc_appr_id, approval_status) as rc_id,
+    SELECT concat_ws('-', rc_id, rc_appr_id, approval_status) as dbt_unique_key,
            *
     FROM {{ source('zuora_revenue', 'zuora_revenue_approval_detail') }}
     QUALIFY RANK() OVER (PARTITION BY rc_appr_id, approver_sequence, approval_rule_id ORDER BY incr_updt_dt DESC) = 1
