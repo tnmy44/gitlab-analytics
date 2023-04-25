e34cb8096253720f378fdecc3c90c7ef9431d36d
Rigerta Demiri <rdemiri@gitlab.com>
2023-04-25 13:26:55 +0200

    Add additional field

--- transform/snowflake-dbt/snapshots/zuora_revenue/zuora_revenue_approval_detail_snapshots.sql
+++ transform/snowflake-dbt/snapshots/zuora_revenue/zuora_revenue_approval_detail_snapshots.sql
@@ -3,12 +3,12 @@
     {{
         config(
           strategy='timestamp',
-          unique_key="rc_id||'-'||rc_appr_id||'-'||approval_status",
+          unique_key="snapshot_id",
           updated_at='incr_updt_dt',
         )
     }}
 
-    SELECT *
+    SELECT *, rc_id || '-' || rc_appr_id || '-' || approval_status as snapshot_id
     FROM {{ source('zuora_revenue', 'zuora_revenue_approval_detail') }}
     QUALIFY RANK() OVER (PARTITION BY rc_appr_id, approver_sequence, approval_rule_id ORDER BY incr_updt_dt DESC) = 1
 
