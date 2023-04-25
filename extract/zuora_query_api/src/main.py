2551ca5e1e64f234db05a4781932a751d0469e43
Paul Armstrong <parmstrong@gitlab.com>
2023-04-25 11:50:52 +0200

    Add logging; correct query extract.

--- extract/zuora_query_api/src/main.py
+++ extract/zuora_query_api/src/main.py
@@ -39,8 +39,9 @@ def main(file_path: str, load_only_table: str = None) -> None:
     for table_spec in manifest_dict:
         logging.info(f"Processing {table_spec}")
         job_id = zq.request_data_query_data(
-                query_string=tables.get(table_spec).get("query")
+                query_string=manifest_dict.get(table_spec).get("query")
         )
+        logging.info(manifest_dict.get(table_spec).get("query"))
         df = zq.get_data_query_file(job_id)
         dataframe_uploader(
                 df,
