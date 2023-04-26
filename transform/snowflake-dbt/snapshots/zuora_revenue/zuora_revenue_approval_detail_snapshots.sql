99d0cd8d198ee6ebc2346b8db21b83b3a33a3576
Justin Wong <juwong@gitlab.com>
2023-04-26 11:43:43 -0400

    add coalesce to approval_status

--- transform/snowflake-dbt/snapshots/zuora_revenue/zuora_revenue_approval_detail_snapshots.sql
+++ transform/snowflake-dbt/snapshots/zuora_revenue/zuora_revenue_approval_detail_snapshots.sql
@@ -1,6 +1,6 @@
 {% snapshot zuora_revenue_approval_detail_snapshots %}
 
-    {{
+{{
         config(
           strategy='timestamp',
           unique_key="snapshot_id",
@@ -8,8 +8,10 @@
         )
     }}
 
-    SELECT *, rc_id || '-' || rc_appr_id || '-' || approval_status as snapshot_id
-    FROM {{ source('zuora_revenue', 'zuora_revenue_approval_detail') }}
-    QUALIFY RANK() OVER (PARTITION BY rc_appr_id, approver_sequence, approval_rule_id ORDER BY incr_updt_dt DESC) = 1
+  SELECT
+    *,
+    rc_id || '-' || rc_appr_id || '-' || COALESCE(approval_status, '_') AS snapshot_id
+  FROM {{ source('zuora_revenue', 'zuora_revenue_approval_detail') }}
+  QUALIFY RANK() OVER (PARTITION BY rc_appr_id, approver_sequence, approval_rule_id ORDER BY incr_updt_dt DESC) = 1
 
 {% endsnapshot %}
