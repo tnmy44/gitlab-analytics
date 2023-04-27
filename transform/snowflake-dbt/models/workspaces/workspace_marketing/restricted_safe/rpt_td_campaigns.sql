63c102652f54f4d7ae9cfdab18cf637d4c33b784
Djordje Micovic <dmicovic@gitlab.com>
2023-04-27 08:12:37 +0000

    Update file rpt_td_campaigns.sql

--- transform/snowflake-dbt/models/workspaces/workspace_marketing/restricted_safe/rpt_td_campaigns.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_marketing/restricted_safe/rpt_td_campaigns.sql
@@ -23,15 +23,18 @@ WITH l2r AS (
    
    rpt_lead_to_revenue.inferred_employee_segment,
    rpt_lead_to_revenue.inferred_geo,
+   rpt_lead_to_revenue.account_demographics_sales_segment,
    rpt_lead_to_revenue.lead_source,
    rpt_lead_to_revenue.accepted_date,
    rpt_lead_to_revenue.bizible_landing_page,
    rpt_lead_to_revenue.bizible_form_url,
+   rpt_lead_to_revenue.touchpoint_type,
 
    rpt_lead_to_revenue.custom_sao,
    rpt_lead_to_revenue.won_custom_net_arr,
    rpt_lead_to_revenue.pipeline_custom_net_arr,
    rpt_lead_to_revenue.won_custom,
+
 
    rpt_lead_to_revenue.person_order_type,
    rpt_lead_to_revenue.opp_order_type,
@@ -61,39 +64,40 @@ WITH l2r AS (
    COALESCE(bizible_landing_page_utm_content, bizible_form_page_utm_content)     AS utm_content,
    COALESCE(bizible_landing_page_utm_allptnr, bizible_form_page_utm_allptnr)     AS utm_allptnr,
    COALESCE(bizible_landing_page_utm_partnerid, bizible_form_page_utm_partnerid) AS utm_partnerid,
-   contains(rpt_lead_to_revenue.BIZIBLE_FORM_URL_RAW, 'https://gitlab.com/-/trial') AS is_trial_signup_touchpoint,
+   contains(rpt_lead_to_revenue.bizible_form_url_raw, 'https://gitlab.com/-/trial') AS is_trial_signup_touchpoint,
 
    fct_campaign.budgeted_cost,
    fct_campaign.actual_cost,
 
    CASE WHEN (LOWER(utm_content) LIKE '%field%'
-             OR campaign_rep_role_name like '%Field Marketing%'
+             OR campaign_rep_role_name LIKE '%Field Marketing%'
              OR budget_holder = 'fmm'
              OR utm_budget = 'fmm') 
              THEN 'Field Marketing'
         WHEN (LOWER(utm_content) LIKE '%abm%'
-             OR campaign_rep_role_name like '%ABM%'
+             OR campaign_rep_role_name LIKE '%ABM%'
              OR budget_holder = 'abm'
              OR utm_budget = 'abm')
              THEN 'Account Based Marketing'
-        WHEN (lower(utm_budget) like '%ptnr%' 
-             OR lower(utm_budget) like '%chnl%')
-             OR (lower(budget_holder) like '%ptnr%' 
-             OR lower(budget_holder) like '%chnl%')
+        WHEN (lower(utm_budget) LIKE '%ptnr%' 
+             OR lower(utm_budget) LIKE '%chnl%')
+             OR (lower(budget_holder) LIKE '%ptnr%' 
+             OR lower(budget_holder) LIKE '%chnl%')
              THEN 'Partner Marketing'
-        WHEN (lower(budget_holder) like '%corp%' 
-             OR lower(utm_budget) like '%corp%')
+        WHEN (lower(budget_holder) LIKE '%corp%' 
+             OR lower(utm_budget) LIKE '%corp%')
              THEN 'Corporate Events'
-        WHEN (lower(budget_holder) like '%dmp%' 
-             OR lower(utm_budget) like '%dmp%')
+        WHEN (lower(budget_holder) LIKE '%dmp%' 
+             OR lower(utm_budget) LIKE '%dmp%')
              THEN 'Digital Marketing'
         ELSE 'No Budget Holder' END AS intergrated_budget_holder
   FROM
   {{ ref('rpt_lead_to_revenue') }}
-    left join {{ ref('dim_campaign') }} on rpt_lead_to_revenue.dim_campaign_id = dim_campaign.dim_campaign_id
-    left join {{ ref('fct_campaign') }} on rpt_lead_to_revenue.dim_campaign_id = fct_campaign.dim_campaign_id
-  where
-  (inferred_geo != 'JIHU' or inferred_geo is null)
+    LEFT JOIN {{ ref('dim_campaign') }} ON rpt_lead_to_revenue.dim_campaign_id = dim_campaign.dim_campaign_id
+    LEFT JOIN {{ ref('fct_campaign') }} ON rpt_lead_to_revenue.dim_campaign_id = fct_campaign.dim_campaign_id
+  WHERE
+  (inferred_geo != 'JIHU' OR inferred_geo IS NULL) 
+  AND ( opp_order_type IN ('1. New - First Order', '2. New - Connected', '3. Growth') OR opp_order_type IS NULL) --excludes churn and contraction opp attribution
 
 )
 
