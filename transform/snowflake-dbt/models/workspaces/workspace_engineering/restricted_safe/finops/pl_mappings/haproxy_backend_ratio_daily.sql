cea67bb858ba721119328628c0037879312f945c
Clement Leroux <cleroux@gitlab.com>
2023-05-02 11:07:10 +0200

    adjust hit models back to ha_proxy_metrics in workspace engineering

--- transform/snowflake-dbt/models/workspaces/workspace_engineering/restricted_safe/finops/pl_mappings/haproxy_backend_ratio_daily.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_engineering/restricted_safe/finops/pl_mappings/haproxy_backend_ratio_daily.sql
@@ -1,13 +1,13 @@
 WITH source AS (
 
     SELECT *
-    FROM {{ ref('thanos_total_haproxy_bytes_out')}}
+    FROM {{ ref('ha_proxy_metrics')}}
 
 )
 
-SELECT date_trunc('day', metric_created_at) as date_day,
-metric_backend as haproxy_backend,
-sum(metric_value)/pow(1024, 3) as usage_in_gb,
-RATIO_TO_REPORT(SUM(usage_in_gb)) OVER (PARTITION BY date_day) AS percent_backend_ratio
+SELECT date_trunc('day', recorded_at) as date_day,
+backend_category as backend_category,
+sum(egress_gibibytes) as usage_in_gib,
+RATIO_TO_REPORT(SUM(egress_gibibytes)) OVER (PARTITION BY date_day) AS percent_backend_ratio
 FROM source
 GROUP BY 1, 2
