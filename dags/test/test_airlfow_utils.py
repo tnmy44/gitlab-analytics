874f4c8bfa765bf74f18966466b98eeebf6e5d32
nfiguera <nfiguera@gitlab.com>
2023-05-04 16:20:18 +0200

    added airflow test file

--- dags/test/test_airlfow_utils.py
+++ dags/test/test_airlfow_utils.py
@@ -15,24 +15,39 @@ DBT_IMAGE = "registry.gitlab.com/gitlab-data/dbt-image:v0.0.1"
 PERMIFROST_IMAGE = "registry.gitlab.com/gitlab-data/permifrost:v0.13.1"
 ANALYST_IMAGE = "registry.gitlab.com/gitlab-data/analyst-image:v0.0.2"
 
-SALES_ANALYTICS_NOTEBOOKS_PATH = "/Users/nfiguera/repos/analytics/sales_analytics_notebooks"
+SALES_ANALYTICS_NOTEBOOKS_PATH = "/Users/nfiguera/repos/analytics/sales_analytics_notebooks/"
 
 
 def get_sales_analytics_notebooks(frequency: str) -> Dict:
-    notebooks_paths = []
+    notebooks = []
     fileNames = []
-    notebooks_folders = []
 
     path = pathlib.Path(f"{SALES_ANALYTICS_NOTEBOOKS_PATH}/{frequency}/")
     
+    n = 0
     for file in path.rglob("*.ipynb"):
-       
+        
+        print(n+1)
         relative_path = file.relative_to(SALES_ANALYTICS_NOTEBOOKS_PATH)
-        notebooks_paths.append(relative_path.as_posix())
-        folder = relative_path.parent.as_posix()
-        notebooks_folders.append(folder)
-        expanded_name = folder.replace('/','_') + '_' +  relative_path.stem
+        notebooks.append(relative_path.as_posix())
+        expanded_name = str(relative_path.parent).replace('/','_') + '_' +  relative_path.stem
         fileNames.append(expanded_name)
-    return list(zip(notebooks_paths, notebooks_folders, fileNames))
+    
+    return dict(zip(notebooks, fileNames))
 
-print (get_sales_analytics_notebooks('daily'))
+
+def test_cd_into_local_path (): 
+    notebooks = get_sales_analytics_notebooks(frequency="daily")
+    print(notebooks)
+    for notebook, task_name in notebooks.items():
+
+        if (notebook is None):
+            break
+
+        absolute_path = pathlib.Path(SALES_ANALYTICS_NOTEBOOKS_PATH) / notebook
+        notebook_parent = absolute_path.parent.as_posix()
+        notebook_filename = absolute_path.name
+        print(notebook_parent)
+        print(notebook_filename)
+
+test_cd_into_local_path()
