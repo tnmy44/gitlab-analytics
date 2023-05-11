31922bc7ebf923efd8f7b4c2b09a0dc24a9fd577
Radovan Bacovic <rbacovic@gitlab.com>
2023-05-11 10:58:11 +0200

    Add DAG fro temp solution.

---
+++ dags/transformation/dbt_temp_prep_instance_ping_full_refresh.py
@@ -0,0 +1,119 @@
+"""
+This DAG runs weekly on TBA to full refresh data for prep_ping_instance downstream models - incremental only
+This is a temporary solution until OKR for permanent fix is done
+"""
+import os
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.utils.trigger_rule import TriggerRule
+from airflow_utils import (
+    DBT_IMAGE,
+    dbt_install_deps_cmd,
+    gitlab_defaults,
+    gitlab_pod_env_vars,
+    slack_failed_task,
+)
+from kube_secrets import (
+    GIT_DATA_TESTS_CONFIG,
+    GIT_DATA_TESTS_PRIVATE_KEY,
+    MCD_DEFAULT_API_ID,
+    MCD_DEFAULT_API_TOKEN,
+    SALT,
+    SALT_EMAIL,
+    SALT_IP,
+    SALT_NAME,
+    SALT_PASSWORD,
+    SNOWFLAKE_ACCOUNT,
+    SNOWFLAKE_LOAD_PASSWORD,
+    SNOWFLAKE_LOAD_ROLE,
+    SNOWFLAKE_LOAD_USER,
+    SNOWFLAKE_LOAD_WAREHOUSE,
+    SNOWFLAKE_PASSWORD,
+    SNOWFLAKE_STATIC_DATABASE,
+    SNOWFLAKE_TRANSFORM_ROLE,
+    SNOWFLAKE_TRANSFORM_SCHEMA,
+    SNOWFLAKE_TRANSFORM_WAREHOUSE,
+    SNOWFLAKE_USER,
+)
+
+# Load the env vars into a dict and set Secrets
+env = os.environ.copy()
+pod_env_vars = {**gitlab_pod_env_vars, **{}}
+
+
+secrets_list = [
+    GIT_DATA_TESTS_PRIVATE_KEY,
+    GIT_DATA_TESTS_CONFIG,
+    SALT,
+    SALT_EMAIL,
+    SALT_IP,
+    SALT_NAME,
+    SALT_PASSWORD,
+    SNOWFLAKE_ACCOUNT,
+    SNOWFLAKE_USER,
+    SNOWFLAKE_PASSWORD,
+    SNOWFLAKE_LOAD_PASSWORD,
+    SNOWFLAKE_LOAD_ROLE,
+    SNOWFLAKE_LOAD_USER,
+    SNOWFLAKE_LOAD_WAREHOUSE,
+    SNOWFLAKE_TRANSFORM_ROLE,
+    SNOWFLAKE_TRANSFORM_WAREHOUSE,
+    SNOWFLAKE_TRANSFORM_SCHEMA,
+    MCD_DEFAULT_API_ID,
+    MCD_DEFAULT_API_TOKEN,
+    SNOWFLAKE_STATIC_DATABASE,
+]
+# Default arguments for the DAG
+default_args = {
+    "catchup": False,
+    "depends_on_past": False,
+    "on_failure_callback": slack_failed_task,
+    "owner": "airflow",
+    "retries": 0,
+    "sla": timedelta(hours=8),
+    "sla_miss_callback": slack_failed_task,
+    "start_date": datetime(2019, 1, 1, 0, 0, 0),
+    "trigger_rule": TriggerRule.ALL_DONE,
+    "dagrun_timeout": timedelta(hours=6),
+}
+
+# Create the DAG
+dag = DAG(
+    "dbt_temp_prep_instance_ping_full_refresh",
+    description="This DAG runs weekly on TBA to full refresh data for prep_ping_instance downstream models - incremental only",
+    default_args=default_args,
+    schedule_interval="0 22 * * WED",
+)
+
+dbt_prep_ping_instance_full_refresh_cmd = f"""
+    {dbt_install_deps_cmd} &&
+    export SNOWFLAKE_TRANSFORM_WAREHOUSE="TRANSFORMING_XL" &&
+    dbt run --profiles-dir profile --target prod --full-refresh
+    --models
+    prep_ping_instance
+    prep_ping_instance_flattened
+    fct_ping_instance
+    mart_ping_instance
+    fct_ping_instance_metric
+    fct_ping_instance_metric_rolling_13_months
+    fct_ping_instance_free_user_metrics
+    fct_behavior_structured_event_service_ping_context
+    wk_mart_snowplow_events_service_ping_metrics
+    wk_fct_snowplow_events_service_ping
+    wk_usage_ping_geo_node_usage ; ret=$?;
+    montecarlo import dbt-run --manifest target/manifest.json --run-results target/run_results.json --project-name gitlab-analysis;
+    python ../../orchestration/upload_dbt_file_to_snowflake.py results; exit $ret
+"""
+
+dbt_full_refresh = KubernetesPodOperator(
+    **gitlab_defaults,
+    image=DBT_IMAGE,
+    task_id="dbt-full-refresh",
+    name="dbt-full-refresh",
+    secrets=secrets_list,
+    env_vars=pod_env_vars,
+    arguments=[dbt_prep_ping_instance_full_refresh_cmd],
+    dag=dag,
+)
