532e51227d37fa33fef5ef7ad939dd8ce7a5af36
Shane McCormack <smccormack@gitlab.com>
2023-05-11 19:33:55 +0000

    Update division_mapped_current criteria

--- transform/snowflake-dbt/models/legacy/bamboohr/xf/bamboohr_job_info_current_division_base.sql
+++ transform/snowflake-dbt/models/legacy/bamboohr/xf/bamboohr_job_info_current_division_base.sql
@@ -32,14 +32,14 @@ WITH job_info AS (
     SELECT DISTINCT 
       division, 
       department,
-      COUNT(bamboo_mapping.employee_id) AS total_employees
+      MAX(effective_date) AS max_effective_date
     FROM bamboo_mapping
     LEFT JOIN job_info
       ON job_info.employee_id = bamboo_mapping.employee_id
     WHERE CURRENT_DATE() BETWEEN effective_date AND COALESCE(effective_end_date, CURRENT_DATE())
       AND bamboo_mapping.termination_date IS NULL
     GROUP BY 1,2
-    QUALIFY ROW_NUMBER() OVER (PARTITION BY department ORDER BY total_employees DESC) =1 
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY department ORDER BY max_effective_date DESC) =1 
     --to account for individuals that have not been transistioned to new division  
 
 )
