26faa8eda7f4787b4e59507b4e22ef8fe20d3336
Radovan Bacovic <rbacovic@gitlab.com>
2023-05-12 11:00:26 +0200

    Models refined and prepared for full refresh.

--- dags/transformation/dbt_temp_prep_instance_ping_full_refresh.py
+++ dags/transformation/dbt_temp_prep_instance_ping_full_refresh.py
@@ -94,14 +94,13 @@ dbt_prep_ping_instance_full_refresh_cmd = f"""
     --models
     prep_ping_instance
     prep_ping_instance_flattened
+    dim_installation
+    dim_ping_instance
     fct_ping_instance
     fct_ping_instance_metric
     fct_ping_instance_metric_rolling_13_months
     fct_ping_instance_free_user_metrics
-    wk_usage_ping_geo_node_usage
-    dim_installation
-    dim_ping_instance
-     ; ret=$?;
+    wk_usage_ping_geo_node_usage; ret=$?;
     montecarlo import dbt-run --manifest target/manifest.json --run-results target/run_results.json --project-name gitlab-analysis;
     python ../../orchestration/upload_dbt_file_to_snowflake.py results; exit $ret
 """
