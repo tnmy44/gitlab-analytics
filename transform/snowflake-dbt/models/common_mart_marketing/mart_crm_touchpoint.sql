88602b3995ca4e9a9be309cbe421bb57f2bf3a84
Robert Kohnke <rkohnke@gitlab.com>
2023-05-15 18:59:39 +0000

    Updating unique ID name

--- transform/snowflake-dbt/models/common_mart_marketing/mart_crm_touchpoint.sql
+++ transform/snowflake-dbt/models/common_mart_marketing/mart_crm_touchpoint.sql
@@ -18,7 +18,7 @@
     SELECT
       -- touchpoint info
       dim_crm_touchpoint.dim_crm_touchpoint_id,
-      {{ dbt_utils.surrogate_key(['fct_crm_touchpoint.dim_crm_person_id','dim_campaign.dim_campaign_id','dim_crm_touchpoint.bizible_touchpoint_date_time']) }} AS tp_unique_id,
+      {{ dbt_utils.surrogate_key(['fct_crm_touchpoint.dim_crm_person_id','dim_campaign.dim_campaign_id','dim_crm_touchpoint.bizible_touchpoint_date_time']) }} AS touchpoint_person_campaign_date_id,
       dim_crm_touchpoint.bizible_touchpoint_date,
       dim_crm_touchpoint.bizible_touchpoint_date_time,
       dim_crm_touchpoint.bizible_touchpoint_month,
@@ -333,5 +333,5 @@
     created_by="@mcooperDD",
     updated_by="@rkohnke",
     created_date="2021-02-18",
-    updated_date="2023-05-10"
+    updated_date="2023-05-15"
 ) }}
