a9f7d0aaf365d1d875c9ee63582afcfecabac6f7
Robert Kohnke <rkohnke@gitlab.com>
2023-05-15 15:09:08 +0000

    adding unique ID and schema notes

--- transform/snowflake-dbt/models/common_mart_marketing/mart_crm_touchpoint.sql
+++ transform/snowflake-dbt/models/common_mart_marketing/mart_crm_touchpoint.sql
@@ -18,6 +18,7 @@
     SELECT
       -- touchpoint info
       dim_crm_touchpoint.dim_crm_touchpoint_id,
+      {{ dbt_utils.surrogate_key(['fct_crm_touchpoint.dim_crm_person_id','dim_campaign.dim_campaign_id','dim_crm_touchpoint.bizible_touchpoint_date_time']) }} AS tp_unique_id,
       dim_crm_touchpoint.bizible_touchpoint_date,
       dim_crm_touchpoint.bizible_touchpoint_date_time,
       dim_crm_touchpoint.bizible_touchpoint_month,
