e522c57784ddfba5aec4adbfec9496358ce4fa8d
Peter Empey <pempey@gitlab.com>
2023-05-17 13:25:53 +0000

    correcteing for rehires

--- transform/snowflake-dbt/models/legacy/bamboohr/sensitive/bamboohr_job_info.sql
+++ transform/snowflake-dbt/models/legacy/bamboohr/sensitive/bamboohr_job_info.sql
@@ -16,7 +16,7 @@ bamboohr_employment_status AS (
       valid_from_date,
       DATEADD(day,1,valid_from_date) AS valid_to_date ---adding a day to capture termination date
     FROM {{ ref('bamboohr_employment_status_xf') }}  
-    WHERE employment_status = 'Terminated'
+    WHERE employment_status = 'Terminated' AND next_employment_status IS NULL
 
 ),
 
