48d00cbdf93dfd989964d43ad2ab0b699f974759
Snehansh Chaturvedi <schaturvedi@gitlab.com>
2023-05-19 20:30:45 +0530

    add fail task

--- extract/mailgun/src/execute.py
+++ extract/mailgun/src/execute.py
@@ -191,7 +191,8 @@ def load_event_logs(event: str, full_refresh: bool = False):
     is_bad_request = check_response(domains, event, start_date, end_date)
 
     if is_bad_request == True:
-        info('Bad API request')
+        error('Bad API request')
+        sys.exit(1)
 
     else:
         info('Extracting logs')
