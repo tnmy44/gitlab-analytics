7d6e839c5daae03a46c9f8d1003aae3ffaeeefe4
Lis Vinueza <lvinueza@gitlab.com>
2023-05-30 21:58:19 +0000

    Add dim_team_sk

--- transform/snowflake-dbt/models/common/facts_general_and_administrative/fct_team_member_position.sql
+++ transform/snowflake-dbt/models/common/facts_general_and_administrative/fct_team_member_position.sql
@@ -3,6 +3,7 @@ WITH final AS (
   SELECT 
     {{ dbt_utils.surrogate_key(['employee_id', 'position_valid_from']) }}  AS team_member_position_pk,
     prep_team_member_position.dim_team_member_sk,
+    prep_team_member_position.dim_team_sk,
     prep_team_member_position.employee_id,
     prep_team_member_position.job_code,
     prep_team_member_position.job_specialty_single,
@@ -25,6 +26,6 @@ WITH final AS (
     cte_ref='final',
     created_by='@lisvinueza',
     updated_by='@lisvinueza',
-    created_date='2023-05-24',
-    updated_date='2023-05-24'
+    created_date='2023-05-30',
+    updated_date='2023-05-30'
 ) }}
