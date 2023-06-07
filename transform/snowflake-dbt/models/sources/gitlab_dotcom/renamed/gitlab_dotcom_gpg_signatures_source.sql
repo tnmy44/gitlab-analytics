d56c11e108165cacc65cd4ede7c357abf99e38bb
Justin Stark <jstark@gitlab.com>
2023-06-07 16:22:51 +0000

    spacing

--- transform/snowflake-dbt/models/sources/gitlab_dotcom/renamed/gitlab_dotcom_gpg_signatures_source.sql
+++ transform/snowflake-dbt/models/sources/gitlab_dotcom/renamed/gitlab_dotcom_gpg_signatures_source.sql
@@ -9,7 +9,7 @@ WITH source AS (
       id::NUMBER                  AS gpg_signatures_id,
       created_at::TIMESTAMP       AS created_at,
       updated_at::TIMESTAMP       AS updated_at,
-      project_id::NUMBER	        AS project_id,
+      project_id::NUMBER          AS project_id,
       gpg_key_id::NUMBER          AS gpg_key_id,
       verification_status::NUMBER AS verification_status
     FROM source
