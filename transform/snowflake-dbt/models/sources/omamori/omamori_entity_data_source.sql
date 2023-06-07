9dbccac09b4364c5bf39a8b4632a76480b450ce8
Justin Wong <juwong@gitlab.com>
2023-06-07 15:59:09 -0400

    update uploaded_at to uploaded_at_gcs

--- transform/snowflake-dbt/models/sources/omamori/omamori_entity_data_source.sql
+++ transform/snowflake-dbt/models/sources/omamori/omamori_entity_data_source.sql
@@ -8,7 +8,7 @@
 WITH source AS (
   SELECT
     PARSE_JSON(value) AS json_value,
-    -- obtain 'uploaded_at' from the parquet filename, i.e:
+    -- obtain 'uploaded_at_gcs' from the parquet filename, i.e:
     -- 'entity_data/20230502/20230502T000025-entity_data-from-19700101T000000-until-20230426T193740.parquet'
     TO_TIMESTAMP(
       SPLIT_PART(
@@ -21,27 +21,27 @@ WITH source AS (
         1
       ),
       'YYYYMMDDTHH24MISS'
-    )                 AS uploaded_at
+    )                 AS uploaded_at_gcs
 
   FROM
     {{ source('omamori', 'entity_data_external') }}
 
   {% if is_incremental() %}
-    -- Filter only for records from new files based off uploaded_at
+    -- Filter only for records from new files based off uploaded_at_gcs
     -- but first filter on the parititioned column (date_part) to speed up query
-    WHERE date_part >= (SELECT COALESCE(MAX(uploaded_at)::DATE, '1970-01-01') AS last_uploaded_at FROM {{ this }})
-      AND uploaded_at > (SELECT COALESCE(MAX(uploaded_at), '1970-01-01') AS last_uploaded_at FROM {{ this }})
+    WHERE date_part >= (SELECT COALESCE(MAX(uploaded_at_gcs)::DATE, '1970-01-01') AS last_uploaded_at FROM {{ this }})
+      AND uploaded_at_gcs > (SELECT COALESCE(MAX(uploaded_at_gcs), '1970-01-01') AS last_uploaded_at FROM {{ this }})
   {% endif %}
 ),
 
 renamed AS (
   SELECT
-    json_value['id']::INT          AS id,
-    json_value['entity_id']::INT   AS entity_id,
+    json_value['id']::INT              AS id,
+    json_value['entity_id']::INT       AS entity_id,
     json_value['entity_type']::VARCHAR AS entity_type,
-    json_value['created_at']::INT  AS created_at,
-    json_value['updated_at']::INT  AS updated_at,
-    uploaded_at
+    json_value['created_at']::INT      AS created_at,
+    json_value['updated_at']::INT      AS updated_at,
+    uploaded_at_gcs
   FROM source
 ),
 
