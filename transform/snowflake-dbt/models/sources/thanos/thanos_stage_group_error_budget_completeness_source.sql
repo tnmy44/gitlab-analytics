c322eadb2259ec20c9660d88d159cb7447a2e9ea
ved prakash <vprakash@gitlab.com>
2023-06-12 21:33:07 +0200

    added source stage_group_error_budget_teams_over_budget_availability

--- transform/snowflake-dbt/models/sources/thanos/thanos_stage_group_error_budget_completeness_source.sql
+++ transform/snowflake-dbt/models/sources/thanos/thanos_stage_group_error_budget_completeness_source.sql
@@ -7,6 +7,7 @@ WITH source AS (
 parsed AS (
 
   SELECT
+    'stage_group_error_budget_completeness'                             AS source_metric_name,
     PARSE_JSON(parse_json_2.value:value[0]::DOUBLE)::INT::TIMESTAMP_NTZ AS metric_created_at,
     NULLIF(parse_json_2.value:value[1], 'NaN')::DOUBLE                  AS metric_value,
     parse_json_1.value['data']['resultType']::VARCHAR                   AS result_type,
@@ -19,7 +20,21 @@ parsed AS (
     LATERAL FLATTEN(input => source.jsontext['stage_group_error_budget_completeness']) parse_json_1,
     LATERAL FLATTEN(input => source.jsontext['stage_group_error_budget_completeness']['body']['data']['result'], outer => TRUE) parse_json_2
   WHERE result_type IS NOT NULL AND status_type IS NOT NULL
-
+  UNION 
+  SELECT
+    'stage_group_error_budget_teams_over_budget_availability'           AS source_metric_name,
+    PARSE_JSON(parse_json_2.value:value[0]::DOUBLE)::INT::TIMESTAMP_NTZ AS metric_created_at,
+    NULLIF(parse_json_2.value:value[1], 'NaN')::DOUBLE                  AS metric_value,
+    parse_json_1.value['data']['resultType']::VARCHAR                   AS result_type,
+    parse_json_1.value['status']::VARCHAR                               AS status_type,
+    parse_json_1.this['message']::VARCHAR                               AS message_type,
+    parse_json_1.this['status_code']::VARCHAR                           AS status_code,
+    parse_json_1.this['success']::VARCHAR                               AS is_success
+  FROM
+    source,
+    LATERAL FLATTEN(input => source.jsontext['stage_group_error_budget_teams_over_budget_availability']) parse_json_1,
+    LATERAL FLATTEN(input => source.jsontext['stage_group_error_budget_teams_over_budget_availability']['body']['data']['result'], outer => TRUE) parse_json_2
+  WHERE result_type IS NOT NULL AND status_type IS NOT NULL
 )
 
 SELECT * FROM
