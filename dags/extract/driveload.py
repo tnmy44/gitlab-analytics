9b29d2dbce52a0d556638a2423637668c20dd99c
Paul Armstrong <parmstrong@gitlab.com>
2023-06-14 14:44:23 +0200

    Move catchup to the correct spot

--- dags/extract/driveload.py
+++ dags/extract/driveload.py
@@ -30,7 +30,6 @@ pod_env_vars = {**gitlab_pod_env_vars, **{}}
 
 # Default arguments for the DAG
 default_args = {
-    "catchup": False,
     "depends_on_past": False,
     "on_failure_callback": slack_failed_task,
     "owner": "airflow",
@@ -55,6 +54,7 @@ dag = DAG(
     default_args=default_args,
     schedule_interval="0 2 * * *",
     concurrency=1,
+    catchup=False,
 )
 
 for folder in folders:
