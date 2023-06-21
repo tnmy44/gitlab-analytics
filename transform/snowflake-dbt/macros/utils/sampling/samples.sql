6b81c53f1aa2ca76bccaa28e569b6426777ff27a
Jean Peguero <jpeguero@gitlab.com>
2023-06-21 13:50:47 +0200

    revert file to master state

--- transform/snowflake-dbt/macros/utils/sampling/samples.sql
+++ transform/snowflake-dbt/macros/utils/sampling/samples.sql
@@ -13,12 +13,6 @@ Example
   {% set samples_yml -%}
 
   samples:
-    - name: version_raw_usage_data_source
-      clause: "{{ sample_table(1) }}"
-    - name: instance_combined_metrics
-      clause: "{{ sample_table(1) }}"
-    - name: version_usage_data_source
-      clause: "{{ sample_table(1) }}"
 
 
   {%- endset %}
