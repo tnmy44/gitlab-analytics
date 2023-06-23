50d66fdb93c15431da0fafeac617774cffb29278
Chris Sharp <csharp@gitlab.com>
2023-06-23 17:46:49 +0100

    update alliance_partner macros to use is_focus_partner

--- transform/snowflake-dbt/macros/common/alliance_partner_short_current.sql
+++ transform/snowflake-dbt/macros/common/alliance_partner_short_current.sql
@@ -1,4 +1,4 @@
-{%- macro alliance_partner_short_current(fulfillment_partner_name, partner_account_name, resale_partner_track, partner_track, deal_path) -%}
+{%- macro alliance_partner_short_current(fulfillment_partner_name, partner_account_name, resale_partner_track, partner_track, deal_path, is_focus_partner) -%}
 
 CASE
   WHEN LOWER({{ fulfillment_partner_name }}) LIKE '%google%' OR LOWER({{ partner_account_name }}) LIKE '%google%'
@@ -9,6 +9,8 @@ CASE
     THEN 'IBM'
   WHEN NOT EQUAL_NULL({{ resale_partner_track }}, 'Technology') AND NOT EQUAL_NULL({{ partner_track }}, 'Technology') AND {{ deal_path }} = 'Partner'
     THEN 'Channel Partners'
+  WHEN {{ is_focus_partner }} = TRUE 
+    THEN 'Channel Focus Partner'
 END
 
 {%- endmacro -%}
