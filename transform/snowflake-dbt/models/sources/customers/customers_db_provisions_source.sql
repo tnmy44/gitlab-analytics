756ba2f521affa1d17e78ba7e9c9260d49788fbf
Carolyn Braza <cbraza@gitlab.com>
2023-07-12 16:33:03 -0700

    initial updates

---
+++ transform/snowflake-dbt/models/sources/customers/customers_db_provisions_source.sql
@@ -0,0 +1,23 @@
+WITH source AS (
+
+    SELECT *
+    FROM {{ source('customers', 'customers_db_provisions') }}
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY subscription_id ORDER BY UPDATED_AT DESC) = 1
+
+), renamed AS (
+
+    SELECT DISTINCT
+      subscription_name::VARCHAR   AS subscription_name,
+      subscription_id::VARCHAR     AS subscription_id,
+      subscription_version::NUMBER AS subscription_version,
+      state::VARCHAR               AS state,
+      created_at::TIMESTAMP        AS created_at,
+      updated_at::TIMESTAMP        AS updated_at,
+      state_reason::NUMBER         AS state_reason
+    FROM source
+
+)
+
+SELECT
+  *
+FROM renamed
