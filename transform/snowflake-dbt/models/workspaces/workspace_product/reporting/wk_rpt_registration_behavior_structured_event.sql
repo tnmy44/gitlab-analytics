d677c82c9fc96970878b7c5363106ef21a394841
Miles Russell <mdrussell@gitlab.com>
2023-07-13 13:35:49 -0500

    apply styling

--- transform/snowflake-dbt/models/workspaces/workspace_product/reporting/wk_rpt_registration_behavior_structured_event.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_product/reporting/wk_rpt_registration_behavior_structured_event.sql
@@ -12,54 +12,54 @@
 
 behavior_structured_event AS (
 
-	SELECT
-        behavior_structured_event_pk,
-		behavior_at,
-		behavior_date,
-		app_id,
-		user_snowplow_domain_id,
-		contexts,
-		page_url_path,
-		page_url_fragment,
-		gsc_google_analytics_client_id,
-		gsc_pseudonymized_user_id,
-		gsc_extra,
-		gsc_plan,
-		gsc_source,
-		event_value,
-		session_index,
-		session_id,
-		event_category,
-		event_action,
-		event_label,
-		event_property,
-		dim_namespace_id,
-		ultimate_parent_namespace_id,
-		namespace_is_internal,
-		namespace_is_ultimate_parent,
-		namespace_type,
-		visibility_level,
-		dim_project_id,
-		device_type,
-		is_device_mobile,
-		dim_behavior_referrer_page_sk,
-		has_gitlab_service_ping_context,
-		has_gitlab_experiment_context,
-		has_customer_standard_context,
-		browser_name,
-		dim_behavior_browser_sk,
-		dim_plan_id,
-		plan_id_modified,
-		plan_name,
-		plan_name_modified
-	FROM mart_behavior_structured_event mart
-	WHERE behavior_at::DATE >= DATEADD(DAY, -190, CURRENT_DATE::DATE)
-	AND LOWER(event_category) LIKE '%registration%'
-    {% if is_incremental() %}
+  SELECT
+    behavior_structured_event_pk,
+    behavior_at,
+    behavior_date,
+    app_id,
+    user_snowplow_domain_id,
+    contexts,
+    page_url_path,
+    page_url_fragment,
+    gsc_google_analytics_client_id,
+    gsc_pseudonymized_user_id,
+    gsc_extra,
+    gsc_plan,
+    gsc_source,
+    event_value,
+    session_index,
+    session_id,
+    event_category,
+    event_action,
+    event_label,
+    event_property,
+    dim_namespace_id,
+    ultimate_parent_namespace_id,
+    namespace_is_internal,
+    namespace_is_ultimate_parent,
+    namespace_type,
+    visibility_level,
+    dim_project_id,
+    device_type,
+    is_device_mobile,
+    dim_behavior_referrer_page_sk,
+    has_gitlab_service_ping_context,
+    has_gitlab_experiment_context,
+    has_customer_standard_context,
+    browser_name,
+    dim_behavior_browser_sk,
+    dim_plan_id,
+    plan_id_modified,
+    plan_name,
+    plan_name_modified
+  FROM mart_behavior_structured_event
+  WHERE behavior_at::DATE >= DATEADD(DAY, -190, CURRENT_DATE::DATE)
+    AND LOWER(event_category) LIKE '%registration%'
+  {% if is_incremental() %}
 
     AND  behavior_at > (SELECT MAX(behavior_at) FROM {{ this }})
 
-    {% endif %}
+  {% endif %}
 
 )
 
--- transform/snowflake-dbt/models/workspaces/workspace_product/reporting/wk_rpt_registration_website_page_view.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_product/reporting/wk_rpt_registration_website_page_view.sql
@@ -9,8 +9,7 @@
     ('fct_behavior_website_page_view','fct_behavior_website_page_view'),
     ('dim_behavior_website_page', 'dim_behavior_website_page'),
     ])
-
-}}, 
+}},
 
 website_page_views AS (
 
@@ -45,17 +44,17 @@ website_page_views AS (
     dim.page_url_query,
     dim.clean_url_path,
     dim.page_url_scheme,
-    dim.page_group, 
-    dim.page_type, 
-    dim.page_sub_type, 
+    dim.page_group,
+    dim.page_type,
+    dim.page_sub_type,
     dim.referrer_medium,
     dim.url_namespace_id,
     dim.url_project_id,
     dim.url_path_category,
     dim.is_url_interacting_with_security
   FROM fct_behavior_website_page_view AS fct
-  JOIN dim_behavior_website_page AS dim 
-    ON fct.dim_behavior_website_page_sk = dim.dim_behavior_website_page_sk  
+  INNER JOIN dim_behavior_website_page AS dim
+    ON fct.dim_behavior_website_page_sk = dim.dim_behavior_website_page_sk
   WHERE dim.clean_url_path IN ( -- all URLs involved in registration flow https://docs.google.com/presentation/d/1bb4vFu6Nd11itFJxNJO4eim-xnFbJw3pHCuknNPXk_g/edit?usp=sharing
     'trial_registrations',
     'trial_registrations/new',
@@ -68,15 +67,16 @@ website_page_views AS (
     'users/sign_up/company/new',
     'users/sign_up/groups/new',
     'namespace/project/learn_gitlab',
-    'namespace/project/learn_gitlab/onboarding')
-    AND fct.event_name = 'page_view' 
-    AND dim.app_id IN ('gitlab','gitlab_customers') -- production app_id
+    'namespace/project/learn_gitlab/onboarding'
+    )
+    AND fct.event_name = 'page_view'
+    AND dim.app_id IN ('gitlab', 'gitlab_customers') -- production app_id
     AND fct.behavior_at::DATE >= DATEADD(DAY, -190, CURRENT_DATE::DATE)
-    {% if is_incremental() %}
+  {% if is_incremental() %}
 
     AND  fct.behavior_at > (SELECT MAX(behavior_at) FROM {{ this }})
 
-    {% endif %}
+  {% endif %}
 
 )
 
