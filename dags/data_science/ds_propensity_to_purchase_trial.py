bf915f4a3cd1ca37393202b893b486ed7b0b1372
Snehansh Chaturvedi <schaturvedi@gitlab.com>
2023-07-21 13:38:54 +0530

    remove affinity and toleration for ds dags

--- dags/data_science/ds_propensity_to_purchase_trial.py
+++ dags/data_science/ds_propensity_to_purchase_trial.py
@@ -23,8 +23,6 @@ from kube_secrets import (
     GITLAB_ANALYTICS_PRIVATE_TOKEN,
 )
 
-from kubernetes_helpers import get_affinity, get_toleration
-
 # Load the env vars into a dict and set Secrets
 env = os.environ.copy()
 GIT_BRANCH = env["GIT_BRANCH"]
@@ -93,8 +91,6 @@ KubernetesPodOperator(
         GITLAB_ANALYTICS_PRIVATE_TOKEN,
     ],
     env_vars=pod_env_vars,
-    affinity=get_affinity("production"),
-    tolerations=get_toleration("production"),
     arguments=[ptpt_scoring_command],
     dag=dag,
 )
