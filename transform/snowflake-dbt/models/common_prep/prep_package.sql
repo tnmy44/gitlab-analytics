eb69a369ccc5afa7a696ed6523fadfb59b6565c2
Michelle Cooper <mcooper@gitlab.com>
2023-07-28 11:44:26 -0700

    create dimension, remove legacy model

--- transform/snowflake-dbt/models/common_prep/prep_package.sql
+++ transform/snowflake-dbt/models/common_prep/prep_package.sql
@@ -4,7 +4,7 @@
 
 {{ config({
     "materialized": "incremental",
-    "unique_key": "dim_package_id"
+    "unique_key": "dim_package_sk"
     })
 }}
 
@@ -16,10 +16,10 @@
     ('prep_project', 'prep_project')
 ]) }}
 
-, gitlab_dotcom_packages_packages_dedupe_source AS (
+, gitlab_dotcom_packages_packages_source AS (
 
     SELECT *
-    FROM {{ ref('gitlab_dotcom_packages_packages_dedupe_source')}}
+    FROM {{ ref('gitlab_dotcom_packages_packages_source')}}
     {% if is_incremental() %}
 
       WHERE updated_at > (SELECT MAX(updated_at) FROM {{this}})
@@ -29,40 +29,41 @@
 ), renamed AS (
   
     SELECT
-      id::NUMBER                                                          AS dim_package_id,
-      
+      {{ dbt_utils.get_surrogate_key(['gitlab_dotcom_packages_packages_source.packages_package_id']) }}     AS dim_package_sk,
+      packages_package_id                                                                                   AS package_id,
       -- FOREIGN KEYS
-      prep_project.dim_project_id                                         AS dim_project_id,
-      prep_namespace.dim_namespace_id,
-      prep_namespace.ultimate_parent_namespace_id,
-      dim_date.date_id                                                    AS created_date_id,
-      IFNULL(dim_namespace_plan_hist.dim_plan_id, 34)                     AS dim_plan_id,
-      gitlab_dotcom_packages_packages_dedupe_source.creator_id            AS creator_id,
-      prep_project.namespace_is_internal,
-      version::VARCHAR                                                    AS package_version,
-      package_type::VARCHAR                                               AS package_type,
-      gitlab_dotcom_packages_packages_dedupe_source.created_at::TIMESTAMP AS created_at,
-      gitlab_dotcom_packages_packages_dedupe_source.updated_at::TIMESTAMP AS updated_at
+      prep_project.dim_project_id                                                                           AS dim_project_id,
+      prep_namespace.dim_namespace_id                                                                       AS dim_namespace_id,
+      prep_namespace.ultimate_parent_namespace_id                                                           AS ultimate_parent_namespace_id,
+      dim_date.date_id                                                                                      AS created_date_id,
+      IFNULL(dim_namespace_plan_hist.dim_plan_id, 34)                                                       AS dim_plan_id,
+      gitlab_dotcom_packages_packages_dedupe_source.creator_id                                              AS creator_id,
 
-    FROM gitlab_dotcom_packages_packages_dedupe_source
+      -- package attributes
+      gitlab_dotcom_packages_packages_source.package_version                                                AS package_version,
+      gitlab_dotcom_packages_packages_source.package_type                                                   AS package_type,
+      gitlab_dotcom_packages_packages_source.created_at                                                     AS created_at,
+      gitlab_dotcom_packages_packages_source.updated_at                                                     AS updated_at
+
+    FROM gitlab_dotcom_packages_packages_source
     LEFT JOIN prep_project 
-      ON gitlab_dotcom_packages_packages_dedupe_source.project_id = prep_project.dim_project_id
+      ON gitlab_dotcom_packages_packages_source.project_id = prep_project.dim_project_id
     LEFT JOIN dim_namespace_plan_hist 
       ON prep_project.ultimate_parent_namespace_id = dim_namespace_plan_hist.dim_namespace_id
-      AND gitlab_dotcom_packages_packages_dedupe_source.created_at >= dim_namespace_plan_hist.valid_from
-      AND gitlab_dotcom_packages_packages_dedupe_source.created_at < COALESCE(dim_namespace_plan_hist.valid_to, '2099-01-01')
+      AND gitlab_dotcom_packages_packages_source.created_at >= dim_namespace_plan_hist.valid_from
+      AND gitlab_dotcom_packages_packages_source.created_at < COALESCE(dim_namespace_plan_hist.valid_to, '2099-01-01')
     LEFT JOIN prep_namespace
       ON prep_project.dim_namespace_id = prep_namespace.dim_namespace_id
       AND is_currently_valid = TRUE
     INNER JOIN dim_date 
-      ON TO_DATE(gitlab_dotcom_packages_packages_dedupe_source.created_at) = dim_date.date_day
+      ON TO_DATE(gitlab_dotcom_packages_packages_source.created_at) = dim_date.date_day
 
 )
 
 {{ dbt_audit(
     cte_ref="renamed",
     created_by="@mpeychet_",
-    updated_by="@mpeychet_",
+    updated_by="@michellecooper",
     created_date="2021-08-05",
-    updated_date="2022-06-01"
+    updated_date="2023-07-28"
 ) }}
