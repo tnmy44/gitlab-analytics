eacea36b61f4abd2d85d06f57213b79f80a7cd1c
snalamaru <snalamaru@gitlab.com>
2023-08-01 12:38:27 -0500

    Updated source file

--- transform/snowflake-dbt/models/sources/customers/customers_db_billing_account_contacts_source.sql
+++ transform/snowflake-dbt/models/sources/customers/customers_db_billing_account_contacts_source.sql
@@ -9,7 +9,7 @@ WITH source AS (
     SELECT DISTINCT
       id::NUMBER                                              AS billing_account_contact_id,
       work_email::VARCHAR                                     AS work_email,
-      zuora_contact_id:VARCHAR                                AS zuora_contact_id,
+      zuora_contact_id::VARCHAR                                AS zuora_contact_id,
       zuora_account_id::VARCHAR                               AS zuora_account_id,
       created_at::TIMESTAMP                                   AS billing_account_contact_created_at,
       updated_at::TIMESTAMP                                   AS billing_account_contact_updated_at
