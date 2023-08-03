1a0e9cb559b00e2894886184bc21fd6cca69e5be
Justin Wong <juwong@gitlab.com>
2023-08-03 15:13:40 -0400

    update dag

--- dags/transformation/dbt_merge_request_diff_commits_backfill.py
+++ dags/transformation/dbt_merge_request_diff_commits_backfill.py
@@ -1,13 +1,10 @@
 """
 
 """
-import json
 import os
-from datetime import date, datetime
-
+from datetime import datetime
 from airflow import DAG
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
-from airflow.operators.dummy_operator import DummyOperator
 from airflow_utils import (
     DBT_IMAGE,
     dbt_install_deps_nosha_cmd,
@@ -15,7 +12,10 @@ from airflow_utils import (
     gitlab_pod_env_vars,
     partitions,
     slack_failed_task,
+    run_command_test_exclude,
 )
+
+from kubernetes_helpers import get_affinity, get_toleration
 from kube_secrets import (
     GIT_DATA_TESTS_PRIVATE_KEY,
     GIT_DATA_TESTS_CONFIG,
@@ -49,28 +49,30 @@ if GIT_BRANCH in ["master", "main"]:
 else:
     target = "ci"
 
-task_secrets = [
-    GIT_DATA_TESTS_PRIVATE_KEY,
+dbt_secrets = [
     GIT_DATA_TESTS_CONFIG,
+    GIT_DATA_TESTS_PRIVATE_KEY,
     SALT,
     SALT_EMAIL,
     SALT_IP,
     SALT_NAME,
     SALT_PASSWORD,
     SNOWFLAKE_ACCOUNT,
+    SNOWFLAKE_LOAD_USER,
+    SNOWFLAKE_LOAD_PASSWORD,
+    SNOWFLAKE_LOAD_ROLE,
+    SNOWFLAKE_LOAD_WAREHOUSE,
     SNOWFLAKE_PASSWORD,
     SNOWFLAKE_TRANSFORM_ROLE,
     SNOWFLAKE_TRANSFORM_SCHEMA,
     SNOWFLAKE_TRANSFORM_WAREHOUSE,
     SNOWFLAKE_USER,
-    SNOWFLAKE_LOAD_PASSWORD,
-    SNOWFLAKE_LOAD_ROLE,
-    SNOWFLAKE_LOAD_USER,
-    SNOWFLAKE_LOAD_WAREHOUSE,
     MCD_DEFAULT_API_ID,
     MCD_DEFAULT_API_TOKEN,
     SNOWFLAKE_STATIC_DATABASE,
 ]
+
+DBT_MODULE_NAME = "gitlab_dotcom_backfill_chunked"
 
 # Default arguments for the DAG
 default_args = {
@@ -78,64 +80,60 @@ default_args = {
     "depends_on_past": False,
     "on_failure_callback": slack_failed_task,
     "owner": "airflow",
-    "start_date": datetime(2019, 1, 1, 0, 0, 0),
+    # TODO: this needs to start at the minimum commit_date of the data
+    "start_date": datetime(2021, 7, 1),
 }
 
 # Create the DAG
 dag = DAG(
     "dbt_merge_request_diff_commits_backfill",
     default_args=default_args,
-    schedule_interval=None,
+    schedule_interval='30 16 * * 0',#
     concurrency=2,
 )
 
 
-def generate_dbt_command(vars_dict):
-    json_dict = json.dumps(vars_dict)
-
-    dbt_generate_command = f"""
+dbt_models_cmd = f"""
         {dbt_install_deps_nosha_cmd} &&
-        export SNOWFLAKE_TRANSFORM_WAREHOUSE="TRANSFORMING_M" &&
-        dbt run --profiles-dir profile --target {target} --models tag:gitlab_dotcom_backfill --vars '{json_dict}' ; ret=$?;
+        dbt run --profiles-dir profile --target {target} --models tag:{DBT_MODULE_NAME} ; ret=$?;
         montecarlo import dbt-run --manifest target/manifest.json --run-results target/run_results.json --project-name gitlab-analysis;
         python ../../orchestration/upload_dbt_file_to_snowflake.py results; exit $ret
         """
 
-    return KubernetesPodOperator(
-        **gitlab_defaults,
-        image=DBT_IMAGE,
-        task_id=f"dbt-snowplow-backfill-{vars_dict['year']}-{vars_dict['month']}",
-        name=f"dbt-snowplow-backfill-{vars_dict['year']}-{vars_dict['month']}",
-        secrets=task_secrets,
-        env_vars=pod_env_vars,
-        arguments=[dbt_generate_command],
-        dag=dag,
-    )
-
-
-dbt_snowplow_combined_cmd = f"""
-        {dbt_install_deps_nosha_cmd} &&
-        dbt run --profiles-dir profile --target {target} --models legacy.snowplow.combined ; ret=$?;
-        montecarlo import dbt-run --manifest target/manifest.json --run-results target/run_results.json --project-name gitlab-analysis;
-        python ../../orchestration/upload_dbt_file_to_snowflake.py results; exit $ret
-        """
-
-dbt_snowplow_combined = KubernetesPodOperator(
+dbt_models_task_name = f"{DBT_MODULE_NAME}-dbt-models"
+dbt_models_task = KubernetesPodOperator(
     **gitlab_defaults,
     image=DBT_IMAGE,
-    task_id=f"dbt-snowplow-combined",
-    name=f"dbt-snowplow-combined",
-    trigger_rule="all_success",
-    secrets=task_secrets,
+    task_id=dbt_models_task_name,
+    name=dbt_models_task_name,
+    secrets=dbt_secrets,
     env_vars=pod_env_vars,
-    arguments=[dbt_snowplow_combined_cmd],
+    arguments=[dbt_models_cmd],
     dag=dag,
 )
 
 
-dummy_operator = DummyOperator(task_id="start", dag=dag)
+'''
+dbt_test_task_name = f"{DBT_MODULE_NAME}-dbt-tests"
+model_test_cmd = f"""
+    {dbt_install_deps_nosha_cmd} &&
+    dbt test --profiles-dir profile --target prod --models tag:{DBT_MODULE_NAME}+ {run_command_test_exclude} ; ret=$?;
+    montecarlo import dbt-run --manifest target/manifest.json --run-results target/run_results.json --project-name gitlab-analysis;
+    python ../../orchestration/upload_dbt_file_to_snowflake.py test; exit $ret
+"""
+dbt_test_task = KubernetesPodOperator(
+    **gitlab_defaults,
+    image=DBT_IMAGE,
+    task_id=dbt_test_task_name,
+    name=dbt_test_task_name,
+    secrets=dbt_secrets,
+    env_vars=gitlab_pod_env_vars,
+    arguments=[model_test_cmd],
+    affinity=get_affinity("production"),
+    tolerations=get_toleration("production"),
+    dag=dag,
+)
+'''
 
-for month in partitions(
-    datetime.strptime("2018-07-01", "%Y-%m-%d").date(), date.today(), "month"
-):
-    dummy_operator >> generate_dbt_command(month) >> dbt_snowplow_combined
+dbt_models_task
+# dbt_models_task >> dbt_test_task
