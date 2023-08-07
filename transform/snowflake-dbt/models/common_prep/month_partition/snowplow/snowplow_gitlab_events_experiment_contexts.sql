d09f488e0f4c8c2f185993037fabf06ca44b9b69
Rigerta Demiri <rdemiri@gitlab.com>
2023-08-07 11:18:34 +0200

    Add models and pump for the churn forecasting scores

--- transform/snowflake-dbt/models/common_prep/month_partition/snowplow/snowplow_gitlab_events_experiment_contexts.sql
+++ transform/snowflake-dbt/models/common_prep/month_partition/snowplow/snowplow_gitlab_events_experiment_contexts.sql
@@ -1,7 +1,7 @@
 {% set year_value = var('year', (run_started_at - modules.datetime.timedelta(1)).strftime('%Y')) %}
 {% set month_value = var('month', (run_started_at - modules.datetime.timedelta(1)).strftime('%m')) %}
-{% set start_date = modules.datetime.date(year=year_value| int , month=month_value | int, day=01) %}
-{% set end_date = (start_date + modules.datetime.timedelta(days=31)).strftime('%Y-%m-01') %}
+{% set start_date = modules.datetime.date(year=year_value| int , month=month_value | int, day=1) %}
+{% set end_date = (start_date + modules.datetime.timedelta(days=31)).strftime('%Y-%m-1') %}
 
 WITH base AS (
 
