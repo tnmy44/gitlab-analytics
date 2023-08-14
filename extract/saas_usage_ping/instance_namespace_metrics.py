b118573143be0da3459243ecc28488a67bbaac56
Radovan Bacovic <rbacovic@gitlab.com>
2023-08-14 18:00:11 +0200

    Test

--- extract/saas_usage_ping/instance_namespace_metrics.py
+++ extract/saas_usage_ping/instance_namespace_metrics.py
@@ -156,7 +156,7 @@ class InstanceNamespaceMetrics:
             conn.execute(f"{sql_ready}")
         except Exception as programming_error:
             info(f"......ERROR: {str(programming_error)}")
-            error_text = str(programming_error)[53:90]
+            error_text = str(programming_error)[86:].replace('(','').replace(")","")
 
             error_record = (f"{self.SQL_INSERT_PART} "
                             f"VALUES "
