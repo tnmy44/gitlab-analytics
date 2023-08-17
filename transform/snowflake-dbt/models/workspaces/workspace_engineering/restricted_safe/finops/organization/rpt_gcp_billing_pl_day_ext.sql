b301e057f70d838774e35b6c3949516774be2502
Dani Deng <ddeng@gitlab.com>
2023-08-17 17:56:20 +0200

    update finance sku categories

--- transform/snowflake-dbt/models/workspaces/workspace_engineering/restricted_safe/finops/organization/rpt_gcp_billing_pl_day_ext.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_engineering/restricted_safe/finops/organization/rpt_gcp_billing_pl_day_ext.sql
@@ -47,146 +47,80 @@ join_product_component AS (
     END AS product_component
   FROM join_path
 
-)
+),
 
-/*
 join_finance_component AS (
 
   SELECT
     *,
     CASE
       -- STORAGE
-      WHEN LOWER(gcp_service_description) LIKE 'cloud storage'
-        AND (LOWER(gcp_sku_description) LIKE '%standard storage%') THEN 'Storage'
-      WHEN LOWER(gcp_service_description) LIKE 'cloud storage'
-        AND (LOWER(gcp_sku_description) LIKE '%coldline storage%') THEN 'Storage'
-      WHEN LOWER(gcp_service_description) LIKE 'cloud storage'
-        AND (LOWER(gcp_sku_description) LIKE '%archive storage%') THEN 'Storage'
-      WHEN LOWER(gcp_service_description) LIKE 'cloud storage'
-        AND (LOWER(gcp_sku_description) LIKE '%nearline storage%') THEN 'Storage'
-      WHEN LOWER(gcp_service_description) LIKE 'cloud storage'
-        AND (LOWER(gcp_sku_description) LIKE '%operations%') THEN 'Storage'
-      WHEN LOWER(gcp_service_description) LIKE 'compute engine'
-        AND LOWER(gcp_sku_description) LIKE '%pd capacity%' THEN 'Storage'
-      WHEN LOWER(gcp_service_description) LIKE 'compute engine'
-        AND LOWER(gcp_sku_description) LIKE '%pd snapshot%' THEN 'Storage'
-      WHEN (LOWER(gcp_service_description) LIKE 'bigquery'
-        AND LOWER(gcp_sku_description) LIKE '%storage%') THEN 'Storage'
-      WHEN (LOWER(gcp_service_description) LIKE 'cloud sql'
-        AND LOWER(gcp_sku_description) LIKE '%storage%') THEN 'Storage'
+      WHEN LOWER(gcp_service_description) = 'compute engine' AND LOWER(gcp_sku_description) LIKE '%pd capacity%' THEN 'Storage'
+      WHEN LOWER(gcp_service_description) = 'compute engine' AND LOWER(gcp_sku_description) LIKE '%pd snapshot%' THEN 'Storage'
+      WHEN (LOWER(gcp_service_description) = 'bigquery' AND LOWER(gcp_sku_description) LIKE '%storage%') THEN 'Storage'
+      WHEN (LOWER(gcp_service_description) = 'cloud sql' AND LOWER(gcp_sku_description) LIKE '%storage%') THEN 'Storage'
+
       -- COMPUTE
-      WHEN LOWER(gcp_sku_description) LIKE '%commitment%' THEN 'Committed Usage'
-      WHEN LOWER(gcp_service_description) LIKE 'bigquery'
-        AND LOWER(gcp_sku_description) NOT LIKE 'storage' THEN 'Compute'
-      WHEN LOWER(gcp_service_description) LIKE 'cloud sql'
-        AND (LOWER(gcp_sku_description) LIKE '%cpu%'
-          OR LOWER(gcp_sku_description) LIKE '%ram%') THEN 'Compute'
-      WHEN LOWER(gcp_service_description) LIKE 'kubernetes engine' THEN 'Compute'
-      WHEN LOWER(gcp_service_description) LIKE 'vertex ai' THEN 'Compute'
-      WHEN LOWER(gcp_service_description) LIKE 'compute engine'
-        AND LOWER(gcp_sku_description) LIKE '%gpu%' THEN 'Compute'
-      WHEN LOWER(gcp_service_description) LIKE '%memorystore%'
-        AND LOWER(gcp_sku_description) LIKE '%capacity%' THEN 'Compute'
-      WHEN LOWER(gcp_service_description) LIKE 'compute engine'
-        AND (LOWER(gcp_sku_description) LIKE '%ram%'
-          OR LOWER(gcp_sku_description) LIKE '%cpu%'
-          OR LOWER(gcp_sku_description) LIKE '%core%') THEN 'Compute'
+      WHEN LOWER(gcp_sku_description) LIKE '%commitment%' THEN 'Committed Usage'--keep
+      WHEN LOWER(gcp_service_description) = 'vertex ai' THEN 'AI/ML'--new category
+      WHEN LOWER(gcp_service_description) = 'compute engine' AND (LOWER(gcp_sku_description) LIKE '%gpu%' or LOWER(gcp_sku_description) like '%prediction%') THEN 'AI/ML'--new category
+      WHEN LOWER(gcp_service_description) = 'kubernetes engine' THEN 'Compute'
+      WHEN LOWER(gcp_service_description) = 'bigquery' AND LOWER(gcp_sku_description) NOT LIKE 'storage' THEN 'Compute'
+      WHEN LOWER(gcp_service_description) = 'cloud sql' AND (LOWER(gcp_sku_description) LIKE '%cpu%' OR LOWER(gcp_sku_description) LIKE '%ram%') THEN 'Compute'
+      WHEN LOWER(gcp_service_description) LIKE '%memorystore%' AND LOWER(gcp_sku_description) LIKE '%capacity%' THEN 'Compute'
+
       -- NETWORKING
-      WHEN LOWER(gcp_service_description) LIKE '%cloud sql%'
-        AND LOWER(gcp_sku_description) LIKE '%networking%' THEN 'Networking'
-      WHEN LOWER(gcp_service_description) LIKE '%cloud pub/sub%' THEN 'Networking'
-      WHEN LOWER(gcp_service_description) LIKE '%memorystore%'
-        AND LOWER(gcp_sku_description) LIKE '%networking%' THEN 'Networking'
-      WHEN LOWER(gcp_service_description) LIKE '%cloud storage%'
-        AND LOWER(gcp_sku_description) LIKE '%egress%' THEN 'Networking'
-      WHEN LOWER(gcp_service_description) LIKE '%cloud storage%'
-        AND LOWER(gcp_sku_description) LIKE '%cdn%' THEN 'Networking'
-      WHEN LOWER(gcp_service_description) = 'networking' THEN 'Networking'
+      WHEN LOWER(gcp_service_description) = 'cloud sql' AND LOWER(gcp_sku_description) LIKE '%networking%' THEN 'Networking'
+      WHEN LOWER(gcp_service_description) = 'cloud pub/sub' THEN 'Networking'--keep
+      WHEN LOWER(gcp_service_description) LIKE '%memorystore%' AND LOWER(gcp_sku_description) LIKE '%networking%' THEN 'Networking'
       WHEN LOWER(gcp_sku_description) LIKE '%load balanc%' THEN 'Networking'
-      WHEN LOWER(gcp_service_description) LIKE '%compute engine%'
-        AND LOWER(gcp_sku_description) LIKE '%ip%' THEN 'Networking'
-      WHEN LOWER(gcp_service_description) LIKE '%compute engine%'
-        AND LOWER(gcp_sku_description) LIKE '%network%' THEN 'Networking'
-      WHEN LOWER(gcp_service_description) LIKE '%cloud storage%'
-        AND (LOWER(gcp_sku_description) LIKE '%network%'
-          OR LOWER(gcp_sku_description) LIKE '%download%') THEN 'Networking'
-      -- LOGGING AND METRICS
-      WHEN LOWER(gcp_service_description) LIKE 'stackdriver monitoring' THEN 'Logging and Metrics'
-      WHEN LOWER(gcp_service_description) LIKE 'cloud logging' THEN 'Logging and Metrics'
+      WHEN LOWER(gcp_service_description) = 'compute engine' AND (LOWER(gcp_sku_description) LIKE '%ip%' or LOWER(gcp_sku_description) LIKE '%network%' or LOWER(gcp_sku_description) LIKE '%upload%'  or LOWER(gcp_sku_description) LIKE '%download%') THEN 'Networking'--keep
+      WHEN LOWER(gcp_service_description) = 'cloud storage' AND (LOWER(gcp_sku_description) LIKE '%network%' OR LOWER(gcp_sku_description) LIKE '%download%' or LOWER(gcp_sku_description) LIKE '%cdn%') THEN 'Networking'
+
       -- SUPPORT
       WHEN LOWER(gcp_sku_description) LIKE '%support%' THEN 'Support'
       WHEN LOWER(gcp_sku_description) LIKE '%security command center%' THEN 'Support'
       WHEN LOWER(gcp_sku_description) LIKE '%marketplace%' THEN 'Support'
-      ELSE 'Other'
+      ELSE gcp_service_description
     END AS finance_sku_type,
 
     CASE
       -- STORAGE
-      WHEN LOWER(gcp_service_description) LIKE 'cloud storage'
-        AND (LOWER(gcp_sku_description) LIKE '%standard storage%') THEN 'Object (storage)'
-      WHEN LOWER(gcp_service_description) LIKE 'cloud storage'
-        AND (LOWER(gcp_sku_description) LIKE '%coldline storage%') THEN 'Object (storage)'
-      WHEN LOWER(gcp_service_description) LIKE 'cloud storage'
-        AND (LOWER(gcp_sku_description) LIKE '%archive storage%') THEN 'Object (storage)'
-      WHEN LOWER(gcp_service_description) LIKE 'cloud storage'
-        AND (LOWER(gcp_sku_description) LIKE '%nearline storage%') THEN 'Object (storage)'
-      WHEN LOWER(gcp_service_description) LIKE 'cloud storage'
-        AND (LOWER(gcp_sku_description) LIKE '%operations%') THEN 'Object (operations)'
-      WHEN LOWER(gcp_service_description) LIKE 'compute engine'
-        AND LOWER(gcp_sku_description) LIKE '%pd capacity%' THEN 'Repository (storage)'
-      WHEN LOWER(gcp_service_description) LIKE 'compute engine'
-        AND LOWER(gcp_sku_description) LIKE '%pd snapshot%' THEN 'Repository (storage)'
-      WHEN (LOWER(gcp_service_description) LIKE 'bigquery'
-        AND LOWER(gcp_sku_description) LIKE '%storage%') THEN 'Data Warehouse (storage)'
-      WHEN (LOWER(gcp_service_description) LIKE 'cloud sql'
-        AND LOWER(gcp_sku_description) LIKE '%storage%') THEN 'Databases (storage)'
+      WHEN LOWER(gcp_service_description) = 'cloud storage' and ((LOWER(gcp_sku_description) LIKE '%standard storage%') or (LOWER(gcp_sku_description) LIKE '%coldline storage%') or (LOWER(gcp_sku_description) LIKE '%archive storage%') or (LOWER(gcp_sku_description) LIKE '%nearline storage%')) THEN 'Object (storage)'
+      WHEN LOWER(gcp_service_description) = 'cloud storage' and LOWER(gcp_sku_description) LIKE '%operations%' THEN 'Object (operations)'
+      WHEN LOWER(gcp_service_description) = 'compute engine' AND (LOWER(gcp_sku_description) LIKE '%pd capacity%' OR LOWER(gcp_sku_description) LIKE '%pd snapshot%') THEN 'Repository (storage) - to be refined'
+      WHEN (LOWER(gcp_service_description) = 'bigquery' AND LOWER(gcp_sku_description) LIKE '%storage%') THEN 'Data Warehouse (storage)'
+      WHEN (LOWER(gcp_service_description) = 'cloud sql' AND LOWER(gcp_sku_description) LIKE '%storage%') THEN 'Databases (storage)'
+
       -- COMPUTE
-      WHEN LOWER(gcp_sku_description) LIKE '%commitment%' THEN 'Committed Usage'
-      WHEN LOWER(gcp_service_description) LIKE 'bigquery'
-        AND LOWER(gcp_sku_description) NOT LIKE 'storage' THEN 'Data Warehouse (compute)'
-      WHEN LOWER(gcp_service_description) LIKE 'cloud sql'
-        AND (LOWER(gcp_sku_description) LIKE '%cpu%'
-          OR LOWER(gcp_sku_description) LIKE '%ram%') THEN 'Data Warehouse (compute)'
-      WHEN LOWER(gcp_service_description) LIKE 'kubernetes engine' THEN 'Container orchestration (compute)'
-      WHEN LOWER(gcp_service_description) LIKE 'vertex ai' THEN 'AI/ML (compute)'
-      WHEN LOWER(gcp_service_description) LIKE 'compute engine'
-        AND LOWER(gcp_sku_description) LIKE '%gpu%' THEN 'AI/ML (compute)'
-      WHEN LOWER(gcp_service_description) LIKE '%memorystore%'
-        AND LOWER(gcp_sku_description) LIKE '%capacity%' THEN 'Memorystores (compute)'
-      WHEN LOWER(gcp_service_description) LIKE 'compute engine'
-        AND (LOWER(gcp_sku_description) LIKE '%ram%'
-          OR LOWER(gcp_sku_description) LIKE '%cpu%'
-          OR LOWER(gcp_sku_description) LIKE '%core%') THEN 'Containers (compute)'
+      WHEN LOWER(gcp_sku_description) LIKE '%commitment%' THEN 'Committed Usage' --keep
+      WHEN LOWER(gcp_service_description) = 'kubernetes engine' THEN 'Container orchestration (compute)'--keep
+      WHEN LOWER(gcp_service_description) = 'vertex ai' THEN 'AI/ML (compute)'
+      WHEN LOWER(gcp_service_description) = 'compute engine' AND LOWER(gcp_sku_description) LIKE '%gpu%' THEN 'AI/ML (compute)'
+      WHEN LOWER(gcp_service_description) LIKE 'bigquery' AND LOWER(gcp_sku_description) NOT LIKE 'storage' THEN 'Data Warehouse (compute)'
+      WHEN LOWER(gcp_service_description) LIKE 'cloud sql' AND (LOWER(gcp_sku_description) LIKE '%cpu%' OR LOWER(gcp_sku_description) LIKE '%ram%') THEN 'Data Warehouse (compute)'
+      WHEN LOWER(gcp_service_description) LIKE '%memorystore%' AND LOWER(gcp_sku_description) LIKE '%capacity%' THEN 'Memorystores (compute)'
+      when LOWER(gcp_service_description) = 'compute engine' then 'Compute'
+
       -- NETWORKING
-      WHEN LOWER(gcp_service_description) LIKE '%cloud sql%'
-        AND LOWER(gcp_sku_description) LIKE '%networking%' THEN 'Databases (networking)'
-      WHEN LOWER(gcp_service_description) LIKE '%cloud pub/sub%' THEN 'Messaging (networking)'
-      WHEN LOWER(gcp_service_description) LIKE '%memorystore%'
-        AND LOWER(gcp_sku_description) LIKE '%networking%' THEN 'Memorystores (networking)'
-      WHEN LOWER(gcp_service_description) LIKE '%cloud storage%'
-        AND LOWER(gcp_sku_description) LIKE '%egress%' THEN 'Object (networking)'
-      WHEN LOWER(gcp_service_description) LIKE '%cloud storage%'
-        AND LOWER(gcp_sku_description) LIKE '%cdn%' THEN 'Object CDN (networking)'
-      WHEN LOWER(gcp_service_description) = 'networking' THEN 'Networking (mixed)'
+      WHEN LOWER(gcp_service_description) = 'cloud sql' AND LOWER(gcp_sku_description) LIKE '%networking%' THEN 'Databases (networking)'
+      WHEN LOWER(gcp_service_description) = 'cloud pub/sub' THEN 'Messaging (networking)'--keep
+      WHEN LOWER(gcp_service_description) LIKE '%memorystore%' AND LOWER(gcp_sku_description) LIKE '%networking%' THEN 'Memorystores (networking)'
+      WHEN LOWER(gcp_service_description) = 'cloud storage' AND LOWER(gcp_sku_description) LIKE '%egress%' THEN case when LOWER(gcp_sku_description) LIKE '%multi-region within%' then 'Object (networking)' else 'Object (networking) - to be refined' end --keep
+      WHEN LOWER(gcp_service_description) = 'cloud storage' AND LOWER(gcp_sku_description) LIKE '%cdn%' THEN case when LOWER(gcp_sku_description) NOT LIKE '%from%' then 'Object CDN (networking)' else 'Object CDN (networking) - to be refined' end--keep
+      WHEN LOWER(gcp_service_description) = 'cloud storage' AND (LOWER(gcp_sku_description) LIKE '%network%' OR LOWER(gcp_sku_description) LIKE '%download%') THEN 'Networking on Buckets'--rename
       WHEN LOWER(gcp_sku_description) LIKE '%load balanc%' THEN 'Load Balancing (networking)'
-      WHEN LOWER(gcp_service_description) LIKE '%compute engine%'
-        AND LOWER(gcp_sku_description) LIKE '%ip%' THEN 'Container networking (networking)'
-      WHEN LOWER(gcp_service_description) LIKE '%compute engine%'
-        AND LOWER(gcp_sku_description) LIKE '%network%' THEN 'Container networking (networking)'
-      WHEN LOWER(gcp_service_description) LIKE '%cloud storage%'
-        AND (LOWER(gcp_sku_description) LIKE '%network%'
-          OR LOWER(gcp_sku_description) LIKE '%download%') THEN 'Container networking (networking)'
-      -- LOGGING AND METRICS
-      WHEN LOWER(gcp_service_description) LIKE 'stackdriver monitoring' THEN 'Metrics (logging and metrics)'
-      WHEN LOWER(gcp_service_description) LIKE 'cloud logging' THEN 'Logging (logging and metrics)'
+      WHEN LOWER(gcp_service_description) = 'networking' THEN 'Networking (mixed) - to be refined'
+
       -- SUPPORT
       WHEN LOWER(gcp_sku_description) LIKE '%support%' THEN 'Google Support (support)'
       WHEN LOWER(gcp_sku_description) LIKE '%security command center%' THEN 'Security (support)'
       WHEN LOWER(gcp_sku_description) LIKE '%marketplace%' THEN 'Marketplace (support)'
-      ELSE 'Other'
+
+      ELSE gcp_service_description
     END AS finance_sku_subtype
   FROM join_product_component
   
-)*/
+)
 
-SELECT * FROM join_product_component
+SELECT * FROM join_finance_component
