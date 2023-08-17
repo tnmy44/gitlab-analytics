2897d8a68bfe79330dfce3c8a7856e308924952d
Paul Armstrong <parmstrong@gitlab.com>
2023-08-17 21:41:41 +0200

    Line up indentation.

--- transform/snowflake-dbt/models/sources/data_science/ptpt_scores_source.sql
+++ transform/snowflake-dbt/models/sources/data_science/ptpt_scores_source.sql
@@ -1,14 +1,14 @@
 WITH source AS (
 
     SELECT
-        namespace_id as namespace_id,
-        score_date   as score_date,
-        score as score,
-        decile as decile,
-        importance as importance,
-        grouping as score_group,
-        insights as insights,
-        uploaded_at::TIMESTAMP as uploaded_at
+        namespace_id            as namespace_id,
+        score_date              as score_date,
+        score                   as score,
+        decile                  as decile,
+        importance              as importance,
+        grouping                as score_group,
+        insights                as insights,
+        uploaded_at::TIMESTAMP  as uploaded_at
     FROM {{ source('data_science', 'ptpt_scores') }}
 )
 
