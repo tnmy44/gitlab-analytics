f7c8f76c840c2ad7e19d570f1d182753714a8d65
Michelle Cooper <mcooper@gitlab.com>
2023-08-23 12:50:10 -0700

    update prep_namespace ref to remove non-current records and update test col name in schema.yml

--- transform/snowflake-dbt/models/common_prep/prep_epic.sql
+++ transform/snowflake-dbt/models/common_prep/prep_epic.sql
@@ -12,14 +12,19 @@
     ('prep_date', 'prep_date'),
     ('prep_namespace_plan_hist', 'prep_namespace_plan_hist'),
     ('plans', 'gitlab_dotcom_plans_source'),
-    ('prep_namespace', 'prep_namespace'),
     ('gitlab_dotcom_routes_source', 'gitlab_dotcom_routes_source'),
     ('prep_label_links', 'prep_label_links'),
     ('prep_labels', 'prep_labels'),
     ('gitlab_dotcom_award_emoji_source', 'gitlab_dotcom_award_emoji_source')
 ]) }}
 
-, gitlab_dotcom_epics_dedupe_source AS (
+,  namespace_prep AS (
+
+    SELECT *
+    FROM {{ ref('prep_namespace') }}
+    WHERE is_currently_valid = TRUE
+
+), gitlab_dotcom_epics_dedupe_source AS (
     
     SELECT *
     FROM {{ ref('gitlab_dotcom_epics_dedupe_source') }} 
@@ -57,7 +62,7 @@
       gitlab_dotcom_epics_dedupe_source.id::NUMBER                                           AS dim_epic_id,
       gitlab_dotcom_epics_dedupe_source.author_id::NUMBER                                    AS author_id,
       gitlab_dotcom_epics_dedupe_source.group_id::NUMBER                                     AS group_id,
-      prep_namespace.ultimate_parent_namespace_id::NUMBER                                    AS ultimate_parent_namespace_id,
+      namespace_prep.ultimate_parent_namespace_id::NUMBER                                    AS ultimate_parent_namespace_id,
       prep_date.date_id::NUMBER                                                              AS created_date_id,
       IFNULL(prep_namespace_plan_hist.dim_plan_id, 34)::NUMBER                               AS dim_plan_id_at_creation,
       gitlab_dotcom_epics_dedupe_source.assignee_id::NUMBER                                  AS assignee_id,
@@ -70,10 +75,10 @@
       gitlab_dotcom_epics_dedupe_source.last_edited_at::TIMESTAMP                            AS epic_last_edited_at,
       gitlab_dotcom_epics_dedupe_source.created_at::TIMESTAMP                                AS created_at,
       gitlab_dotcom_epics_dedupe_source.updated_at::TIMESTAMP                                AS updated_at,
-      IFF(prep_namespace.visibility_level = 'private',
+      IFF(namespace_prep.visibility_level = 'private',
         'private - masked',
         gitlab_dotcom_epics_dedupe_source.title::VARCHAR)                                    AS epic_title,
-      IFF(prep_namespace.visibility_level = 'private',
+      IFF(namespace_prep.visibility_level = 'private',
         'private - masked',
         gitlab_dotcom_epics_dedupe_source.description::VARCHAR)                              AS epic_description,
       gitlab_dotcom_epics_dedupe_source.closed_at::TIMESTAMP                                 AS closed_at,
@@ -82,23 +87,23 @@
       gitlab_dotcom_epics_dedupe_source.relative_position::NUMBER                            AS relative_position,
       gitlab_dotcom_epics_dedupe_source.start_date_sourcing_epic_id::NUMBER                  AS start_date_sourcing_epic_id,
       gitlab_dotcom_epics_dedupe_source.confidential::BOOLEAN                                AS is_confidential,
-      prep_namespace.namespace_is_internal                                                   AS is_internal_epic,
+      namespace_prep.namespace_is_internal                                                   AS is_internal_epic,
       {{ map_state_id('gitlab_dotcom_epics_dedupe_source.state_id') }}                       AS state_name,
       LENGTH(gitlab_dotcom_epics_dedupe_source.title)::NUMBER                                AS epic_title_length,
       LENGTH(gitlab_dotcom_epics_dedupe_source.description)::NUMBER                          AS epic_description_length,
-      IFF(prep_namespace.visibility_level = 'private',
+      IFF(namespace_prep.visibility_level = 'private',
         'private - masked',
         'https://gitlab.com/groups/' || gitlab_dotcom_routes_source.path || '/-/epics/' || gitlab_dotcom_epics_dedupe_source.iid)
                                                                                              AS epic_url,
-      IFF(prep_namespace.visibility_level = 'private',
+      IFF(namespace_prep.visibility_level = 'private',
         ARRAY_CONSTRUCT('private - masked'),
         agg_labels.labels)                                                                   AS labels,
       IFNULL(upvote_count.upvote_count, 0)                                                   AS upvote_count
     FROM gitlab_dotcom_epics_dedupe_source
     LEFT JOIN prep_namespace
-        ON gitlab_dotcom_epics_dedupe_source.group_id = prep_namespace.dim_namespace_id
+        ON gitlab_dotcom_epics_dedupe_source.group_id = namespace_prep.dim_namespace_id
     LEFT JOIN prep_namespace_plan_hist
-        ON prep_namespace.ultimate_parent_namespace_id = prep_namespace_plan_hist.dim_namespace_id
+        ON namespace_prep.ultimate_parent_namespace_id = prep_namespace_plan_hist.dim_namespace_id
         AND gitlab_dotcom_epics_dedupe_source.created_at >= prep_namespace_plan_hist.valid_from
         AND gitlab_dotcom_epics_dedupe_source.created_at < COALESCE(prep_namespace_plan_hist.valid_to, '2099-01-01')
     LEFT JOIN prep_date
