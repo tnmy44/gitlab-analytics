e2cebd9f5e104167073921b78511dd57516149e4
Paul Armstrong <parmstrong@gitlab.com>
2023-08-24 15:42:33 +0200

    Return xcom logic.

--- dags/extract/gitlab_dbs_extract_load_backfill.py
+++ dags/extract/gitlab_dbs_extract_load_backfill.py
@@ -129,8 +129,14 @@ def get_last_loaded(dag_name: str) -> Union[None, str]:
     if dag_name == "el_gitlab_ops":
         return None
 
-    return "{{{{ task_instance.xcom_pull('{}', include_prior_dates=True)['max_data_available'] }}}}".format(
-        task_identifier + "-pgp-extract"
+    xcom_date = datetime.now() - timedelta(hours=54)
+
+    return (
+        "{{{{ task_instance.xcom_pull('{task_id}', include_prior_dates=True)['max_data_available'] | "
+        "default('{default_date}', true) }}}}".format(
+                task_id=task_identifier + "-pgp-extract",
+                default_date=xcom_date.strftime("%Y-%m-%dT%H:%M:%S") + "+00:00",
+        )
     )
 
 
