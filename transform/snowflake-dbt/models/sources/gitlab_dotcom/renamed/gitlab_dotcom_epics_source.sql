60a1733d45a2b361a111f74662935a5cdc4c1de9
jjstark <jstark@gitlab.com>
2023-10-04 10:25:36 -0700

    formatting

--- transform/snowflake-dbt/models/sources/gitlab_dotcom/renamed/gitlab_dotcom_epics_source.sql
+++ transform/snowflake-dbt/models/sources/gitlab_dotcom/renamed/gitlab_dotcom_epics_source.sql
@@ -31,10 +31,10 @@ WITH all_epics AS (
 interal_epics AS (
 
   SELECT
-    id::NUMBER                                               AS internal_epic_id,
-    iid::NUMBER                                              AS internal_epic_iid,
-    title::VARCHAR                                           AS internal_epic_title,
-    description::VARCHAR                                     AS internal_epic_description
+    id::NUMBER                                     AS internal_epic_id,
+    iid::NUMBER                                    AS internal_epic_iid,
+    title::VARCHAR                                 AS internal_epic_title,
+    description::VARCHAR                           AS internal_epic_description
   FROM {{ ref('gitlab_dotcom_epics_internal_only_dedupe_source')}}
 
 ),
@@ -42,30 +42,30 @@ interal_epics AS (
 joined AS (
 
   SELECT
-    all_epics.epic_id                                AS epic_id,
-    all_epics.group_id                               AS group_id,
-    all_epics.author_id                              AS author_id,
-    all_epics.assignee_id                            AS assignee_id,
-    all_epics.epic_internal_id                       AS epic_internal_id,
-    all_epics.updated_by_id                          AS updated_by_id,
-    all_epics.last_edited_by_id                      AS last_edited_by_id,
-    all_epics.lock_version                           AS lock_version,
-    all_epics.epic_start_date                        AS epic_start_date,
-    all_epics.epic_end_date                          AS epic_end_date,
-    all_epics.epic_last_edited_at                    AS epic_last_edited_at,
-    all_epics.created_at                             AS created_at,
-    all_epics.updated_at                             AS updated_at,
-    interal_epics.internal_epic_title                AS epic_title,
-    interal_epics.internal_epic_description          AS epic_description,
-    all_epics.closed_at                              AS closed_at,
-    all_epics.state_id                               AS state_id,
-    all_epics.parent_id                              AS parent_id,
-    all_epics.relative_position                      AS relative_position,
-    all_epics.start_date_sourcing_epic_id            AS start_date_sourcing_epic_id,
-    all_epics.is_confidential                        AS is_confidential,
-    all_epics.state                                  AS state,
-    all_epics.epic_title_length                      AS epic_title_length,
-    all_epics.epic_description_length                AS epic_description_length
+    all_epics.epic_id                              AS epic_id,
+    all_epics.group_id                             AS group_id,
+    all_epics.author_id                            AS author_id,
+    all_epics.assignee_id                          AS assignee_id,
+    all_epics.epic_internal_id                     AS epic_internal_id,
+    all_epics.updated_by_id                        AS updated_by_id,
+    all_epics.last_edited_by_id                    AS last_edited_by_id,
+    all_epics.lock_version                         AS lock_version,
+    all_epics.epic_start_date                      AS epic_start_date,
+    all_epics.epic_end_date                        AS epic_end_date,
+    all_epics.epic_last_edited_at                  AS epic_last_edited_at,
+    all_epics.created_at                           AS created_at,
+    all_epics.updated_at                           AS updated_at,
+    interal_epics.internal_epic_title              AS epic_title,
+    interal_epics.internal_epic_description        AS epic_description,
+    all_epics.closed_at                            AS closed_at,
+    all_epics.state_id                             AS state_id,
+    all_epics.parent_id                            AS parent_id,
+    all_epics.relative_position                    AS relative_position,
+    all_epics.start_date_sourcing_epic_id          AS start_date_sourcing_epic_id,
+    all_epics.is_confidential                      AS is_confidential,
+    all_epics.state                                AS state,
+    all_epics.epic_title_length                    AS epic_title_length,
+    all_epics.epic_description_length              AS epic_description_length
   FROM all_epics
   LEFT JOIN interal_epics
     ON all_epics.epic_id = interal_epics.internal_epic_id
