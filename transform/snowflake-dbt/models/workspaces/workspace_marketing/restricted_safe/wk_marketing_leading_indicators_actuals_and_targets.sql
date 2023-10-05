4429dd536419dbc430ec2dcc3e42c24bbd788109
Robert Kohnke <rkohnke@gitlab.com>
2023-10-05 15:14:31 +0000

    Adding opp fields to target and actual L2R

--- transform/snowflake-dbt/models/workspaces/workspace_marketing/restricted_safe/wk_marketing_leading_indicators_actuals_and_targets.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_marketing/restricted_safe/wk_marketing_leading_indicators_actuals_and_targets.sql
@@ -51,7 +51,7 @@
         lead_source,
         source_buckets,
         inquiry_sum,
-        mql_sum,
+        mql_sum, 
 
     --Person Dates
         true_inquiry_date,
@@ -65,6 +65,8 @@
         crm_opp_owner_region_stamped,
         crm_opp_owner_area_stamped,
         sales_qualified_source_name,
+        opp_lead_source,
+        opp_lead_source_bucket,
 
     --Opportunity Dates
         sales_accepted_date,
@@ -77,6 +79,10 @@
         bizible_marketing_channel,
         bizible_marketing_channel_path,
         bizible_medium,
+        opp_touchpoint_offer_type_grouped,
+        opp_touchpoint_offer_type,
+        opp_bizible_marketing_channel,
+        opp_bizible_marketing_channel_path,
 
     --Flags
         is_mql,
@@ -179,14 +185,13 @@
         END AS crm_opp_owner_sales_segment_stamped_clean,
         crm_opp_owner_geo_stamped,
         email_domain_type,
-        lead_source,
-        source_buckets,
+        opp_lead_source,
+        opp_lead_source_bucket,
         sales_qualified_source_name,
         parent_crm_account_lam,
         parent_crm_account_lam_dev_count,
-        bizible_marketing_channel,
-        bizible_marketing_channel_path,
-        bizible_medium,
+        opp_bizible_marketing_channel,
+        opp_bizible_marketing_channel_path,
         crm_opp_owner_region_stamped,
         crm_opp_owner_area_stamped,
         CASE 
@@ -224,7 +229,7 @@
         parent_crm_account_lam_dev_count,
         bizible_marketing_channel,
         bizible_marketing_channel_path,
-        bizible_medium,
+        -- bizible_medium,
         'Inquiry' AS metric_type,
         COUNT(DISTINCT actual_inquiry) AS metric_value
     FROM inquiry_prep
@@ -251,7 +256,7 @@
         parent_crm_account_lam_dev_count,
         bizible_marketing_channel,
         bizible_marketing_channel_path,
-        bizible_medium,
+        -- bizible_medium,
         'MQL' AS metric_type,
         COUNT(DISTINCT mqls) AS metric_value
     FROM mql_prep
@@ -271,14 +276,14 @@
         crm_opp_owner_area_stamped AS area,
         sales_qualified_source_name,
         opp_order_type AS order_type,
-        lead_source,
-        source_buckets,
+        opp_lead_source,
+        opp_source_buckets,
         email_domain_type,
         parent_crm_account_lam,
         parent_crm_account_lam_dev_count,
-        bizible_marketing_channel,
-        bizible_marketing_channel_path,
-        bizible_medium,
+        opp_bizible_marketing_channel,
+        opp_bizible_marketing_channel_path,
+        -- bizible_medium,
         'SAO' AS metric_type,
         COUNT(DISTINCT saos) AS metric_value
     FROM sao_prep
@@ -305,7 +310,7 @@
         parent_crm_account_lam_dev_count,
         bizible_marketing_channel,
         bizible_marketing_channel_path,
-        bizible_medium,
+        -- bizible_medium,
         metric_type,
         metric_value
     FROM inquiries
@@ -329,7 +334,7 @@
         parent_crm_account_lam_dev_count,
         bizible_marketing_channel,
         bizible_marketing_channel_path,
-        bizible_medium,
+        -- bizible_medium,
         metric_type,
         metric_value
     FROM mqls
@@ -346,14 +351,14 @@
         area,
         region,
         sales_qualified_source_name,
-        lead_source,
-        source_buckets,
+        opp_lead_source,
+        opp_source_buckets,
         email_domain_type,
         parent_crm_account_lam,
         parent_crm_account_lam_dev_count,
-        bizible_marketing_channel,
-        bizible_marketing_channel_path,
-        bizible_medium,
+        opp_bizible_marketing_channel,
+        opp_bizible_marketing_channel_path,
+        -- bizible_medium,
         metric_type,
         metric_value
     FROM saos
@@ -379,7 +384,7 @@
     parent_crm_account_lam_dev_count,
     bizible_marketing_channel,
     bizible_marketing_channel_path,
-    bizible_medium,
+    -- bizible_medium,
     metric_type,
     metric_value
   FROM intermediate
@@ -403,7 +408,7 @@
     NULL AS parent_crm_account_lam_dev_count,
     NULL AS bizible_marketing_channel,
     NULL AS bizible_marketing_channel_path,
-    NULL AS bizible_medium,
+    -- NULL AS bizible_medium,
     kpi_name AS metric_type,
     daily_allocated_target AS metric_value
   FROM targets
@@ -413,8 +418,8 @@
 {{ dbt_audit(
     cte_ref="final",
     created_by="@rkohnke",
-    updated_by="@degan",
+    updated_by="@rkohnke",
     created_date="2023-08-22",
-    updated_date="2023-08-24",
+    updated_date="2023-10-05",
   ) }}
 
