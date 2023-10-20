6f5539bbfd8791a00ba3a25d92fb6d9f885df980
jjstark <jstark@gitlab.com>
2023-10-20 15:21:29 -0700

    wrong file

--- transform/snowflake-dbt/models/sources/gitlab_ops/gitlab_ops_ci_builds_source.sql
+++ transform/snowflake-dbt/models/sources/gitlab_ops/gitlab_ops_ci_builds_source.sql
@@ -1,16 +1,10 @@
-WITH all_rows_source AS (
+WITH source AS (
 
   SELECT *
   FROM {{ source('gitlab_ops', 'ci_builds') }}
   QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY _uploaded_at DESC) = 1
 
-), internal_rows_source AS (
-
-  SELECT *
-  FROM {{ source('gitlab_ops', 'ci_builds_internal_only') }}
-  QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY _uploaded_at DESC) = 1
-
-), all_rows_renamed AS (
+), renamed AS (
 
   SELECT
     id::NUMBER                        AS ci_build_id,
@@ -48,63 +42,7 @@ WITH all_rows_source AS (
     failure_reason::VARCHAR           AS failure_reason,
     scheduled_at::TIMESTAMP           AS ci_build_scheduled_at,
     upstream_pipeline_id::NUMBER      AS upstream_pipeline_id
-  FROM all_rows_source
-
-), internal_rows_renamed AS (
-
-  SELECT
-    id::NUMBER                        AS internal_ci_build_id,
-    updated_at::TIMESTAMP             AS internal_updated_at,
-    name::VARCHAR                     AS internal_name,
-    stage::VARCHAR                    AS internal_stage,
-    ref::VARCHAR                      AS internal_ref,
-    description::VARCHAR              AS internal_description,
-    project_id::NUMBER                AS internal_project_id
-  from internal_rows_source
-
-), joined AS (
-
-  SELECT
-
-    id                                AS ci_build_id,
-    status                            AS status,
-    finished_at                       AS finished_at,
-    created_at                        AS created_at,
-    updated_at                        AS updated_at,
-    started_at                        AS started_at,
-    runner_id                         AS ci_build_runner_id,
-    coverage                          AS coverage,
-    commit_id                         AS ci_build_commit_id,
-    internal_name                     AS ci_build_name,
-    options                           AS options,
-    allow_failure                     AS allow_failure,
-    internal_stage                    AS stage,
-    trigger_request_id                AS ci_build_trigger_request_id,
-    stage_idx                         AS stage_idx,
-    tag                               AS tag,
-    internal_ref                      AS ref,
-    user_id                           AS ci_build_user_id,
-    type                              AS type,
-    internal_description              AS description,
-    project_id                        AS ci_build_project_id,
-    erased_by_id                      AS ci_build_erased_by_id,
-    erased_at                         AS ci_build_erased_at,
-    artifacts_expire_at               AS ci_build_artifacts_expire_at,
-    environment                       AS environment,
-    queued_at                         AS ci_build_queued_at,
-    lock_version                      AS lock_version,
-    coverage_regex                    AS coverage_regex,
-    auto_canceled_by_id               AS ci_build_auto_canceled_by_id,
-    retried                           AS retried,
-    stage_id                          AS ci_build_stage_id,
-    protected                         AS protected,
-    failure_reason                    AS failure_reason,
-    scheduled_at                      AS ci_build_scheduled_at,
-    upstream_pipeline_id              AS upstream_pipeline_id
-
-  FROM all_rows_renamed
-  LEFT JOIN internal_rows_renamed 
-    ON all_rows_renamed.ci_build_id = internal_rows_renamed.internal_ci_build_id
+  FROM source
 
 )
 
@@ -113,12 +51,3 @@ SELECT *
 FROM renamed
 ORDER BY updated_at
 
-
-
-id
-updated_at
-name
-stage
-ref
-description
-project_id
