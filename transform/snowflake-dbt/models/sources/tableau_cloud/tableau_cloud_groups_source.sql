96658528e8f4369ddfc3fdc1722be6e6250ec711
Peter Empey <pempey@gitlab.com>
2023-10-23 12:25:04 -0600

    complete tableau_cloud source tables

--- transform/snowflake-dbt/models/sources/tableau_cloud/tableau_cloud_groups_source.sql
+++ transform/snowflake-dbt/models/sources/tableau_cloud/tableau_cloud_groups_source.sql
@@ -4,17 +4,17 @@ WITH source AS (
 ),
 
 renamed AS (
-  SELECT 
-    "Site LUID"::VARCHAR AS site_luid,
-    "Group LUID"::VARCHAR AS group_luid,
-    "User LUID"::VARCHAR AS user_luid,
-    "Site Name"::VARCHAR AS site_name,
-    "Group Name"::VARCHAR AS group_name,
-    "User Email"::VARCHAR AS user_email,
-    "Group Minimum Site Role"::VARCHAR AS group_minium_site_role,
-    "Group Is License On Sign In"::BOOLEAN AS is_license_on_sign_in,
+  SELECT
+    "Site LUID"::VARCHAR                     AS site_luid,
+    "Group LUID"::VARCHAR                    AS group_luid,
+    "User LUID"::VARCHAR                     AS user_luid,
+    "Site Name"::VARCHAR                     AS site_name,
+    "Group Name"::VARCHAR                    AS group_name,
+    "User Email"::VARCHAR                    AS user_email,
+    "Group Minimum Site Role"::VARCHAR       AS group_minium_site_role,
+    "Group Is License On Sign In"::BOOLEAN   AS is_license_on_sign_in,
     "Admin Insights Published At"::TIMESTAMP AS admin_insights_published_at,
-    uploaded_at
+    uploaded_at::TIMESTAMP                   AS uploaded_at
   FROM source
 )
 
--- transform/snowflake-dbt/models/sources/tableau_cloud/tableau_cloud_permissions_source.sql
+++ transform/snowflake-dbt/models/sources/tableau_cloud/tableau_cloud_permissions_source.sql
@@ -4,30 +4,31 @@ WITH source AS (
 ),
 
 renamed AS (
-  SELECT 
-    "Grantee Type"::VARCHAR AS grantee_type,
-    "Grantee LUID"::VARCHAR AS grantee_luid,
-    "Grantee Name"::VARCHAR AS grantee_name,
-    "Item Hyperlink"::VARCHAR AS item_hyperlink,
-    "Item LUID"::VARCHAR AS item_luid,
-    "Item Name"::VARCHAR AS item_name,
-    "Item Type"::VARCHAR AS item_type,
-    "Item Parent Project Name"::VARCHAR AS item_parent_project_name,
-    "Top Parent Project Name"::VARCHAR AS top_parent_project_name,
+  SELECT
+    "Site LUID"::VARCHAR                            AS site_luid,
+    "Grantee LUID"::VARCHAR                         AS grantee_luid,
+    "Grantee Type"::VARCHAR                         AS grantee_type,
+    "Item LUID"::VARCHAR                            AS item_luid,
+    "Item Type"::VARCHAR                            AS item_type,
+    "Capability Type"::VARCHAR                      AS capability_type,
+    "Site Name"::VARCHAR                            AS site_name,
+    "Grantee Name"::VARCHAR                         AS grantee_name,
+    "Item Name"::VARCHAR                            AS item_name,
+    "Item Hyperlink"::VARCHAR                       AS item_hyperlink,
+    "Item Parent Project Name"::VARCHAR             AS item_parent_project_name,
+    "Top Parent Project Name"::VARCHAR              AS top_parent_project_name,
     "Controlling Permissions Project Name"::VARCHAR AS controlling_permissions_project_name,
-    "Capability Type"::VARCHAR AS capability_type,
-    "Permission Value"::NUMBER AS permission_value,
-    "Permission Description"::VARCHAR AS permission_description,
-    "Site Name"::VARCHAR AS site_name,
-    "Site LUID"::VARCHAR AS site_luid,
-    NULLIF("User Email"::VARCHAR,'NA') AS user_email,
-    NULLIF("User LUID"::VARCHAR,'NA') AS user_luid,
-    NULLIF("User Site Role"::VARCHAR,'NA') AS user_site_role,
-    "Admin Insights Published At"::TIMESTAMP AS admin_insights_published_at,
-    "Has Permission?"::BOOLEAN AS has_permission,
-    uploaded_at::TIMESTAMP AS uploaded_at
+    "Permission Value"::NUMBER                      AS permission_value,
+    "Permission Description"::VARCHAR               AS permission_description,
+    NULLIF("User Email"::VARCHAR, 'NA')             AS user_email,
+    NULLIF("User LUID"::VARCHAR, 'NA')              AS user_luid,
+    NULLIF("User Site Role"::VARCHAR, 'NA')         AS user_site_role,
+    "Admin Insights Published At"::TIMESTAMP        AS admin_insights_published_at,
+    "Has Permission?"::BOOLEAN                      AS has_permission, -- noqa: L057
+    uploaded_at::TIMESTAMP                          AS uploaded_at
   FROM source
 )
 
 SELECT *
 FROM renamed
+QUALIFY ROW_NUMBER() OVER (PARTITION BY site_luid, grantee_luid, item_luid, capability_type, permission_value ORDER BY uploaded_at DESC) = 1
