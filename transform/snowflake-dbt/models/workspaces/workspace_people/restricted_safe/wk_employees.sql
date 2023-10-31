5d6d045c733c1b5de2d41a87f4d27c105432f087
Lis Vinueza <lvinueza@gitlab.com>
2023-10-31 23:52:39 +0000

    Fix spacing

--- transform/snowflake-dbt/models/workspaces/workspace_people/restricted_safe/wk_employees.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_people/restricted_safe/wk_employees.sql
@@ -7,10 +7,10 @@ WITH bamboo_mapping AS (
 
 gender_workday AS (
 
-	SELECT 
+  SELECT 
     employee_id,
-		gender,
-		gender_dropdown
+    gender,
+    gender_dropdown
   FROM {{ ref('workday_employee_mapping_source')}}
   WHERE is_current
 
@@ -18,49 +18,49 @@ gender_workday AS (
 
 email AS (
 
-	SELECT 
+  SELECT 
     employee_id,
 		last_work_email
-	FROM {{ref('employee_directory')}}
-	GROUP BY 1, 2 
+  FROM {{ref('employee_directory')}}
+  GROUP BY 1, 2 
   QUALIFY ROW_NUMBER() OVER (PARTITION BY employee_id ORDER BY last_work_email DESC) = 1
 
 ),
 
 dob AS (
 
-	SELECT 
+  SELECT 
     employee_id,
-		gitlab_username,
-		date_of_birth
-	FROM {{ref('workday_employee_mapping_source')}}
+	  gitlab_username,
+	  date_of_birth
+  FROM {{ref('workday_employee_mapping_source')}}
   QUALIFY ROW_NUMBER() OVER (PARTITION BY employee_id ORDER BY uploaded_at DESC) = 1
 
 )  
 
 SELECT 
-   bamboo_mapping.employee_number,
-	 bamboo_mapping.employee_id,
-	 bamboo_mapping.first_name,
-	 bamboo_mapping.last_name,
-	 bamboo_mapping.hire_date,
-	 bamboo_mapping.termination_date,
-	 bamboo_mapping.first_inactive_date,
-	 bamboo_mapping.age_cohort,
-	 bamboo_mapping.gender                                                       AS gender,
-	 COALESCE(gender_workday.gender_dropdown, bamboo_mapping.gender)             AS gender_identity,
-	 bamboo_mapping.ethnicity,
-	 bamboo_mapping.country,
-	 bamboo_mapping.nationality,
-	 bamboo_mapping.region,
-	 bamboo_mapping.region_modified,
-	 bamboo_mapping.gender_region,
-	 bamboo_mapping.greenhouse_candidate_id,
-	 bamboo_mapping.last_updated_date,
-	 bamboo_mapping.urg_group,
-	 email.last_work_email,
-	 dob.gitlab_username,
-	 dob.date_of_birth
+	bamboo_mapping.employee_number,
+	bamboo_mapping.employee_id,
+	bamboo_mapping.first_name,
+	bamboo_mapping.last_name,
+	bamboo_mapping.hire_date,
+	bamboo_mapping.termination_date,
+	bamboo_mapping.first_inactive_date,
+	bamboo_mapping.age_cohort,
+	bamboo_mapping.gender                                                       AS gender,
+	COALESCE(gender_workday.gender_dropdown, bamboo_mapping.gender)             AS gender_identity,
+	bamboo_mapping.ethnicity,
+	bamboo_mapping.country,
+	bamboo_mapping.nationality,
+	bamboo_mapping.region,
+	bamboo_mapping.region_modified,
+	bamboo_mapping.gender_region,
+	bamboo_mapping.greenhouse_candidate_id,
+	bamboo_mapping.last_updated_date,
+	bamboo_mapping.urg_group,
+	email.last_work_email,
+	dob.gitlab_username,
+	dob.date_of_birth
 FROM bamboo_mapping
 LEFT JOIN email 
   ON bamboo_mapping.employee_id = email.employee_id
