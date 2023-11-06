ec84e80bf432c503d503392bff56dc3b2e12836f
Kevin Dietz <kdietz@gitlab.com>
2023-11-06 15:01:05 -0600

    remove score_group limits

--- transform/snowflake-dbt/models/common_prep/prep_ptpf_scores_by_user.sql
+++ transform/snowflake-dbt/models/common_prep/prep_ptpf_scores_by_user.sql
@@ -1,27 +1,76 @@
 {{ simple_cte([
-    ('ptpf_scores', 'ptpf_scores_source'),
     ('prep_namespace', 'prep_namespace'),
     ('gitlab_dotcom_users_source', 'gitlab_dotcom_users_source')
     ])
 }}
 
+, ptpf_scores AS (
 
-, namespace_creator_ptpf_score AS (
+    SELECT
+      namespace_id,
+      score_date,
+      score,
+      decile,
+      score_group,
+      insights,
+      days_since_trial_start
+    FROM {{ ref('ptpf_scores_source') }}
+
+), score_dates AS (
+    
+    SELECT DISTINCT score_date
+    FROM ptpf_scores
+  
+), last_dates AS (
+  
+    SELECT
+      FIRST_VALUE(score_date) OVER(ORDER BY score_date DESC)  AS last_score_date,
+      NTH_VALUE(score_date, 2) OVER(ORDER BY score_date DESC) AS after_last_score_date
+    FROM score_dates
+    LIMIT 1
+
+), ptpf_scores_last AS (
+
+    SELECT *
+    FROM ptpf_scores
+    WHERE score_date IN (SELECT last_score_date FROM last_dates)
+  
+), ptpf_scores_last_2 AS (
+  
+    SELECT *
+    FROM ptpf_scores
+    WHERE score_date IN (SELECT after_last_score_date FROM last_dates)
+
+), namespace_creator_ptpf_score AS (
 
     SELECT
       COALESCE(users.notification_email, users.email) AS email_address,
-      ptpf_scores.namespace_id,
-      ptpf_scores.score,
-      ptpf_scores.insights,
-      ptpf_scores.days_since_trial_start,
-      ptpf_scores.score_group,
-      ptpf_scores.score_date::DATE                    AS score_date
+      ptpf_scores_last.namespace_id,
+      ptpf_scores_last.score,
+      ptpf_scores_last.insights,
+      ptpf_scores_last.days_since_trial_start,
+      ptpf_scores_last.score_group,
+      ptpf_scores_last.score_date::DATE                    AS score_date
     FROM prep_namespace
     INNER JOIN gitlab_dotcom_users_source users
       ON prep_namespace.creator_id = users.user_id
-    INNER JOIN ptpf_scores
-      ON prep_namespace.dim_namespace_id = ptpf_scores.namespace_id
-    QUALIFY ROW_NUMBER() OVER(PARTITION BY email_address, score_date ORDER BY score DESC) = 1
+    INNER JOIN ptpf_scores_last
+      ON prep_namespace.dim_namespace_id = ptpf_scores_last.namespace_id
+    QUALIFY ROW_NUMBER() OVER(PARTITION BY email_address ORDER BY score DESC) = 1
+
+), namespace_creator_ptpf_score_last_2 AS (
+
+    SELECT
+      COALESCE(users.notification_email, users.email) AS email_address,
+      ptpf_scores_last_2.insights,
+      ptpf_scores_last_2.score_group,
+      ptpf_scores_last_2.score_date
+    FROM prep_namespace
+    INNER JOIN gitlab_dotcom_users_source users
+      ON prep_namespace.creator_id = users.user_id
+    INNER JOIN ptpf_scores_last_2
+      ON prep_namespace.dim_namespace_id = ptpf_scores_last_2.namespace_id
+    QUALIFY ROW_NUMBER() OVER(PARTITION BY email_address ORDER BY score DESC) = 1
 
 )
 
@@ -31,6 +80,11 @@ SELECT
   namespace_creator_ptpf_score.score,
   namespace_creator_ptpf_score.insights,
   namespace_creator_ptpf_score.score_group,
-  namespace_creator_ptpf_score.score_date
+  namespace_creator_ptpf_score.score_date,
+  namespace_creator_ptpf_score.days_since_trial_start,
+  namespace_creator_ptpf_score_last_2.insights          AS past_insights,
+  namespace_creator_ptpf_score_last_2.score_group       AS past_score_group,
+  namespace_creator_ptpf_score_last_2.score_date::DATE  AS past_score_date
 FROM namespace_creator_ptpf_score
-
+LEFT JOIN namespace_creator_ptpf_score_last_2
+  ON namespace_creator_ptpf_score.email_address = namespace_creator_ptpf_score_last_2.email_address
