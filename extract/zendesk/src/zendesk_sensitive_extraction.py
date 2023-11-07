cc37092878db5f4c3dfa5708dbbfac11f920a45f
ved prakash <vprakash@gitlab.com>
2023-11-07 13:58:06 +0000

    update function name

--- extract/zendesk/src/zendesk_sensitive_extraction.py
+++ extract/zendesk/src/zendesk_sensitive_extraction.py
@@ -17,8 +17,8 @@ if __name__ == "__main__":
     getLogger("snowflake.connector.cursor").disabled = True
     fire.Fire(
         {
-            "refactor_ticket_audits": refactor_ticket_audits_read_gcp,
-            "refactor_tickets": refactor_tickets_read_gcp,
+            "refactor_ticket_audits": refactor_ticket_audits_read_gcs,
+            "refactor_tickets": refactor_tickets_read_gcs,
         }
     )
     info("Complete.")
