a103264a7bf741a230914868b4fc94da5a6b3856
Snehansh Chaturvedi <schaturvedi@gitlab.com>
2023-11-07 13:05:46 +0530

    correct typo

--- transform/snowflake-dbt/models/sources/zendesk/zendesk_tickets_dedupe_source.sql
+++ transform/snowflake-dbt/models/sources/zendesk/zendesk_tickets_dedupe_source.sql
@@ -8,7 +8,7 @@ SELECT *
 FROM {{ source('zendesk', 'tickets') }}
 {% if is_incremental() %}
 
-WHERE _uploaded_at >= (SELECT MAX(updated_at) FROM {{this}})
+WHERE updated_at >= (SELECT MAX(updated_at) FROM {{this}})
 
 {% endif %}
 QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1
