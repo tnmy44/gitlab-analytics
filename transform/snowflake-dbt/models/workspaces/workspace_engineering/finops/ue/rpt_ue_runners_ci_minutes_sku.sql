402e309558f6d21e8d2ba75c73960ef1f72976a3
Clement Leroux <cleroux@gitlab.com>
2023-11-21 13:29:55 +0100

    add all skus in ue ci minutes skus

--- transform/snowflake-dbt/models/workspaces/workspace_engineering/finops/ue/rpt_ue_runners_ci_minutes_sku.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_engineering/finops/ue/rpt_ue_runners_ci_minutes_sku.sql
@@ -8,7 +8,7 @@ SELECT date_day,
     gcp_project_id,
     gcp_service_description,
     gcp_sku_description,
-    'minutes' as pricing_unit,
+    pricing_unit,
     pl_category,
     level_3,
     CASE WHEN level_4 = 'linux xlarge gpu' then 'linux xlarge' else level_4 end as level_4,
@@ -16,8 +16,6 @@ SELECT date_day,
     sum(net_cost) as net_cost
 FROM cost_data
 where level_4 IN ('linux small ', 'linux medium', 'linux large', 'linux xlarge gpu', 'linux medium gpu', 'linux private internal')
-and (gcp_sku_description like 'N2D AMD Instance Core running in%'
-or gcp_sku_description like 'N1 Predefined Instance Core running%')
 and date_day >= '2023-02-01'
 group by 1,2,3,4,5,6,7,8
 order by 1 desc
