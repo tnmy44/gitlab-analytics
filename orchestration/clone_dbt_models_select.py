b3292c3599a08e7b248e50fff3ea525f59e56658
Clement Leroux <cleroux@gitlab.com>
2023-11-23 14:23:31 +0100

    black formatting

--- orchestration/clone_dbt_models_select.py
+++ orchestration/clone_dbt_models_select.py
@@ -162,7 +162,7 @@ class DbtModelClone:
         """
 
         sorted_list = self.clean_dbt_input(model_input)
-        
+
         # Total number of models to process
         total_models = len(sorted_list)
         current_model = 0  # Initializing the current model counter
@@ -188,7 +188,9 @@ class DbtModelClone:
             output_table_name = f""""{self.branch_name}_{full_name[1:]}"""
             output_schema_name = output_table_name.replace(f'."{table_name}"', "")
 
-            logger.info(f"Processing {output_table_name}: {current_model}/{total_models} (current/total)")
+            logger.info(
+                f"Processing {output_table_name}: {current_model}/{total_models} (current/total)"
+            )
 
             query = f"""
                 SELECT
