ea870e029ca26b3c79c2db71ec27b56e427d146a
Snehansh Chaturvedi <schaturvedi@gitlab.com>
2023-11-29 14:01:20 +0530

    update data type

--- transform/snowflake-dbt/models/sources/zendesk/zendesk_tickets_source.sql
+++ transform/snowflake-dbt/models/sources/zendesk/zendesk_tickets_source.sql
@@ -31,7 +31,7 @@ renamed AS (
       satisfaction_rating['score']::VARCHAR   AS satisfaction_rating_score,
       via['channel']::VARCHAR                 AS submission_channel,
       --IFF(custom_fields='[{}]','',custom_fields) AS ticket_custom_field_values,
-      --custom_fields::VARCHAR                  AS ticket_custom_field_values,
+      custom_fields::VARCHAR                  AS ticket_custom_field_values,
       --dates
       updated_at::DATE                        AS date_updated,
       created_at                              AS ticket_created_at
