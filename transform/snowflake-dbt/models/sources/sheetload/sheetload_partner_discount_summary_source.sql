b9d0a68dadefb05c8f134cfc88a5582dd8d8a8d9
Chris Sharp <csharp@gitlab.com>
2023-12-06 17:55:07 +0000

    add partner discount sheetload

---
+++ transform/snowflake-dbt/models/sources/sheetload/sheetload_partner_discount_summary_source.sql
@@ -0,0 +1,19 @@
+WITH source AS (
+
+    SELECT *
+    FROM {{ source('sheetload', 'partner_discount_summary') }}
+
+), renamed AS (
+
+    SELECT
+      FY AS fiscal_year_name_fy,
+      "Opportunity ID (18)"::VARCHAR    AS dim_crm_opportunity_id,
+      "Close Date"::DATE                AS close_date,
+      "Lessor of Discounts"::NUMBER     AS discount_percent,
+      "Partner Type"::VARCHAR           AS partner_type
+    FROM source
+
+)
+
+SELECT *
+FROM renamed
