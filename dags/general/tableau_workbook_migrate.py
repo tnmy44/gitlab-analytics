b71d3527b4891146f70e72e84359c908017ba2b3
Paul Armstrong <parmstrong@gitlab.com>
2023-12-07 21:14:36 +0200

    Corrections, update to latest test provision_plan.yaml

--- dags/general/tableau_workbook_migrate.py
+++ dags/general/tableau_workbook_migrate.py
@@ -56,7 +56,7 @@ dag = DAG(
 # tableau Extract
 tableau_workbook_migrate_cmd = f"""
     {clone_and_setup_extraction_cmd} &&
-    TableauConMan provision-users --yaml_path='./tableau_con_man_config/src/provision_plan.yaml'
+    TableauConMan provision-settings --yaml_path='./tableau_con_man_config/src/provision_plan.yaml'
 """
 
 # having both xcom flag flavors since we're in an airflow version where one is being deprecated
