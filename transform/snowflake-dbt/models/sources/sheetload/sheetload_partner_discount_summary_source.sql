92faa08f82a91a7870465436497a195172e75ca9
Chris Sharp <csharp@gitlab.com>
2023-12-07 16:17:31 +0000

    rename field

--- transform/snowflake-dbt/models/sources/sheetload/sheetload_partner_discount_summary_source.sql
+++ transform/snowflake-dbt/models/sources/sheetload/sheetload_partner_discount_summary_source.sql
@@ -7,7 +7,7 @@ WITH source AS (
 
     SELECT
       FY AS fiscal_year_name_fy,
-      "Opportunity ID (18)"::VARCHAR    AS dim_crm_opportunity_id,
+      "Opportunity ID"::VARCHAR         AS dim_crm_opportunity_id,
       "Close Date"::DATE                AS close_date,
       "Lessor of Discounts"::NUMBER     AS discount_percent,
       "Partner Type"::VARCHAR           AS partner_type
