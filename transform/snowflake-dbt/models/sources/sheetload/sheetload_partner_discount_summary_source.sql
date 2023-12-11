059c0b40181193137265a2b482b4571125e420b0
Chris Sharp <csharp@gitlab.com>
2023-12-11 13:51:55 +0000

    change date column to varchar

--- transform/snowflake-dbt/models/sources/sheetload/sheetload_partner_discount_summary_source.sql
+++ transform/snowflake-dbt/models/sources/sheetload/sheetload_partner_discount_summary_source.sql
@@ -8,7 +8,7 @@ WITH source AS (
     SELECT
       fy::VARCHAR                     AS fiscal_year_name_fy,
       opportunity_id::VARCHAR         AS dim_crm_opportunity_id,
-      close_date::DATE                AS close_date,
+      close_date::VARCHAR             AS close_date,
       lessor_of_discounts::NUMBER     AS discount_percent,
       partner_type::VARCHAR           AS partner_type
     FROM source
