f8d572ef6271ec32402d8e00e1d406118f9aecbe
ved prakash <vprakash@gitlab.com>
2023-12-20 11:14:14 +0100

    added truncate logging if exisit

--- extract/zuora_query_api/src/main.py
+++ extract/zuora_query_api/src/main.py
@@ -73,7 +73,7 @@ def main(file_path: str, load_only_table: str = None) -> None:
             logging.info(" Truncate the table to allow full load")
             truncate_table = query_executor(
                 zq.snowflake_engine,
-                "TRUNCATE TABLE IF EXISTS ZUORA_QUERY_API.chargecontractualvalue",
+                f"TRUNCATE TABLE IF EXISTS {config_dict.get('SNOWFLAKE_LOAD_DATABASE')}.ZUORA_QUERY_API.chargecontractualvalue",
             )
             logging.info(truncate_table)
             for start_end_date in date_interval_list:
