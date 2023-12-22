f30a385a0c123b601007fa99aebfc539071717c0
ved prakash <vprakash@gitlab.com>
2023-12-22 13:31:20 +0100

    sqlfluff formatting

--- transform/snowflake-dbt/models/sources/zuora_query_api/zuora_query_api_charge_contractual_value_source.sql
+++ transform/snowflake-dbt/models/sources/zuora_query_api/zuora_query_api_charge_contractual_value_source.sql
@@ -1,32 +1,31 @@
 WITH source AS (
 
-    SELECT *
-    FROM {{ source('zuora_query_api', 'chargecontractualvalue') }}
+  SELECT *
+  FROM {{ source('zuora_query_api', 'chargecontractualvalue') }}
 
-), renamed AS (
+),
 
-    SELECT
-        "ID"::TEXT                                                      AS charge_contractual_value_id      ,
-        "AMOUNT"::TEXT                                                  AS amount                           ,
-        "createdBy"::TEXT                                               AS created_by                       ,
-        TO_TIMESTAMP(CONVERT_TIMEZONE('UTC',"createdOn"))::TIMESTAMP    AS created_on                       ,
-        "CURRENCY"::TEXT                                                AS currency                         ,
-        "ELP"::NUMBER                                                   AS elp                              ,
-        "elpTaxAmount"::NUMBER                                          AS elp_tax_amount                   ,
-        "estimatedEvergreenEndDate"::TEXT                               AS estimated_ever_green_end_date    ,
-        "ratePlanChargeId"::TEXT                                        AS rate_plan_charge_id              ,
-        "REASON"::TEXT                                                  AS reason                           ,
-        "subscriptionId"::TEXT                                          AS subscription_id                  ,
-        "taxAmount"::TEXT                                               AS tax_amount                       ,
-        "updatedBy"::TEXT                                               AS updated_by                       ,
-        TO_TIMESTAMP(CONVERT_TIMEZONE('UTC',"updatedOn"))::TIMESTAMP    AS updated_on                       ,
-        TO_TIMESTAMP_NTZ(CAST("_UPLOADED_AT" AS INT))::TIMESTAMP        AS uploaded_at
-    FROM source
+renamed AS (
+
+  SELECT
+    "ID"::TEXT                                                    AS charge_contractual_value_id,
+    "AMOUNT"::TEXT                                                AS amount,
+    "createdBy"::TEXT                                             AS created_by,
+    TO_TIMESTAMP(CONVERT_TIMEZONE('UTC', "createdOn"))::TIMESTAMP AS created_on,
+    "CURRENCY"::TEXT                                              AS currency,
+    "ELP"::NUMBER                                                 AS elp,
+    "elpTaxAmount"::NUMBER                                        AS elp_tax_amount,
+    "estimatedEvergreenEndDate"::TEXT                             AS estimated_ever_green_end_date,
+    "ratePlanChargeId"::TEXT                                      AS rate_plan_charge_id,
+    "REASON"::TEXT                                                AS reason,
+    "subscriptionId"::TEXT                                        AS subscription_id,
+    "taxAmount"::TEXT                                             AS tax_amount,
+    "updatedBy"::TEXT                                             AS updated_by,
+    TO_TIMESTAMP(CONVERT_TIMEZONE('UTC', "updatedOn"))::TIMESTAMP AS updated_on,
+    TO_TIMESTAMP_NTZ("_UPLOADED_AT"::INT)::TIMESTAMP              AS uploaded_at
+  FROM source
 
 )
 
 SELECT *
 FROM renamed
-
-
-
