dc48ac75866ef3362758580ee7ff5314f01457bb
Lis Vinueza <lvinueza@gitlab.com>
2023-12-22 17:25:44 -0500

    Fix comments

--- transform/snowflake-dbt/models/workspaces/workspace_sales/restricted_safe/wk_fct_sales_funnel_target_daily_pivoted.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_sales/restricted_safe/wk_fct_sales_funnel_target_daily_pivoted.sql
@@ -1,7 +1,7 @@
 WITH final AS (
   
   SELECT  
-   target_date, 
+    target_date, 
     target_date_id,
     report_target_date,
     days_of_month,
@@ -14,7 +14,7 @@ WITH final AS (
     dim_order_type_id,
     dim_sales_qualified_source_id,
 
-    // CHURN % ATR
+    -- CHURN % ATR
     MAX(CASE WHEN kpi_name = 'Churn % ATR' THEN daily_allocated_target ELSE 0 END) AS "Churn % ATR Daily",
     MAX(CASE WHEN kpi_name = 'Churn % ATR' THEN monthly_allocated_target ELSE 0 END) AS "Churn % ATR Monthly",
     MAX(CASE WHEN kpi_name = 'Churn % ATR' THEN wtd_allocated_target ELSE 0 END) AS "Churn % ATR WTD",
@@ -22,7 +22,7 @@ WITH final AS (
     MAX(CASE WHEN kpi_name = 'Churn % ATR' THEN qtd_allocated_target ELSE 0 END) AS "Churn % ATR QTD",
     MAX(CASE WHEN kpi_name = 'Churn % ATR' THEN ytd_allocated_target ELSE 0 END) AS "Churn % ATR YTD",
 
-    // CHURN AMOUNT
+    -- CHURN AMOUNT
     MAX(CASE WHEN kpi_name = 'Churn Amount' THEN daily_allocated_target ELSE 0 END) AS "Churn Amount Daily",
     MAX(CASE WHEN kpi_name = 'Churn Amount' THEN monthly_allocated_target ELSE 0 END) AS "Churn Amount Monthly",
     MAX(CASE WHEN kpi_name = 'Churn Amount' THEN wtd_allocated_target ELSE 0 END) AS "Churn Amount WTD",
@@ -30,7 +30,7 @@ WITH final AS (
     MAX(CASE WHEN kpi_name = 'Churn Amount' THEN qtd_allocated_target ELSE 0 END) AS "Churn Amount QTD",
     MAX(CASE WHEN kpi_name = 'Churn Amount' THEN ytd_allocated_target ELSE 0 END) AS "Churn Amount YTD",
 
-    // CHURN/CONTRACTION AMOUNT
+    -- CHURN/CONTRACTION AMOUNT
     MAX(CASE WHEN kpi_name = 'Churn/Contraction Amount' THEN daily_allocated_target ELSE 0 END) AS "Churn/Contraction Amount Daily",
     MAX(CASE WHEN kpi_name = 'Churn/Contraction Amount' THEN monthly_allocated_target ELSE 0 END) AS "Churn/Contraction Amount Monthly",
     MAX(CASE WHEN kpi_name = 'Churn/Contraction Amount' THEN wtd_allocated_target ELSE 0 END) AS "Churn/Contraction Amount WTD",
@@ -38,7 +38,7 @@ WITH final AS (
     MAX(CASE WHEN kpi_name = 'Churn/Contraction Amount' THEN qtd_allocated_target ELSE 0 END) AS "Churn/Contraction Amount QTD",
     MAX(CASE WHEN kpi_name = 'Churn/Contraction Amount' THEN ytd_allocated_target ELSE 0 END) AS "Churn/Contraction Amount YTD",
 
-    // CONTRACTION % ATR
+    -- CONTRACTION % ATR
     MAX(CASE WHEN kpi_name = 'Contraction % ATR' THEN daily_allocated_target ELSE 0 END) AS "Contraction % ATR Daily",
     MAX(CASE WHEN kpi_name = 'Contraction % ATR' THEN monthly_allocated_target ELSE 0 END) AS "Contraction % ATR Monthly",
     MAX(CASE WHEN kpi_name = 'Contraction % ATR' THEN wtd_allocated_target ELSE 0 END) AS "Contraction % ATR WTD",
@@ -46,7 +46,7 @@ WITH final AS (
     MAX(CASE WHEN kpi_name = 'Contraction % ATR' THEN qtd_allocated_target ELSE 0 END) AS "Contraction % ATR QTD",
     MAX(CASE WHEN kpi_name = 'Contraction % ATR' THEN ytd_allocated_target ELSE 0 END) AS "Contraction % ATR YTD",
 
-    // CONTRACTION AMOUNT
+    -- CONTRACTION AMOUNT
     MAX(CASE WHEN kpi_name = 'Contraction Amount' THEN daily_allocated_target ELSE 0 END) AS "Contraction Amount Daily",
     MAX(CASE WHEN kpi_name = 'Contraction Amount' THEN monthly_allocated_target ELSE 0 END) AS "Contraction Amount Monthly",
     MAX(CASE WHEN kpi_name = 'Contraction Amount' THEN wtd_allocated_target ELSE 0 END) AS "Contraction Amount WTD",
@@ -54,7 +54,7 @@ WITH final AS (
     MAX(CASE WHEN kpi_name = 'Contraction Amount' THEN qtd_allocated_target ELSE 0 END) AS "Contraction Amount QTD",
     MAX(CASE WHEN kpi_name = 'Contraction Amount' THEN ytd_allocated_target ELSE 0 END) AS "Contraction Amount YTD",
 
-    // DEALS
+    -- DEALS
     MAX(CASE WHEN kpi_name = 'Deals' THEN daily_allocated_target ELSE 0 END) AS "Deals Daily",
     MAX(CASE WHEN kpi_name = 'Deals' THEN monthly_allocated_target ELSE 0 END) AS "Deals Monthly",
     MAX(CASE WHEN kpi_name = 'Deals' THEN wtd_allocated_target ELSE 0 END) AS "Deals WTD",
@@ -62,7 +62,7 @@ WITH final AS (
     MAX(CASE WHEN kpi_name = 'Deals' THEN qtd_allocated_target ELSE 0 END) AS "Deals QTD",
     MAX(CASE WHEN kpi_name = 'Deals' THEN ytd_allocated_target ELSE 0 END) AS "Deals YTD",
     
-    // EXPANSION % ATR 
+    -- EXPANSION % ATR 
     MAX(CASE WHEN kpi_name = 'Expansion % ATR' THEN daily_allocated_target ELSE 0 END) AS "Expansion % ATR Daily",
     MAX(CASE WHEN kpi_name = 'Expansion % ATR' THEN monthly_allocated_target ELSE 0 END) AS "Expansion % ATR Monthly",
     MAX(CASE WHEN kpi_name = 'Expansion % ATR' THEN wtd_allocated_target ELSE 0 END) AS "Expansion % ATR WTD",
@@ -70,7 +70,7 @@ WITH final AS (
     MAX(CASE WHEN kpi_name = 'Expansion % ATR' THEN qtd_allocated_target ELSE 0 END) AS "Expansion % ATR QTD",
     MAX(CASE WHEN kpi_name = 'Expansion % ATR' THEN ytd_allocated_target ELSE 0 END) AS "Expansion % ATR YTD",
 
-    // EXPANSION AMOUNT
+    -- EXPANSION AMOUNT
     MAX(CASE WHEN kpi_name = 'Expansion Amount' THEN daily_allocated_target ELSE 0 END) AS "Expansion Amount Daily",
     MAX(CASE WHEN kpi_name = 'Expansion Amount' THEN monthly_allocated_target ELSE 0 END) AS "Expansion Amount Monthly",
     MAX(CASE WHEN kpi_name = 'Expansion Amount' THEN wtd_allocated_target ELSE 0 END) AS "Expansion Amount WTD",
@@ -78,7 +78,7 @@ WITH final AS (
     MAX(CASE WHEN kpi_name = 'Expansion Amount' THEN qtd_allocated_target ELSE 0 END) AS "Expansion Amount QTD",
     MAX(CASE WHEN kpi_name = 'Expansion Amount' THEN ytd_allocated_target ELSE 0 END) AS "Expansion Amount YTD",
 
-    // HIGH LAM STAGE CLOSED WON OPPORTUNITIES
+    -- HIGH LAM STAGE CLOSED WON OPPORTUNITIES
     MAX(CASE WHEN kpi_name = 'High LAM Stage Closed Won Opportunities' THEN daily_allocated_target ELSE 0 END) AS "High LAM Stage Closed Won Opportunities Daily",
     MAX(CASE WHEN kpi_name = 'High LAM Stage Closed Won Opportunities' THEN monthly_allocated_target ELSE 0 END) AS "High LAM Stage Closed Won Opportunities Monthly",
     MAX(CASE WHEN kpi_name = 'High LAM Stage Closed Won Opportunities' THEN wtd_allocated_target ELSE 0 END) AS "High LAM Stage Closed Won Opportunities WTD",
@@ -86,7 +86,7 @@ WITH final AS (
     MAX(CASE WHEN kpi_name = 'High LAM Stage Closed Won Opportunities' THEN qtd_allocated_target ELSE 0 END) AS "High LAM Stage Closed Won Opportunities QTD",
     MAX(CASE WHEN kpi_name = 'High LAM Stage Closed Won Opportunities' THEN ytd_allocated_target ELSE 0 END) AS "High LAM Stage Closed Won Opportunities YTD",
 
-    // MQL
+    -- MQL
     MAX(CASE WHEN kpi_name = 'MQL' THEN daily_allocated_target ELSE 0 END) AS "MQL Daily",
     MAX(CASE WHEN kpi_name = 'MQL' THEN monthly_allocated_target ELSE 0 END) AS "MQL Monthly",
     MAX(CASE WHEN kpi_name = 'MQL' THEN wtd_allocated_target ELSE 0 END) AS "MQL WTD",
@@ -94,7 +94,7 @@ WITH final AS (
     MAX(CASE WHEN kpi_name = 'MQL' THEN qtd_allocated_target ELSE 0 END) AS "MQL QTD",
     MAX(CASE WHEN kpi_name = 'MQL' THEN ytd_allocated_target ELSE 0 END) AS "MQL YTD",
 
-    // NET ARR
+    -- NET ARR
     MAX(CASE WHEN kpi_name = 'Net ARR' THEN daily_allocated_target ELSE 0 END) AS "Net ARR Daily",
     MAX(CASE WHEN kpi_name = 'Net ARR' THEN monthly_allocated_target ELSE 0 END) AS "Net ARR Monthly",
     MAX(CASE WHEN kpi_name = 'Net ARR' THEN wtd_allocated_target ELSE 0 END) AS "Net ARR WTD",
@@ -102,7 +102,7 @@ WITH final AS (
     MAX(CASE WHEN kpi_name = 'Net ARR' THEN qtd_allocated_target ELSE 0 END) AS "Net ARR QTD",
     MAX(CASE WHEN kpi_name = 'Net ARR' THEN ytd_allocated_target ELSE 0 END) AS "Net ARR YTD",
 
-    // NET ARR COMPANY
+    --NET ARR COMPANY
     MAX(CASE WHEN kpi_name = 'Net ARR Company' THEN daily_allocated_target ELSE 0 END) AS "Net ARR Company Daily",
     MAX(CASE WHEN kpi_name = 'Net ARR Company' THEN monthly_allocated_target ELSE 0 END) AS "Net ARR Company Monthly",
     MAX(CASE WHEN kpi_name = 'Net ARR Company' THEN wtd_allocated_target ELSE 0 END) AS "Net ARR Company WTD",
@@ -110,7 +110,7 @@ WITH final AS (
     MAX(CASE WHEN kpi_name = 'Net ARR Company' THEN qtd_allocated_target ELSE 0 END) AS "Net ARR Company QTD",
     MAX(CASE WHEN kpi_name = 'Net ARR Company' THEN ytd_allocated_target ELSE 0 END) AS "Net ARR Company YTD",
 
-    // NET ARR PIPELINE CREATED
+    -- NET ARR PIPELINE CREATED
     MAX(CASE WHEN kpi_name = 'Net ARR Pipeline Created' THEN daily_allocated_target ELSE 0 END) AS "Net ARR Pipeline Created Daily",
     MAX(CASE WHEN kpi_name = 'Net ARR Pipeline Created' THEN monthly_allocated_target ELSE 0 END) AS "Net ARR Pipeline Created Monthly",
     MAX(CASE WHEN kpi_name = 'Net ARR Pipeline Created' THEN wtd_allocated_target ELSE 0 END) AS "Net ARR Pipeline Created WTD",
@@ -118,7 +118,7 @@ WITH final AS (
     MAX(CASE WHEN kpi_name = 'Net ARR Pipeline Created' THEN qtd_allocated_target ELSE 0 END) AS "Net ARR Pipeline Created QTD",
     MAX(CASE WHEN kpi_name = 'Net ARR Pipeline Created' THEN ytd_allocated_target ELSE 0 END) AS "Net ARR Pipeline Created YTD",
 
-    // NEW LOGOS
+    -- NEW LOGOS
     MAX(CASE WHEN kpi_name = 'New Logos' THEN daily_allocated_target ELSE 0 END) AS "New Logos Daily",
     MAX(CASE WHEN kpi_name = 'New Logos' THEN monthly_allocated_target ELSE 0 END) AS "New Logos Monthly",
     MAX(CASE WHEN kpi_name = 'New Logos' THEN wtd_allocated_target ELSE 0 END) AS "New Logos WTD",
@@ -126,7 +126,7 @@ WITH final AS (
     MAX(CASE WHEN kpi_name = 'New Logos' THEN qtd_allocated_target ELSE 0 END) AS "New Logos QTD",
     MAX(CASE WHEN kpi_name = 'New Logos' THEN ytd_allocated_target ELSE 0 END) AS "New Logos YTD",
      
-    // PRO SERVE AMOUNT
+    -- PRO SERVE AMOUNT
     MAX(CASE WHEN kpi_name = 'ProServe Amount' THEN daily_allocated_target ELSE 0 END) AS "ProServe Amount Daily",
     MAX(CASE WHEN kpi_name = 'ProServe Amount' THEN monthly_allocated_target ELSE 0 END) AS "ProServe Amount Monthly",
     MAX(CASE WHEN kpi_name = 'ProServe Amount' THEN wtd_allocated_target ELSE 0 END) AS "ProServe Amount",
@@ -134,7 +134,7 @@ WITH final AS (
     MAX(CASE WHEN kpi_name = 'ProServe Amount' THEN qtd_allocated_target ELSE 0 END) AS "ProServe Amount",
     MAX(CASE WHEN kpi_name = 'ProServe Amount' THEN ytd_allocated_target ELSE 0 END) AS "ProServe Amount",
 
-    // STAGE 1 OPPORTUNITIES
+    -- STAGE 1 OPPORTUNITIES
     MAX(CASE WHEN kpi_name = 'Stage 1 Opportunities' THEN daily_allocated_target ELSE 0 END) AS "Stage 1 Opportunities Daily",
     MAX(CASE WHEN kpi_name = 'Stage 1 Opportunities' THEN monthly_allocated_target ELSE 0 END) AS "Stage 1 Opportunities Monthly",
     MAX(CASE WHEN kpi_name = 'Stage 1 Opportunities' THEN wtd_allocated_target ELSE 0 END) AS "Stage 1 Opportunities WTD",
@@ -142,7 +142,7 @@ WITH final AS (
     MAX(CASE WHEN kpi_name = 'Stage 1 Opportunities' THEN qtd_allocated_target ELSE 0 END) AS "Stage 1 Opportunities QTD",
     MAX(CASE WHEN kpi_name = 'Stage 1 Opportunities' THEN ytd_allocated_target ELSE 0 END) AS "Stage 1 Opportunities YTD",
 
-    // TOTAL CLOSED
+    -- TOTAL CLOSED
     MAX(CASE WHEN kpi_name = 'Total Closed' THEN daily_allocated_target ELSE 0 END) AS "Total Closed Daily",
     MAX(CASE WHEN kpi_name = 'Total Closed' THEN monthly_allocated_target ELSE 0 END) AS "Total Closed Monthly",
     MAX(CASE WHEN kpi_name = 'Total Closed' THEN wtd_allocated_target ELSE 0 END) AS "Total Closed WTD",
@@ -150,7 +150,7 @@ WITH final AS (
     MAX(CASE WHEN kpi_name = 'Total Closed' THEN qtd_allocated_target ELSE 0 END) AS "Total Closed QTD",
     MAX(CASE WHEN kpi_name = 'Total Closed' THEN ytd_allocated_target ELSE 0 END) AS "Total Closed YTD",
  
-    // TRIALS
+    -- TRIALS
     MAX(CASE WHEN kpi_name = 'Trials' THEN daily_allocated_target ELSE 0 END) AS "Trials Daily",
     MAX(CASE WHEN kpi_name = 'Trials' THEN monthly_allocated_target ELSE 0 END) AS "Trials Monthly",
     MAX(CASE WHEN kpi_name = 'Trials' THEN wtd_allocated_target ELSE 0 END) AS "Trials WTD",
