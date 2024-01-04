ed842ef84abf46ef475064b1aaa9d87fd8f311c4
snalamaru <snalamaru@gitlab.com>
2024-01-03 18:06:26 -0600

    Added new object for SFDC

---
+++ transform/snowflake-dbt/models/sources/sfdc/sfdc_permission_set_assignment.sql
@@ -0,0 +1,35 @@
+WITH source AS (
+SELECT * FROM {{ source('salesforce', 'PermissionSetAssignment') }}
+
+
+
+
+), renamed AS (
+       SELECT
+      
+         --keys
+         id AS permission_set_assignment_id,
+         assigneeid AS assignee_id,
+
+
+         --info
+         isactive AS is_active,
+         permissionsetgroupid AS permission_set_group_id,
+         permissionsetid AS permission_set_id,
+
+
+         --metadata
+         systemmodstamp AS system_mod_stamp,
+         _sdc_batched_at AS sfdc_batched_at,
+         _sdc_extracted_at AS sfdc_extracted_at,
+         _sdc_received_at AS sfdc_received_at,
+         _sdc_sequence AS sfdc_sequence,
+         _sdc_table_version AS sfdc_table_version
+      
+       FROM source
+)
+
+
+SELECT *
+FROM renamed
+
