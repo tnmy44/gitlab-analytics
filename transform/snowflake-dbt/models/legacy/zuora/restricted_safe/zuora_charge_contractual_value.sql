0c11131089a62c5cb18721365f617d8e47431eab
ved prakash <vprakash@gitlab.com>
2024-01-09 16:54:49 +0100

    removed model from legacy

--- transform/snowflake-dbt/models/legacy/zuora/restricted_safe/zuora_charge_contractual_value.sql
+++
@@ -1,7 +0,0 @@
-WITH source AS (
-
-    SELECT *
-    FROM {{ ref('zuora_query_api_charge_contractual_value_source') }}
-)
-SELECT *
-FROM source
--- transform/snowflake-dbt/models/legacy/zuora/restricted_safe/zuora_charge_metrics_discount_allocation_detail.sql
+++
@@ -1,7 +0,0 @@
-WITH source AS (
-
-    SELECT *
-    FROM {{ ref('zuora_query_api_charge_metrics_discount_allocation_detail_source') }}
-)
-SELECT *
-FROM source
