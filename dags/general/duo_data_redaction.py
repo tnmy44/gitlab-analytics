c853e893964ad72af913d112328a53469c45f697
jjstark <jstark@gitlab.com>
2024-01-10 10:08:10 -0800

    add dag

---
+++ dags/general/duo_data_redaction.py
@@ -0,0 +1,89 @@
+import os
+from datetime import datetime, timedelta
+from yaml import safe_load, YAMLError
+
+from airflow import DAG
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow_utils import (
+    DATA_IMAGE,
+    clone_repo_cmd,
+    gitlab_defaults,
+    slack_failed_task,
+    REPO_BASE_PATH,
+)
+from kube_secrets import (
+    SNOWFLAKE_ACCOUNT,
+    SNOWFLAKE_LOAD_DATABASE,
+    SNOWFLAKE_LOAD_WAREHOUSE,
+    SNOWFLAKE_PASSWORD,
+    SNOWFLAKE_USER,
+)
+from kubernetes_helpers import get_affinity, get_toleration
+
+# Load the env vars into a dict and set Secrets
+env = os.environ.copy()
+GIT_BRANCH = env["GIT_BRANCH"]
+pod_env_vars = {
+    "CI_PROJECT_DIR": "/analytics",
+    "SNOWFLAKE_PROD_DATABASE": "PROD",
+}
+
+# Default arguments for the DAG
+default_args = {
+    "depends_on_past": False,
+    "on_failure_callback": slack_failed_task,
+    "owner": "airflow",
+    "retries": 0,
+    "retry_delay": timedelta(minutes=1),
+    "start_date": datetime(2021, 3, 14),
+    "dagrun_timeout": timedelta(hours=1),
+}
+
+# Create the DAG
+dag = DAG(
+    "duo_data_redaction",
+    default_args=default_args,
+    # schedule_interval="0 5 * * *",
+    catchup=False,
+)
+
+snowplow_tables = [
+    {
+        "name":"raw.snowplow.gitlab_events",
+        "key":"event_id",
+        "column": "contexts",
+        "action": "update json",
+    },
+]
+
+for table in snowplow_tables:
+    task_identifier = table["name"].replace(".", "-").replace("_", "-")
+
+    run_redaction_command = f"""
+      {clone_repo_cmd} &&
+      export PYTHONPATH="$CI_PROJECT_DIR/orchestration/:$PYTHONPATH" &&
+      python3 /analytics/orchestration/redact_duo_feedback.py \
+        --table={table["name"]} \
+        --key={table["key"]} \
+        --column={table["column"]} \
+        --action={table["action"]}
+        """
+
+    run_redaction = KubernetesPodOperator(
+        **gitlab_defaults,
+        image=DATA_IMAGE,
+        task_id=task_identifier,
+        name=task_identifier,
+        secrets=[
+            SNOWFLAKE_USER,
+            SNOWFLAKE_PASSWORD,
+            SNOWFLAKE_ACCOUNT,
+            SNOWFLAKE_LOAD_DATABASE,
+            SNOWFLAKE_LOAD_WAREHOUSE,
+        ],
+        env_vars=pod_env_vars,
+        affinity=get_affinity("extraction"),
+        tolerations=get_toleration("extraction"),
+        arguments=[run_redaction_command],
+        dag=dag,
+    )
---
+++ orchestration/redact_duo_feedback.py
@@ -0,0 +1,45 @@
+import json
+import logging
+import sys
+from os import environ as env
+
+from fire import Fire
+from gitlabdata.orchestration_utils import snowflake_engine_factory
+from sqlalchemy.engine import Engine
+from snowflake.sqlalchemy import URL
+from sqlalchemy import create_engine
+from sqlalchemy.exc import ProgrammingError
+
+def get_records_with_extended_feedback(engine: Engine) -> List[str]:
+    """
+    retrieves snowplow events with Duo extended feedback populated
+    """
+
+    query = """
+    SELECT event_id, contexts
+    FROM testing_db.test.snowplow_gitlab_events_clone
+    WHERE se_label ='response_feedback'
+--  AND contexts like '%"extendedFeedback":%'
+    """
+
+    try:
+        logging.info("Getting snowplow events with extended feedback")
+        connection = engine.connect()
+        duo_feedback_events = [row[0] for row in connection.execute(query).fetchall()]
+    except:
+        logging.info("Failed to get snowplow events")
+    finally:
+        connection.close()
+        engine.dispose()
+
+    return duo_feedback_events
+
+def redact_extended_feedback():    
+    config_dict = env.copy()
+    engine = snowflake_engine_factory(config_dict, "SYSADMIN")
+    get_records_with_extended_feedback(engine)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=20)
+    Fireredact_extended_feedback)
+    logging.info(duo_feedback_events)
