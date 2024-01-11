a0b35908b4f1f647a723f7fcdd0773f69841b6bf
michellecooper <mcooper@gitlab.com>
2024-01-11 08:22:02 -0800

    missing comma

--- transform/snowflake-dbt/models/common_prep/month_partition/snowplow/snowplow_gitlab_events_ide_extension_version_context.sql
+++ transform/snowflake-dbt/models/common_prep/month_partition/snowplow/snowplow_gitlab_events_ide_extension_version_context.sql
@@ -34,30 +34,16 @@ WITH filtered_source as (
     FROM filtered_source
 
 ), events_with_context_flattened AS (
-    /*
-    we need to extract the GitLab standard context fields from the contexts JSON provided in the raw events
-    A contexts json look like a list of context attached to an event:
 
-    The GitLab standard context which we are looking for is defined by schema at:
-    https://gitlab.com/gitlab-org/iglu/-/blob/master/public/schemas/com.gitlab/gitlab_standard/jsonschema/1-0-5
-
-    To in this CTE for any event, we use LATERAL FLATTEN to create one row per context per event.
-    We then extract the context schema and the context data
-    */
     SELECT 
       base.*,
       f.value['schema']::VARCHAR  AS context_data_schema,
       f.value['data']             AS context_data
+    FROM base,
     lateral flatten(input => TRY_PARSE_JSON(contexts), path => 'data') f
-    WHERE ide_extension_version_context_data_schema LIKE 'iglu:com.gitlab/ide_extension_version/jsonschema/%'
 
 )
 
-/*
-in this CTE we take the results from the previous CTE and isolate the only context we are interested in:
-the gitlab standard context, which has this context schema: iglu:com.gitlab/gitlab_standard/jsonschema/1-0-5
-Then we extract the id from the context_data column
-*/
 SELECT
     events_with_context_flattened.event_id::VARCHAR        AS event_id,
     events_with_context_flattened.derived_tstamp,
@@ -69,4 +55,4 @@ SELECT
     context_data['ide_version']::VARCHAR                   AS ide_version,
     context_data['language_server_version']::NUMBER        AS language_server_version
 FROM events_with_context_flattened
-WHERE context_data_schema like 'iglu:com.gitlab/gitlab_standard/jsonschema/%'
+HERE ide_extension_version_context_data_schema LIKE 'iglu:com.gitlab/ide_extension_version/jsonschema/%'
