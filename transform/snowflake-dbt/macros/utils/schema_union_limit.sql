60c4e85238a4bce8d97aae1ee47da0d7c7539f16
michellecooper <mcooper@gitlab.com>
2024-01-16 15:24:49 -0800

    add ability to run prep_snowplow_unnested_events_all locally for new columns

--- transform/snowflake-dbt/macros/utils/schema_union_limit.sql
+++ transform/snowflake-dbt/macros/utils/schema_union_limit.sql
@@ -1,8 +1,17 @@
-{% macro schema_union_limit(schema_part, table_name, column_name, day_limit=30, database_name=none, boolean_filter_statement=none) %}
+{% macro schema_union_limit(schema_part, table_name, column_name, day_limit=30, database_name=none, boolean_filter_statement=none, local=none) %}
 
 WITH base_union AS (
 
-  {{ schema_union_all(schema_part, table_name, database_name=database_name, day_limit=day_limit, boolean_filter_statement=boolean_filter_statement) }}
+{%- if local != 'no' -%}
+
+  {{ schema_union_all(schema_part, table_name, database_name=database_name, day_limit=day_limit, boolean_filter_statement=boolean_filter_statement, local='yes') }}
+
+{%- else -%}
+
+  {{ schema_union_all(schema_part, table_name, database_name=database_name, day_limit=day_limit, boolean_filter_statement=boolean_filter_statement, local=none) }}
+
+{%- endif -%}
+
 
 ) 
 
