9bb3ad964e293afdadfd05d67bdda7089d7f9f1e
jjstark <jstark@gitlab.com>
2024-01-17 08:20:36 -0800

    add vars

--- orchestration/redact_duo_feedback.py
+++ orchestration/redact_duo_feedback.py
@@ -12,7 +12,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.exc import ProgrammingError
 
 
-def get_records_with_extended_feedback(table, key, column, tstamp_column, engine: Engine) -> List[str]:
+def get_records_with_extended_feedback(table, key, column, tstamp_column):
     """
     retrieves snowplow events with Duo extended feedback populated
     """
@@ -26,6 +26,8 @@ def get_records_with_extended_feedback(table, key, column, tstamp_column, engine
     """
 
     try:
+        config_dict = env.copy()
+        engine = snowflake_engine_factory(config_dict, "SYSADMIN")
         logging.info("Getting snowplow events with extended feedback")
         connection = engine.connect()
         duo_feedback_events = connection.execute(query).fetchall()
@@ -48,13 +50,12 @@ def get_records_with_extended_feedback(table, key, column, tstamp_column, engine
     return duo_feedback_events
 
 
-def redact_extended_feedback():
-    config_dict = env.copy()
-    engine = snowflake_engine_factory(config_dict, "SYSADMIN")
-    records = get_records_with_extended_feedback(table, key, column, tstamp_column, engine)
+def redact_extended_feedback(table, key, column, tstamp_column):
+    
+    records = get_records_with_extended_feedback(table, key, column, tstamp_column)
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=20)
-    Fire(redact_extended_feedback())
+    Fire(redact_extended_feedback)
     logging.info("completed")
