79c2505356e5e7a07dab90ef9b54723e04481b3f
Justin Wong <juwong@gitlab.com>
2024-01-19 16:23:04 -0500

    add pgp_is_deleted field
    sqlfluff
    
    fix field name
    
    remove cast
    
    sqlfluff

--- transform/snowflake-dbt/models/sources/gitlab_dotcom/renamed/gitlab_dotcom_ci_job_artifacts_source.sql
+++ transform/snowflake-dbt/models/sources/gitlab_dotcom/renamed/gitlab_dotcom_ci_job_artifacts_source.sql
@@ -3,12 +3,14 @@ WITH source AS (
   SELECT *
   FROM {{ ref('gitlab_dotcom_ci_job_artifacts_dedupe_source') }}
 
-), renamed AS (
+),
+
+renamed AS (
 
   SELECT
-    id::NUMBER           AS ci_job_artifact_id,
-    project_id::NUMBER   AS project_id,
-    job_id::NUMBER       AS ci_job_id,
+    id::NUMBER            AS ci_job_artifact_id,
+    project_id::NUMBER    AS project_id,
+    job_id::NUMBER        AS ci_job_id,
     file_type             AS file_type,
     size                  AS size,
     created_at::TIMESTAMP AS created_at,
@@ -18,7 +20,8 @@ WITH source AS (
     file_store            AS file_store,
     file_format           AS file_format,
     file_location         AS file_location,
-    locked                AS locked
+    locked                AS locked,
+    pgp_is_deleted        AS is_pgp_deleted
   FROM source
 
 )
