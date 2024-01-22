07823435da7610cf41fed261ebbab7dabaea08f9
Sushma  N <snalamaru@gitlab.com>
2024-01-22 22:25:24 +0000

    Updated code

--- transform/snowflake-dbt/models/workspaces/workspace_sales/restricted_safe/wk_rpt_crm_opportunity_reduced.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_sales/restricted_safe/wk_rpt_crm_opportunity_reduced.sql
@@ -1,15 +1,25 @@
-select 
-    dim_crm_opportunity_id,
-    opportunity_name,
-    close_date,
-    cp_review_notes,
-    qsr_notes,
-    renewal_forecast_health,
-    renewal_manager,
-    risk_reasons,
-    tam_notes,
-    arr,
-    forecasted_churn_for_clari,
-    net_arr
-from 
-{{ref('mart_crm_opportunity')}} 
+{{ config(materialized='view') }}
+
+WITH final AS (
+
+    SELECT 
+      dim_crm_opportunity_id,
+      opportunity_name,
+      close_date,
+      cp_review_notes,
+      qsr_notes,
+      renewal_forecast_health,
+      renewal_manager,
+      risk_reasons,
+      tam_notes,
+      arr,
+      forecasted_churn_for_clari,
+      net_arr
+
+    FROM {{ref('mart_crm_opportunity')}} 
+
+)
+
+SELECT * 
+FROM final
+
