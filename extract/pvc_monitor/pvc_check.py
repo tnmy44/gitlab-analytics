e4ae19e4d077f7b0f53b5eeedfbf2924684454e0
Paul Armstrong <parmstrong@gitlab.com>
2024-01-24 15:14:08 +0200

    Correction.

--- extract/pvc_monitor/pvc_check.py
+++ extract/pvc_monitor/pvc_check.py
@@ -27,6 +27,8 @@ def list_buckets():
 
         # Get additional details
         bucket = storage_client.get_bucket(bucket.name)
+        bucket.reload()
+        print(dir(bucket))
         print(f"Current Space Used: {bucket.usage / (1024**3):.2f} GB")  # Convert bytes to GB
         print(f"Capacity: {bucket.quota / (1024**3):.2f} GB")  # Convert bytes to GB
 
