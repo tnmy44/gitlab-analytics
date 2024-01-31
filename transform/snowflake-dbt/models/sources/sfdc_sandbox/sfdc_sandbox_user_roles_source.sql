7cd5dbe42e66e18517a9bdc4a8a3a50242be3739
Chris Sharp <csharp@gitlab.com>
2024-01-31 16:44:06 +0000

    rename fields

--- transform/snowflake-dbt/models/sources/sfdc_sandbox/sfdc_sandbox_user_roles_source.sql
+++ transform/snowflake-dbt/models/sources/sfdc_sandbox/sfdc_sandbox_user_roles_source.sql
@@ -3,13 +3,29 @@ WITH base AS (
     SELECT * 
     FROM {{ source('salesforce_sandbox', 'user_role') }}
 
+), renamed AS (
+
+    SELECT
+      id                    AS user_role_id,
+      name                  AS user_role_name,
+
+      --metadata
+      lastmodifiedbyid      AS last_modified_id,
+      lastmodifieddate      AS last_modified_date,
+      systemmodstamp
+
+    FROM base
 )
 
 SELECT *
-FROM base
-
+FROM renamed
 
 UNION ALL
 
-SELECT * 
+SELECT 
+  id,
+  name,
+  lastmodifiedbyid,
+  lastmodifieddate,
+  systemmodstamp
 FROM {{ ref ('fy25_sfdc_sandbox_user_roles_hierarchy') }}
