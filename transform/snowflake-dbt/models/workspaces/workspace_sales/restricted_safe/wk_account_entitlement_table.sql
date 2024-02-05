5080abb6ebdf60c1ed020000eb4538dcbbe44d2f
Jong Lee <jonglee@gitlab.com>
2024-02-04 22:58:42 -0500

    change reference tables

--- transform/snowflake-dbt/models/workspaces/workspace_sales/restricted_safe/wk_account_entitlement_table.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_sales/restricted_safe/wk_account_entitlement_table.sql
@@ -3,50 +3,50 @@
 With ExpandedGroupMembers AS (
     -- Recursive CTE to expand group memberships
     SELECT 
-        gm.USERORGROUPID,
-        gm.GROUPID
+        gm.user_or_group_id,
+        gm.group_id
     FROM 
         {{ ref('sfdc_group_member_source') }} as gm
     WHERE 
-        NOT EXISTS (SELECT 1 FROM {{ ref('sfdc_group_source') }} WHERE id = gm.USERORGROUPID)
+        NOT EXISTS (SELECT 1 FROM {{ ref('sfdc_group_source') }} WHERE id = gm.user_or_group_id)
     UNION ALL
     SELECT 
-        gm.USERORGROUPID,
-        egm.GROUPID
+        gm.user_or_group_id,
+        egm.group_id
     FROM 
         {{ ref('sfdc_group_member_source') }} as gm
     JOIN 
-        ExpandedGroupMembers egm ON gm.GROUPID = egm.USERORGROUPID
+        ExpandedGroupMembers egm ON gm.group_id = egm.user_or_group_id
     WHERE 
-        EXISTS (SELECT 1 FROM {{ ref('sfdc_group_source') }} WHERE ID = gm.USERORGROUPID)
+        EXISTS (SELECT 1 FROM {{ ref('sfdc_group_source') }} WHERE ID = gm.user_or_group_id)
 ),
 UserRolesHierarchiesTerritoriesProfiles AS (
     -- CTE for user roles, hierarchies, territories, and profiles
     SELECT 
-        u.Id AS UserId,
-        u.ProfileId,
-        u.UserRoleId,
-        r.Name AS RoleName,
-        u.ManagerId,
+        u.Id AS User_Id,
+        u.Profile_Id,
+        u.User_Role_Id,
+        r.Name AS Role_Name,
+        u.Manager_Id,
         ut.Territory2Id,
-        p.PERMISSIONSVIEWALLDATA AS CanViewAllData,
-        p.PERMISSIONSMODIFYALLDATA AS CanModifyAllData
+        p.is_permissions_view_all_data AS CanViewAllData,
+        p.is_permissions_modify_all_data AS CanModifyAllData
     FROM 
         {{ ref('sfdc_users_source') }} as u
     LEFT JOIN 
-        {{ ref('sfdc_user_roles_source') }} r ON u.UserRoleId = r.Id
+        {{ ref('sfdc_user_roles_source') }} r ON u.User_Role_Id = r.Id
     LEFT JOIN 
-         {{ ref('sfdc_profile_source') }} p ON u.ProfileId = p.ID
+         {{ ref('sfdc_profile_source') }} p ON u.Profile_Id = p.ID
     LEFT JOIN 
         raw.salesforce_v2_stitch.USERTERRITORY2ASSOCIATION ut ON u.Id = ut.UserId
     WHERE 
-        u.IsActive = true
+        u.Is_Active = true
 )
 SELECT 
     u.Id, 
-    u.Username,
-    u.ProfileId,
-    a.AccountId,
+    u.User_name,
+    u.Profile_Id,
+    a.Account_Id,
     -- Apply profile-based permissions to determine the effective access level
     CASE
         WHEN urht.CanModifyAllData = true THEN '1'
@@ -55,9 +55,9 @@ SELECT
     END AS EffectiveAccountAccessLevel,
     urht.Territory2Id
 FROM 
-    raw.salesforce_v2_stitch.USER u
+    {{ ref('sfdc_users_source') }} as u
 JOIN 
-    accountshare a ON u.Id = a.UserOrGroupId OR a.UserOrGroupId IN (SELECT USERORGROUPID FROM ExpandedGroupMembers)
+    {{ ref('wk_prep_crm_account_share_active') }} a ON u.Id = a.User_Or_Group_Id OR a.User_Or_Group_Id IN (SELECT USERORGROUPID FROM ExpandedGroupMembers)
 LEFT JOIN 
-    UserRolesHierarchiesTerritoriesProfiles urht ON u.Id = urht.UserId
-QUALIFY ROW_NUMBER() OVER (PARTITION BY u.Username, a.AccountId ORDER BY a.AccountAccessLevel DESC) = 1;
+    UserRolesHierarchiesTerritoriesProfiles urht ON u.Id = urht.User_Id
+QUALIFY ROW_NUMBER() OVER (PARTITION BY u.User_name, a.Account_Id ORDER BY a.Account_Access_Level DESC) = 1;
