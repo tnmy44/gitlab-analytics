2229cbf032ab7fadbe6c1fa7ac2ac94c23f1fda0
Clement Leroux <cleroux@gitlab.com>
2024-02-05 14:48:30 +0100

    add workspace model

--- transform/snowflake-dbt/models/workspaces/workspace_engineering/finops/elastic/rpt_elastic_billing_itemized.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_engineering/finops/elastic/rpt_elastic_billing_itemized.sql
@@ -1,8 +1,38 @@
-WITH source_itemized as (
+WITH data_transfer_source as (
 
-  SELECT *
-  FROM {{ ref('elastic_billing_source')}}
+SELECT * FROM {{ ref ('elastic_billing_data_transfer_source')}}
 
+),
+
+resource_source as (
+
+SELECT * FROM {{ ref ('elastic_billing_resources_source')}}
+
+),
+
+renamed as (
+
+SELECT 
+    extraction_start_date,
+    extraction_end_date,
+    'data_transfer' as charge_type,
+    sku,
+    cost,
+    resource_quantity_value as usage,
+    formated_quantity_value as usage_unit,
+    ARRAY_CONSTRUCT(type, resource_name) as details
+FROM data_transfer_source
+UNION ALL
+SELECT
+    extraction_start_date,
+    extraction_end_date,
+    'capacity' as charge_type,
+    sku,
+    cost,
+    hours as usage,
+    'hours' as usage_unit,
+    ARRAY_CONSTRUCT(resource_start_date, resource_end_date, instance_count, price_per_hour, name)
+ FROM resource_source
 )
 
-SELECT * FROM source_itemized
+SELECT * FROM renamed
