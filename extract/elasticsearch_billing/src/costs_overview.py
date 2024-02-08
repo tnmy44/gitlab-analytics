ef529b8536d9cafb691611ade657af48e90a26a3
Snehansh Chaturvedi <schaturvedi@gitlab.com>
2024-02-08 18:37:17 +0530

    debug

--- extract/elasticsearch_billing/src/costs_overview.py
+++ extract/elasticsearch_billing/src/costs_overview.py
@@ -78,25 +78,42 @@ def get_costs_overview_backfill():
         f"Extraction starting from date {extraction_start_date} till date {extraction_end_date}"
     )
     output_list = []
-    # iterate each month in between extraction_start_date and extraction_end_date and call API
-    for month in range(extraction_start_date.month, extraction_end_date.month + 1):
-        current_month = date(extraction_start_date.year, month, 1)
-        start_date = current_month
-        # if month == extraction_end_date.month:
-        #     end_date = extraction_end_date
-        # update end_date to ending date of next month
-        end_date = date(current_month.year, current_month.month + 1, 1) - timedelta(
-            days=1
-        )
-        info(f"{start_date} till {end_date}")
-        costs_endpoint_url = f"/billing/costs/{org_id}?from={start_date}&to={end_date}"
-        data = get_response(costs_endpoint_url)
-        row_list = [
-            data,
-            start_date,
-            end_date,
-        ]
-        output_list.append(row_list)
+    # iterate each month over the years in between extraction_start_date and extraction_end_date and call API for each month
+    
+    for year in range(extraction_start_date.year, extraction_end_date.year + 1):
+        for month in range(1, 13):
+            current_month = date(year, month, 1)
+            start_date = current_month
+            end_date = date(year, month + 1, 1) - timedelta(days=1)
+            if start_date >= extraction_start_date and end_date <= extraction_end_date:
+                info(f"{start_date} till {end_date}")
+                costs_endpoint_url = f"/billing/costs/{org_id}?from={start_date}&to={end_date}"
+                data = get_response(costs_endpoint_url)
+                row_list = [
+                    data,
+                    start_date,
+                    end_date,
+                ]
+                output_list.append(row_list)
+
+    # for month in range(extraction_start_date.month, extraction_end_date.month + 1):
+    #     current_month = date(extraction_start_date.year, month, 1)
+    #     start_date = current_month
+    #     # if month == extraction_end_date.month:
+    #     #     end_date = extraction_end_date
+    #     # update end_date to ending date of next month
+    #     end_date = date(current_month.year, current_month.month + 1, 1) - timedelta(
+    #         days=1
+    #     )
+    #     info(f"{start_date} till {end_date}")
+    #     costs_endpoint_url = f"/billing/costs/{org_id}?from={start_date}&to={end_date}"
+    #     data = get_response(costs_endpoint_url)
+    #     row_list = [
+    #         data,
+    #         start_date,
+    #         end_date,
+    #     ]
+    #     output_list.append(row_list)
     # upload this data to snowflake
     columns_list = [
         "payload",
