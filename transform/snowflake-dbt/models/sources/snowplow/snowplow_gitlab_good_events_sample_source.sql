1a5daeec79001c0a23c328d1a15aa2b9f08d45f5
Utkarsh <usinghal@gitlab.com>
2024-02-08 17:52:20 +0530

    DROP columns for Snowplow IP enrichment

--- transform/snowflake-dbt/models/sources/snowplow/snowplow_gitlab_good_events_sample_source.sql
+++ transform/snowflake-dbt/models/sources/snowplow/snowplow_gitlab_good_events_sample_source.sql
@@ -1,7 +1,8 @@
 WITH source as (
 
-    SELECT *
-    FROM {{ source('gitlab_snowplow', 'events_sample') }}
+  SELECT
+    {{ dbt_utils.star(from=source('gitlab_snowplow', 'events_sample'), except=['geo_zipcode', 'geo_latitude', 'geo_longitude', 'user_ipaddress']) }}
+  FROM {{ source('gitlab_snowplow', 'events_sample') }}
 
 )
 
