566905661425a9f0d36c584f7923aae39a1c8fc3
Justin Wong <juwong@gitlab.com>
2024-02-13 12:48:47 -0500

    manual revert of 40ab4d325c

--- extract/gitlab_saas_postgres_pipeline/postgres_pipeline/postgres_utils.py
+++ extract/gitlab_saas_postgres_pipeline/postgres_pipeline/postgres_utils.py
@@ -609,7 +609,6 @@ def chunk_and_upload_metadata(
     if rows_uploaded > 0:
         is_export_completed = max_last_extracted_id >= max_source_id
         # upload to Snowflake before writing metadata=complete for safety
-        """
         if is_export_completed:
             upload_to_snowflake_after_extraction(
                 chunk_df,
@@ -618,7 +617,6 @@ def chunk_and_upload_metadata(
                 initial_load_start_date,
                 advanced_metadata,
             )
-        """
         # only write metadata after all chunks have been written because chunks aren't ordered, can lead to false last_extracted_id
         write_metadata(
             database_kwargs["metadata_engine"],
