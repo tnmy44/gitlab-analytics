a5b7ddf8925d1c4d0a145dae6f958bba2cda0f35
michellecooper <mcooper@gitlab.com>
2024-02-15 11:24:37 -0800

    update exclude from timezone to zipcode

--- transform/snowflake-dbt/models/legacy/snowplow/combined/30_day/snowplow_page_views_30.sql
+++ transform/snowflake-dbt/models/legacy/snowplow/combined/30_day/snowplow_page_views_30.sql
@@ -5,4 +5,4 @@
 
 -- depends_on: {{ ref('snowplow_page_views') }}
 
-{{ schema_union_limit('snowplow_', 'snowplow_page_views', 'page_view_start', 30, database_name=env_var('SNOWFLAKE_PREP_DATABASE'), excluded_col = ['geo_timezone', 'geo_latitude', 'geo_longitude', 'ip_address']) }}
+{{ schema_union_limit('snowplow_', 'snowplow_page_views', 'page_view_start', 30, database_name=env_var('SNOWFLAKE_PREP_DATABASE'), excluded_col = ['geo_zipcode', 'geo_latitude', 'geo_longitude', 'ip_address']) }}
