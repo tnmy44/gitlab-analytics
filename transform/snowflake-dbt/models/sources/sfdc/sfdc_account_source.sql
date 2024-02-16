84978c3b4e692a9982bbe0a4aa45ea39dcb2533c
Jong Lee <jonglee@gitlab.com>
2024-02-16 10:54:40 -0500

    added executive_sponsor_id

--- transform/snowflake-dbt/models/sources/sfdc/sfdc_account_source.sql
+++ transform/snowflake-dbt/models/sources/sfdc/sfdc_account_source.sql
@@ -41,6 +41,8 @@ renamed AS (
     business_development_rep__c AS business_development_rep,
     dedicated_service_engineer__c AS dedicated_service_engineer,
     sdr_assigned__c AS sales_development_rep,
+    executive_sponsor__c AS executive_sponsor_id,
+
     -- solutions_architect__c                     AS solutions_architect,
     technical_account_manager_lu__c AS technical_account_manager_id,
 
