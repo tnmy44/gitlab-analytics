ab915bf2d91a98d4a2cd714d8d6cff6848caf5ab
Jong Lee <jonglee@gitlab.com>
2024-02-21 17:02:51 -0500

    fixed macros

--- transform/snowflake-dbt/models/common_mart_marketing/restricted_safe/rpt_l2r_campaign_interactions_paid_account.sql
+++ transform/snowflake-dbt/models/common_mart_marketing/restricted_safe/rpt_l2r_campaign_interactions_paid_account.sql
@@ -1,29 +1,39 @@
 {{ config(materialized='table') }}
 
 {{ simple_cte([
-    ('campaigns', 'rpt_l2r_campaign_interactions'),
-    ('account', 'mart_crm_account')
+    ('rpt_l2r_campaign_interactions', 'rpt_l2r_campaign_interactions'),
+    ('mart_crm_account', 'mart_crm_account')
 ]) }}
 
-, final AS (
+, campaigns AS (
 
-  select
-    campaigns.*,
-  from
-    campaigns 
-   inner join
-      account 
-      on campaigns.dim_crm_account_id = account.dim_crm_account_id 
-where
-   account.crm_account_type = 'Customer' 
-   and account.is_deleted = false
-   and touchpoint_type = 'Person Touchpoint'
+  SELECT
+      {{ dbt_utils.star(
+           from = ref('rpt_l2r_campaign_interactions'), 
+           except=['CREATED_BY','UPDATED_BY','MODEL_CREATED_DATE','MODEL_UPDATED_DATE','DBT_UPDATED_AT','DBT_CREATED_AT']
+           ) 
+      }}
+  FROM
+    {{ ref('rpt_l2r_campaign_interactions') }}
+
+   WHERE touchpoint_type = 'Person Touchpoint'
+
+), final AS (
+
+  SELECT
+   campaigns.* 
+  FROM campaigns 
+  INNER JOIN mart_crm_account 
+    ON mart_crm_account.dim_crm_account_id = campaigns.dim_crm_account_id
+   WHERE mart_crm_account.is_deleted = FALSE
+   AND mart_crm_account.crm_account_type = 'Customer'
+
 )
 
 {{ dbt_audit(
     cte_ref="final",
     created_by="@jonglee1218",
     updated_by="@jonglee1218",
-    created_date="2024-02-15",
-    updated_date="2024-02-15",
+    created_date="2024-02-21",
+    updated_date="2024-02-21",
   ) }}
