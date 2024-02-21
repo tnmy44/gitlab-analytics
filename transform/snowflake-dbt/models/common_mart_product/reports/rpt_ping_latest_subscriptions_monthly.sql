7b2a211f26483d76da8264fd32451a5c1153e238
Chris Sharp <csharp@gitlab.com>
2024-02-21 12:30:22 +0000

    improve condition readability

--- transform/snowflake-dbt/models/common_mart_product/reports/rpt_ping_latest_subscriptions_monthly.sql
+++ transform/snowflake-dbt/models/common_mart_product/reports/rpt_ping_latest_subscriptions_monthly.sql
@@ -116,7 +116,7 @@ Aggregate mart_charge information (used as the basis of truth), this gets rid of
         AND subscription_status IN ('Active','Cancelled')
         AND dim_product_detail.product_tier_name != 'Storage'
         -- filter added to fix https://gitlab.com/gitlab-data/analytics/-/issues/19656
-        AND (dim_product_detail.product_rate_plan_name != 'True-Up (Annual) - Dedicated - Ultimate'
+        AND NOT (dim_product_detail.product_rate_plan_name = 'True-Up (Annual) - Dedicated - Ultimate'
         AND arr = 0)
         AND DATE_TRUNC('MONTH', CURRENT_DATE) > arr_month
       {{ dbt_utils.group_by(n=4)}}
