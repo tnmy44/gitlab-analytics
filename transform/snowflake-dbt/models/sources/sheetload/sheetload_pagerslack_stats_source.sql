c84b1efdf91d01ed11c18f0c5e11bcc33911b466
Dani Deng <ddeng@gitlab.com>
2024-02-28 11:09:08 -0800

    fix timestamp data type

--- transform/snowflake-dbt/models/sources/sheetload/sheetload_pagerslack_stats_source.sql
+++ transform/snowflake-dbt/models/sources/sheetload/sheetload_pagerslack_stats_source.sql
@@ -6,11 +6,11 @@ WITH SOURCE AS (
 ),renamed AS ( 
 
     SELECT incident_url::VARCHAR AS incident_url,
-           TRY_TO_TIMESTAMP_NTZ("reported_at")::DATE AS reported_at,
+           reported_at::TIMESTAMP AS reported_at,
            reported_by::VARCHAR AS reported_by,
            response_type::VARCHAR AS response_type,
            response_type_copy::VARCHAR AS response_type_copy,
-           TRY_TO_TIMESTAMP_NTZ("time_at_response")::DATE AS time_at_response,
+           time_at_response::TIMESTAMP AS time_at_response,
            timezone::VARCHAR AS timezone,
            unavailable::BOOLEAN AS unavailable,
            weekend_pinged::BOOLEAN AS weekend_pinged,
