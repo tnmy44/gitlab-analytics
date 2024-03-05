43ebf325d2f5dee0f60fadf34c507d9d61d61312
Dani Deng <ddeng@gitlab.com>
2024-03-05 12:33:30 -0800

    update calculation fields

--- transform/snowflake-dbt/models/sources/sheetload/sheetload_pagerslack_stats_source.sql
+++ transform/snowflake-dbt/models/sources/sheetload/sheetload_pagerslack_stats_source.sql
@@ -1,26 +1,38 @@
-WITH SOURCE AS ( 
+WITH source AS (
 
-   SELECT *
-   FROM {{ source('sheetload','pagerslack_stats') }} 
+    SELECT *
+    FROM {{ ref('sheetload','pagerslack_stats') }}
 
-),renamed AS ( 
+),renamed AS (
 
-    SELECT incident_url::VARCHAR AS incident_url,
-           reported_at::TIMESTAMP AS reported_at,
-           reported_by::VARCHAR AS reported_by,
-           response_type::VARCHAR AS response_type,
-           response_type_copy::VARCHAR AS response_type_copy,
-           time_at_response::TIMESTAMP AS time_at_response,
-           timezone::VARCHAR AS timezone,
-           unavailable::BOOLEAN AS unavailable,
-           weekend_pinged::BOOLEAN AS weekend_pinged,
-           attempts::NUMBER AS attempts,
-           escalations::NUMBER AS escalations,
-           minutes_to_response::NUMBER AS minutes_to_response,
-           time_to_response::NUMBER AS time_to_response
-   FROM source 
+    SELECT
+        attempts::NUMBER                AS attempts,
+        weekend_pinged::BOOLEAN         AS weekend_pinged,
+        unavailable::BOOLEAN            AS unavailable,
+        incident_url::VARCHAR           AS incident_url,
+        reported_by::VARCHAR            AS reported_by,
+        reported_at::TIMESTAMP          AS reported_at,
+        time_to_response::NUMBER        AS time_to_response,
+        time_at_response::TIMESTAMP     AS time_at_response,
+        1                               AS escalations,
+        CASE
+            WHEN date_part('hour', time_at_response::TIMESTAMP) > 0
+                AND date_part('hour', time_at_response::TIMESTAMP) < 8
+            THEN 'APAC'
+            WHEN date_part('hour', time_at_response::TIMESTAMP) >= 8
+                AND date_part('hour', time_at_response::TIMESTAMP) <= 16
+            THEN 'EMEA'
+            ELSE 'AMER'
+        END                             AS timezone,
+        time_to_response::NUMBER/60000  AS minutes_to_response,
+        iff(unavailable::BOOLEAN=True, 'Response', 'No response')
+                                        AS response_type_copy,
+        iff(unavailable::BOOLEAN=True, 'Escalated', 'Bot')
+                                        AS response_type
+    FROM
+        source
 
-   )
+)
 
 SELECT *
 FROM renamed
