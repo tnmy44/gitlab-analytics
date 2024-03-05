03d4fe0342bbf0e2a00215824d8a588be02e36c5
Chris Sharp <csharp@gitlab.com>
2024-03-05 15:44:32 +0000

    formatting

--- transform/snowflake-dbt/models/sources/sfdc/sfdc_bizible_person_source.sql
+++ transform/snowflake-dbt/models/sources/sfdc/sfdc_bizible_person_source.sql
@@ -15,9 +15,9 @@ WITH source AS (
       contact.bizible2__lead__c            AS contact_lead_id,
       base.bizible2__contact__c            AS bizible_contact_id,
       base.isdeleted::BOOLEAN              AS is_deleted
-    FROM RAW.SALESFORCE_V2_STITCH.BIZIBLE2__BIZIBLE_PERSON__C base
-    LEFT JOIN RAW.SALESFORCE_V2_STITCH.BIZIBLE2__BIZIBLE_PERSON__C contact
-        ON base.bizible2__contact__c=contact.bizible2__contact__c
+    FROM source AS base
+    LEFT JOIN source AS contact
+        ON base.bizible2__contact__c = contact.bizible2__contact__c
     WHERE is_deleted = 'FALSE' 
     QUALIFY (ROW_NUMBER() OVER(PARTITION BY bizible_contact_id ORDER BY base.lastmodifieddate DESC, base.createddate DESC) = 1
         OR ROW_NUMBER() OVER(PARTITION BY bizible_lead_id ORDER BY base.lastmodifieddate DESC, base.createddate DESC) = 1 )
