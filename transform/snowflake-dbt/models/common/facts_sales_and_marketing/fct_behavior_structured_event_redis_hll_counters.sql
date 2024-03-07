7c9446d4b54b4de638d1c2f68123167b8c24a1b8
Utkarsh <usinghal@gitlab.com>
2024-03-07 18:44:11 +0530

    revert fct_behavior_structured_event_redis_hll_counters updation

--- transform/snowflake-dbt/models/common/facts_sales_and_marketing/fct_behavior_structured_event_redis_hll_counters.sql
+++ transform/snowflake-dbt/models/common/facts_sales_and_marketing/fct_behavior_structured_event_redis_hll_counters.sql
@@ -36,7 +36,6 @@
       fct_behavior_structured_event.session_id,
       fct_behavior_structured_event.user_snowplow_domain_id,
       fct_behavior_structured_event.contexts,
-      dim_behavior_event.event_action,
 
       -- Degenerate Dimensions (Gitlab Standard Context Attributes)
       fct_behavior_structured_event.gsc_google_analytics_client_id,
