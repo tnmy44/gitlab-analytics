ff60e0192da86cfc8e666f16c8ee834363e46765
Miles Russell <mdrussell@gitlab.com>
2024-03-07 11:34:34 -0600

    updating prep model

---
+++ transform/snowflake-dbt/models/common_prep/prep_latest_seat_link_installation.sql
@@ -0,0 +1,32 @@
+{{ config(
+    tags=["mnpi_exception"]
+) }}
+
+WITH seat_link AS (
+
+    SELECT
+      zuora_subscription_id AS dim_subscription_id,
+      zuora_subscription_name AS subscription_name,
+      hostname AS host_name,
+      uuid AS dim_instance_id,
+      order_id,
+      report_timestamp,
+      report_date,
+      license_starts_on,
+      created_at,
+      updated_at,
+      active_user_count,
+      license_user_count,
+      max_historical_user_count
+    FROM {{ ref('customers_db_license_seat_links_source') }}
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY host_name, dim_instance_id ORDER BY report_date DESC, updated_at DESC) = 1
+
+)
+
+{{ dbt_audit(
+    cte_ref="seat_link",
+    created_by="@mdrussell",
+    updated_by="@mdrussell",
+    created_date="2024-03-06",
+    updated_date="2024-03-07"
+) }}
