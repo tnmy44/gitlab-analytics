91b6fc7a7bc972dab31cea81a1df4381d37f57d2
Snehansh Chaturvedi <schaturvedi@gitlab.com>
2024-03-19 17:20:47 +0530

    modify test

--- orchestration/tableau_dependency_query/test/test_tableau_query.py
+++ orchestration/tableau_dependency_query/test/test_tableau_query.py
@@ -40,12 +40,6 @@ def test_query_table():
     full_table_path = "raw:tap_zendesk.tickets"
     expected_mcon = "MCON++07a010bd-4365-442a-9bec-c06ac57c28dc++557c5de5-160a-4026-99fb-9ec2985c510b++table++raw:tap_zendesk.tickets"
 
-    # Mock get_response to return fake response
-    def mock_get_response():
-        return {"data": {"getTable": {"tableId": "my_table", "mcon": expected_mcon}}}
-
-    # Patch get_response with mock
-    with patch("get_response", side_effect=mock_get_response):
-        actual_mcon = query_table(full_table_path)
+    actual_mcon = query_table(full_table_path)
 
     assert actual_mcon == expected_mcon
