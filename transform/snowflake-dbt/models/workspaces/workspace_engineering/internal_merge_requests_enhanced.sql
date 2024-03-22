f4f49595f1f8949fd62f7291f0c8c90cab0614b0
Dani Deng <ddeng@gitlab.com>
2024-03-22 09:54:45 +0100

    remove reviewer array

--- transform/snowflake-dbt/models/workspaces/workspace_engineering/internal_merge_requests_enhanced.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_engineering/internal_merge_requests_enhanced.sql
@@ -113,16 +113,6 @@ first_commit AS (
 
 ),
 
-reviews_by_users AS (
-
-  SELECT
-    merge_request_id,
-    ARRAY_AGG(DISTINCT author_id) AS review_user_id
-  FROM {{ ref('wk_gitlab_dotcom_reviews') }}
-  GROUP BY 1
-
-),
-
 base AS (
 
   SELECT
@@ -192,8 +182,7 @@ base AS (
     IFF(ARRAY_CONTAINS('infradev'::VARIANT, internal_merge_requests.labels), TRUE, FALSE)                                                                                                                                                                                                                               AS is_infradev,
     ARRAY_CONTAINS('customer'::VARIANT, internal_merge_requests.labels)                                                                                                                                                                                                                                                 AS is_customer_related,
     internal_merge_requests.is_part_of_product,
-    ROUND(TIMESTAMPDIFF(HOURS, first_commit.first_commit_created_at, internal_merge_requests.merged_at) / 24, 2)                                                                                                                                                                                                        AS days_from_first_commit_to_merge,
-    review_user_id
+    ROUND(TIMESTAMPDIFF(HOURS, first_commit.first_commit_created_at, internal_merge_requests.merged_at) / 24, 2)                                                                                                                                                                                                        AS days_from_first_commit_to_merge
   FROM internal_merge_requests
   LEFT JOIN {{ ref('dim_project') }} AS projects
     ON internal_merge_requests.target_project_id = projects.dim_project_id
@@ -204,7 +193,6 @@ base AS (
   LEFT JOIN milestones
     ON internal_merge_requests.milestone_id = milestones.milestone_id
   LEFT JOIN first_commit ON internal_merge_requests.merge_request_id = first_commit.merge_request_id
-  LEFT JOIN reviews_by_users ON internal_merge_requests.merge_request_id = reviews_by_users.merge_request_id
 
 )
 
