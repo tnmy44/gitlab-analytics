aca7ade1da7152ac0f4975b4432dfd6658e67f7e
Justin Wong <juwong@gitlab.com>
2024-03-26 11:20:07 -0600

    add explicit types

--- transform/snowflake-dbt/models/sources/gitlab_dotcom/renamed/scd/gitlab_dotcom_catalog_resource_versions_source.sql
+++ transform/snowflake-dbt/models/sources/gitlab_dotcom/renamed/scd/gitlab_dotcom_catalog_resource_versions_source.sql
@@ -9,19 +9,16 @@ WITH source AS (
 renamed AS (
 
   SELECT
-
-
-
-    id,
-    release_id,
-    catalog_resource_id,
-    project_id,
-    created_at,
-    released_at,
-    semver_major,
-    semver_minor,
-    semver_patch,
-    semver_prerelease
+    id::INT                    AS id,
+    release_id::INT            AS release_id,
+    catalog_resource_id::INT   AS catalog_resource_id,
+    project_id::INT            AS project_id,
+    created_at::TIMESTAMP      AS created_at,
+    released_at::TIMESTAMP     AS released_at,
+    semver_major::INT          AS semver_major,
+    semver_minor::INT          AS semver_minor,
+    semver_patch::INT          AS semver_patch,
+    semver_prerelease::VARCHAR AS semver_prerelease
   FROM source
 
 )
--- transform/snowflake-dbt/models/sources/gitlab_dotcom/renamed/scd/gitlab_dotcom_catalog_resources_source.sql
+++ transform/snowflake-dbt/models/sources/gitlab_dotcom/renamed/scd/gitlab_dotcom_catalog_resources_source.sql
@@ -9,19 +9,18 @@ WITH source AS (
 renamed AS (
 
   SELECT
-
-    id,
-    project_id,
-    created_at,
-    state,
-    latest_released_at,
-    name,
-    description,
-    visibility_level,
-    search_vector,
-    verification_level,
-    last_30_day_usage_count,
-    last_30_day_usage_count_updated_at
+    id::INT                                       AS id,
+    project_id::INT                               AS project_id,
+    created_at::TIMESTAMP                         AS created_at,
+    state::INT                                    AS state,
+    latest_released_at::TIMESTAMP                 AS latest_released_at,
+    name::VARCHAR                                 AS name,
+    description::VARCHAR                          AS description,
+    visibility_level::INT                         AS visibility_level,
+    search_vector::VARCHAR                        AS search_vector,
+    verification_level::INT                       AS verification_level,
+    last_30_day_usage_count::INT                  AS last_30_day_usage_count,
+    last_30_day_usage_count_updated_at::TIMESTAMP AS last_30_day_usage_count_updated_at
   FROM source
 
 )
