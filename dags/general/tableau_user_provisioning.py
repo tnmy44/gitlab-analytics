e1974136f52a99569452cdb0780f1472cdf2f7de
Paul Armstrong <parmstrong@gitlab.com>
2024-03-27 11:27:38 +0200

    Correct path.

--- dags/general/tableau_user_provisioning.py
+++ dags/general/tableau_user_provisioning.py
@@ -58,10 +58,7 @@ dag = DAG(
 # tableau Extract
 tableau_workbook_migrate_cmd = f"""
     {clone_and_setup_extraction_cmd} &&
-    pwd && 
-    cd ./ && 
-    pwd && 
-    TableauConMan provision-settings --yaml_path='./tableau_con_man_config/src/provision_plan.yaml'
+    TableauConMan provision-settings --yaml_path='analytics/extract/tableau_con_man_config/src/provision_plan.yaml'
 """
 
 # having both xcom flag flavors since we're in an airflow version where one is being deprecated
