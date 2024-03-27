b40e94fcb59b02f0a2c824d83d6940b321083e5b
David Egan <degan@gitlab.com>
2024-03-27 00:10:44 +0000

    Add deal name to vartopia reg object

--- transform/snowflake-dbt/models/sources/sfdc/sfdc_vartopia_drs_registration_source.sql
+++ transform/snowflake-dbt/models/sources/sfdc/sfdc_vartopia_drs_registration_source.sql
@@ -9,11 +9,15 @@ WITH source AS (
       VartopiaDRS__Vendor_Deal_ID__c::VARCHAR      AS deal_id,
       VartopiaDRS__Opportunity__c::VARCHAR         AS linked_opportunity_id,
       Name::VARCHAR                                AS registration_name,
+      VARTOPIADRS__DEAL_NAME__C::VARCHAR           AS deal_registration_name,
       CAM_Approval_Status__c::VARCHAR              AS partner_manager_approval_status,
+      CAM_Denial_Reason::VARCHAR                   AS partner_manager_denial_reason,
       VartopiaDRS__Approved_Date__c::VARCHAR       AS deal_registration_approval_date,
       VartopiaDRS__Picklist_1__c::VARCHAR          AS deal_registration_type,
-      VartopiaDRS__MDF_Campaign__c::VARCHAR        AS gitLab_marketing_campaign,
+      VartopiaDRS__MDF_Campaign__c::VARCHAR        AS gitlab_marketing_campaign,
       VartopiaDRS__Campaign_1__c::VARCHAR          AS distributor_marketing_campaign,
+      VARTOPIADRS__DISTRIBUTOR_ACCOUNT__C::VARCHAR AS distributor_account_id,
+      VARTOPIADRS__SUBMITTER_TYPE__C::VARCHAR      AS deal_submitter_type,
 
       -- metadata
       createdbyid::VARCHAR                         AS created_by_id,
