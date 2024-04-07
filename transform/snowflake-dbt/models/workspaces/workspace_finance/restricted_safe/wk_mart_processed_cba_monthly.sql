5c4abee5274a2fcf2b2627329ec6fd9f4345275c
apiaseczna@gitlab.com <apiaseczna@gitlab.com>
2024-04-07 21:34:47 +0100

    Updating cba table workspace

--- transform/snowflake-dbt/models/workspaces/workspace_finance/restricted_safe/wk_mart_processed_cba_monthly.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_finance/restricted_safe/wk_mart_processed_cba_monthly.sql
@@ -33,6 +33,22 @@ ORDER BY cba_decrease_date
 
 ),
 
+overpayment AS
+
+(
+
+SELECT 
+DATE(DATE_TRUNC('month', wk_finance_fct_credit_balance_adjustment.credit_balance_adjustment_date)) AS overpayment_date,
+SUM(wk_finance_fct_credit_balance_adjustment.credit_balance_adjustment_amount) AS overpayment
+FROM prod.restricted_safe_workspace_finance.wk_finance_fct_credit_balance_adjustment
+WHERE wk_finance_fct_credit_balance_adjustment.credit_balance_adjustment_type = 'Increase'
+AND wk_finance_fct_credit_balance_adjustment.credit_balance_adjustment_status = 'Processed'
+AND wk_finance_fct_credit_balance_adjustment.INVOICE_ID = ''
+GROUP BY overpayment_date
+ORDER BY overpayment_date
+
+),
+
 final AS
 
 (
@@ -41,9 +57,11 @@ SELECT
 COALESCE(increase_cba.cba_increase_date, decrease_cba.cba_decrease_date) AS cba_date,
 COALESCE(increase_cba.increase,0) AS increase,
 COALESCE(decrease_cba.decrease,0) AS decrease,
+COALESCE(overpayment.overpayment,0) AS overpayment,
 SUM(increase_cba.increase - decrease_cba.decrease) OVER (ORDER BY cba_date) AS credit_balance_per_month
 FROM increase_cba
 FULL OUTER JOIN decrease_cba ON decrease_cba.cba_decrease_date = increase_cba.cba_increase_date
+FULL OUTER JOIN overpayment ON overpayment.overpayment_date = increase_cba.cba_increase_date
 ORDER BY cba_date
 
 )
@@ -53,6 +71,6 @@ cte_ref="final",
 created_by="@apiaseczna",
 updated_by="@apiaseczna",
 created_date="2024-03-29",
-updated_date="2024-03-29"
+updated_date="2024-04-08"
 ) }}
 
