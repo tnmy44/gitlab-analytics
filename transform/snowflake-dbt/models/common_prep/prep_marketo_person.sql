a80f4ee536132109e05c23d44ae3d9de7018c50a
Peter Empey <pempey@gitlab.com>
2024-04-11 12:02:18 +0000

    Adjust surrogate_key

--- transform/snowflake-dbt/models/common_prep/prep_marketo_person.sql
+++ transform/snowflake-dbt/models/common_prep/prep_marketo_person.sql
@@ -2,15 +2,7 @@ WITH final AS (
 
     SELECT
         marketo_lead_id AS dim_marketo_person_id,
-        md5(
-            cast(
-                coalesce(
-                    cast(sfdc_contact_id as TEXT),
-                    cast(sfdc_lead_id as TEXT),
-                    '_dbt_utils_surrogate_key_null_'
-                ) as TEXT
-            )
-        ) AS dim_crm_person_id,
+        {{ dbt_utils.generate_surrogate_key(['sfdc_contact_id', 'sfdc_lead_id)']) }} AS dim_crm_person_id,
         email_hash,
         sfdc_lead_id,
         sfdc_contact_id,
