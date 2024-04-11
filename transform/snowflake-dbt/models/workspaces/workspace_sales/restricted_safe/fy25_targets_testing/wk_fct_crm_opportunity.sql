6b5b4fd46862d367e7fc9dcf7d8f5b5dffe87c52
Chris Sharp <csharp@gitlab.com>
2024-04-11 14:41:17 +0100

    add current_account_set_role CASE statements

--- transform/snowflake-dbt/models/workspaces/workspace_sales/restricted_safe/fy25_targets_testing/wk_fct_crm_opportunity.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_sales/restricted_safe/fy25_targets_testing/wk_fct_crm_opportunity.sql
@@ -104,12 +104,12 @@
       {{ dbt_utils.generate_surrogate_key(['sfdc_opportunity.crm_opp_owner_geo_stamped']) }}                                      AS dim_crm_opp_owner_geo_stamped_id,
       {{ dbt_utils.generate_surrogate_key(['sfdc_opportunity.crm_opp_owner_region_stamped']) }}                                   AS dim_crm_opp_owner_region_stamped_id,
       {{ dbt_utils.generate_surrogate_key(['sfdc_opportunity.crm_opp_owner_area_stamped']) }}                                     AS dim_crm_opp_owner_area_stamped_id,
-      {{ get_keyed_nulls('prep_crm_user_hierarchy.dim_crm_user_role_name_id') }}                                                  AS dim_crm_user_role_name_id,
-      {{ get_keyed_nulls('prep_crm_user_hierarchy.dim_crm_user_role_level_1_id') }}                                               AS dim_crm_user_role_level_1_id,
-      {{ get_keyed_nulls('prep_crm_user_hierarchy.dim_crm_user_role_level_2_id') }}                                               AS dim_crm_user_role_level_2_id,
-      {{ get_keyed_nulls('prep_crm_user_hierarchy.dim_crm_user_role_level_3_id') }}                                               AS dim_crm_user_role_level_3_id,
-      {{ get_keyed_nulls('prep_crm_user_hierarchy.dim_crm_user_role_level_4_id') }}                                               AS dim_crm_user_role_level_4_id,
-      {{ get_keyed_nulls('prep_crm_user_hierarchy.dim_crm_user_role_level_5_id') }}                                               AS dim_crm_user_role_level_5_id,
+      {{ get_keyed_nulls('prep_crm_user_hierarchy.dim_crm_user_role_name_id') }}                                                  AS dim_crm_opp_owner_role_name_id,
+      {{ get_keyed_nulls('prep_crm_user_hierarchy.dim_crm_user_role_level_1_id') }}                                               AS dim_crm_opp_owner_role_level_1_id,
+      {{ get_keyed_nulls('prep_crm_user_hierarchy.dim_crm_user_role_level_2_id') }}                                               AS dim_crm_opp_owner_role_level_2_id,
+      {{ get_keyed_nulls('prep_crm_user_hierarchy.dim_crm_user_role_level_3_id') }}                                               AS dim_crm_opp_owner_role_level_3_id,
+      {{ get_keyed_nulls('prep_crm_user_hierarchy.dim_crm_user_role_level_4_id') }}                                               AS dim_crm_opp_owner_role_level_4_id,
+      {{ get_keyed_nulls('prep_crm_user_hierarchy.dim_crm_user_role_level_5_id') }}                                               AS dim_crm_opp_owner_role_level_5_id,
       {{ get_keyed_nulls('sales_rep.dim_crm_user_hierarchy_sk') }}                                                                AS dim_crm_user_hierarchy_live_sk,
       {{ get_keyed_nulls('sales_rep.dim_crm_user_business_unit_id') }}                                                            AS dim_crm_user_business_unit_id,
       {{ get_keyed_nulls('sales_rep.dim_crm_user_sales_segment_id') }}                                                            AS dim_crm_user_sales_segment_id,
@@ -177,6 +177,36 @@
           THEN sales_rep_account.crm_user_business_unit
         ELSE sfdc_opportunity.crm_opp_owner_business_unit_stamped
       END                                                                                                                         AS crm_current_account_set_business_unit,
+      CASE
+        WHEN close_fiscal_year < prep_date.current_fiscal_year
+          THEN sales_rep_account.user_role_name
+        ELSE sales_rep.user_role_name
+      END                                                                                                                         AS crm_current_account_set_role_name,
+      CASE
+        WHEN close_fiscal_year < prep_date.current_fiscal_year
+          THEN sales_rep_account.user_role_level_1
+        ELSE sales_rep.user_role_level_1
+      END                                                                                                                         AS crm_current_account_set_role_level_1,
+      CASE
+        WHEN close_fiscal_year < prep_date.current_fiscal_year
+          THEN sales_rep_account.user_role_level_2
+        ELSE sales_rep.user_role_level_2
+      END                                                                                                                         AS crm_current_account_set_role_level_2,
+      CASE
+        WHEN close_fiscal_year < prep_date.current_fiscal_year
+          THEN sales_rep_account.user_role_level_3
+        ELSE sales_rep.user_role_level_3
+      END                                                                                                                         AS crm_current_account_set_role_level_3,
+      CASE
+        WHEN close_fiscal_year < prep_date.current_fiscal_year
+          THEN sales_rep_account.user_role_level_4
+        ELSE sales_rep.user_role_level_4
+      END                                                                                                                         AS crm_current_account_set_role_level_4,
+      CASE
+        WHEN close_fiscal_year < prep_date.current_fiscal_year
+          THEN sales_rep_account.user_role_level_5
+        ELSE sales_rep.user_role_level_5
+      END                                                                                                                         AS crm_current_account_set_role_level_5,
 
       sfdc_opportunity.ssp_id,
       sfdc_opportunity.ga_client_id,
