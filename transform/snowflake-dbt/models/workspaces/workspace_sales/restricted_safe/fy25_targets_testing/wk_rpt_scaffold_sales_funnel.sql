86c1f9a2d69bb2fcb7aedae6e783bfac09a7ea7b
Chris Sharp <csharp@gitlab.com>
2024-04-15 20:16:45 +0100

    add geo fields

--- transform/snowflake-dbt/models/workspaces/workspace_sales/restricted_safe/fy25_targets_testing/wk_rpt_scaffold_sales_funnel.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_sales/restricted_safe/fy25_targets_testing/wk_rpt_scaffold_sales_funnel.sql
@@ -5,27 +5,88 @@
 {{ simple_cte([
     ('fct_sales_funnel_target_daily', 'wk_fct_sales_funnel_target_daily'),
     ('fct_sales_funnel_actual', 'wk_fct_sales_funnel_actual'),
-    ('dim_sales_funnel_kpi', 'wk_dim_sales_funnel_kpi')
+    ('dim_sales_funnel_kpi', 'wk_dim_sales_funnel_kpi'),
+    ('dim_crm_user_hierarchy', 'wk_dim_crm_user_hierarchy')
 ]) }}
 
-SELECT
-  fct_sales_funnel_actual.actual_date_id                  AS date_id,
-  dim_sales_funnel_kpi.dim_sales_funnel_kpi_sk,
-  fct_sales_funnel_actual.dim_hierarchy_sk,
-  fct_sales_funnel_actual.dim_order_type_id,
-  fct_sales_funnel_actual.dim_sales_qualified_source_id
-FROM fct_sales_funnel_actual
-LEFT JOIN dim_sales_funnel_kpi
-  ON dim_sales_funnel_kpi.sales_funnel_kpi_name = fct_sales_funnel_actual.sales_funnel_kpi_name
+, geo_fields AS (
 
-UNION
+    SELECT DISTINCT
+      actual_date_id, 
+      dim_sales_funnel_kpi_sk,
+      dim_hierarchy_sk, 
+      dim_order_type_id,
+      dim_sales_qualified_source_id,
+      geo_name, 
+      region_name, 
+      area_name
+    FROM fct_sales_funnel_actual
+    LEFT JOIN dim_sales_funnel_kpi
+      ON fct_sales_funnel_actual.sales_funnel_kpi_name = dim_sales_funnel_kpi.sales_funnel_kpi_name
 
-SELECT 
-  fct_sales_funnel_target_daily.target_date_id,
-  dim_sales_funnel_kpi.dim_sales_funnel_kpi_sk,
-  fct_sales_funnel_target_daily.dim_crm_user_hierarchy_sk,
-  fct_sales_funnel_target_daily.dim_order_type_id,
-  fct_sales_funnel_target_daily.dim_sales_qualified_source_id
-FROM fct_sales_funnel_target_daily
-LEFT JOIN dim_sales_funnel_kpi
-  ON dim_sales_funnel_kpi.sales_funnel_kpi_name = fct_sales_funnel_target_daily.kpi_name
+    UNION
+
+    SELECT DISTINCT
+      fct_sales_funnel_target_daily.target_date_id, 
+      fct_sales_funnel_target_daily.dim_sales_funnel_kpi_sk,
+      fct_sales_funnel_target_daily.dim_crm_user_hierarchy_sk, 
+      fct_sales_funnel_target_daily.dim_order_type_id,
+      fct_sales_funnel_target_daily.dim_sales_qualified_source_id,
+      dim_crm_user_hierarchy.crm_user_geo, 
+      dim_crm_user_hierarchy.crm_user_region, 
+      dim_crm_user_hierarchy.crm_user_area
+    FROM fct_sales_funnel_target_daily
+    LEFT JOIN dim_crm_user_hierarchy
+      ON fct_sales_funnel_target_daily.dim_crm_user_hierarchy_sk = dim_crm_user_hierarchy.dim_crm_user_hierarchy_sk
+    LEFT JOIN dim_sales_funnel_kpi
+      ON fct_sales_funnel_target_daily.kpi_name = dim_sales_funnel_kpi.sales_funnel_kpi_name
+)
+
+, unioned AS (
+
+    SELECT
+    fct_sales_funnel_actual.actual_date_id                  AS date_id,
+    dim_sales_funnel_kpi.dim_sales_funnel_kpi_sk,
+    fct_sales_funnel_actual.dim_hierarchy_sk,
+    fct_sales_funnel_actual.dim_order_type_id,
+    fct_sales_funnel_actual.dim_sales_qualified_source_id
+    FROM fct_sales_funnel_actual
+    LEFT JOIN dim_sales_funnel_kpi
+      ON dim_sales_funnel_kpi.sales_funnel_kpi_name = fct_sales_funnel_actual.sales_funnel_kpi_name
+
+    UNION
+
+    SELECT 
+    fct_sales_funnel_target_daily.target_date_id,
+    dim_sales_funnel_kpi.dim_sales_funnel_kpi_sk,
+    fct_sales_funnel_target_daily.dim_crm_user_hierarchy_sk,
+    fct_sales_funnel_target_daily.dim_order_type_id,
+    fct_sales_funnel_target_daily.dim_sales_qualified_source_id
+    FROM fct_sales_funnel_target_daily
+    LEFT JOIN dim_sales_funnel_kpi
+      ON dim_sales_funnel_kpi.sales_funnel_kpi_name = fct_sales_funnel_target_daily.kpi_name
+
+)
+
+, final AS (
+
+    SELECT 
+      unioned.date_id,
+      unioned.dim_sales_funnel_kpi_sk,
+      unioned.dim_hierarchy_sk,
+      unioned.dim_order_type_id,
+      unioned.dim_sales_qualified_source_id,
+      geo_fields.geo_name, 
+      geo_fields.region_name, 
+      geo_fields.area_name
+    FROM unioned
+    LEFT JOIN geo_fields
+      ON unioned.date_id = geo_fields.actual_date_id 
+      AND unioned.dim_sales_funnel_kpi_sk = geo_fields.dim_sales_funnel_kpi_sk
+      AND unioned.dim_hierarchy_sk = geo_fields.dim_hierarchy_sk
+      AND unioned.dim_order_type_id = geo_fields.dim_order_type_id
+      AND unioned.dim_sales_qualified_source_id = geo_fields.dim_sales_qualified_source_id
+)
+
+SELECT *
+FROM final
