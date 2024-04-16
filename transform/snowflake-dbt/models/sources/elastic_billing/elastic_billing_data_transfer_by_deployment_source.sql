c921211554820ea3db12af13036eceeb75fc383e
Raul Rendon <rrendon@gitlab.com>
2024-04-16 19:23:06 +0000

    adding two models for elastic billing data by deployment

---
+++ transform/snowflake-dbt/models/sources/elastic_billing/elastic_billing_data_transfer_by_deployment_source.sql
@@ -0,0 +1,33 @@
+with source as (  
+  SELECT *
+  FROM  {{ source('elastic_billing', 'itemized_costs_by_deployment') }}
+),
+
+parsed as (
+  SELECT 
+    source.deployment_id,
+    source.extraction_start_date,
+    source.extraction_end_date,
+    CASE storage_and_transfer.value['sku']::VARCHAR
+      WHEN 'gcp.data-transfer-in' THEN 'gcp_data_transfer_in'
+      WHEN 'gcp.data-transfer-inter-node' THEN 'gcp_data_transfer_inter_node'
+      WHEN 'gcp.data-transfer-out' THEN 'gcp_data_transfer_out'
+      WHEN 'gcp.snapshot-api-1k' THEN 'gcp_snapshot_storage_api'
+      WHEN 'gcp.snapshot-storage' THEN 'gcp_snapshot_storage'
+    END AS charge_type,
+    storage_and_transfer.value['cost']::FLOAT AS cost,
+    storage_and_transfer.value['name']::VARCHAR AS resource_name,
+    storage_and_transfer.value['quantity']['value']::FLOAT AS resource_quantity_value,
+    storage_and_transfer.value['quantity']['formatted_value']::VARCHAR AS formated_quantity_value,
+    storage_and_transfer.value['rate']['value']::FLOAT AS rate_value,
+    storage_and_transfer.value['rate']['formatted_value']::VARCHAR AS formated_rate_value,
+    storage_and_transfer.value['sku']::VARCHAR AS sku,
+    storage_and_transfer.value['type']::VARCHAR AS type
+  FROM source
+      INNER JOIN LATERAL FLATTEN(input => PARSE_JSON(payload), outer => TRUE) AS dims
+      INNER JOIN LATERAL FLATTEN(input => PARSE_JSON(dims.value), outer => TRUE, mode => 'ARRAY') AS storage_and_transfer
+  WHERE dims.path = 'data_transfer_and_storage'
+) 
+
+SELECT * 
+FROM parsed
