87bfb33353d8a7d463f828f8f4ac82324d7a4b12
Clement Leroux <cleroux@gitlab.com>
2024-04-16 20:29:23 +0200

    fix join error

--- transform/snowflake-dbt/models/workspaces/workspace_engineering/restricted_safe/ai/ai_vertex_cost.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_engineering/restricted_safe/ai/ai_vertex_cost.sql
@@ -7,26 +7,23 @@ WITH credits AS ( --excluding migration credit between march 7th and March 27th
   WHERE LOWER(cr.credit_description) NOT LIKE '%1709765302259%'
   GROUP BY cr.source_primary_key
 
-),
+)
+
+,
 
 daily_costs AS (
 
-  SELECT
-    DATE(usage_start_time)                          AS date_day,
-    SUM(cost_before_credits) + SUM(cr.total_credit) AS net_cost
+  SELECT *
   FROM {{ ref('gcp_billing_export_xf') }} AS xf
-  LEFT JOIN credits AS cr ON xf.source_primary_key = cr.source_primary_key
   WHERE DATE_TRUNC('month', DATE(usage_start_time)) >= '2023-11-01'
     AND xf.project_id IN ('unreview-poc-390200e5')
-    AND service_description = 'Vertex AI'
-  GROUP BY 1
-  ORDER BY 1 DESC
+    AND xf.service_description = 'Vertex AI'
+
 )
 
 SELECT
-  d.date_day,
-  d.net_cost                                                                           AS daily_net_cost,
-  SUM(d.net_cost) OVER (ORDER BY d.date_day ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS rolling_7d_net_cost,
-  SUM(d.net_cost) OVER (ORDER BY d.date_day ROWS BETWEEN 27 PRECEDING AND CURRENT ROW) AS rolling_28d_net_cost
-FROM daily_costs d
-ORDER BY d.date_day DESC
+  DATE(usage_start_time)                                       AS date_day,
+  SUM(cost_before_credits) + COALESCE(SUM(cr.total_credit), 0) AS cost_before_credits
+FROM daily_costs
+LEFT JOIN credits AS cr ON daily_costs.source_primary_key = cr.source_primary_key
+GROUP BY 1
