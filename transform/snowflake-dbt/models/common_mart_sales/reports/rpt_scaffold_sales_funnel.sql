d7624f1174965d2ad21a03a2d37d39f8867fa45f
Chris Sharp <csharp@gitlab.com>
2024-04-17 16:29:55 +0100

    add geo and role names

--- transform/snowflake-dbt/models/common_mart_sales/reports/rpt_scaffold_sales_funnel.sql
+++ transform/snowflake-dbt/models/common_mart_sales/reports/rpt_scaffold_sales_funnel.sql
@@ -8,24 +8,41 @@
     ('dim_sales_funnel_kpi', 'dim_sales_funnel_kpi')
 ]) }}
 
-SELECT
-  fct_sales_funnel_actual.actual_date_id                  AS date_id,
-  dim_sales_funnel_kpi.dim_sales_funnel_kpi_sk,
-  fct_sales_funnel_actual.dim_hierarchy_sk,
-  fct_sales_funnel_actual.dim_order_type_id,
-  fct_sales_funnel_actual.dim_sales_qualified_source_id
-FROM fct_sales_funnel_actual
-LEFT JOIN dim_sales_funnel_kpi
-  ON dim_sales_funnel_kpi.sales_funnel_kpi_name = fct_sales_funnel_actual.sales_funnel_kpi_name
+, final AS (
 
-UNION
+    SELECT
+    fct_sales_funnel_actual.actual_date_id                  AS date_id,
+    dim_sales_funnel_kpi.dim_sales_funnel_kpi_sk,
+    fct_sales_funnel_actual.dim_hierarchy_sk,
+    fct_sales_funnel_actual.dim_order_type_id,
+    fct_sales_funnel_actual.dim_sales_qualified_source_id,
+    fct_sales_funnel_actual.geo_name,
+    fct_sales_funnel_actual.region_name,
+    fct_sales_funnel_actual.area_name,
+    fct_sales_funnel_actual.sales_segment_name,
+    fct_sales_funnel_actual.business_unit_name
+    FROM fct_sales_funnel_actual
+    LEFT JOIN dim_sales_funnel_kpi
+      ON dim_sales_funnel_kpi.sales_funnel_kpi_name = fct_sales_funnel_actual.sales_funnel_kpi_name
 
-SELECT 
-  fct_sales_funnel_target_daily.target_date_id,
-  dim_sales_funnel_kpi.dim_sales_funnel_kpi_sk,
-  fct_sales_funnel_target_daily.dim_crm_user_hierarchy_sk,
-  fct_sales_funnel_target_daily.dim_order_type_id,
-  fct_sales_funnel_target_daily.dim_sales_qualified_source_id
-FROM fct_sales_funnel_target_daily
-LEFT JOIN dim_sales_funnel_kpi
-  ON dim_sales_funnel_kpi.sales_funnel_kpi_name = fct_sales_funnel_target_daily.kpi_name
+    UNION
+
+    SELECT 
+    fct_sales_funnel_target_daily.target_date_id,
+    dim_sales_funnel_kpi.dim_sales_funnel_kpi_sk,
+    fct_sales_funnel_target_daily.dim_crm_user_hierarchy_sk,
+    fct_sales_funnel_target_daily.dim_order_type_id,
+    fct_sales_funnel_target_daily.dim_sales_qualified_source_id,
+    fct_sales_funnel_target_daily.geo_name, 
+    fct_sales_funnel_target_daily.region_name, 
+    fct_sales_funnel_target_daily.area_name,
+    fct_sales_funnel_target_daily.sales_segment_name,
+    fct_sales_funnel_target_daily.business_unit_name
+    FROM fct_sales_funnel_target_daily
+    LEFT JOIN dim_sales_funnel_kpi
+      ON dim_sales_funnel_kpi.sales_funnel_kpi_name = fct_sales_funnel_target_daily.kpi_name
+
+)
+
+SELECT *
+FROM final
