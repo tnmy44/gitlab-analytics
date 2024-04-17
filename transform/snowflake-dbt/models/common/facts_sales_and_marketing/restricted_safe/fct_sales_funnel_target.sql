a60cbb270cf49ed638109d4ad51a95b6920579e8
Chris Sharp <csharp@gitlab.com>
2024-04-17 17:13:41 +0100

    add geo hierarchy fields

--- transform/snowflake-dbt/models/common/facts_sales_and_marketing/restricted_safe/fct_sales_funnel_target.sql
+++ transform/snowflake-dbt/models/common/facts_sales_and_marketing/restricted_safe/fct_sales_funnel_target.sql
@@ -16,6 +16,8 @@
                                  'prep_sales_funnel_target.first_day_of_month', 
                                  'prep_sales_funnel_target.sales_qualified_source',
                                  'prep_sales_funnel_target.order_type',
+                                 'prep_sales_funnel_target.user_geo',
+                                 'prep_sales_funnel_target.user_segment'
                                  ]) }}                                                AS sales_funnel_target_id,
      prep_sales_funnel_target.kpi_name,
      prep_sales_funnel_kpi.dim_sales_funnel_kpi_sk,
@@ -27,6 +29,11 @@
      prep_sales_funnel_target.order_type,
      {{ get_keyed_nulls('order_type.dim_order_type_id') }}                            AS dim_order_type_id,
      prep_sales_funnel_target.area                                                    AS crm_user_sales_segment_geo_region_area,
+     prep_sales_funnel_target.user_geo                                                AS geo_name,
+     prep_sales_funnel_target.user_region                                             AS region_name,
+     prep_sales_funnel_target.user_area                                               AS area_name,
+     prep_sales_funnel_target.user_segment                                            AS sales_segment_name,
+     prep_sales_funnel_target.user_business_unit                                      AS business_unit_name,
      prep_crm_user_hierarchy.dim_crm_user_hierarchy_id                                AS dim_crm_user_hierarchy_live_id,
      prep_crm_user_hierarchy.dim_crm_user_business_unit_id,
      prep_crm_user_hierarchy.dim_crm_user_sales_segment_id,
@@ -34,7 +41,7 @@
      prep_crm_user_hierarchy.dim_crm_user_region_id,
      prep_crm_user_hierarchy.dim_crm_user_area_id,
      prep_crm_user_hierarchy.dim_crm_user_hierarchy_id                                AS dim_crm_user_hierarchy_stamped_id,
-     prep_crm_user_hierarchy.dim_crm_user_hierarchy_sk,
+     {{ get_keyed_nulls('prep_crm_user_hierarchy.dim_crm_user_hierarchy_sk') }}       AS dim_crm_user_hierarchy_sk,
      prep_sales_funnel_target.dim_crm_user_hierarchy_sk                               AS target_dim_crm_user_hierarchy_sk,
      prep_crm_user_hierarchy.dim_crm_user_business_unit_id                            AS dim_crm_opp_owner_business_unit_stamped_id,
      prep_crm_user_hierarchy.dim_crm_user_sales_segment_id                            AS dim_crm_opp_owner_sales_segment_stamped_id,
@@ -49,11 +56,12 @@
       ON {{ sales_funnel_text_slugify("prep_sales_funnel_target.order_type") }} = {{ sales_funnel_text_slugify("order_type.order_type_name") }}
     LEFT JOIN prep_crm_user_hierarchy
       ON prep_sales_funnel_target.dim_crm_user_hierarchy_sk = prep_crm_user_hierarchy.dim_crm_user_hierarchy_sk
+        AND prep_sales_funnel_target.fiscal_year = prep_crm_user_hierarchy.fiscal_year
     LEFT JOIN prep_sales_funnel_kpi
       ON {{ sales_funnel_text_slugify("prep_sales_funnel_kpi.sales_funnel_kpi_name") }} = {{ sales_funnel_text_slugify("prep_sales_funnel_target.kpi_name") }}
     LEFT JOIN prep_date
       ON prep_date.date_actual = prep_sales_funnel_target.first_day_of_month
-    {{ dbt_utils.group_by(n=24)}}
+    {{ dbt_utils.group_by(n=29)}}
 
 )
 
@@ -62,5 +70,5 @@
     created_by="@michellecooper",
     updated_by="@chrissharp",
     created_date="2023-02-07",
-    updated_date="2024-03-27"
+    updated_date="2024-04-17"
 ) }}
