347c42a44687d97deb24256c3380cf9cb8498658
michellecooper <mcooper@gitlab.com>
2024-04-18 15:24:51 -0700

    revert sample updates since testing is done

--- transform/snowflake-dbt/models/sources/snowplow/snowplow_gitlab_good_events_sample_source.sql
+++ transform/snowflake-dbt/models/sources/snowplow/snowplow_gitlab_good_events_sample_source.sql
@@ -1,10 +1,8 @@
 WITH source as (
 
   SELECT
-    {{ dbt_utils.star(from=source('gitlab_snowplow', 'events'), except=['geo_zipcode', 'geo_latitude', 'geo_longitude', 'user_ipaddress']) }}
-  FROM {{ source('gitlab_snowplow', 'events') }}
-  WHERE TRY_TO_TIMESTAMP(derived_tstamp)::DATE >= '2024-04-01'
-    AND TRY_TO_TIMESTAMP(derived_tstamp)::DATE <= '2024-04-07'
+    {{ dbt_utils.star(from=source('gitlab_snowplow', 'events_sample'), except=['geo_zipcode', 'geo_latitude', 'geo_longitude', 'user_ipaddress']) }}
+  FROM {{ source('gitlab_snowplow', 'events_sample') }}
 
 )
 
