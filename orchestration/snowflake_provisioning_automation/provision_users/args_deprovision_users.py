214ae303904bd401f0ed1369fcad07a00ab4aac9
Justin Wong <juwong@gitlab.com>
2024-04-19 10:38:48 -0600

    init args_deprovision

---
+++ orchestration/snowflake_provisioning_automation/provision_users/args_deprovision_users.py
@@ -0,0 +1,34 @@
+"""
+This module args.py parses the command line arguments provided by the user.
+"""
+
+import argparse
+
+
+def parse_arguments() -> argparse.Namespace:
+    """
+    The user can pass in the following arguemnts:
+        --users-to-remove
+        --test-run
+
+    All arguments are optional- if no args passed in,
+    then default arguments for `roles` and `users` are used.
+    """
+    parser = argparse.ArgumentParser(description="Provision users in Snowflake options")
+    parser.add_argument(
+        "-ua",
+        "--users-to-remove",
+        nargs="+",
+        type=str,
+        default=None,
+        help="users to remove from Snowflake",
+    )
+    # by default, only print snowflake queries, don't run in Snowflake
+    parser.add_argument(
+        "-t",
+        "--test-run",
+        action=argparse.BooleanOptionalAction,
+        default=True,
+        help="If test, only print the sql statements, rather than running them in Snowflake",
+    )
+    return parser.parse_args()
