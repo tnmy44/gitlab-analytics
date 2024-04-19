355e5bdb9dca700d5b8cdc2b8ce5dd54dff351bf
Jong Lee <jonglee@gitlab.com>
2024-04-19 09:17:16 -0400

    added yearlies target

--- transform/snowflake-dbt/models/common_prep/restricted_safe/prep_yearlies_target.sql
+++ transform/snowflake-dbt/models/common_prep/restricted_safe/prep_yearlies_target.sql
@@ -8,7 +8,7 @@ SELECT
    yearly_description,
    is_mnpi,
    REPLACE(quarter_name, '_', '-') AS quarter,
-   TO_DECIMAL(target, 18, 2) AS targets_raw
+   TO_DECIMAL(target, 18, 4) AS targets_raw
 FROM
    {{ ref('sheetload_fy25_yearlies_target_source') }} UNPIVOT(target FOR quarter_name IN 
    (
