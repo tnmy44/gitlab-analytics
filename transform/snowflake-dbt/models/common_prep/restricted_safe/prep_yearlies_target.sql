6d4dfc9a9107db1bae76a31a9ab912234e0dedda
Jong Lee <jonglee@gitlab.com>
2024-04-19 13:52:12 -0400

    added actuals model

--- transform/snowflake-dbt/models/common_prep/restricted_safe/prep_yearlies_target.sql
+++ transform/snowflake-dbt/models/common_prep/restricted_safe/prep_yearlies_target.sql
@@ -1,6 +1,21 @@
 {{ config(materialized='view') }}
 
-WITH final AS (
+WITH prep as (
+  
+ SELECT 
+  yearly_name,
+  yearly_dri,
+  yearly_description,
+  is_mnpi,
+  ifnull(FY25_Q4,-1) as FY25_Q4,
+  ifnull(FY25_Q3,-1) as FY25_Q3,
+  ifnull(FY25_Q2,-1) as FY25_Q2,
+  ifnull(FY25_Q1,-1) as FY25_Q1
+
+FROM
+  {{ ref('sheetload_fy25_yearlies_target_source') }}
+),
+final AS (
 
 SELECT
    yearly_name,
@@ -8,9 +23,9 @@ SELECT
    yearly_description,
    is_mnpi,
    REPLACE(quarter_name, '_', '-') AS quarter,
-   TO_DECIMAL(target, 18, 4) AS targets_raw
+   case when target = -1 then null else target end AS targets_raw
 FROM
-   {{ ref('sheetload_fy25_yearlies_target_source') }} UNPIVOT(target FOR quarter_name IN 
+   prep UNPIVOT(target FOR quarter_name IN 
    (
       "FY25_Q4",
       "FY25_Q3",
--- transform/snowflake-dbt/models/workspaces/workspace_finance/restricted_safe/wk_prep_yearlies_actual.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_finance/restricted_safe/wk_prep_yearlies_actual.sql
@@ -4,10 +4,82 @@
     ('dim_subscription', 'dim_subscription'),
     ('dim_date', 'dim_date'),
     ('dim_crm_account', 'dim_crm_account'),
-    ('mart_crm_opportunity', 'mart_crm_opportunity')
+    ('mart_crm_opportunity', 'mart_crm_opportunity'),
+    ('rpt_product_usage_health_score', 'rpt_product_usage_health_score'),
+    ('mart_arr_all', 'mart_arr_with_zero_dollar_charges'),
+    ('dim_date','dim_date')
 ]) }},
 
-churn_contraction_1 as (
+total_arr_final AS (
+SELECT 
+    '1.1 Total ARR' AS yearly_name,
+    'PROD.RESTRICTED_SAFE_COMMON_MART_SALES.MART_CRM_OPPORTUNITY' AS source_table,
+     close_fiscal_quarter_name AS quarter,
+     SUM(booked_net_arr) AS actuals_raw
+    
+    FROM mart_crm_opportunity
+    WHERE stage_name = 'Closed Won'
+    AND close_fiscal_quarter_name like '%FY25%'
+
+    GROUP BY 1,2,3
+),
+
+duo_final AS (
+
+  SELECT 
+    '2.1 Duo' AS yearly_name,
+    'PROD.RESTRICTED_SAFE_COMMON_MART_SALES.MART_CRM_OPPORTUNITY' AS source_table,
+     close_fiscal_quarter_name AS quarter,
+     SUM(booked_net_arr) AS actuals_raw,
+    
+    FROM mart_crm_opportunity
+    WHERE stage_name = 'Closed Won'
+    AND close_fiscal_quarter_name like '%FY25%'
+    AND product_category like '%Duo%'
+
+    GROUP BY 1,2,3
+),
+
+adoption_metrics_1 AS (
+  SELECT
+    *
+  FROM rpt_product_usage_health_score
+  WHERE is_primary_instance_subscription = true
+
+),
+adoption_metrics_2 AS (
+  SELECT
+    '3.1 Adoption Metrics' AS yearly_name,
+    'PROD.RESTRICTED_SAFE_COMMON_MART_SALES.MART_ARR, PROD.COMMON_MART_PRODUCT.RPT_PRODUCT_USAGE_HEALTH_SCORE' AS source_table,
+    fiscal_quarter_name_fy,
+    arr_month,
+    CASE WHEN adoption_metrics_1.dim_subscription_id_original is not null then true else false end AS usage_data_sent,
+    SUM(arr) AS arr,
+    SUM(arr)/SUM(SUM(arr)) OVER (partition by arr_month) AS percent_of_total
+  FROM
+   mart_arr_all
+   left join 
+      adoption_metrics_1
+      on arr_month = snapshot_month
+      AND mart_arr_all.dim_subscription_id_original = adoption_metrics_1.dim_subscription_id_original
+      AND mart_arr_all.product_delivery_type = adoption_metrics_1.delivery_type
+
+  GROUP BY 1,2,3,4,5
+
+),
+adoption_metrics_final AS (
+
+  SELECT 
+    yearly_name,
+    source_table,
+    fiscal_quarter_name_fy AS quarter,
+    percent_of_total AS actuals_raw
+  FROM
+    adoption_metrics_2 
+  WHERE fiscal_quarter_name_fy like 'FY25%'
+  AND usage_data_sent = true
+),
+churn_contraction_1 AS (
 
    SELECT
       mart_crm_opportunity.CLOSE_FISCAL_QUARTER_NAME,
@@ -39,30 +111,103 @@ churn_contraction_1 as (
       )
       AND mart_crm_opportunity.CLOSE_MONTH BETWEEN '2022-02-01' AND date_trunc('month', current_date) 
 ),
-
-churn_contraction_2 as (
+churn_contraction_2 AS (
 
    SELECT
       CLOSE_FISCAL_QUARTER_NAME,
-      sum(case when order_type in ('4. Contraction','5. Churn - Partial','6. Churn - Final') then renewal_net_arr end) as renewal_net_arr_loss,     
-      sum(arr_basis_for_clari) as atr 
+      SUM(CASE WHEN order_type in ('4. Contraction','5. Churn - Partial','6. Churn - Final') then renewal_net_arr end) AS renewal_net_arr_loss,     
+      SUM(arr_bASis_for_clari) AS atr 
    FROM
-      churn_contraction_first 
-   group by
+      churn_contraction_1
+   GROUP BY
       1 
+),
+churn_contraction_final AS (
+
+SELECT
+   '3.4 Churn AND Contraction' AS yearly_name,
+   'PROD.RESTRICTED_SAFE_COMMON_SALES.MART_CRM_OPPORTUNITY, PROD.COMMON.DIM_SUBSCRIPTION' AS source_table,
+   CLOSE_FISCAL_QUARTER_NAME AS quarter,
+   renewal_net_arr_loss / atr* - 1 AS actuals_raw
+FROM
+   churn_contraction_2
+WHERE quarter like 'FY25%'
+),
+ultimate_arr_final AS (
+SELECT 
+    '4.2 Ultimate ARR' AS yearly_name,
+    'PROD.RESTRICTED_SAFE_COMMON_MART_SALES.MART_CRM_OPPORTUNITY' AS source_table,
+     close_fiscal_quarter_name AS quarter,
+     SUM(booked_net_arr) AS actuals_raw
+    
+    FROM mart_crm_opportunity
+    WHERE stage_name = 'Closed Won'
+    AND close_fiscal_quarter_name like '%FY25%'
+    AND product_category like '%Ultimate%'
+    AND product_category not like '%Dedicated%'
+
+    GROUP BY 1,2,3
+),
+dedicated_arr_final AS (
+  SELECT 
+    '4.3 Dedicated ARR' AS yearly_name,
+    'PROD.RESTRICTED_SAFE_COMMON_MART_SALES.MART_CRM_OPPORTUNITY' AS source_table,
+     close_fiscal_quarter_name AS quarter,
+     SUM(booked_net_arr) AS actuals_raw
+    
+    FROM mart_crm_opportunity
+    WHERE stage_name = 'Closed Won'
+    AND close_fiscal_quarter_name like '%FY25%'
+    AND product_category like '%Dedicated%'
+
+    GROUP BY 1,2,3
+
+),
+plan_arr_final AS (
+  SELECT 
+    '4.4 Plan' AS yearly_name,
+    'PROD.RESTRICTED_SAFE_COMMON_MART_SALES.MART_CRM_OPPORTUNITY' AS source_table,
+     close_fiscal_quarter_name AS quarter,
+     SUM(booked_net_arr) AS actuals_raw
+    
+    FROM mart_crm_opportunity
+    WHERE stage_name = 'Closed Won'
+    AND close_fiscal_quarter_name like '%FY25%'
+    AND product_category like '%Agile%'
+
+    GROUP BY 1,2,3
+
+),
+final AS (
+
+SELECT * FROM total_arr_final 
+
+UNION 
+
+SELECT * FROM duo_final
+
+UNION
+
+SELECT * FROM adoption_metrics_final
+
+UNION
+
+SELECT * FROM churn_contraction_final
+
+UNION 
+
+SELECT * FROM ultimate_arr_final
+
+UNION 
+
+SELECT * FROM dedicated_arr_final
+
+UNION
+
+SELECT * FROM plan_arr_final
 )
 
-select
-   '3.4 Churn and Contraction' as yearly_name,
-   'Chris Weber, Sherrod Patching' as yearly_dri,
-   'Churn and contraction under % of ATR' as yearly_description,
-    true as is_mnpi,
-   'PROD.RESTRICTED_SAFE_COMMON_SALES.MART_CRM_OPPORTUNITY' as source_table,
-   CLOSE_FISCAL_QUARTER_NAME as quarter,
-   renewal_net_arr_loss / atr* - 1 as actuals_raw
-from
-   churn_contraction_2
-where quarter like 'FY25%'
- 
-order by
-   1 desc
+SELECT 
+  * 
+FROM 
+  final
