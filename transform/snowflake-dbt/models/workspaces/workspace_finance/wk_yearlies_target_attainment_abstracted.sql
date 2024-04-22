107f0fb1d9b8fdcf4e3607c445347aa47525cbba
Jong Lee <jonglee@gitlab.com>
2024-04-22 11:52:33 -0400

    sql styleguide logic changes

--- transform/snowflake-dbt/models/workspaces/workspace_finance/wk_yearlies_target_attainment_abstracted.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_finance/wk_yearlies_target_attainment_abstracted.sql
@@ -5,7 +5,7 @@ SELECT
   yearly_dri,
   yearly_description,
   quarter,
-  case when is_mnpi = true then 1 else targets_raw end as targets_abstracted,
-  case when is_mnpi = true then target_attainment else actuals_raw end as actuals_abstracted
+  IFF(is_mnpi = true, 1, targets_raw) as targets_abstracted,
+  IFF(is_mnpi = true, target_attainment, actuals_raw) as actuals_abstracted
 FROM
   {{ ref('wk_yearlies_target_attainment_mnpi') }} AS yearlies_mnpi
