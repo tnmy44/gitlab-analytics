c1cb84ce22e893250b06d2aeb34226809a87b05d
apiaseczna@gitlab.com <apiaseczna@gitlab.com>
2024-04-23 10:13:21 +0100

    Adding invoice aging detail table

---
+++ transform/snowflake-dbt/models/common/facts_financial/restricted_safe/fct_invoice_aging_detail.sql
@@ -0,0 +1,44 @@
+{{ config(
+    materialized="table",
+    tags=["mnpi"]
+) }}
+
+
+
+WITH zuora_invoice_aging_detail AS (
+
+/* grain: transaction ID. The Invoice Aging detail report provides a list of invoices that have outstanding amounts as of the end of the accounting period.  */
+
+    SELECT *
+    FROM {{ source('zuora', 'invoice_aging_detail') }}
+    
+), final AS (
+
+SELECT
+   -- primary key 
+      zuora_invoice_aging_detail.id                          	               AS invoice_aging_detail_id,
+
+   -- keys
+      zuora_invoice_aging_detail.invoiceid               	                   AS invoice_id,     
+      zuora_invoice_aging_detail.accountingperiodid                  	       AS accounting_period_id,
+
+   -- invoice aging detail dates
+      zuora_invoice_aging_detail.accountingperiodenddate                     AS accounting_period_end_date,
+     {{ get_date_id('zuora_invoice_aging_detail.accountingperiodenddate') }} AS accounting_period_end_date_id,
+
+   -- additive fields
+      zuora_invoice_aging_detail.accountbalanceimpact                      	 AS account_balance_impact,
+      zuora_invoice_aging_detail.daysoverdue                     	           AS days_overdue
+    
+
+
+FROM zuora_invoice_aging_detail
+)
+
+{{ dbt_audit(
+cte_ref="final",
+created_by="@apiaseczna",
+updated_by="@apiaseczna",
+created_date="2024-04-23",
+updated_date="2024-04-23"
+) }}
