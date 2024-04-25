f69912039ef89a6a0056e16c0a1303bca822fd88
Chris Sharp <csharp@gitlab.com>
2024-04-25 15:06:41 +0100

    remove comment

--- transform/snowflake-dbt/models/common_prep/prep_crm_user_hierarchy.sql
+++ transform/snowflake-dbt/models/common_prep/prep_crm_user_hierarchy.sql
@@ -259,7 +259,6 @@ SELECT
     FROM prep_crm_opportunity
     INNER JOIN prep_crm_user_roles
       ON prep_crm_opportunity.opportunity_owner_role = prep_crm_user_roles.user_role_name 
-      --prep_crm_opportunity.stamped_opportunity_owner_role__c = prep_crm_user.user_role_name
     WHERE is_live = 1
       AND prep_crm_opportunity.close_fiscal_year IS NOT NULL
     AND prep_crm_opportunity.close_fiscal_year >= 2025
