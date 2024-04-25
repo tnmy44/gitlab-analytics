49c0f38f3d573ce266a63b78d4bab9d04613dff7
apiaseczna@gitlab.com <apiaseczna@gitlab.com>
2024-04-25 10:39:36 +0100

    Fixing error

--- transform/snowflake-dbt/models/sources/zuora/zuora_invoice_aging_detail_source.sql
+++ transform/snowflake-dbt/models/sources/zuora/zuora_invoice_aging_detail_source.sql
@@ -10,22 +10,22 @@ WITH source AS (
     SELECT *
     FROM {{ source('zuora', 'invoice_aging_detail') }}
 
-), renamed AS(
+), renamed AS (
 
     SELECT
    -- primary key 
-      zuora_invoice_aging_detail.id                          	               AS invoice_aging_detail_id,
+      source.id                          	               AS invoice_aging_detail_id,
 
    -- keys
-      zuora_invoice_aging_detail.invoiceid               	                   AS invoice_id,     
-      zuora_invoice_aging_detail.accountingperiodid                  	       AS accounting_period_id,
+      source.invoiceid               	                   AS invoice_id,     
+      source.accountingperiodid                  	       AS accounting_period_id,
 
    -- invoice aging detail dates
-      zuora_invoice_aging_detail.accountingperiodenddate                     AS accounting_period_end_date,
+      source.accountingperiodenddate                     AS accounting_period_end_date,
 
    -- additive fields
-      zuora_invoice_aging_detail.accountbalanceimpact                      	 AS account_balance_impact,
-      zuora_invoice_aging_detail.daysoverdue                     	           AS days_overdue,
+      source.accountbalanceimpact                      	 AS account_balance_impact,
+      source.daysoverdue                     	           AS days_overdue,
     
 
       -- ext1, ext2, ext3, ... ext9
