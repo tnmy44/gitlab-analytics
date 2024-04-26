adb582bce811154a43bd4e98e1b20921601646b8
Clement Leroux <cleroux@gitlab.com>
2024-04-26 07:46:15 +0200

    get daily, non cumulated cost and usage metrics in wk model

--- transform/snowflake-dbt/models/workspaces/workspace_engineering/finops/elastic/rpt_elastic_billing_itemized.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_engineering/finops/elastic/rpt_elastic_billing_itemized.sql
@@ -1,38 +1,77 @@
-WITH data_transfer_source as (
+WITH data_transfer_source AS (
 
-SELECT * FROM {{ ref ('elastic_billing_data_transfer_source')}}
-
-),
-
-resource_source as (
-
-SELECT * FROM {{ ref ('elastic_billing_resources_source')}}
+  SELECT * FROM {{ ref ('elastic_billing_data_transfer_source') }}
 
 ),
 
-renamed as (
+resource_source AS (
 
-SELECT 
+  SELECT * FROM {{ ref ('elastic_billing_resources_source') }}
+
+),
+
+renamed AS (
+
+  SELECT
     extraction_start_date,
     extraction_end_date,
-    'data_transfer' as charge_type,
+    'data_transfer'                      AS charge_type,
     sku,
     cost,
-    resource_quantity_value as usage,
-    formated_quantity_value as usage_unit,
-    ARRAY_CONSTRUCT(type, resource_name) as details
-FROM data_transfer_source
-UNION ALL
-SELECT
+    resource_quantity_value              AS usage,
+    CASE WHEN formated_quantity_value LIKE '%GB%' THEN 'bytes'
+      WHEN formated_quantity_value LIKE '%requests%' THEN 'requests'
+    END                                  AS usage_unit,
+    ARRAY_CONSTRUCT(type, resource_name) AS details
+  FROM data_transfer_source
+  UNION ALL
+  SELECT
     extraction_start_date,
     extraction_end_date,
-    'capacity' as charge_type,
+    'capacity' AS charge_type,
     sku,
     cost,
-    hours as usage,
-    'hours' as usage_unit,
+    hours      AS usage,
+    'hours'    AS usage_unit,
     ARRAY_CONSTRUCT(resource_start_date, resource_end_date, instance_count, price_per_hour, name)
- FROM resource_source
+  FROM resource_source
+),
+
+
+transformed AS (
+  SELECT
+    extraction_end_date                        AS day,
+    charge_type,
+    sku,
+    usage_unit,
+    LAG(cost, 1) OVER (PARTITION BY
+      extraction_start_date,
+      charge_type,
+      sku,
+      usage_unit
+    ORDER BY extraction_end_date DESC) - cost  AS daily_cost,
+    LAG(usage, 1) OVER (PARTITION BY
+      extraction_start_date,
+      charge_type,
+      sku,
+      usage_unit
+    ORDER BY extraction_end_date DESC) - usage AS daily_usage,
+    cost,
+    usage,
+    details
+  FROM renamed
 )
 
-SELECT * FROM renamed
+SELECT
+  day,
+  charge_type,
+  sku,
+  usage_unit,
+  daily_cost,
+  cost,
+  daily_usage,
+  usage,
+  details
+FROM transformed
+WHERE daily_usage IS NOT NULL
+  AND daily_cost IS NOT NULL
--- transform/snowflake-dbt/models/workspaces/workspace_engineering/finops/elastic/rpt_elastic_billing_itemized_by_deployment.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_engineering/finops/elastic/rpt_elastic_billing_itemized_by_deployment.sql
@@ -1,43 +1,84 @@
-WITH data_transfer_source as (
+WITH data_transfer_source AS (
 
-SELECT * 
-FROM {{ ref ('elastic_billing_data_transfer_by_deployment_source')}}
-
-),
-
-resource_source as (
-
-SELECT * 
-FROM {{ ref ('elastic_billing_resources_by_deployment_source')}}
+  SELECT *
+  FROM {{ ref ('elastic_billing_data_transfer_by_deployment_source') }}
 
 ),
 
-renamed as (
+resource_source AS (
 
-SELECT
+  SELECT *
+  FROM {{ ref ('elastic_billing_resources_by_deployment_source') }}
+
+),
+
+renamed AS (
+
+  SELECT
     deployment_id,
     extraction_start_date,
     extraction_end_date,
-    'data_transfer' as charge_type,
+    'data_transfer'                      AS charge_type,
     sku,
     cost,
-    resource_quantity_value as usage,
-    formated_quantity_value as usage_unit,
-    ARRAY_CONSTRUCT(type, resource_name) as details
-FROM data_transfer_source
-UNION ALL
-SELECT
+    resource_quantity_value              AS usage,
+    CASE WHEN formated_quantity_value LIKE '%GB%' THEN 'bytes'
+      WHEN formated_quantity_value LIKE '%requests%' THEN 'requests'
+    END                                  AS usage_unit,
+    ARRAY_CONSTRUCT(type, resource_name) AS details
+  FROM data_transfer_source
+  UNION ALL
+  SELECT
     deployment_id,
     extraction_start_date,
     extraction_end_date,
-    'capacity' as charge_type,
+    'capacity' AS charge_type,
     sku,
     cost,
-    hours as usage,
-    'hours' as usage_unit,
+    hours      AS usage,
+    'hours'    AS usage_unit,
     ARRAY_CONSTRUCT(resource_start_date, resource_end_date, instance_count, price_per_hour, resource_name)
-FROM resource_source
+  FROM resource_source
+),
+
+transformed AS (
+  SELECT
+    extraction_end_date                        AS day,
+    deployment_id,
+    charge_type,
+    sku,
+    usage_unit,
+    LAG(cost, 1) OVER (PARTITION BY
+      extraction_start_date,
+      deployment_id,
+      charge_type,
+      sku,
+      usage_unit
+    ORDER BY extraction_end_date DESC) - cost  AS daily_cost,
+    LAG(usage, 1) OVER (PARTITION BY
+      extraction_start_date,
+      deployment_id,
+      charge_type,
+      sku,
+      usage_unit
+    ORDER BY extraction_end_date DESC) - usage AS daily_usage,
+    cost,
+    usage,
+    details
+  FROM renamed
 )
 
-SELECT * 
-FROM renamed
+SELECT
+  day,
+  deployment_id,
+  charge_type,
+  sku,
+  usage_unit,
+  daily_cost,
+  cost,
+  daily_usage,
+  usage,
+  details
+FROM transformed
+WHERE daily_usage IS NOT NULL
+  AND daily_cost IS NOT NULL
