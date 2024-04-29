441f6880575e1c8090bbb13603bf4f682fc1289c
Clement Leroux <cleroux@gitlab.com>
2024-04-29 10:46:03 +0200

    add unit-economics requests gitaly model

--- transform/snowflake-dbt/models/workspaces/workspace_engineering/thanos_gitaly_requests.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_engineering/thanos_gitaly_requests.sql
@@ -3,13 +3,28 @@ WITH source AS (
   SELECT *
   FROM {{ ref('thanos_gitaly_rps') }}
   WHERE is_success = TRUE
+),
+
+transformed AS (
+
+  SELECT
+    DATE_TRUNC('day', metric_created_at) AS day,
+    SUM(metric_value) * 3600             AS requests
+  FROM source
+  GROUP BY 1
+),
+
+int AS (
+  SELECT
+    day           AS day,
+    SUM(requests) AS requests
+  FROM transformed
+  GROUP BY day
 )
 
 SELECT
-  DATE_TRUNC('day', metric_created_at)                                            AS day,
-  AVG(metric_value)                                                               AS avg_gitaly_rps,
-  SUM(metric_value * 3600)                                                        AS total_gitaly_requests,
-  SUM(metric_value) OVER (ORDER BY day ROWS BETWEEN 6 PRECEDING AND CURRENT ROW)  AS rolling_7d_requests,
-  SUM(metric_value) OVER (ORDER BY day ROWS BETWEEN 27 PRECEDING AND CURRENT ROW) AS rolling_28d_requests
-FROM source
-GROUP BY 1
+  day,
+  requests,
+  SUM(requests) OVER (ORDER BY day ROWS BETWEEN 6 PRECEDING AND CURRENT ROW)  AS rolling_7d_requests,
+  SUM(requests) OVER (ORDER BY day ROWS BETWEEN 27 PRECEDING AND CURRENT ROW) AS rolling_28d_requests
+FROM int
