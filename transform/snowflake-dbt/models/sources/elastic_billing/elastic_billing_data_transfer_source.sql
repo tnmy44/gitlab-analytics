01d949027a1639fc5387bcdfcb8a76f7b4da13d6
Miles Russell <mdrussell@gitlab.com>
2024-04-29 12:37:22 -0500

    SQL style commit

--- transform/snowflake-dbt/models/sources/elastic_billing/elastic_billing_data_transfer_source.sql
+++ transform/snowflake-dbt/models/sources/elastic_billing/elastic_billing_data_transfer_source.sql
@@ -1,10 +1,10 @@
-with source as (  
+WITH source AS (
   SELECT *
-  FROM  {{ source('elastic_billing', 'itemized_costs') }}
+  FROM {{ source('elastic_billing', 'itemized_costs') }}
 ),
 
-parsed as (
-  SELECT 
+parsed AS (
+  SELECT
     source.extraction_start_date,
     source.extraction_end_date,
     CASE resources_and_transfer.value['sku']::VARCHAR
@@ -13,26 +13,26 @@ parsed as (
       WHEN 'gcp.data-transfer-out' THEN 'gcp_data_transfer_out'
       WHEN 'gcp.snapshot-api-1k' THEN 'gcp_snapshot_storage_api'
       WHEN 'gcp.snapshot-storage' THEN 'gcp_snapshot_storage'
-    END AS charge_type,
-    resources_and_transfer.value['cost']::FLOAT AS cost,
-    resources_and_transfer.value['name']::VARCHAR AS resource_name,
-    resources_and_transfer.value['quantity']['value']::FLOAT AS resource_quantity_value,
+    END                                                                  AS charge_type,
+    resources_and_transfer.value['cost']::FLOAT                          AS cost,
+    resources_and_transfer.value['name']::VARCHAR                        AS resource_name,
+    resources_and_transfer.value['quantity']['value']::FLOAT             AS resource_quantity_value,
     resources_and_transfer.value['quantity']['formatted_value']::VARCHAR AS formated_quantity_value,
-    resources_and_transfer.value['rate']['value']::FLOAT AS rate_value,
-    resources_and_transfer.value['rate']['formatted_value']::VARCHAR AS formated_rate_value,
-    resources_and_transfer.value['sku']::VARCHAR AS sku,
-    resources_and_transfer.value['type']::VARCHAR AS type,
-    to_timestamp(source._uploaded_at::int) AS _uploaded_at
+    resources_and_transfer.value['rate']['value']::FLOAT                 AS rate_value,
+    resources_and_transfer.value['rate']['formatted_value']::VARCHAR     AS formated_rate_value,
+    resources_and_transfer.value['sku']::VARCHAR                         AS sku,
+    resources_and_transfer.value['type']::VARCHAR                        AS type,
+    TO_TIMESTAMP(source._uploaded_at::INT)                               AS _uploaded_at
   FROM source
-      INNER JOIN LATERAL FLATTEN(input => PARSE_JSON(payload), outer => TRUE) AS dims
-      INNER JOIN LATERAL FLATTEN(input => PARSE_JSON(dims.value), outer => TRUE, mode => 'ARRAY') AS resources_and_transfer
+  INNER JOIN LATERAL FLATTEN(input => PARSE_JSON(payload), outer => TRUE) AS dims
+  INNER JOIN LATERAL FLATTEN(input => PARSE_JSON(dims.value), outer => TRUE, mode => 'ARRAY') AS resources_and_transfer
   WHERE dims.path = 'data_transfer_and_storage'
-  QUALIFY 
-    ROW_NUMBER () OVER (
-        PARTITION BY source.extraction_end_date, resources_and_transfer.value['sku']::VARCHAR
-            ORDER BY to_timestamp(source._uploaded_at::int) DESC
+  QUALIFY
+    ROW_NUMBER() OVER (
+      PARTITION BY source.extraction_end_date, resources_and_transfer.value['sku']::VARCHAR
+      ORDER BY TO_TIMESTAMP(source._uploaded_at::INT) DESC
     ) = 1
-) 
+)
 
-SELECT * 
+SELECT *
 FROM parsed
--- transform/snowflake-dbt/models/sources/elastic_billing/elastic_billing_resources_by_deployment_source.sql
+++ transform/snowflake-dbt/models/sources/elastic_billing/elastic_billing_resources_by_deployment_source.sql
@@ -1,33 +1,33 @@
-with source as (  
+WITH source AS (
   SELECT *
-  FROM  {{ source('elastic_billing', 'itemized_costs_by_deployment') }}
+  FROM {{ source('elastic_billing', 'itemized_costs_by_deployment') }}
 ),
 
-parsed as (
-  SELECT 
+parsed AS (
+  SELECT
     source.deployment_id,
     source.extraction_start_date,
     source.extraction_end_date,
     resources.value['period']['start']::TIMESTAMP AS resource_start_date,
-    resources.value['period']['end']::TIMESTAMP AS resource_end_date,
-    resources.value['hours']::float AS hours,
-    resources.value['instance_count']::NUMBER AS instance_count,
-    resources.value['kind']::varchar AS kind,
-    resources.value['price']::varchar AS cost,
-    resources.value['name']::varchar AS resource_name,
-    resources.value['price_per_hour']::float AS price_per_hour,
-    resources.value['sku']::varchar AS sku,
-    to_timestamp(source._uploaded_at::int) AS _uploaded_at
+    resources.value['period']['end']::TIMESTAMP   AS resource_end_date,
+    resources.value['hours']::FLOAT               AS hours,
+    resources.value['instance_count']::NUMBER     AS instance_count,
+    resources.value['kind']::VARCHAR              AS kind,
+    resources.value['price']::VARCHAR             AS cost,
+    resources.value['name']::VARCHAR              AS resource_name,
+    resources.value['price_per_hour']::FLOAT      AS price_per_hour,
+    resources.value['sku']::VARCHAR               AS sku,
+    TO_TIMESTAMP(source._uploaded_at::INT)        AS _uploaded_at
   FROM source
-      INNER JOIN LATERAL FLATTEN(input => PARSE_JSON(payload), outer => TRUE) AS dims
-      INNER JOIN LATERAL FLATTEN(input => PARSE_JSON(dims.value), outer => TRUE, mode => 'ARRAY') AS resources
-  WHERE dims.path = 'resources'    
-  QUALIFY 
-    ROW_NUMBER () OVER (
-        PARTITION BY source.extraction_end_date, source.deployment_id, resources.value['sku']::varchar
-            ORDER BY to_timestamp(source._uploaded_at::int) DESC
+  INNER JOIN LATERAL FLATTEN(input => PARSE_JSON(payload), outer => TRUE) AS dims
+  INNER JOIN LATERAL FLATTEN(input => PARSE_JSON(dims.value), outer => TRUE, mode => 'ARRAY') AS resources
+  WHERE dims.path = 'resources'
+  QUALIFY
+    ROW_NUMBER() OVER (
+      PARTITION BY source.extraction_end_date, source.deployment_id, resources.value['sku']::VARCHAR
+      ORDER BY TO_TIMESTAMP(source._uploaded_at::INT) DESC
     ) = 1
 )
 
-SELECT * 
+SELECT *
 FROM parsed
