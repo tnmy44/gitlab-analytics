219d347d5372f771bb543ec68ff61ff38711622a
apiaseczna@gitlab.com <apiaseczna@gitlab.com>
2024-04-30 19:26:47 +0100

    Adding iia

--- transform/snowflake-dbt/models/common/facts_financial/restricted_safe/fct_invoice_item_adjustment.sql
+++ transform/snowflake-dbt/models/common/facts_financial/restricted_safe/fct_invoice_item_adjustment.sql
@@ -3,54 +3,53 @@
     tags=["mnpi"]
 ) }}
 
-
-
-WITH zuora_account AS (
-
-    SELECT *
-    FROM {{ ref('zuora_account_source') }} 
-    WHERE is_deleted = FALSE
-
-), zuora_invoice_item_adjustment AS (
-
 /* grain: lowest grain is a invoice item adjustment made on the invoice */
 
-    SELECT *
-    FROM {{ source('zuora', 'invoice_item_adjustment') }}
-    
-), final AS (
+{{ simple_cte([
+    ('zuora_invoice_item_adjustment_source', 'zuora_invoice_item_adjustment_source'),
+    ('zuora_account_source', 'zuora_account_source')
+]) }},
 
-SELECT
-   -- primary key 
-      zuora_invoice_item_adjustment.id                          		   AS invoice_item_adjustment_id,
+zuora_account AS (
 
-   -- keys
-      zuora_invoice_item_adjustment.adjustmentnumber                   AS invoice_item_adjustment_number,
-      zuora_invoice_item_adjustment.accountid                  		     AS account_id,
-      zuora_invoice_item_adjustment.invoiceid                  		     AS invoice_id,
-      zuora_invoice_item_adjustment.accountingperiodid                 AS accounting_period_id,
+  SELECT *
+  FROM zuora_account_source
+  WHERE is_deleted = FALSE
+
+),
+
+final AS (
+
+  SELECT
+    -- primary key 
+    zuora_invoice_item_adjustment_source.invoice_item_adjustment_id,
+
+    -- keys
+    zuora_invoice_item_adjustment_source.invoice_item_adjustment_number,
+    zuora_invoice_item_adjustment_source.account_id,
+    zuora_invoice_item_adjustment_source.invoice_id,
+    zuora_invoice_item_adjustment_source.accounting_period_id,
 
 
-   -- invoice item adjustment dates
-      zuora_invoice_item_adjustment.adjustmentdate                     AS invoice_item_adjustment_date,
-     {{ get_date_id('zuora_invoice_item_adjustment.adjustmentdate') }} AS invoice_item_adjustment_date_id,
+    -- invoice item adjustment dates
+    zuora_invoice_item_adjustment_source.invoice_item_adjustment_date,
+    {{ get_date_id('zuora_invoice_item_adjustment_source.adjustmentdate') }} AS invoice_item_adjustment_date_id,
 
-   -- additive fields
-      zuora_invoice_item_adjustment.amount                        	   AS invoice_item_adjustment_amount,
-      zuora_invoice_item_adjustment.status                      		   AS invoice_item_adjustment_status,
-      zuora_invoice_item_adjustment.type                     		       AS invoice_item_adjustment_type
+    -- additive fields
+    zuora_invoice_item_adjustment_source.invoice_item_adjustment_amount,
+    zuora_invoice_item_adjustment_source.invoice_item_adjustment_status,
+    zuora_invoice_item_adjustment_source.invoice_item_adjustment_type
 
 
-FROM zuora_invoice_item_adjustment
-INNER JOIN zuora_account
-  ON zuora_invoice_item_adjustment.accountid = zuora_account.account_id
+  FROM zuora_invoice_item_adjustment_source
+  INNER JOIN zuora_account
+    ON zuora_invoice_item_adjustment_source.account_id = zuora_account.account_id
 )
 
 {{ dbt_audit(
 cte_ref="final",
 created_by="@apiaseczna",
 updated_by="@apiaseczna",
-created_date="2024-04-23",
-updated_date="2024-04-23"
+created_date="2024-04-30",
+updated_date="2024-04-30"
 ) }}
-
