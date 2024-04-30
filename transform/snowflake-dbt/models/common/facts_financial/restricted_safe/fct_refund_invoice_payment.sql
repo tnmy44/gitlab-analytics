0a68e2cf22229612a6ef900f6c3f52e85e50533f
apiaseczna@gitlab.com <apiaseczna@gitlab.com>
2024-04-30 16:02:59 +0100

    Adding refund invoice payment

--- transform/snowflake-dbt/models/common/facts_financial/restricted_safe/fct_refund_invoice_payment.sql
+++ transform/snowflake-dbt/models/common/facts_financial/restricted_safe/fct_refund_invoice_payment.sql
@@ -3,54 +3,53 @@
     tags=["mnpi"]
 ) }}
 
-
-
-WITH zuora_account AS (
-
-    SELECT *
-    FROM {{ ref('zuora_account_source') }} 
-    WHERE is_deleted = FALSE
-
-), zuora_refund_invoice_payment AS (
-
 /* grain: lowest grain a refund made on the invoice */
 
-    SELECT *
-    FROM {{ source('zuora', 'refund_invoice_payment') }}
-    
-), final AS (
+{{ simple_cte([
+    ('zuora_refund_invoice_payment_source', 'zuora_refund_invoice_payment_source'),
+    ('zuora_account_source', 'zuora_account_source')
+]) }},
 
-SELECT
-   -- primary key 
-      zuora_refund_invoice_payment.id                          		 AS refund_invoice_payment_id,
+zuora_account AS (
 
-   -- keys
-      zuora_refund_invoice_payment.invoiceid		                   AS invoice_id,
-     zuora_refund_invoice_payment.refundid               		       AS refund_id,  
-      zuora_refund_invoice_payment.paymentid               		     AS payment_id,     
-      zuora_refund_invoice_payment.accountid                  		 AS account_id,
-      zuora_refund_invoice_payment.accountingperiodid              AS accounting_period_id,
+  SELECT *
+  FROM zuora_account_source
+  WHERE is_deleted = FALSE
+
+),
+
+final AS (
+
+  SELECT
+    -- primary key 
+    zuora_refund_invoice_payment_source.refund_invoice_payment_id,
+
+    -- keys
+    zuora_refund_invoice_payment_source.invoice_id,
+    zuora_refund_invoice_payment_source.refund_id,
+    zuora_refund_invoice_payment_source.payment_id,
+    zuora_refund_invoice_payment_source.account_id,
+    zuora_refund_invoice_payment_source.accounting_period_id,
 
 
-   -- refund invoice payment dates
-      zuora_refund_invoice_payment.createddate                     AS refund_invoice_payment_date,
-     {{ get_date_id('zuora_refund_invoice_payment.createddate') }} AS refund_invoice_payment_date_id,
+    -- refund invoice payment dates
+    zuora_refund_invoice_payment_source.refund_invoice_payment_date,
+    {{ get_date_id('zuora_refund_invoice_payment_source.refund_invoice_payment_date') }} AS refund_invoice_payment_date_id,
 
-   -- additive fields
-      zuora_refund_invoice_payment.refundamount                    AS refund_invoice_payment_amount
+    -- additive fields
+    zuora_refund_invoice_payment_source.refund_invoice_payment_amount
 
 
 
-FROM zuora_refund_invoice_payment
-INNER JOIN zuora_account
-  ON zuora_refund_invoice_payment.accountid = zuora_account.account_id
+  FROM zuora_refund_invoice_payment_source
+  INNER JOIN zuora_account
+    ON zuora_refund_invoice_payment_source.account_id = zuora_account.account_id
 )
 
 {{ dbt_audit(
 cte_ref="final",
 created_by="@apiaseczna",
 updated_by="@apiaseczna",
-created_date="2024-04-23",
-updated_date="2024-04-23"
+created_date="2024-04-30",
+updated_date="2024-04-30"
 ) }}
-
