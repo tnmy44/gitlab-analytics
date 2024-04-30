3e36632f5c6de6480e5dcf05f690e69266bfe8ba
apiaseczna@gitlab.com <apiaseczna@gitlab.com>
2024-04-30 17:55:04 +0100

    Adding cba

--- transform/snowflake-dbt/models/common/facts_financial/restricted_safe/fct_credit_balance_adjustment.sql
+++ transform/snowflake-dbt/models/common/facts_financial/restricted_safe/fct_credit_balance_adjustment.sql
@@ -3,54 +3,53 @@
     tags=["mnpi"]
 ) }}
 
-
-
-WITH zuora_account AS (
-
-    SELECT *
-    FROM {{ ref('zuora_account_source') }} 
-    WHERE is_deleted = FALSE
-
-), zuora_credit_balance_adjustment AS (
-
 /* grain: lowest grain is a credit balance adjustment made on the invoice or account */
 
-    SELECT *
-    FROM {{ source('zuora', 'credit_balance_adjustment') }}
-    
-), final AS (
+{{ simple_cte([
+    ('zuora_credit_balance_adjustment_source', 'zuora_credit_balance_adjustment_source'),
+    ('zuora_account_source', 'zuora_account_source')
+]) }},
 
-SELECT
-   -- primary key 
-      zuora_credit_balance_adjustment.id                          		   AS credit_balance_adjustment_id,
+zuora_account AS (
 
-   -- keys
-      zuora_credit_balance_adjustment.number                             AS credit_balance_adjustment_number,
-      zuora_credit_balance_adjustment.accountid                  		     AS account_id,
-      zuora_credit_balance_adjustment.invoiceid                  		     AS invoice_id,
-      zuora_credit_balance_adjustment.accountingperiodid                 AS accounting_period_id,
+  SELECT *
+  FROM zuora_account_source
+  WHERE is_deleted = FALSE
+
+),
+
+final AS (
+
+  SELECT
+    -- primary key 
+    zuora_credit_balance_adjustment_source.credit_balance_adjustment_id,
+
+    -- keys
+    zuora_credit_balance_adjustment_source.credit_balance_adjustment_number,
+    zuora_credit_balance_adjustment_source.account_id,
+    zuora_credit_balance_adjustment_source.invoice_id,
+    zuora_credit_balance_adjustment_source.accounting_period_id,
 
 
-   -- credit balance adjustment dates
-      zuora_credit_balance_adjustment.adjustmentdate                     AS credit_balance_adjustment_date,
-     {{ get_date_id('zuora_credit_balance_adjustment.adjustmentdate') }} AS credit_balance_adjustment_date_id,
+    -- credit balance adjustment dates
+    zuora_credit_balance_adjustment_source.credit_balance_adjustment_date,
+    {{ get_date_id('zuora_credit_balance_adjustment_source.credit_balance_adjustment_date') }} AS credit_balance_adjustment_date_id,
 
-   -- additive fields
-      zuora_credit_balance_adjustment.amount                        	   AS credit_balance_adjustment_amount,
-      zuora_credit_balance_adjustment.status                      		   AS credit_balance_adjustment_status,
-      zuora_credit_balance_adjustment.type                     		       AS credit_balance_adjustment_type
+    -- additive fields
+    zuora_credit_balance_adjustment_source.credit_balance_adjustment_amount,
+    zuora_credit_balance_adjustment_source.credit_balance_adjustment_status,
+    zuora_credit_balance_adjustment_source.credit_balance_adjustment_type
 
 
-FROM zuora_credit_balance_adjustment
-INNER JOIN zuora_account
-  ON zuora_credit_balance_adjustment.accountid = zuora_account.account_id
+  FROM zuora_credit_balance_adjustment_source
+  INNER JOIN zuora_account
+    ON zuora_credit_balance_adjustment_source.account_id = zuora_account.account_id
 )
 
 {{ dbt_audit(
 cte_ref="final",
 created_by="@apiaseczna",
 updated_by="@apiaseczna",
-created_date="2024-04-23",
-updated_date="2024-04-23"
+created_date="2024-04-30",
+updated_date="2024-04-30"
 ) }}
-
