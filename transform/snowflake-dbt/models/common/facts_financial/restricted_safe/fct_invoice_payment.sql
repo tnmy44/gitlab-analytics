f9810d7721738b7c8451ad7c576382aa27110c71
apiaseczna@gitlab.com <apiaseczna@gitlab.com>
2024-04-30 12:09:31 +0100

    Updating invoice payment

--- transform/snowflake-dbt/models/common/facts_financial/restricted_safe/fct_invoice_payment.sql
+++ transform/snowflake-dbt/models/common/facts_financial/restricted_safe/fct_invoice_payment.sql
@@ -3,53 +3,51 @@
     tags=["mnpi"]
 ) }}
 
+/* grain: lowest grain: a payment applied to the invoice */
 
+{{ simple_cte([
+    ('zuora_invoice_payment_source', 'zuora_invoice_payment_source'),
+    ('zuora_account_source', 'zuora_account_source')
+]) }},
 
-WITH zuora_account AS (
+zuora_account AS (
 
     SELECT *
-    FROM {{ ref('zuora_account_source') }} 
+    FROM zuora_account_source
     WHERE is_deleted = FALSE
-
-), zuora_invoice_payment AS (
-
-/* grain: lowest grain a payment applied to the invoice */
-
-    SELECT *
-    FROM {{ source('zuora', 'invoice_payment') }}
     
 ), final AS (
 
 SELECT
    -- primary key 
-      zuora_invoice_payment.id                          		AS invoice_payment_id,
+      zuora_invoice_payment_source.invoice_payment_id,
 
    -- keys
-      zuora_invoice_payment.invoiceid		                    AS invoice_id,
-      zuora_invoice_payment.paymentid               		    AS payment_id,     
-      zuora_invoice_payment.accountid                  		  AS account_id,
-      zuora_invoice_payment.accountingperiodid              AS accounting_period_id,
+      zuora_invoice_payment_source.invoice_id,
+      zuora_invoice_payment_source.payment_id,     
+      zuora_invoice_payment_source.account_id,
+      zuora_invoice_payment_source.accounting_period_id,
 
 
    -- invoice payment dates
-      zuora_invoice_payment.createddate                     AS invoice_payment_date,
-     {{ get_date_id('zuora_invoice_payment.createddate') }} AS invoice_payment_date_id,
+      zuora_invoice_payment_source.invoice_payment_date,
+     {{ get_date_id('zuora_invoice_payment_source.createddate') }} AS invoice_payment_date_id,
 
    -- additive fields
-      zuora_invoice_payment.amount                        	AS invoice_payment_amount
+      zuora_invoice_payment_source.invoice_payment_amount
 
 
 
-FROM zuora_invoice_payment
+FROM zuora_invoice_payment_source
 INNER JOIN zuora_account
-  ON zuora_invoice_payment.accountid = zuora_account.account_id
+  ON zuora_invoice_payment_source.accountid = zuora_account.account_id
 )
 
 {{ dbt_audit(
 cte_ref="final",
 created_by="@apiaseczna",
 updated_by="@apiaseczna",
-created_date="2024-04-23",
-updated_date="2024-04-23"
+created_date="2024-04-30",
+updated_date="2024-04-30"
 ) }}
 
