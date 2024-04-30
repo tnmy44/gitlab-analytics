bd4bd7b8b4c43e1ef9872cec7d11199ca637fee2
apiaseczna@gitlab.com <apiaseczna@gitlab.com>
2024-04-30 16:20:20 +0100

    Updating payment

--- transform/snowflake-dbt/models/common/facts_financial/restricted_safe/fct_payment.sql
+++ transform/snowflake-dbt/models/common/facts_financial/restricted_safe/fct_payment.sql
@@ -3,55 +3,50 @@
     tags=["mnpi"]
 ) }}
 
-
-
-WITH zuora_account AS (
-
-  SELECT *
-  FROM {{ ref('zuora_account_source') }}
-  WHERE is_deleted = FALSE
-
-),
-
-zuora_payment AS (
-
 /* grain: lowest grain one payment, one payment may be split and applied to several invoices */
 
-  SELECT *
-  FROM {{ source('zuora', 'payment') }}
+{{ simple_cte([
+    ('zuora_payment_source', 'zuora_payment_source'),
+    ('zuora_account_source', 'zuora_account_source')
+]) }},
 
-),
+zuora_account AS (
 
-final AS (
+    SELECT *
+    FROM zuora_account_source
+    WHERE is_deleted = FALSE
+    
+), final AS (
 
-  SELECT
-    -- primary key 
-    zuora_payment.id                                                          AS payment_id,
+SELECT
+   -- primary key 
+      zuora_payment_source.payment_id,
 
-    -- keys
-    zuora_payment.paymentnumber                                               AS payment_number,
-    zuora_payment.accountid                                                   AS account_id,
+   -- keys
+      zuora_payment_source.payment_number,     
+      zuora_payment_source.account_id,
 
-    -- payment dates
-    zuora_payment.effectivedate                                               AS payment_date,
-    {{ get_date_id('zuora_payment.effectivedate') }} AS payment_date_id,
+   -- payment dates
+      zuora_payment_source.payment_date,
+     {{ get_date_id('zuora_payment_source.payment_date') }} AS payment_date_id,
 
-    -- additive fields
-    zuora_payment.status                                                      AS payment_status,
-    zuora_payment.type                                                        AS payment_type,
-    zuora_payment.amount                                                      AS payment_amount
+   -- additive fields
+      zuora_payment_source.payment_status,
+      zuora_payment_source.payment_type,
+      zuora_payment_source.payment_amount
 
 
 
-  FROM zuora_payment
-  INNER JOIN zuora_account
-    ON zuora_payment.accountid = zuora_account.account_id
+FROM zuora_payment_source
+INNER JOIN zuora_account
+  ON zuora_payment_source.account_id = zuora_account.account_id
 )
 
 {{ dbt_audit(
 cte_ref="final",
 created_by="@apiaseczna",
 updated_by="@apiaseczna",
-created_date="2024-04-23",
-updated_date="2024-04-23"
+created_date="2024-04-30",
+updated_date="2024-04-30"
 ) }}
+
