c920deffbcf1767be9aa06f6fc75a83db841e9aa
apiaseczna@gitlab.com <apiaseczna@gitlab.com>
2024-04-30 16:18:02 +0100

    Adding refund

--- transform/snowflake-dbt/models/common/facts_financial/restricted_safe/fct_refund.sql
+++ transform/snowflake-dbt/models/common/facts_financial/restricted_safe/fct_refund.sql
@@ -3,57 +3,54 @@
     tags=["mnpi"]
 ) }}
 
+/* grain: lowest grain is a refund made out off an invoice or account */
 
+{{ simple_cte([
+    ('zuora_refund_source', 'zuora_refund_source'),
+    ('zuora_account_source', 'zuora_account_source')
+]) }},
 
-WITH zuora_account AS (
+zuora_account AS (
 
   SELECT *
-  FROM {{ ref('zuora_account_source') }}
+  FROM zuora_account_source
   WHERE is_deleted = FALSE
-
-),
-
-zuora_refund AS (
-
-/* grain: lowest grain is a refund made it off an invoice or account */
-
-  SELECT *
-  FROM {{ source('zuora', 'refund') }}
 
 ),
 
 final AS (
 
-  SELECT
-    -- primary key 
-    zuora_refund.id                                                       AS refund_id,
+SELECT
+   -- primary key 
+      zuora_refund_source.refund_id,
 
-    -- keys
-    zuora_refund.accountid                                                AS account_id,
-    zuora_refund.refundnumber                                             AS refund_number,
-
-
-    -- refund dates
-    zuora_refund.refunddate                                               AS refund_date,
-    {{ get_date_id('zuora_refund.refunddate') }} AS refund_date_id,
-
-    -- additive fields
-    zuora_refund.amount                                                   AS refund_amount,
-    zuora_refund.status                                                   AS refund_status,
-    zuora_refund.type                                                     AS refund_type
+   -- keys
+      zuora_refund_source.account_id,
+      zuora_refund_source.refund_number,
 
 
+   -- refund dates
+      zuora_refund_source.refund_date,
+     {{ get_date_id('zuora_refund_source.refund_date') }} AS refund_date_id,
+
+   -- additive fields
+      zuora_refund_source.refund_amount,
+      zuora_refund_source.refund_status,
+      zuora_refund_source.refund_type
 
 
-  FROM zuora_refund
-  INNER JOIN zuora_account
-    ON zuora_refund.accountid = zuora_account.account_id
+
+
+FROM zuora_refund_source
+INNER JOIN zuora_account
+  ON zuora_refund.account_id = zuora_account.account_id
 )
 
 {{ dbt_audit(
 cte_ref="final",
 created_by="@apiaseczna",
 updated_by="@apiaseczna",
-created_date="2024-04-23",
-updated_date="2024-04-23"
+created_date="2024-04-30",
+updated_date="2024-04-30"
 ) }}
+
