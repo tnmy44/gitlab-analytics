1d456bd6bc72eb5dd432ee3243e76eec2dce1d46
apiaseczna@gitlab.com <apiaseczna@gitlab.com>
2024-05-01 18:42:00 +0100

    Adding sk to refund invoice

--- transform/snowflake-dbt/models/common/facts_financial/restricted_safe/fct_refund_invoice_payment.sql
+++ transform/snowflake-dbt/models/common/facts_financial/restricted_safe/fct_refund_invoice_payment.sql
@@ -3,17 +3,17 @@
     tags=["mnpi"]
 ) }}
 
-/* grain: lowest grain a refund made on the invoice */
+/* grain: lowest grain a refund made off the invoice */
 
 {{ simple_cte([
     ('zuora_refund_invoice_payment_source', 'zuora_refund_invoice_payment_source'),
-    ('zuora_account_source', 'zuora_account_source')
+    ('prep_billing_account', 'prep_billing_account')
 ]) }},
 
 zuora_account AS (
 
   SELECT *
-  FROM zuora_account_source
+  FROM prep_billing_account
   WHERE is_deleted = FALSE
 
 ),
@@ -21,35 +21,38 @@ zuora_account AS (
 final AS (
 
   SELECT
-    -- primary key 
+    --Primary key 
+    {{ dbt_utils.generate_surrogate_key(['zuora_refund_invoice_payment_source.refund_invoice_payment_id']) }} AS refund_invoice_payment_pk,
+
+    --Natural key 
     zuora_refund_invoice_payment_source.refund_invoice_payment_id,
 
-    -- keys
-    zuora_refund_invoice_payment_source.invoice_id,
+    --Foreign keys
+    zuora_refund_invoice_payment_source.dim_invoice_id,
     zuora_refund_invoice_payment_source.refund_id,
     zuora_refund_invoice_payment_source.payment_id,
-    zuora_refund_invoice_payment_source.account_id,
+    zuora_account.dim_billing_account_id,
     zuora_refund_invoice_payment_source.accounting_period_id,
 
 
-    -- refund invoice payment dates
+    --Refund invoice payment dates
     zuora_refund_invoice_payment_source.refund_invoice_payment_date,
     {{ get_date_id('zuora_refund_invoice_payment_source.refund_invoice_payment_date') }} AS refund_invoice_payment_date_id,
 
-    -- additive fields
+    --Additive fields
     zuora_refund_invoice_payment_source.refund_invoice_payment_amount
 
 
 
   FROM zuora_refund_invoice_payment_source
   INNER JOIN zuora_account
-    ON zuora_refund_invoice_payment_source.account_id = zuora_account.account_id
+    ON zuora_refund_invoice_payment_source.account_id = zuora_account.dim_billing_account_id
 )
 
 {{ dbt_audit(
 cte_ref="final",
 created_by="@apiaseczna",
 updated_by="@apiaseczna",
-created_date="2024-04-30",
-updated_date="2024-04-30"
+created_date="2024-05-01",
+updated_date="2024-05-01"
 ) }}
