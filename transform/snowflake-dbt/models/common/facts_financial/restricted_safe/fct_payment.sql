1e222af1c4aaddb6f3a6c2fb9b27feefe20a82fe
apiaseczna@gitlab.com <apiaseczna@gitlab.com>
2024-05-01 12:03:50 +0100

    Updating payment

--- transform/snowflake-dbt/models/common/facts_financial/restricted_safe/fct_payment.sql
+++ transform/snowflake-dbt/models/common/facts_financial/restricted_safe/fct_payment.sql
@@ -7,46 +7,51 @@
 
 {{ simple_cte([
     ('zuora_payment_source', 'zuora_payment_source'),
-    ('zuora_account_source', 'zuora_account_source')
+    ('prep_billing_account', 'prep_billing_account')
 ]) }},
 
 zuora_account AS (
 
-    SELECT *
-    FROM zuora_account_source
-    WHERE is_deleted = FALSE
-    
-), final AS (
+  SELECT *
+  FROM prep_billing_account
+  WHERE is_deleted = FALSE
 
-SELECT
-   -- primary key 
-      zuora_payment_source.payment_id,
+),
 
-   -- keys
-      zuora_payment_source.payment_number,     
-      zuora_payment_source.account_id,
+final AS (
 
-   -- payment dates
-      zuora_payment_source.payment_date,
-     {{ get_date_id('zuora_payment_source.payment_date') }} AS payment_date_id,
+  SELECT
+    --Primary key 
+    zuora_payment_source.payment_id                                                 AS payment_pk,
 
-   -- additive fields
-      zuora_payment_source.payment_status,
-      zuora_payment_source.payment_type,
-      zuora_payment_source.payment_amount
+    --Natural key
+    zuora_payment_source.payment_number,
+
+    --Foreign keys     
+    zuora_account.dim_billing_account_id,
+
+    --Payment dates
+    zuora_payment_source.payment_date,
+    {{ get_date_id('zuora_payment_source.payment_date') }} AS payment_date_id,
+
+    --Degenerative dimensions
+    zuora_payment_source.payment_status,
+    zuora_payment_source.payment_type,
+
+    --Additive fields
+    zuora_payment_source.payment_amount
 
 
 
-FROM zuora_payment_source
-INNER JOIN zuora_account
-  ON zuora_payment_source.account_id = zuora_account.account_id
+  FROM zuora_payment_source
+  INNER JOIN zuora_account
+    ON zuora_payment_source.account_id = zuora_account.zuora_account.dim_billing_account_id
 )
 
 {{ dbt_audit(
 cte_ref="final",
 created_by="@apiaseczna",
 updated_by="@apiaseczna",
-created_date="2024-04-30",
-updated_date="2024-04-30"
+created_date="2024-05-01",
+updated_date="2024-05-01"
 ) }}
-
