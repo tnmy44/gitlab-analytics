4399e15f936f3e3d3231e997ef2d6c8b175d88d8
apiaseczna@gitlab.com <apiaseczna@gitlab.com>
2024-05-01 19:26:05 +0100

    Adding sk to refund

--- transform/snowflake-dbt/models/common/facts_financial/restricted_safe/fct_refund.sql
+++ transform/snowflake-dbt/models/common/facts_financial/restricted_safe/fct_refund.sql
@@ -7,50 +7,53 @@
 
 {{ simple_cte([
     ('zuora_refund_source', 'zuora_refund_source'),
-    ('zuora_account_source', 'zuora_account_source')
+    ('prep_billing_account', 'prep_billing_account')
 ]) }},
 
 zuora_account AS (
 
   SELECT *
-  FROM zuora_account_source
+  FROM prep_billing_account
   WHERE is_deleted = FALSE
 
 ),
 
 final AS (
 
-SELECT
-   -- primary key 
-      zuora_refund_source.refund_id,
+  SELECT
+    --Primary key
+    {{ dbt_utils.generate_surrogate_key(['zuora_refund_source.refund_id']) }} AS refund_pk,
 
-   -- keys
-      zuora_refund_source.account_id,
-      zuora_refund_source.refund_number,
+    --Natural key 
+    zuora_refund_source.refund_id,
 
-
-   -- refund dates
-      zuora_refund_source.refund_date,
-     {{ get_date_id('zuora_refund_source.refund_date') }} AS refund_date_id,
-
-   -- additive fields
-      zuora_refund_source.refund_amount,
-      zuora_refund_source.refund_status,
-      zuora_refund_source.refund_type
+    --Foreign keys
+    zuora_account.dim_billing_account_id,
+    zuora_refund_source.refund_number,
 
 
+    --Refund dates
+    zuora_refund_source.refund_date,
+    {{ get_date_id('zuora_refund_source.refund_date') }} AS refund_date_id,
+
+    --Degenerative dimensions
+    zuora_refund_source.refund_status,
+    zuora_refund_source.refund_type,
+
+    --Additive fields
+    zuora_refund_source.refund_amount
 
 
-FROM zuora_refund_source
-INNER JOIN zuora_account
-  ON zuora_refund_source.account_id = zuora_account.account_id
+
+  FROM zuora_refund_source
+  INNER JOIN zuora_account
+    ON zuora_refund_source.account_id = zuora_account.dim_billing_account_id
 )
 
 {{ dbt_audit(
 cte_ref="final",
 created_by="@apiaseczna",
 updated_by="@apiaseczna",
-created_date="2024-04-30",
-updated_date="2024-04-30"
+created_date="2024-05-01",
+updated_date="2024-05-01"
 ) }}
-
