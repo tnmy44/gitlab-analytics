515400a2467e220c921e5e5842f4086f8d328e33
michellecooper <mcooper@gitlab.com>
2024-05-03 14:57:27 -0700

    create fct_behavior_structured_event_90 and remove legacy models

---
+++ transform/snowflake-dbt/models/common/facts_sales_and_marketing/fct_behavior_structured_event_90.sql
@@ -0,0 +1,33 @@
+{{config(
+
+    materialized='incremental',
+    unique_key='behavior_structured_event_pk',
+    tags=['product'],
+    on_schema_change='sync_all_columns',
+    post_hook=["{{ rolling_window_delete('behavior_at','day',90) }}"],
+    cluster_by=['behavior_at::DATE']
+  )
+
+}}
+
+WITH structured_event_90_days AS (
+   
+    SELECT *
+    FROM {{ ref('fct_behavior_structured_event') }}
+    WHERE behavior_at >= DATEADD('day', -90, CURRENT_DATE)
+    {% if is_incremental() %}
+
+      AND behavior_at >= (SELECT MAX(behavior_at) FROM {{this}})
+
+    {% endif %}
+
+
+)
+
+{{ dbt_audit(
+    cte_ref="structured_event_90_days",
+    created_by="@michellecooper",
+    updated_by="@michellecooper",
+    created_date="2024-05-03",
+    updated_date="2024-05-03"
+) }}
