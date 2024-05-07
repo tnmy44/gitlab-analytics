e3869dc392ef8542594fe0eddd067206f3ec9425
Justin Wong <juwong@gitlab.com>
2024-05-07 11:58:20 -0600

    remove testing reference

--- transform/snowflake-dbt/models/sources/sirt_alert_app/sirt_alertapp_data_source.sql
+++ transform/snowflake-dbt/models/sources/sirt_alert_app/sirt_alertapp_data_source.sql
@@ -1,7 +1,26 @@
 WITH
 source AS (
-  SELECT * FROM
-    {{ source('sirt_alertapp', 'sirt_alertapp_data_testing') }}
+  SELECT
+  alertid::int as alertid,
+  ack_time::int as ack_time,
+  alert_name::varchar as alert_name,
+  alert_time::int as alert_time,
+  assignee::varchar as assignee,
+  change_assignee::varchar as change_assignee,
+  close_alert::int as close_alert,
+  comments::varchar as comments,
+  convert_incident::boolean as convert_incident,
+  to_date(db_date, 'YY-MM-DD') db_date,
+  event_time::int as event_time,
+  feature::varchar as feature,
+  label::varchar as label,
+  mitre_tac::varchar as mitre_tac,
+  mitre_teq::varchar as mitre_teq,
+  sentiment::varchar as sentiment,
+  gcs_uploaded_at::date as gcs_uploaded_at,
+  snowflake_uploaded_at::datetime as snowflake_uploaded_at
+  FROM
+    {{ source('sirt_alertapp', 'sirt_alertapp_data') }}
 ),
 
 dedupped AS (
