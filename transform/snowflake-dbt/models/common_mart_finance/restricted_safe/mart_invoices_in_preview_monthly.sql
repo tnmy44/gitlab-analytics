135408249c9178d2c64dbbd45c5328bd6ffe2a10
apiaseczna@gitlab.com <apiaseczna@gitlab.com>
2024-05-07 13:23:59 +0100

    Updating formatting

--- transform/snowflake-dbt/models/common_mart_finance/restricted_safe/mart_invoices_in_preview_monthly.sql
+++ transform/snowflake-dbt/models/common_mart_finance/restricted_safe/mart_invoices_in_preview_monthly.sql
@@ -7,13 +7,13 @@ WITH basis AS (
 
 /* Determine the count and amount of invoices to be billed in the future aka invoices in preview or pending invoices */
 
-SELECT 
-DATE_TRUNC('month',(DATE(invoice_start_date))) AS period,
-SUM(invoice_amount) AS pending_invoice_amount,
-COUNT(invoice_amount) AS pending_invoice_count
-FROM {{ ref('driveload_pending_invoices_report_source') }} 
-GROUP BY period
-ORDER BY period
+  SELECT
+    DATE_TRUNC('month', (DATE(invoice_start_date))) AS period,
+    SUM(invoice_amount)                             AS pending_invoice_amount,
+    COUNT(invoice_amount)                           AS pending_invoice_count
+  FROM {{ ref('driveload_pending_invoices_report_source') }}
+  GROUP BY period
+  ORDER BY period
 
 ),
 
@@ -21,15 +21,15 @@ final AS (
 
 /* Add fiscal year and quarter */
 
-SELECT
-basis.period,
-dim_date.fiscal_year               AS fiscal_year,
-dim_date.fiscal_quarter_name_fy    AS fiscal_quarter,
-basis.pending_invoice_amount       AS pending_invoice_amount,
-basis.pending_invoice_count        AS pending_invoice_count
-FROM basis
-LEFT JOIN {{ ref('dim_date') }} ON dim_date.date_actual = basis.period
-ORDER BY period
+  SELECT
+    basis.period,
+    dim_date.fiscal_year            AS fiscal_year,
+    dim_date.fiscal_quarter_name_fy AS fiscal_quarter,
+    basis.pending_invoice_amount    AS pending_invoice_amount,
+    basis.pending_invoice_count     AS pending_invoice_count
+  FROM basis
+  LEFT JOIN {{ ref('dim_date') }} ON basis.period = dim_date.date_actual
+  ORDER BY period
 
 )
 
@@ -41,4 +41,3 @@ updated_by="@apiaseczna",
 created_date="2024-05-07",
 updated_date="2024-05-07"
 ) }}
-
