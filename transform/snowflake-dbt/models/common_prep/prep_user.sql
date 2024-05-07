1b05f3be066fd1b3d4af1e817d9cd0728b7e7ce3
Justin Wong <juwong@gitlab.com>
2024-05-07 16:05:56 -0600

    add account age and highest_paid_subscription_plan_id to prep_users

--- transform/snowflake-dbt/models/common_prep/prep_user.sql
+++ transform/snowflake-dbt/models/common_prep/prep_user.sql
@@ -17,35 +17,35 @@
     ('gitlab_dotcom_user_details_source','gitlab_dotcom_user_details_source'),
     ('customers_db_leads_source','customers_db_leads_source')
 
-]) }}, 
+]) }},
 
 email_classification_dedup AS (
 
-  SELECT 
+  SELECT
     *
   FROM email_classification
   QUALIFY ROW_NUMBER() OVER(PARTITION BY domain ORDER BY domain DESC) = 1
 
-), 
+),
 
 closest_provider AS (
 
   SELECT
     source.user_id AS user_id,
     identity.identity_provider AS identity_provider
-  FROM source                                                       
-  LEFT JOIN identity 
+  FROM source
+  LEFT JOIN identity
     ON source.user_id = identity.user_id
-  WHERE 
+  WHERE
     identity.user_id IS NOT NULL
-  QUALIFY ROW_NUMBER() OVER(PARTITION BY source.user_id 
+  QUALIFY ROW_NUMBER() OVER(PARTITION BY source.user_id
       ORDER BY TIMEDIFF(MILLISECONDS,source.created_at,COALESCE(identity.created_at,{{var('infinity_future')}})) ASC) = 1
 
-), 
+),
 
 user_preferences AS (
 
-  SELECT 
+  SELECT
     user_id AS user_id,
     COALESCE(setup_for_company::VARCHAR,'Unknown') AS setup_for_company
   FROM gitlab_dotcom_user_preferences_source
@@ -54,15 +54,15 @@ user_preferences AS (
 
 user_details AS (
 
-  SELECT 
+  SELECT
     user_id AS user_id,
     CASE COALESCE(registration_objective,-1)
-      WHEN 0 THEN 'basics' 
-      WHEN 1 THEN 'move_repository' 
-      WHEN 2 THEN 'code_storage' 
-      WHEN 3 THEN 'exploring' 
-      WHEN 4 THEN 'ci' 
-      WHEN 5 THEN 'other' 
+      WHEN 0 THEN 'basics'
+      WHEN 1 THEN 'move_repository'
+      WHEN 2 THEN 'code_storage'
+      WHEN 3 THEN 'exploring'
+      WHEN 4 THEN 'ci'
+      WHEN 5 THEN 'other'
       WHEN 6 THEN 'joining_team'
       WHEN -1 THEN 'Unknown'
     END AS jobs_to_be_done
@@ -72,7 +72,7 @@ user_details AS (
 
 customer_leads AS (
 
-  SELECT 
+  SELECT
     user_id AS user_id,
     COALESCE(MAX(is_for_business_use)::VARCHAR,'Unknown') AS for_business_use,
     COALESCE(MAX(employees_bucket)::VARCHAR,'Unknown') AS employee_count,
@@ -84,19 +84,25 @@ customer_leads AS (
 
 ),
 
+highest_paid_subscription_plan AS (
+
+    SELECT *
+    FROM {{ ref('gitlab_dotcom_highest_paid_subscription_plan') }}
+
+),
 
 renamed AS (
 
   SELECT
     --surrogate_key
     {{ dbt_utils.generate_surrogate_key(['source.user_id']) }}  AS dim_user_sk,
-    
+
     --natural_key
     source.user_id,
-    
+
     --legacy natural_key to be deprecated during change management plan
     source.user_id AS dim_user_id,
-    
+
     --Other attributes
     source.remember_created_at AS remember_created_at,
     source.sign_in_count AS sign_in_count,
@@ -107,9 +113,10 @@ renamed AS (
     source.updated_at AS updated_at,
     source.is_admin AS is_admin,
     source.state AS user_state,
-    CASE 
+    TIMESTAMPDIFF(DAYS, source.created_at, CURRENT_TIMESTAMP(2)) AS account_age,
+    CASE
       WHEN source.state in ('blocked', 'banned') THEN TRUE
-      ELSE FALSE 
+      ELSE FALSE
     END AS is_blocked_user,
     source.notification_email_domain AS notification_email_domain,
     notification_email_domain.classification AS notification_email_domain_classification,
@@ -123,14 +130,16 @@ renamed AS (
 
     -- Expanded Attributes  (Not Found = Joined Row Not found for the Attribute)
     COALESCE(source.role,'Unknown') AS role,
-    COALESCE(TO_DATE(source.last_activity_on)::VARCHAR,'Unknown') AS last_activity_date,              
-    COALESCE(TO_DATE(source.last_sign_in_at)::VARCHAR,'Unknown')  AS last_sign_in_date,               
-    COALESCE(user_preferences.setup_for_company,'Not Found') AS setup_for_company,               
+    COALESCE(TO_DATE(source.last_activity_on)::VARCHAR,'Unknown') AS last_activity_date,
+    COALESCE(TO_DATE(source.last_sign_in_at)::VARCHAR,'Unknown')  AS last_sign_in_date,
+    COALESCE(user_preferences.setup_for_company,'Not Found') AS setup_for_company,
     COALESCE(user_details.jobs_to_be_done,'Not Found') AS jobs_to_be_done,
-    COALESCE(customer_leads.for_business_use,'Not Found') AS for_business_use,                 
+    COALESCE(customer_leads.for_business_use,'Not Found') AS for_business_use,
     COALESCE(customer_leads.employee_count,'Not Found') AS employee_count,
     COALESCE(customer_leads.country,'Not Found') AS country,
-    COALESCE(customer_leads.state,'Not Found') AS state
+    COALESCE(customer_leads.state,'Not Found') AS state,
+    highest_paid_subscription_plan.highest_paid_subscription_plan_id
+
 
   FROM source
   LEFT JOIN dim_date
@@ -144,13 +153,15 @@ renamed AS (
   LEFT JOIN email_classification_dedup AS commit_email_domain
     ON commit_email_domain.domain = source.commit_email_domain
   LEFT JOIN closest_provider AS closest_provider
-    ON source.user_id = closest_provider.user_id  
+    ON source.user_id = closest_provider.user_id
   LEFT JOIN user_preferences  AS user_preferences
     ON source.user_id = user_preferences.user_id
   LEFT JOIN user_details AS user_details
     ON source.user_id = user_details.user_id
   LEFT JOIN customer_leads AS customer_leads
     ON source.user_id = customer_leads.user_id
+  LEFT JOIN highest_paid_subscription_plan
+    ON source.user_id = highest_paid_subscription_plan.user_id
 
 )
 
