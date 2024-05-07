8e682a43953c9d01beb9d7428ef5a7651595bf6e
apiaseczna@gitlab.com <apiaseczna@gitlab.com>
2024-05-07 16:42:15 +0100

    Adding tests and sections to refunds

--- transform/snowflake-dbt/models/common_mart_finance/restricted_safe/mart_processed_refunds_monthly.sql
+++ transform/snowflake-dbt/models/common_mart_finance/restricted_safe/mart_processed_refunds_monthly.sql
@@ -8,8 +8,12 @@ WITH final AS (
 /* View sum of refund amounts processed monthly; period indicates accounting period in the billing system */
 
   SELECT
+    --Primary key
     DATE_TRUNC('month', (DATE(refund_date))) AS period,
+
+    --Aggregated amounts
     SUM(refund_amount)                       AS refund_amount
+
   FROM {{ ref('fct_refund') }}
   WHERE refund_status = 'Processed'
   GROUP BY period
@@ -22,7 +26,7 @@ WITH final AS (
 cte_ref="final",
 created_by="@apiaseczna",
 updated_by="@apiaseczna",
-created_date="2024-05-03",
-updated_date="2024-05-03"
+created_date="2024-05-07",
+updated_date="2024-05-07"
 ) }}
 
