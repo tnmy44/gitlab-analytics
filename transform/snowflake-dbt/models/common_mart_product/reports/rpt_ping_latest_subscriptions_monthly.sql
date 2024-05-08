506e8a35e15085985dc1e29a37476149620de440
Chris Sharp <csharp@gitlab.com>
2024-05-08 17:54:13 +0100

    filter out 'Add On Services' subs with arr = 0

--- transform/snowflake-dbt/models/common_mart_product/reports/rpt_ping_latest_subscriptions_monthly.sql
+++ transform/snowflake-dbt/models/common_mart_product/reports/rpt_ping_latest_subscriptions_monthly.sql
@@ -119,7 +119,7 @@ Aggregate mart_charge information (used as the basis of truth), this gets rid of
         AND dim_product_detail.product_tier_name != 'Storage'
         -- filter added to fix https://gitlab.com/gitlab-data/analytics/-/issues/19656
         -- updated in https://gitlab.com/gitlab-data/analytics/-/issues/20559
-        AND NOT (dim_product_detail.product_rate_plan_name IN ('True-Up (Annual) - Dedicated - Ultimate', 'Dedicated - GitLab Duo Pro - Monthly', 'Dedicated - Success Plan Services - 1 Year')
+        AND NOT (dim_product_detail.product_category = 'Add On Services'
                 AND arr = 0)        
         AND DATE_TRUNC('MONTH', CURRENT_DATE) > arr_month
       {{ dbt_utils.group_by(n=4)}}
