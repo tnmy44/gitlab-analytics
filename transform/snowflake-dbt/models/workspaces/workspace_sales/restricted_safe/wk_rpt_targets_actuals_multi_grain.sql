a5fb3f1a7777fb7975aea774b09f4d3946e754df
Lis Vinueza <lvinueza@gitlab.com>
2024-05-08 16:44:23 +0000

    Update wk_rpt_targets_actuals_multi_grain.sql

--- transform/snowflake-dbt/models/workspaces/workspace_sales/restricted_safe/wk_rpt_targets_actuals_multi_grain.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_sales/restricted_safe/wk_rpt_targets_actuals_multi_grain.sql
@@ -899,7 +899,7 @@ SELECT
   MAX(CASE 
         WHEN source != 'targets_actuals' AND snapshot_date < CURRENT_DATE() THEN snapshot_date 
         ELSE NULL 
-    END) OVER () AS max_snapshot_date,
+    END) OVER () AS max_snapshot_date, -- We want to ensure we have the max_snapshot_date that comes from the actuals in every row but excluding the future dates we have in the targets data 
   FLOOR((DATEDIFF(day, current_first_day_of_fiscal_quarter, max_snapshot_date) / 7)) 
                                                                         AS most_recent_snapshot_week
 FROM unioned
