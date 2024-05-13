80774ad0816fe291742feed3a1460a43f8516d89
Matthew Petersen <mpetersen@gitlab.com>
2024-05-13 00:25:39 +0000

    fix alias

--- transform/snowflake-dbt/models/workspaces/workspace_product/wk_unify_ai_reporting.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_product/wk_unify_ai_reporting.sql
@@ -505,7 +505,7 @@ p.ping_created_date_week AS date_day,
 'chat' AS ai_feature,
 p.ping_product_tier AS plan,
 'External' AS internal_or_external,
-p.delivery_type,
+p.ping_deployment_type AS delivery_type,
 COALESCE(p.metric_value,0)::INT,
 'WAU' AS metric
 FROM 
