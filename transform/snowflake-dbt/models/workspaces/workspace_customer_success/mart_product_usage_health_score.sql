b0e2820f654c5774dde76db99574f750fd0b54df
Jong Lee <jonglee@gitlab.com>
2024-05-17 03:24:42 +0000

    Update 2 files
    - /transform/snowflake-dbt/models/common_mart_product/reports/rpt_product_usage_health_score.sql
    - /transform/snowflake-dbt/models/workspaces/workspace_customer_success/mart_product_usage_health_score.sql

--- transform/snowflake-dbt/models/workspaces/workspace_customer_success/mart_product_usage_health_score.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_customer_success/mart_product_usage_health_score.sql
@@ -8,7 +8,7 @@
 {{ simple_cte([
     ('paid_user_metrics', 'mart_product_usage_paid_user_metrics_monthly'),
     ('dim_crm_account', 'dim_crm_account'),
-    ('mart_arr', 'mart_arr')
+    ('mart_arr_all', 'mart_arr_with_zero_dollar_charges')
 ])}}
 
 , joined AS (
@@ -270,10 +270,11 @@
 FROM paid_user_metrics
 LEFT JOIN dim_crm_account
     ON paid_user_metrics.dim_crm_account_id = dim_crm_account.dim_crm_account_id
-LEFT JOIN mart_arr
-    ON paid_user_metrics.dim_subscription_id_original = mart_arr.dim_subscription_id_original
-    AND paid_user_metrics.snapshot_month = mart_arr.arr_month
-    AND paid_user_metrics.delivery_type = mart_arr.product_delivery_type
+LEFT JOIN mart_arr_all
+    ON paid_user_metrics.dim_subscription_id_original = mart_arr_all.dim_subscription_id_original
+    AND paid_user_metrics.snapshot_month = mart_arr_all.arr_month
+    AND paid_user_metrics.delivery_type = mart_arr_all.product_delivery_type
+    AND mart_arr_all.product_category = 'Base Products'
 WHERE paid_user_metrics.license_user_count != 0
 qualify row_number() OVER (PARTITION BY paid_user_metrics.snapshot_month, instance_identifier ORDER BY paid_user_metrics.ping_created_at DESC NULLs last) = 1
 
@@ -282,7 +283,7 @@ qualify row_number() OVER (PARTITION BY paid_user_metrics.snapshot_month, instan
 {{ dbt_audit(
     cte_ref="joined",
     created_by="@jngCES",
-    updated_by="@bbutterfield",
+    updated_by="@jonglee1218",
     created_date="2023-03-30",
-    updated_date="2023-08-05"
+    updated_date="2024-05-16"
 ) }}
