560cb2c4ad8b0da292d6101e80245b21dd2f8779
Robert Kohnke <rkohnke@gitlab.com>
2024-05-20 18:25:13 +0000

    macro update

--- transform/snowflake-dbt/macros/common/utm_parsing.sql
+++ transform/snowflake-dbt/macros/common/utm_parsing.sql
@@ -20,5 +20,5 @@ CASE
             CASE WHEN uses_new_utm_format THEN SPLIT_PART({{utm_column_name}} , '_', 1) END AS utm_content_offer,
             CASE WHEN uses_new_utm_format THEN SPLIT_PART({{utm_column_name}} , '_', 2) END AS utm_content_asset_type,
             CASE WHEN uses_new_utm_format THEN SPLIT_PART({{utm_column_name}} , '_', 3) END AS utm_content_industry,
-
+        END
 {%- endmacro -%}
