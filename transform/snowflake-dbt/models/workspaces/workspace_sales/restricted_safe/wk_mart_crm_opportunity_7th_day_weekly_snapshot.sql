a8fd1a595e5bc8e13468887f9111642af8a85481
Lis Vinueza <lvinueza@gitlab.com>
2024-05-20 18:37:27 +0000

    Update wk_mart_crm_opportunity_7th_day_weekly_snapshot.sql

--- transform/snowflake-dbt/models/workspaces/workspace_sales/restricted_safe/wk_mart_crm_opportunity_7th_day_weekly_snapshot.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_sales/restricted_safe/wk_mart_crm_opportunity_7th_day_weekly_snapshot.sql
@@ -202,7 +202,7 @@ final AS (
     fulfillment_partner.crm_account_name AS fulfillment_partner_name,
 
     -- Dates
-    dim_date.current_day_name,  
+    DAYNAME(dim_date.current_date_actual) AS current_day_name,  --need to add this field to date_details
     dim_date.current_date_actual,
     dim_date.current_fiscal_year,
     dim_date.current_first_day_of_fiscal_year,
@@ -212,9 +212,13 @@ final AS (
     dim_date.current_day_of_month,
     dim_date.current_day_of_fiscal_quarter,
     dim_date.current_day_of_fiscal_year,
-    dim_date.current_first_day_of_week,
-    dim_date.current_week_of_fiscal_quarter_normalised,
-    dim_date.current_week_of_fiscal_quarter,
+    CASE WHEN current_day_name = 'Sun' THEN dim_date.current_date_actual
+      ELSE DATEADD('day', -1, DATE_TRUNC('week', dim_date.current_date_actual)) END     
+                                                                    AS current_first_day_of_week,
+    FLOOR((DATEDIFF(day, dim_date.current_first_day_of_fiscal_quarter, dim_date.current_date_actual) / 7))                   
+                                                                    AS current_week_of_fiscal_quarter_normalised,
+    FLOOR((DATEDIFF(day, dim_date.current_first_day_of_fiscal_quarter, dim_date.current_date_actual) / 7)) 
+                                                                    AS current_week_of_fiscal_quarter,
     created_date.date_actual                                        AS created_date,
     created_date.first_day_of_month                                 AS created_month,
     created_date.first_day_of_fiscal_quarter                        AS created_fiscal_quarter_date,
@@ -362,9 +366,10 @@ final AS (
     dim_date.week_of_fiscal_quarter_normalised                      AS snapshot_week_of_fiscal_quarter_normalised,
     dim_date.is_first_day_of_fiscal_quarter_week                    AS snapshot_is_first_day_of_fiscal_quarter_week,
     dim_date.days_until_last_day_of_month                           AS snapshot_days_until_last_day_of_month,
-    dim_date.week_of_fiscal_quarter                                 AS snapshot_week_of_fiscal_quarter,
+    FLOOR((DATEDIFF(day, dim_date.first_day_of_fiscal_quarter, fct_crm_opportunity.snapshot_date) / 7)) 
+                                                                    AS snapshot_week_of_fiscal_quarter,
 
-    -- Additive fields
+    --additive fields
     fct_crm_opportunity.positive_booked_deal_count_in_snapshot_quarter,
     fct_crm_opportunity.positive_booked_net_arr_in_snapshot_quarter,
     fct_crm_opportunity.positive_open_deal_count_in_snapshot_quarter,
