7626c8f04e05a03502c1fcf36504478f22324a02
jjstark <jstark@gitlab.com>
2024-05-21 13:38:19 -0700

    debug duo's test

--- extract/test/test_big_query_exporter.py
+++ extract/test/test_big_query_exporter.py
@@ -1,29 +1,31 @@
 import pytest
 from unittest.mock import mock_open, patch
-
-
+from extract.bigquery.src.bigquery_export import (
+    get_export,
+    set_bucket_path,
+    get_partition,
+    get_billing_data_query,
+)
 @pytest.fixture
-# def mock_file():
-#     data = 'stream: {project: test_project, credentials: test_creds, exports: [{name: test_export, export_query: "SELECT * FROM table"}]}'
-#     return mock_open(read_data=data)
+def mock_file():
+    data = 'stream: {project: test_project, credentials: test_creds, exports: [{name: test_export, export_query: "SELECT * FROM table"}]}'
+    return mock_open(read_data=data)
 
 
-# def test_get_export(mock_file, monkeypatch):
-#     from extract.bigquery.src.bigquery_export import get_export
+def test_get_export(mock_file, monkeypatch):
 
-#     with patch("builtins.open", mock_file):
-#         project, creds, export = get_export("test_export", "config.yaml")
-#         assert project == "test_project"
-#         assert creds == "test_creds"
-#         assert export == {"name": "test_export", "export_query": "SELECT * FROM table"}
+    with patch("builtins.open", mock_file):
+        project, creds, export = get_export("test_export", "config.yaml")
+        assert project == "test_project"
+        assert creds == "test_creds"
+        assert export == {"name": "test_export", "export_query": "SELECT * FROM table"}
 
-#     with patch("builtins.open", mock_file):
-#         project, creds, export = get_export(None, "config.yaml")
-#         assert export == {"name": "test_export", "export_query": "SELECT * FROM table"}
+    with patch("builtins.open", mock_file):
+        project, creds, export = get_export(None, "config.yaml")
+        assert export == {"name": "test_export", "export_query": "SELECT * FROM table"}
 
 
 def test_set_bucket_path():
-    from extract.bigquery.src.bigquery_export import set_bucket_path
 
     export = {"bucket_path": "gs://test/path"}
     set_bucket_path("master", export)
@@ -39,10 +41,13 @@ def test_set_bucket_path():
     [(None, "2023-04-01"), ("d", "2023-04-01"), ("m", "2023-04")],
 )
 def test_get_partition(partition_date_part, expected_partition, monkeypatch):
-    from extract.bigquery.src.bigquery_export import get_partition
 
     monkeypatch.setenv("EXPORT_DATE", "2023-04-01")
-    export = {}
+    export = {
+        "bucket_path": "gs://test/path",
+        "export_query": 'SELECT * FROM table WHERE date = "{EXPORT_DATE}"',
+        "partition_date_part": "d",
+    }
     if partition_date_part:
         export["partition_date_part"] = partition_date_part
     partition = get_partition(export)
@@ -57,8 +62,6 @@ def test_get_partition(partition_date_part, expected_partition, monkeypatch):
     ],
 )
 def test_get_billing_data_query(branch, expected_path, monkeypatch):
-    from extract.bigquery.src.bigquery_export import get_billing_data_query
-
     monkeypatch.setenv("EXPORT_DATE", "2023-04-01")
     monkeypatch.setenv("GIT_BRANCH", branch)
     export = {
