35b182d72f8deb6d1db9180466c4801a13610d1f
Peter Empey <pempey@gitlab.com>
2024-05-21 07:33:42 -0600

    first draft

--- transform/snowflake-dbt/models/common/facts_product_and_engineering/fct_ping_instance_metric_weekly.sql
+++ transform/snowflake-dbt/models/common/facts_product_and_engineering/fct_ping_instance_metric_weekly.sql
@@ -1,8 +1,11 @@
+{% set filter_date = (run_started_at - modules.datetime.timedelta(days=8)).strftime('%Y-%m-%d') %}
+
 {{ config(
     tags=["product", "mnpi_exception"],
     materialized = "incremental",
     unique_key = "ping_instance_metric_id",
-    on_schema_change="sync_all_columns"
+    on_schema_change="sync_all_columns",
+    post_hook=["DELETE FROM {{ this }} WHERE is_current_ping = FALSE"]
 ) }}
 
 {{ simple_cte([
@@ -20,7 +23,7 @@
       TRY_TO_NUMBER(metric_value::TEXT) AS metric_value
     FROM {{ ref('fct_ping_instance_metric') }}
     {% if is_incremental() %}
-    WHERE uploaded_at >= (SELECT MAX(uploaded_at) FROM {{this}})
+    WHERE uploaded_at > '{{ filter_date }}'
     {% endif %}
 
 ),
@@ -29,23 +32,29 @@ time_frame_7_day_metrics AS (
 
     SELECT
       fct_ping_instance_metric.*,
-      dim_ping_metric.time_frame
+      dim_ping_metric.time_frame,
+      dim_ping_instance.is_last_ping_of_week AS is_current_ping
     FROM fct_ping_instance_metric
     INNER JOIN dim_ping_metric
       ON fct_ping_instance_metric.metrics_path = dim_ping_metric.metrics_path
     INNER JOIN dim_ping_instance
       ON fct_ping_instance_metric.dim_ping_instance_id = dim_ping_instance.dim_ping_instance_id
-    WHERE time_frame = ('7d')
-      AND is_last_ping_of_week = TRUE
-      AND has_timed_out = FALSE
-      AND metric_value IS NOT NULL
+    WHERE dim_ping_metric.time_frame = ('7d')
+      {% if is_incremental() %}
+      AND dim_ping_instance.next_ping_uploaded_at > '{{ filter_date }}'
+      {% else %}
+      -- Only filtered on a full refresh as the post_hook DELETE step applies the filter during an incremental build
+      AND dim_ping_instance.is_last_ping_of_week = TRUE
+      {% endif %}
+      AND fct_ping_instance_metric.has_timed_out = FALSE
+      AND fct_ping_instance_metric.metric_value IS NOT NULL
 
 )
 
 {{ dbt_audit(
     cte_ref="time_frame_7_day_metrics",
     created_by="@iweeks",
-    updated_by="@iweeks",
+    updated_by="@pempey",
     created_date="2022-08-08",
-    updated_date="2024-03-21"
+    updated_date="2024-05-21"
 ) }}
