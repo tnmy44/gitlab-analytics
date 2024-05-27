c9488559a1f51c1f5d4cf1ff1c3c7b4b82bee2b6
Paul Armstrong <parmstrong@gitlab.com>
2024-05-27 14:06:33 +0200

    Remove unused variable. Add needed variable.

--- transform/snowflake-dbt/macros/warehouse/gdpr_bulk_delete.sql
+++ transform/snowflake-dbt/macros/warehouse/gdpr_bulk_delete.sql
@@ -1,4 +1,4 @@
-{% macro gdpr_bulk_delete(run_queries=False) %}
+{% macro gdpr_bulk_delete() %}
 
 {%- call statement('gdpr_deletions', fetch_result=True) %}
     select SHA2(TRIM(LOWER(email_address)))
@@ -13,7 +13,7 @@
 
       {% for data_row in values %}
 
-        {{ gdpr_delete(data_row[0])}}
+        {{ gdpr_delete(data_row[0], run_queries='True')}}
         {%- call statement('remove_data', fetch_result=True) %}
             DELETE FROM {{ source('driveload', 'gdpr_delete_requests') }}
             WHERE  SHA2(TRIM(LOWER(email_address))) = '{{data_row[0]}}'
