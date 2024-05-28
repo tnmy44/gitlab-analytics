f56ccea753435ac6be7028c4023f46ffff75af6d
Chris Sharp <csharp@gitlab.com>
2024-05-28 13:49:08 +0100

    resolve conflicts

--- transform/snowflake-dbt/models/common_prep/restricted_safe/prep_crm_opportunity.sql
+++ transform/snowflake-dbt/models/common_prep/restricted_safe/prep_crm_opportunity.sql
@@ -1485,8 +1485,8 @@ LEFT JOIN cw_base
 {{ dbt_audit(
     cte_ref="final",
     created_by="@michellecooper",
-    updated_by="@lisvinueza",
+    updated_by="@chrissharp",
     created_date="2022-02-23",
-    updated_date="2024-05-09"
+    updated_date="2024-05-28"
 ) }}
 
