4260eda3f859b0992021fd554a145689dd74ac85
Rakhi Reddy <rakreddy@gitlab.com>
2024-05-28 12:32:58 +0530

    Updated prep_pto logic

--- transform/snowflake-dbt/models/common_prep/prep_pto.sql
+++ transform/snowflake-dbt/models/common_prep/prep_pto.sql
@@ -1,6 +1,32 @@
-WITH pto AS (
+WITH source AS (
+
+    SELECT *
+    FROM {{ ref('gitlab_pto_source') }}
+),
+
+map AS (
+
   SELECT *
-  FROM {{ ref('gitlab_pto') }}
+  FROM {{ ref('map_employee_id') }}
+),
+
+report AS (
+  SELECT
+    {{ dbt_utils.star(from=ref('gitlab_pto_source'), except=["HR_EMPLOYEE_ID"]) }},
+    CASE pto_status
+      WHEN 'AP' THEN 'Approved'
+      WHEN 'DN' THEN 'Denied'
+      WHEN 'RQ' THEN 'Requested'
+      WHEN 'CN' THEN 'Cancelled'
+    END AS pto_status_name,
+    map.wk_employee_id AS hr_employee_id
+  FROM source
+  LEFT JOIN map
+    ON source.hr_employee_id = map.bhr_employee_id
+),
+pto AS (
+  SELECT *
+  FROM report
 ),
 prep_date AS (
   SELECT *
