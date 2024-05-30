8f3c1a3380713b86ee83f52d4cbf294329113863
michellecooper <mcooper@gitlab.com>
2024-05-30 15:57:53 -0700

    remove audit cols

--- transform/snowflake-dbt/models/common/facts_sales_and_marketing/fct_behavior_structured_event_90.sql
+++ transform/snowflake-dbt/models/common/facts_sales_and_marketing/fct_behavior_structured_event_90.sql
@@ -12,15 +12,23 @@
 
 WITH structured_event_90_days AS (
    
-    SELECT *
-    FROM {{ ref('fct_behavior_structured_event') }}
-    WHERE behavior_at >= DATEADD('day', -90, CURRENT_DATE)
+    SELECT
+    {{ 
+      dbt_utils.star(from=ref('fct_behavior_structured_event'), 
+      except=[
+        'CREATED_BY',
+        'UPDATED_BY',
+        'MODEL_CREATED_DATE',
+        'MODEL_UPDATED_DATE',
+        'DBT_CREATED_AT',
+        'DBT_UPDATED_AT'
+        ]) 
+    }}
+  FROM {{ ref('fct_behavior_structured_event') }}
+  WHERE DATE_TRUNC(MONTH, behavior_at) >= DATEADD(DAY, -90, DATE_TRUNC(DAY, CURRENT_DATE))
     {% if is_incremental() %}
-
-      AND behavior_at >= (SELECT MAX(behavior_at) FROM {{this}})
-
+      AND behavior_at >= (SELECT MAX(behavior_at) FROM {{ this }})
     {% endif %}
-
 
 )
 
