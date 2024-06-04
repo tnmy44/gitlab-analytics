37502fcd683bb8535acd4135e3267f081fa74dae
Utkarsh <usinghal@gitlab.com>
2024-06-04 18:26:52 +0530

    Update oldest_subscription JOIN logic

--- transform/snowflake-dbt/models/common/dimensions_shared/dim_subscription.sql
+++ transform/snowflake-dbt/models/common/dimensions_shared/dim_subscription.sql
@@ -48,26 +48,7 @@ WITH prep_amendment AS (
     GROUP BY 1
     HAVING nbr_records > 1
 
-), oldest_subscription_in_cohort AS (
-  -- oldest subs is being filltered with the join condition
-  SELECT 
-
-    -- Oldest subcription cohort keys
-    subscription.dim_subscription_id AS dim_oldest_subscription_in_cohort_id,
-    subscription.dim_crm_account_id AS dim_oldest_crm_account_in_cohort_id,
-
-    -- Oldest subcription cohort information
-    subscription_lineage.subscription_cohort_month AS oldest_subscription_cohort_month
-  
-  FROM subscription
-  LEFT JOIN subscription_lineage
-    ON subscription_lineage.oldest_subscription_in_cohort = subscription.subscription_name_slugify
-  WHERE subscription.subscription_status IN ('Active', 'Cancelled') -- Is this required?
-    AND subscription.subscription_name_slugify NOT IN (SELECT subscription_name_slugify FROM data_quality_filter_subscription_slugify)
-  QUALIFY ROW_NUMBER() OVER (PARTITION BY dim_oldest_subscription_in_cohort_id ORDER BY oldest_subscription_cohort_month DESC) = 1 -- Remove duplicates
-
-
-), final AS (
+), joined AS (
 
   SELECT
     --Surrogate Key
@@ -90,10 +71,6 @@ WITH prep_amendment AS (
     subscription.dim_crm_opportunity_id_current_open_renewal,
     subscription.dim_crm_opportunity_id_closed_lost_renewal,
     {{ get_keyed_nulls('prep_amendment.dim_amendment_id') }}                        AS dim_amendment_id_subscription,
-
-    -- Oldest subcription cohort keys
-    oldest_subscription.dim_oldest_subscription_in_cohort_id,
-    oldest_subscription.dim_oldest_crm_account_in_cohort_id,
 
     --Subscription Information
     subscription.created_by_id,
@@ -126,9 +103,6 @@ WITH prep_amendment AS (
     subscription.was_purchased_through_reseller,
     subscription.multi_year_deal_subscription_linkage,
 
-    -- Oldest subcription cohort information
-    oldest_subscription.oldest_subscription_cohort_month,
-
     --Date Information
     subscription.subscription_start_date,
     subscription.subscription_end_date,
@@ -157,12 +131,106 @@ WITH prep_amendment AS (
   FROM subscription
   LEFT JOIN subscription_lineage
     ON subscription_lineage.subscription_name_slugify = subscription.subscription_name_slugify
-  LEFT JOIN oldest_subscription_in_cohort AS oldest_subscription
-    ON oldest_subscription.dim_oldest_subscription_in_cohort_id = subscription.dim_subscription_id
   LEFT JOIN prep_amendment
     ON subscription.dim_amendment_id_subscription = prep_amendment.dim_amendment_id
   LEFT JOIN subscription_opportunity_mapping
     ON subscription.dim_subscription_id = subscription_opportunity_mapping.dim_subscription_id
+
+), final AS (
+
+    /*
+    This CTE fetches the subscription id, crm account id, subscription start date and subscription cohort month for the oldest subscription in the lineage a given subscription is a part of. 
+    */
+
+    SELECT
+      --Surrogate Key
+      subscription.dim_subscription_id,
+
+      --Natural Key
+      subscription.subscription_name,
+      subscription.subscription_version,
+
+      --Common Dimension Keys
+      subscription.dim_crm_account_id,
+      subscription.dim_billing_account_id,
+      subscription.dim_billing_account_id_invoice_owner_account,
+      subscription.dim_billing_account_id_creator_account,
+      subscription.dim_crm_opportunity_id,
+      subscription.dim_crm_opportunity_id_current_open_renewal,
+      subscription.dim_crm_opportunity_id_closed_lost_renewal,
+      subscription.dim_amendment_id_subscription,
+
+      -- Oldest subscription cohort keys
+      oldest_subscription.dim_subscription_id AS dim_oldest_subscription_in_cohort_id,
+      oldest_subscription.dim_crm_account_id AS dim_oldest_crm_account_in_cohort_id,
+
+      --Subscription Information
+      subscription.created_by_id,
+      subscription.updated_by_id,
+      subscription.dim_subscription_id_original,
+      subscription.dim_subscription_id_previous,
+      subscription.subscription_name_slugify,
+      subscription.subscription_status,
+      subscription.namespace_id,
+      subscription.namespace_name,
+      subscription.zuora_renewal_subscription_name,
+      subscription.zuora_renewal_subscription_name_slugify,
+      subscription.current_term,
+      subscription.renewal_term,
+      subscription.renewal_term_period_type,
+      subscription.eoa_starter_bronze_offer_accepted,
+      subscription.subscription_sales_type,
+      subscription.auto_renew_native_hist,
+      subscription.auto_renew_customerdot_hist,
+      subscription.turn_on_cloud_licensing,
+      subscription.turn_on_operational_metrics,
+      subscription.contract_operational_metrics,
+      subscription.contract_auto_renewal,
+      subscription.turn_on_auto_renewal,
+      subscription.contract_seat_reconciliation,
+      subscription.turn_on_seat_reconciliation,
+      subscription.is_questionable_opportunity_mapping,
+      subscription.invoice_owner_account,
+      subscription.creator_account,
+      subscription.was_purchased_through_reseller,
+      subscription.multi_year_deal_subscription_linkage,
+
+      -- Oldest subscription date and cohort information
+      oldest_subscription.subscription_start_date   AS oldest_subscription_start_date,
+      oldest_subscription.subscription_cohort_month AS oldest_subscription_cohort_month,
+
+      --Date Information
+      subscription.subscription_start_date,
+      subscription.subscription_end_date,
+      subscription.subscription_start_month,
+      subscription.subscription_end_month,
+      subscription.subscription_end_fiscal_year,
+      subscription.subscription_created_date,
+      subscription.subscription_updated_date,
+      subscription.term_start_date,
+      subscription.term_end_date,
+      subscription.term_start_month,
+      subscription.term_end_month,
+      subscription.term_start_fiscal_year,
+      subscription.term_end_fiscal_year,
+      subscription.is_single_fiscal_year_term_subscription,
+      subscription.second_active_renewal_month,
+      subscription.cancelled_date,
+
+      --Lineage and Cohort Information
+      subscription.subscription_lineage,
+      subscription.oldest_subscription_in_cohort,
+      subscription.subscription_cohort_month,
+      subscription.subscription_cohort_quarter,
+      subscription.subscription_cohort_year
+      
+    FROM joined AS subscription
+    LEFT JOIN joined AS oldest_subscription
+      ON subscription.oldest_subscription_in_cohort = oldest_subscription.subscription_name_slugify
+      AND subscription.subscription_status IN ('Active', 'Cancelled')
+      AND subscription.subscription_name_slugify NOT IN (SELECT subscription_name_slugify FROM data_quality_filter_subscription_slugify)
+      AND oldest_subscription.subscription_status IN ('Active', 'Cancelled')
+      AND oldest_subscription.subscription_name_slugify NOT IN (SELECT subscription_name_slugify FROM data_quality_filter_subscription_slugify)
 
 )
 
@@ -171,5 +239,5 @@ WITH prep_amendment AS (
     created_by="@snalamaru",
     updated_by="@utkarsh060",
     created_date="2020-12-16",
-    updated_date="2024-05-27"
+    updated_date="2024-06-03"
 ) }}
