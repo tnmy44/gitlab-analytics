36aad398679ce525f699f9923cc36695714b779e
Lis Vinueza <lvinueza@gitlab.com>
2024-06-06 14:45:21 +0200

    Update docs and joins

--- transform/snowflake-dbt/models/common/facts_sales_and_marketing/restricted_safe/fct_crm_opportunity_daily_snapshot.sql
+++ transform/snowflake-dbt/models/common/facts_sales_and_marketing/restricted_safe/fct_crm_opportunity_daily_snapshot.sql
@@ -27,7 +27,7 @@ final AS (
     {{ get_keyed_nulls('prep_crm_opportunity.dim_crm_user_id') }}                                                               AS dim_crm_user_id,
     {{ get_keyed_nulls('prep_crm_opportunity.dim_crm_account_user_id') }}                                                       AS dim_crm_account_user_id,
     {{ get_keyed_nulls('order_type.dim_order_type_id') }}                                                                       AS dim_order_type_id,
-    {{ get_keyed_nulls('order_type_live.dim_order_type_id') }}                                                                  AS dim_order_type_current_id,
+    {{ get_keyed_nulls('order_type_current.dim_order_type_id') }}                                                                  AS dim_order_type_current_id,
     {{ get_keyed_nulls('dr_partner_engagement.dim_dr_partner_engagement_id') }}                                                 AS dim_dr_partner_engagement_id,
     {{ get_keyed_nulls('channel_type.dim_channel_type_id') }}                                                                   AS dim_channel_type_id,
     {{ get_keyed_nulls('sales_qualified_source.dim_sales_qualified_source_id') }}                                               AS dim_sales_qualified_source_id,
