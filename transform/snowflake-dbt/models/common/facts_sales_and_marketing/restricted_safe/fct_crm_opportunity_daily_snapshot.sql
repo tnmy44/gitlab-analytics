6c005dfd6c6ed4fb5a6afdd72ced53ca0aca4567
Chris Sharp <csharp@gitlab.com>
2024-06-07 18:51:45 +0100

    resolve conflicts

--- transform/snowflake-dbt/models/common/facts_sales_and_marketing/restricted_safe/fct_crm_opportunity_daily_snapshot.sql
+++ transform/snowflake-dbt/models/common/facts_sales_and_marketing/restricted_safe/fct_crm_opportunity_daily_snapshot.sql
@@ -619,7 +619,7 @@ final AS (
 {{ dbt_audit(
     cte_ref="final",
     created_by="@michellecooper",
-    updated_by="@rakhireddy",
+    updated_by="@chrissharp",
     created_date="2022-02-23",
-    updated_date="2024-06-13"
+    updated_date="2024-06-07"
 ) }}
