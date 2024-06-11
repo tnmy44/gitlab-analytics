bc7daafe562b0aedf4c9716e1bbd721a0e0ec3db
michellecooper <mcooper@gitlab.com>
2024-06-11 09:48:41 -0700

    create project_ci_cd_settings snapshot

---
+++ transform/snowflake-dbt/models/legacy/snapshots/base/gitlab_dotcom_project_ci_cd_settings_snapshots_base.sql
@@ -0,0 +1,37 @@
+{{ config({
+    "alias": "gitlab_dotcom_project_ci_cd_settings_snapshot"
+    })
+}}
+
+WITH source AS (
+
+    SELECT *
+    FROM {{ source('snapshots', 'gitlab_dotcom_project_ci_cd_settings_snapshots') }}
+
+), renamed AS (
+
+    SELECT
+      dbt_scd_id::VARCHAR                                       AS project_ci_cd_settings_snapshot_id,
+      id::NUMBER                                                AS project_ci_cd_settings_id,
+      project_id::NUMBER                                        AS project_id,
+      group_runners_enabled::BOOLEAN                            AS group_runners_enabled,
+      merge_pipelines_enabled::BOOLEAN                          AS merge_pipelines_enabled,
+      default_git_depth:NUMBER                                  AS default_git_depth,
+      forward_deployment_enabled::BOOLEAN                       AS forward_deployment_enabled,
+      merge_trains_enabled::BOOLEAN                             AS merge_trains_enabled,
+      auto_rollback_enabled::BOOLEAN                            AS auto_rollback_enabled,
+      keep_latest_artifact::BOOLEAN                             AS keep_latest_artifact,
+      restrict_user_defined_variables::BOOLEAN                  AS restrict_user_defined_variables,
+      job_token_scope_enabled::BOOLEAN                          AS job_token_scope_enabled,
+      runner_token_expiration_interval::NUMBER                  AS runner_token_expiration_interval,
+      separated_caches::BOOLEAN                                 AS separated_caches,
+      allow_fork_pipelines_to_run_in_parent_project::BOOLEAN    AS allow_fork_pipelines_to_run_in_parent_project,
+      inbound_job_token_scope_enabled::BOOLEAN                  AS inbound_job_token_scope_enabled
+      "DBT_VALID_FROM"::TIMESTAMP                               AS valid_from,
+      "DBT_VALID_TO"::TIMESTAMP                                 AS valid_to
+    FROM source
+
+)
+
+SELECT *
+FROM renamed
