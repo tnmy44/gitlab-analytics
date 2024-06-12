0def456a767994686a69de5dbddafa601e36cbc0
Utkarsh <usinghal@gitlab.com>
2024-06-12 12:49:52 +0530

    remove audit_features_28_days_user

--- transform/snowflake-dbt/models/common_mart_product/reports/rpt_gainsight_metrics_monthly_paid_self_managed.sql
+++ transform/snowflake-dbt/models/common_mart_product/reports/rpt_gainsight_metrics_monthly_paid_self_managed.sql
@@ -251,7 +251,6 @@
       ping_instance_wave_sm.ci_internal_pipelines_28_days_event,
       -- Wave 9
       ping_instance_wave_sm.ci_builds_28_days_event,
-      ping_instance_wave_sm.audit_features_28_days_user,
       ping_instance_wave_sm.groups_all_time_event,
       ping_instance_wave_sm.commit_ci_config_file_7_days_user,
       ping_instance_wave_sm.ci_pipeline_config_repository_all_time_user,
@@ -300,12 +299,12 @@
           instance_type_ordering.ordering_field ASC --prioritizing Production instances
     ) = 1
 
-)   
+)
 
 {{ dbt_audit(
     cte_ref="joined",
     created_by="@mdrussell",
-    updated_by="@mdrussell",
+    updated_by="@utkarsh060",
     created_date="2022-10-12",
-    updated_date="2024-04-18"
+    updated_date="2024-06-11"
 ) }}
