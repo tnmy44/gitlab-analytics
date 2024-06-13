8f0a457cea7c95fb69c5a2c6a0950b2197b6d2a9
Radovan Bacovic <rbacovic@gitlab.com>
2024-06-13 11:59:57 +0200

    Models added.

---
+++ transform/snowflake-dbt/models/sources/commonroom/commonroom_community_member_source.sql
@@ -0,0 +1,21 @@
+WITH source AS
+(
+
+SELECT first_activity_date::TIMESTAMP_NTZ AS first_activity_date,
+       first_activity_source::VARCHAR     AS first_activity_source,
+       full_name::VARCHAR                 AS full_name,
+       impact_points_all_time::NUMBER     AS impact_points_all_time,
+       last_activity_date::TIMESTAMP_NTZ  AS last_activity_date,
+       location::VARCHAR                  AS location,
+       member_tokens::VARCHAR             AS member_tokens,
+       primary_email::VARCHAR             AS primary_email,
+       profiles::VARCHAR                  AS profiles,
+       segment_names::VARCHAR             AS segment_names,
+       tags::VARCHAR                      AS tags,
+       _uploaded_at::TIMESTAMP            AS _uploaded_at
+   FROM {{ source('commonroom', 'community_member') }}
+
+)
+
+SELECT *
+  FROM base;
---
+++ transform/snowflake-dbt/models/sources/commonroom/commonroom_organization_source.sql
@@ -0,0 +1,20 @@
+WITH source AS
+(
+
+SELECT approx_revenue_max::NUMBER     AS approx_revenue_max,
+       approx_revenue_min::NUMBER     AS approx_revenue_min,
+       domain::VARCHAR                AS domain,
+       employee_count::NUMBER         AS employee_count,
+       first_seen_date::TIMESTAMP_NTZ AS first_seen_date,
+       first_seen_source::VARCHAR     AS first_seen_source,
+       last_seen_date::TIMESTAMP_NTZ  AS last_seen_date,
+       location::VARCHAR              AS location,
+       member_count::NUMBER           AS member_count,
+       organization_name::VARCHAR     AS organization_name,
+       _uploaded_at::TIMESTAMP        AS _uploaded_at
+   FROM {{ source('commonroom', 'organization') }}
+
+)
+
+SELECT *
+  FROM base;
