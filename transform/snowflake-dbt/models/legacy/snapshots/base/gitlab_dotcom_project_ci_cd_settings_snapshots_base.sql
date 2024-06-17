802a1a45ee6add1e1447573de5023a12fd1cae73
michellecooper <mcooper@gitlab.com>
2024-06-17 14:31:47 -0700

    move logic to base model

--- transform/snowflake-dbt/models/legacy/snapshots/base/gitlab_dotcom_project_ci_cd_settings_snapshots_base.sql
+++ transform/snowflake-dbt/models/legacy/snapshots/base/gitlab_dotcom_project_ci_cd_settings_snapshots_base.sql
@@ -1,37 +1,77 @@
 {{ config({
-    "alias": "gitlab_dotcom_project_ci_cd_settings_snapshot"
+    "alias": "gitlab_dotcom_project_ci_cd_settings_snapshot",
+    "materialized":"incremental",
+    "unique_key":"record_checksum"
+  )
     })
 }}
 
 WITH source AS (
 
-    SELECT *
-    FROM {{ ref('gitlab_dotcom_project_ci_cd_settings_snapshots') }}
+  SELECT
+    *,
+    DATEADD('sec', _uploaded_at, '1970-01-01')::TIMESTAMP AS uploaded_at,
+    -- This is a replication of the surrogate key function
+    MD5(
+      COALESCE(project_id::VARCHAR, 'null_text')
+      || COALESCE(group_runners_enabled::VARCHAR, 'null_text')
+      || COALESCE(merge_pipelines_enabled::VARCHAR, 'null_text')
+      || COALESCE(default_git_depth::VARCHAR, 'null_text')
+      || COALESCE(forward_deployment_enabled::VARCHAR, 'null_text')
+      || COALESCE(merge_trains_enabled::VARCHAR, 'null_text')
+      || COALESCE(auto_rollback_enabled::VARCHAR, 'null_text')
+      || COALESCE(keep_latest_artifact::VARCHAR, 'null_text')
+      || COALESCE(restrict_user_defined_variables::VARCHAR, 'null_text')
+      || COALESCE(job_token_scope_enabled::VARCHAR, 'null_text')
+      || COALESCE(runner_token_expiration_interval::VARCHAR, 'null_text')
+      || COALESCE(separated_caches::VARCHAR, 'null_text')
+      || COALESCE(allow_fork_pipelines_to_run_in_parent_project::VARCHAR, 'null_text')
+      || COALESCE(inbound_job_token_scope_enabled::VARCHAR, 'null_text')
+    ) AS record_checksum
+  FROM {{ source('gitlab_dotcom', 'project_ci_cd_settings') }}
+  {% if is_incremental() %}
 
-), renamed AS (
+  WHERE uploaded_at > (SELECT MAX(record_checked_at) FROM {{ this }} )
+    AND record_checksum not in (SELECT record_checksum FROM {{ this }} WHERE valid_to is null )
 
-    SELECT
-      dbt_scd_id::VARCHAR                                       AS project_ci_cd_settings_snapshot_id,
-      id::NUMBER                                                AS project_ci_cd_settings_id,
-      project_id::NUMBER                                        AS project_id,
-      group_runners_enabled::BOOLEAN                            AS group_runners_enabled,
-      merge_pipelines_enabled::BOOLEAN                          AS merge_pipelines_enabled,
-      default_git_depth:NUMBER                                  AS default_git_depth,
-      forward_deployment_enabled::BOOLEAN                       AS forward_deployment_enabled,
-      merge_trains_enabled::BOOLEAN                             AS merge_trains_enabled,
-      auto_rollback_enabled::BOOLEAN                            AS auto_rollback_enabled,
-      keep_latest_artifact::BOOLEAN                             AS keep_latest_artifact,
-      restrict_user_defined_variables::BOOLEAN                  AS restrict_user_defined_variables,
-      job_token_scope_enabled::BOOLEAN                          AS job_token_scope_enabled,
-      runner_token_expiration_interval::NUMBER                  AS runner_token_expiration_interval,
-      separated_caches::BOOLEAN                                 AS separated_caches,
-      allow_fork_pipelines_to_run_in_parent_project::BOOLEAN    AS allow_fork_pipelines_to_run_in_parent_project,
-      inbound_job_token_scope_enabled::BOOLEAN                  AS inbound_job_token_scope_enabled,
-      "DBT_VALID_FROM"::TIMESTAMP                               AS valid_from,
-      "DBT_VALID_TO"::TIMESTAMP                                 AS valid_to
-    FROM source
+  {% endif %}
+
+),
+
+base AS (
+
+  SELECT
+    *,
+    LEAD(_uploaded_at) OVER (PARTITION BY id ORDER BY _uploaded_at)          AS next_uploaded_at,
+    LAG(record_checksum, 1, '') OVER (PARTITION BY id ORDER BY _uploaded_at) AS lag_checksum,
+    CONDITIONAL_TRUE_EVENT(record_checksum != lag_checksum)
+      OVER (PARTITION BY id ORDER BY _uploaded_at)                           AS checksum_group
+  FROM source
 
 )
 
-SELECT *
-FROM renamed
+SELECT
+  id                                   AS project_ci_cd_settings_snapshot_id,
+  project_id,
+  group_runners_enabled,
+  merge_pipelines_enabled,
+  default_git_depth,
+  forward_deployment_enabled,
+  merge_trains_enabled,
+  auto_rollback_enabled,
+  keep_latest_artifact,
+  restrict_user_defined_variables,
+  job_token_scope_enabled,
+  runner_token_expiration_interval,
+  separated_caches,
+  allow_fork_pipelines_to_run_in_parent_project,
+  inbound_job_token_scope_enabled,
+  record_checksum,
+  CURRENT_TIMESTAMP                    AS record_checked_at,
+  TO_TIMESTAMP(MIN(_uploaded_at)::INT) AS valid_from,
+  IFF(
+    MAX(COALESCE(next_uploaded_at, 9999999999) = 9999999999),
+    NULL, TO_TIMESTAMP(MAX(next_uploaded_at)::INT)
+  )                                    AS valid_to
+FROM base
+GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, checksum_group
