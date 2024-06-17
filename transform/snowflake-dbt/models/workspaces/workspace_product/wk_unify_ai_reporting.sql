18a85097276549e05f4719e6fee5a424e73fadd3
Matthew Petersen <mpetersen@gitlab.com>
2024-06-17 00:18:10 +0000

    Update file wk_unify_ai_reporting.sql

--- transform/snowflake-dbt/models/workspaces/workspace_product/wk_unify_ai_reporting.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_product/wk_unify_ai_reporting.sql
@@ -7,7 +7,7 @@
     ('dim_date', 'dim_date')
 ]) }},
 
-prep AS (
+dotcom_prep AS (
 
   SELECT
     event_label,
@@ -31,9 +31,35 @@ prep AS (
     AND event_category = 'Llm::ExecuteMethodService'
     AND event_label != 'code_suggestions'
 
+), ai_features AS (
+SELECT
+DISTINCT 
+event_label 
+FROM 
+dotcom_prep
+UNION ALL 
+SELECT
+'All'
 ), 
-
-metric_prep AS (
+(
+SELECT
+DISTINCT 
+plan 
+FROM 
+dotcom_prep
+UNION ALL 
+SELECT
+'All'
+), int_ext_all AS (
+SELECT
+DISTINCT 
+internal_or_external 
+FROM 
+dotcom_prep
+UNION ALL 
+SELECT
+'All'
+), metric_prep AS (
   
   SELECT *
   FROM
@@ -43,246 +69,75 @@ metric_prep AS (
     AND is_last_ping_of_month = TRUE
     AND ping_created_date_month > '2024-01-01'
     AND ping_deployment_type != 'GitLab.com'
+    AND  metrics_path IN  (
+    'redis_hll_counters.count_distinct_user_id_from_request_duo_chat_response_monthly' ,
+    'redis_hll_counters.count_distinct_user_id_from_request_duo_chat_response_weekly'
+    )
 
-),
+), prep AS 
+(
+SELECT
+*
+FROM
+dotcom_prep p 
+LEFT JOIN 
+ai_features f 
+ON 
+p.event_label = f.event_label OR f.event_label = 'All' 
+LEFT JOIN 
+int_ext_all i 
+ON 
+p.internal_or_external = i.internal_or_external OR i.internal_or_external = 'All' 
+), DAU AS (
 
-week_all AS (
+  SELECT
+    behavior_date           AS _date,
+    event_label,
+    plan,
+    internal_or_external,
+    'DAU' AS metric,
+    COUNT(DISTINCT gsc_pseudonymized_user_id) AS user_count
+  FROM prep
+  WHERE
+
+  GROUP BY ALL
+
+), WAU AS (
 
   SELECT
     DATE_TRUNC(WEEK, behavior_date)           AS _date,
     event_label,
-    'All'                                     AS plan,
-    'All'                                     AS internal_or_external,
-    COUNT(DISTINCT gsc_pseudonymized_user_id) AS user_count
-  FROM prep
-  GROUP BY ALL
-
-),
-
-week_add_plan AS (
-
-  SELECT
-    DATE_TRUNC(WEEK, behavior_date)           AS _date,
-    event_label,
-    plan_name,
-    'All',
-    COUNT(DISTINCT gsc_pseudonymized_user_id) AS user_count
-  FROM prep
-  GROUP BY ALL
-
-),
-
-week_split AS (
-
-  SELECT
-    DATE_TRUNC(WEEK, behavior_date)           AS _date,
-    event_label,
-    plan_name,
+    plan,
     internal_or_external,
+    'WAU' AS metric,
     COUNT(DISTINCT gsc_pseudonymized_user_id) AS user_count
   FROM prep
+  WHERE
+  DATE_TRUNC(WEEK, behavior_date) < DATE_TRUNC(WEEK, CURRENT_DATE)  
   GROUP BY ALL
 
-),
-
-week_int_ext AS (
+), MAU AS (
 
   SELECT
-    DATE_TRUNC(WEEK, behavior_date)           AS _date,
+    DATE_TRUNC(MONTH, behavior_date)           AS _date,
     event_label,
-    'All',
+    plan,
     internal_or_external,
+    'MAU' AS metric,
     COUNT(DISTINCT gsc_pseudonymized_user_id) AS user_count
   FROM prep
+  WHERE
+  DATE_TRUNC(MONTH, behavior_date) < DATE_TRUNC(MONTH, CURRENT_DATE) 
   GROUP BY ALL
 
-),
-
-wau AS (
-
-  SELECT *
-  FROM week_all
-  WHERE _date < DATE_TRUNC(WEEK, CURRENT_DATE())
-
-  UNION ALL
-
-  SELECT *
-  FROM week_add_plan
-  WHERE _date < DATE_TRUNC(WEEK, CURRENT_DATE())
-
-  UNION ALL
-
-  SELECT *
-  FROM week_split
-  WHERE _date < DATE_TRUNC(WEEK, CURRENT_DATE())
-
-  UNION ALL
-
-  SELECT *
-  FROM week_int_ext
-  WHERE _date < DATE_TRUNC(WEEK, CURRENT_DATE())
-
-),
-
-day_all AS (
-
-  SELECT
-    DATE_TRUNC(DAY, behavior_date)            AS _date,
-    event_label,
-    'All'                                     AS plan,
-    'All'                                     AS internal_or_external,
-    COUNT(DISTINCT gsc_pseudonymized_user_id) AS metric_value
-  FROM prep
-  GROUP BY ALL
-
-),
-
-day_add_plan AS (
-
-  SELECT
-    DATE_TRUNC(DAY, behavior_date)            AS _date,
-    event_label,
-    plan_name,
-    'All',
-    COUNT(DISTINCT gsc_pseudonymized_user_id) AS user_count
-  FROM prep
-  GROUP BY ALL
-
-),
-
-day_split AS (
-
-  SELECT
-    DATE_TRUNC(DAY, behavior_date)            AS _date,
-    event_label,
-    plan_name,
-    internal_or_external,
-    COUNT(DISTINCT gsc_pseudonymized_user_id) AS user_count
-  FROM prep
-  GROUP BY ALL
-
-),
-
-day_int_ext AS (
-  
-  SELECT
-    DATE_TRUNC(DAY, behavior_date)            AS _date,
-    event_label,
-    'All',
-    internal_or_external,
-    COUNT(DISTINCT gsc_pseudonymized_user_id) AS user_count
-  FROM prep
-  GROUP BY ALL
-
-),
-
-dau AS (
-  
-  SELECT *
-  FROM day_all
-  
-  UNION ALL
-  
-  SELECT *
-  FROM day_add_plan
-
-  UNION ALL
-  
-  SELECT *
-  FROM day_split
-  
-  UNION ALL
-  
-  SELECT *
-  FROM day_int_ext
-
-),
-
-month_all AS (
-  
-  SELECT
-    DATE_TRUNC(MONTH, behavior_date)          AS _date,
-    event_label,
-    'All'                                     AS plan,
-    'All'                                     AS internal_or_external,
-    COUNT(DISTINCT gsc_pseudonymized_user_id) AS user_count
-  FROM prep
-  GROUP BY ALL
-
-),
-
-month_add_plan AS (
-  
-  SELECT
-    DATE_TRUNC(MONTH, behavior_date)          AS _date,
-    event_label,
-    plan_name,
-    'All',
-    COUNT(DISTINCT gsc_pseudonymized_user_id) AS user_count
-  FROM prep
-  GROUP BY ALL
-
-),
-
-month_split AS (
-  
-  SELECT
-    DATE_TRUNC(MONTH, behavior_date)          AS _date,
-    event_label,
-    plan_name,
-    internal_or_external,
-    COUNT(DISTINCT gsc_pseudonymized_user_id) AS user_count
-  FROM prep
-  GROUP BY ALL
-
-),
-
-month_int_ext AS (
-
-  SELECT
-    DATE_TRUNC(MONTH, behavior_date)          AS _date,
-    event_label,
-    'All',
-    internal_or_external,
-    COUNT(DISTINCT gsc_pseudonymized_user_id) AS user_count
-  FROM
-    prep
-  GROUP BY ALL
-
-),
-
-mau AS (
-  
-  SELECT *
-  FROM month_all
-  WHERE _date < DATE_TRUNC(MONTH, CURRENT_DATE())
-  
-  UNION ALL
-  
-  SELECT *
-  FROM month_add_plan
-  WHERE _date < DATE_TRUNC(MONTH, CURRENT_DATE())
-  
-  UNION ALL
-  
-  SELECT *
-  FROM month_split
-  WHERE _date < DATE_TRUNC(MONTH, CURRENT_DATE())
-  
-  UNION ALL
-  
-  SELECT *
-  FROM month_int_ext
-  WHERE _date < DATE_TRUNC(MONTH, CURRENT_DATE())
-
-),
-
+), 
 weekly_retention_grouped AS (
   SELECT
     prep.current_week,
     prep.event_label,
     prep.plan_name,
     prep.internal_or_external,
+    'Weekly Retention' AS metric,
     (COUNT(DISTINCT e2.gsc_pseudonymized_user_id) / COUNT(DISTINCT prep.gsc_pseudonymized_user_id)) AS retention_rate
   FROM prep
   LEFT JOIN prep AS e2
@@ -301,6 +156,7 @@ monthly_retention_grouped AS (
     prep.event_label,
     prep.plan_name,
     prep.internal_or_external,
+    'Monthly Retention' AS metric,
     (COUNT(DISTINCT e2.gsc_pseudonymized_user_id) / COUNT(DISTINCT prep.gsc_pseudonymized_user_id)) AS retention_rate
   FROM prep
   LEFT JOIN prep AS e2
@@ -310,207 +166,36 @@ monthly_retention_grouped AS (
   WHERE prep.next_month < DATE_TRUNC(MONTH, CURRENT_DATE())
   GROUP BY ALL
   HAVING COUNT(DISTINCT prep.gsc_pseudonymized_user_id) > 0
-
-),
-
-weekly_retention_no_plan AS (
-  
-  SELECT
-    prep.current_week,
-    prep.event_label,
-    'All',
-    prep.internal_or_external,
-    (COUNT(DISTINCT e2.gsc_pseudonymized_user_id)/COUNT(DISTINCT prep.gsc_pseudonymized_user_id)) AS retention_rate
-  FROM prep
-  LEFT JOIN prep AS e2 
-    ON e2.event_label = prep.event_label 
-      AND e2.gsc_pseudonymized_user_id = prep.gsc_pseudonymized_user_id 
-      AND e2.current_week = prep.next_week
-  WHERE prep.next_week < DATE_TRUNC(WEEK,CURRENT_DATE())
-  GROUP BY ALL
-  HAVING COUNT(DISTINCT prep.gsc_pseudonymized_user_id) > 0
-
-),
-
-monthly_retention_no_plan AS (
-
-  SELECT
-    prep.current_month,
-    prep.event_label,
-    'All',
-    prep.internal_or_external,
-    (COUNT(DISTINCT e2.gsc_pseudonymized_user_id) / COUNT(DISTINCT prep.gsc_pseudonymized_user_id)) AS retention_rate
-  FROM prep
-  LEFT JOIN prep AS e2
-    ON e2.event_label = prep.event_label 
-      AND e2.gsc_pseudonymized_user_id = prep.gsc_pseudonymized_user_id 
-      AND e2.current_month = prep.next_month
-  WHERE prep.next_month < DATE_TRUNC(MONTH, CURRENT_DATE())
-  GROUP BY ALL
-  HAVING COUNT(DISTINCT prep.gsc_pseudonymized_user_id) > 0
-
-),
-
-weekly_retention_no_intext AS (
-
-  SELECT
-    prep.current_week,
-    prep.event_label,
-    prep.plan_name,
-    'All',
-    (COUNT(DISTINCT e2.gsc_pseudonymized_user_id) / COUNT(DISTINCT prep.gsc_pseudonymized_user_id)) AS retention_rate
-  FROM prep
-  LEFT JOIN prep AS e2
-    ON e2.event_label = prep.event_label 
-      AND e2.gsc_pseudonymized_user_id = prep.gsc_pseudonymized_user_id 
-      AND e2.current_week = prep.next_week
-  WHERE prep.next_week < DATE_TRUNC(WEEK, CURRENT_DATE())
-  GROUP BY ALL
-  HAVING COUNT(DISTINCT prep.gsc_pseudonymized_user_id) > 0
-
-),
-
-monthly_retention_no_intext AS (
-
-  SELECT
-    prep.current_month,
-    prep.event_label,
-    prep.plan_name,
-    'All',
-    (COUNT(DISTINCT e2.gsc_pseudonymized_user_id) / COUNT(DISTINCT prep.gsc_pseudonymized_user_id)) AS retention_rate
-  FROM prep
-  LEFT JOIN prep AS e2
-    ON e2.event_label = prep.event_label 
-      AND e2.gsc_pseudonymized_user_id = prep.gsc_pseudonymized_user_id 
-      AND e2.current_month = prep.next_month
-  WHERE prep.next_month < DATE_TRUNC(MONTH, CURRENT_DATE())
-  GROUP BY ALL
-  HAVING COUNT(DISTINCT prep.gsc_pseudonymized_user_id) > 0
-
-),
-
-weekly_retention_all AS (
-  SELECT
-    prep.current_week,
-    prep.event_label,
-    'All',
-    'All',
-    (COUNT(DISTINCT e2.gsc_pseudonymized_user_id) / COUNT(DISTINCT prep.gsc_pseudonymized_user_id)) AS retention_rate
-  FROM prep
-  LEFT JOIN prep AS e2
-    ON e2.event_label = prep.event_label 
-      AND e2.gsc_pseudonymized_user_id = prep.gsc_pseudonymized_user_id 
-      AND e2.current_week = prep.next_week
-  WHERE prep.next_week < DATE_TRUNC(WEEK, CURRENT_DATE())
-  GROUP BY ALL
-  HAVING COUNT(DISTINCT prep.gsc_pseudonymized_user_id) > 0
-
-),
-
-monthly_retention_all AS (
-
-  SELECT
-    prep.current_month,
-    prep.event_label,
-    'All',
-    'All',
-    (COUNT(DISTINCT e2.gsc_pseudonymized_user_id) / COUNT(DISTINCT prep.gsc_pseudonymized_user_id)) AS retention_rate
-  FROM prep
-  LEFT JOIN prep AS e2
-    ON e2.event_label = prep.event_label 
-      AND e2.gsc_pseudonymized_user_id = prep.gsc_pseudonymized_user_id 
-      AND e2.current_month = prep.next_month
-  WHERE prep.next_month < DATE_TRUNC(MONTH, CURRENT_DATE())
-  GROUP BY ALL
-  HAVING COUNT(DISTINCT prep.gsc_pseudonymized_user_id) > 0
-),
-
-retentions AS (
-  SELECT
-    *,
-    'Weekly Retention' AS metric
-  FROM weekly_retention_grouped
-  
-  UNION ALL
-  
-  SELECT
-    *,
-    'Monthly Retention' AS metric
-  FROM monthly_retention_grouped
-  
-  UNION ALL
-  
-  SELECT
-    *,
-    'Weekly Retention' AS metric
-  FROM weekly_retention_no_plan
-  
-  UNION ALL
-  
-  SELECT
-    *,
-    'Monthly Retention' AS metric
-  FROM monthly_retention_no_plan
-  
-  UNION ALL
-  
-  SELECT
-    *,
-    'Weekly Retention' AS metric
-  FROM weekly_retention_no_intext
-  
-  UNION ALL
-  
-  SELECT
-    *,
-    'Monthly Retention' AS metric
-  FROM monthly_retention_no_intext
-  
-  UNION ALL
-  
-  SELECT
-    *,
-    'Weekly Retention' AS metric
-  FROM weekly_retention_all
-  
-  UNION ALL
-  
-  SELECT
-    *,
-    'Monthly Retention' AS metric
-  FROM monthly_retention_all
-
 
 ),
 
 metrics AS (
   
   SELECT
-    *,
-    'DAU' AS metric
-  FROM dau
+    *
+  FROM DAU
   
   UNION ALL
 
   SELECT
-    *,
-    'WAU' AS metric
-  FROM wau
-  WHERE _date < DATE_TRUNC(WEEK, CURRENT_DATE)
+    *
+  FROM WAU
   
   UNION ALL
 
   SELECT
-    *,
-    'MAU' AS metric
-  FROM mau
-  WHERE _date < DATE_TRUNC(MONTH, CURRENT_DATE)
+    *
+  FROM MAU
   
   UNION ALL
 
   SELECT *
-  FROM retentions
+  FROM weekly_retention_grouped
 
+  UNION ALL
+
+  SELECT *
+  FROM monthly_retention_grouped
 ),
 
 unify AS (
