91c771306bbffdc4869ed01cf7d552de274e73c2
Snehansh Chaturvedi <schaturvedi@gitlab.com>
2024-06-18 21:13:39 +0530

    apply suggestions

--- extract/gitlab_saas_postgres_pipeline/test/test_saas_deletes_metadata_conditions.py
+++ extract/gitlab_saas_postgres_pipeline/test/test_saas_deletes_metadata_conditions.py
@@ -63,12 +63,10 @@ class TestCheckDeletes:
         - Create test metdata table
         - Create a mock PostgresPipelineTable object
         """
-        manifest_file_path = "extract/gitlab_saas_postgres_pipeline/manifests/el_gitlab_dotcom_db_manifest.yaml"
         manifest_connection_info_file_path = "extract/gitlab_saas_postgres_pipeline/manifests/el_saas_connection_info.yaml"
         manifest_dict_connection_info = manifest_reader(
             manifest_connection_info_file_path
         )
-        manifest_dict = manifest_reader(manifest_file_path)
         env = os.environ.copy()
 
         # Create a mock source/self.target_engine
