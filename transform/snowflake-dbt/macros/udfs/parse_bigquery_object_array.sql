0519b18e2453367373cff95a81450f301506251c
jjstark <jstark@gitlab.com>
2024-06-20 10:35:40 -0700

    udf revert, perms

--- transform/snowflake-dbt/macros/udfs/parse_bigquery_object_array.sql
+++ transform/snowflake-dbt/macros/udfs/parse_bigquery_object_array.sql
@@ -8,27 +8,27 @@
 {% for db in production_databases %}
     {%- if target.name in production_targets -%}
 
-    CREATE OR REPLACE FUNCTION "{{ db | trim }}".{{target.schema}}.parse_bigquery_object_array(input_array VARIANT)
+    CREATE OR REPLACE FUNCTION "{{ db | trim }}".{{target.schema}}.parse_bigquery_object_array(V VARIANT)
 
     {%- else -%}
 
-    CREATE OR REPLACE FUNCTION "{{ target.database | trim }}_{{ db | trim }}".{{target.schema}}.parse_bigquery_object_array(input_array VARIANT)
+    CREATE OR REPLACE FUNCTION "{{ target.database | trim }}_{{ db | trim }}".{{target.schema}}.parse_bigquery_object_array(V VARIANT)
 
     {% endif %}
-    RETURNS variant
+    RETURNS string
     LANGUAGE JAVASCRIPT
     AS 
     $$
     var result = {};
-    for (const array_member of input_array) {
-    if (array_member.value){
-        for (const [key, value] of Object.entries(array_member.value)) {
+    for (const x of input_object) {
+    if (x.value){
+        for (const [key, value] of Object.entries(x.value)) {
         if ( key != 'set_timestamp_micros') {
-            result[array_member.key] = value;
+            result[x.key] = value;
         }
         }
     } else {
-        result[array_member.key] = null;
+        result[x.key] = null;
     }
     }
     return result
