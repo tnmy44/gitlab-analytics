e43864dff3b6abb263bee2e2824e7e74ba221147
jjstark <jstark@gitlab.com>
2024-06-24 10:41:21 -0700

    sqlfluff

--- transform/snowflake-dbt/models/data_quality/stale_dev_db_tables.sql
+++ transform/snowflake-dbt/models/data_quality/stale_dev_db_tables.sql
@@ -3,27 +3,29 @@
     ('snowflake_tables', 'snowflake_tables_source'),
     ('snowflake_users', 'snowflake_show_users_source'),
     ('dim_team_member', 'dim_team_member'),
-]) }}
+]) }},
 
-, dev_databases as (
-  
-  select
-    database_id, 
+dev_databases AS (
+
+  SELECT
+    database_id,
     database_name,
-    database_owner,
-  from snowflake_databases
-  where database_owner not in (
-    'TRANSFORMER',
-    'ACCOUNTADMIN',
-    'SYSADMIN',
-    'LOADER',
-    'STATIC_ADMIN'
-  )
-  and database_name like '%' || snowflake_databases.database_owner || '_%' 
+    database_owner
+  FROM snowflake_databases
+  WHERE database_owner NOT IN (
+      'TRANSFORMER',
+      'ACCOUNTADMIN',
+      'SYSADMIN',
+      'LOADER',
+      'STATIC_ADMIN'
+    )
+    AND database_name LIKE '%' || snowflake_databases.database_owner || '_%'
 
-), stale_dev_db_tables as (
+),
 
-  select 
+stale_dev_db_tables AS (
+
+  SELECT
     snowflake_tables.table_name,
     snowflake_tables.table_schema,
     snowflake_tables.table_catalog,
@@ -31,52 +33,57 @@
     snowflake_tables.created,
     snowflake_tables.last_altered,
     snowflake_tables.table_owner,
-    dev_databases.database_owner,
-  from snowflake_tables
-  inner join dev_databases on snowflake_tables.table_catalog_id = dev_databases.database_id
-  where table_schema !='INFORMATION_SCHEMA'
-    and snowflake_tables.table_type = 'BASE TABLE'
-    and snowflake_tables.last_altered <= dateadd(d, {{ var('dev_db_object_expiration') }}, current_timestamp())
-  order by dev_databases.database_owner
+    dev_databases.database_owner
+  FROM snowflake_tables
+  INNER JOIN dev_databases ON snowflake_tables.table_catalog_id = dev_databases.database_id
+  WHERE table_schema != 'INFORMATION_SCHEMA'
+    AND snowflake_tables.table_type = 'BASE TABLE'
+    AND snowflake_tables.last_altered <= DATEADD(D, {{ var('dev_db_object_expiration') }}, CURRENT_TIMESTAMP())
+  ORDER BY dev_databases.database_owner
 
-), snowflake_dev_users as (
+),
 
-  select 
+snowflake_dev_users AS (
+
+  SELECT
     user_name,
-    email,
-  from snowflake_users 
-  where is_disabled = false
-    and user_name in (
-      select distinct
-        database_owner
-      from stale_dev_db_tables
+    email
+  FROM snowflake_users
+  WHERE is_disabled = FALSE
+    AND user_name IN (
+      SELECT DISTINCT database_owner
+      FROM stale_dev_db_tables
     )
 
-), gitlab_team_members as (
+),
 
-  select
+gitlab_team_members AS (
+
+  SELECT
     work_email,
-    gitlab_username,
-  from prod.common.dim_team_member
-  where is_current = true
-  and is_current_team_member = true  
+    gitlab_username
+  FROM prod.common.dim_team_member
+  WHERE is_current = TRUE
+    AND is_current_team_member = TRUE
 
-), final as (
+),
 
-select
-  stale_dev_db_tables.table_name,
-  stale_dev_db_tables.table_schema,
-  stale_dev_db_tables.table_catalog,
-  stale_dev_db_tables.created,
-  stale_dev_db_tables.last_altered,
-  stale_dev_db_tables.database_owner,
-  gitlab_team_members.gitlab_username,
-from stale_dev_db_tables
-left join snowflake_dev_users on lower(stale_dev_db_tables.database_owner) = lower(snowflake_dev_users.user_name)
-left join gitlab_team_members on lower(gitlab_team_members.work_email) = lower(snowflake_dev_users.email)
-  
+final AS (
+
+  SELECT
+    stale_dev_db_tables.table_name,
+    stale_dev_db_tables.table_schema,
+    stale_dev_db_tables.table_catalog,
+    stale_dev_db_tables.created,
+    stale_dev_db_tables.last_altered,
+    stale_dev_db_tables.database_owner,
+    gitlab_team_members.gitlab_username
+  FROM stale_dev_db_tables
+  LEFT JOIN snowflake_dev_users ON LOWER(stale_dev_db_tables.database_owner) = LOWER(snowflake_dev_users.user_name)
+  LEFT JOIN gitlab_team_members ON LOWER(gitlab_team_members.work_email) = LOWER(snowflake_dev_users.email)
+
 )
 
 
-select *
-from final
+SELECT *
+FROM final
