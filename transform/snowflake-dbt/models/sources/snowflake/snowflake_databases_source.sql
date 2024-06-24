f1bfc37c15a87b72c8300a5341ef74154b5dfcd5
jjstark <jstark@gitlab.com>
2024-06-24 10:25:48 -0700

    simplify source models

--- transform/snowflake-dbt/models/sources/snowflake/snowflake_databases_source.sql
+++ transform/snowflake-dbt/models/sources/snowflake/snowflake_databases_source.sql
@@ -1,14 +1,3 @@
-WITH source AS (
-
-    SELECT *
-    FROM {{ source('snowflake_account_usage','databases') }}
-
-), intermediate AS (
-
-    SELECT *
-    FROM source
-
-)
 
 SELECT *
-FROM intermediate
+FROM {{ source('snowflake_account_usage','databases') }}
--- transform/snowflake-dbt/models/sources/snowflake/snowflake_tables_source.sql
+++ transform/snowflake-dbt/models/sources/snowflake/snowflake_tables_source.sql
@@ -1,14 +1,3 @@
-WITH source AS (
-
-    SELECT *
-    FROM {{ source('snowflake_account_usage','tables') }}
-
-), intermediate AS (
-
-    SELECT *
-    FROM source
-
-)
 
 SELECT *
-FROM intermediate
+FROM {{ source('snowflake_account_usage','tables') }}
