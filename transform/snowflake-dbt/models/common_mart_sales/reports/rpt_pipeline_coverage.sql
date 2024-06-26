0c0c8d09ae67d2f70027f1fd730a972a3fd4d3f5
Lis Vinueza <lvinueza@gitlab.com>
2024-06-26 23:11:10 +0200

    Fix report

--- transform/snowflake-dbt/models/common_mart_sales/reports/rpt_pipeline_coverage.sql
+++ transform/snowflake-dbt/models/common_mart_sales/reports/rpt_pipeline_coverage.sql
@@ -23,15 +23,8 @@ total_targets AS (
   SELECT
     targets.fiscal_quarter_name,
     targets.fiscal_year,
-    targets.dim_crm_user_hierarchy_sk                                                    AS dim_crm_current_account_set_hierarchy_sk,
-    targets.crm_user_sales_segment,
-    targets.crm_user_sales_segment_grouped,
-    targets.crm_user_business_unit,
-    targets.crm_user_geo,
-    targets.crm_user_region,
-    targets.crm_user_area,
-    targets.crm_user_sales_segment_region_grouped,
-    targets.order_type_name                                                              AS order_type,
+    targets.dim_crm_user_hierarchy_sk,
+    targets.order_type_name,
     targets.order_type_grouped,
     targets.sales_qualified_source_name,
     targets.sales_qualified_source_grouped,
@@ -42,16 +35,115 @@ total_targets AS (
 
 ),
 
-
 daily_actuals AS (
 
   SELECT
-
+    actuals.snapshot_fiscal_quarter_name,
+    actuals.snapshot_date,
+    actuals.snapshot_id,
+    actuals.dim_crm_current_account_set_hierarchy_sk,
     actuals.sales_qualified_source_name,
     actuals.sales_qualified_source_grouped,
     actuals.order_type,
     actuals.order_type_grouped,
+    SUM(booked_net_arr_in_snapshot_quarter)     AS booked_net_arr_in_snapshot_quarter,
+    SUM(open_1plus_net_arr_in_snapshot_quarter) AS open_1plus_net_arr_in_snapshot_quarter,
+    SUM(open_3plus_net_arr_in_snapshot_quarter) AS open_3plus_net_arr_in_snapshot_quarter,
+    SUM(open_4plus_net_arr_in_snapshot_quarter) AS open_4plus_net_arr_in_snapshot_quarter
+
+  FROM actuals
+  GROUP BY ALL
+),
+
+quarterly_actuals AS (
+
+  SELECT
+    actuals.snapshot_fiscal_quarter_name,
     actuals.dim_crm_current_account_set_hierarchy_sk,
+    actuals.sales_qualified_source_name,
+    actuals.sales_qualified_source_grouped,
+    actuals.order_type,
+    actuals.order_type_grouped,
+    SUM(actuals.booked_net_arr_in_snapshot_quarter) AS total_booked_net_arr
+  FROM actuals
+  WHERE snapshot_date = snapshot_last_day_of_fiscal_quarter
+  GROUP BY ALL
+
+),
+
+combined_data AS (
+
+  SELECT
+    dim_crm_current_account_set_hierarchy_sk,
+    sales_qualified_source_name,
+    sales_qualified_source_grouped,
+    order_type,
+    order_type_grouped,
+    snapshot_fiscal_quarter_name
+  FROM daily_actuals
+
+  UNION
+
+  SELECT
+    dim_crm_user_hierarchy_sk,
+    sales_qualified_source_name,
+    sales_qualified_source_grouped,
+    order_type_name,
+    order_type_grouped,
+    fiscal_quarter_name
+  FROM total_targets
+
+  UNION
+
+  SELECT
+    dim_crm_current_account_set_hierarchy_sk,
+    sales_qualified_source_name,
+    sales_qualified_source_grouped,
+    order_type,
+    order_type_grouped,
+    snapshot_fiscal_quarter_name
+  FROM quarterly_actuals
+
+),
+
+base AS (
+
+  /*
+    Cross join all dimensions (hierarchy, qualified source, order type) and
+    the dates to create a comprehensive set of all possible combinations of these dimensions and dates.
+    This exhaustive combination is essential for scenarios where we need to account for all possible configurations in our analysis,
+    ensuring that no combination is overlooked.
+
+    When we eventually join this set of combinations with the quarterly actuals,
+    it ensures that even the newly introduced dimensions are accounted for.
+  */
+
+  SELECT
+    combined_data.dim_crm_current_account_set_hierarchy_sk,
+    combined_data.sales_qualified_source_name,
+    combined_data.sales_qualified_source_grouped,
+    combined_data.order_type,
+    combined_data.order_type_grouped,
+    spine.date_id,
+    spine.day_7 AS date_actual,
+    combined_data.snapshot_fiscal_quarter_name
+  FROM combined_data
+  INNER JOIN spine
+    ON combined_data.snapshot_fiscal_quarter_name = spine.fiscal_quarter_name
+
+),
+
+final AS (
+
+  SELECT
+    base.date_actual,
+    base.dim_crm_current_account_set_hierarchy_sk,
+    base.sales_qualified_source_name,
+    base.sales_qualified_source_grouped,
+    base.order_type,
+    base.order_type_grouped,
+
+    -- hierarchy fields
     actuals.crm_current_account_set_sales_segment,
     actuals.crm_current_account_set_geo,
     actuals.crm_current_account_set_region,
@@ -78,7 +170,6 @@ daily_actuals AS (
     actuals.current_first_day_of_week,
     actuals.current_week_of_fiscal_quarter_normalised,
     actuals.current_week_of_fiscal_quarter,
-    actuals.snapshot_id,
     actuals.snapshot_day,
     actuals.snapshot_date,
     actuals.snapshot_month,
@@ -128,179 +219,6 @@ daily_actuals AS (
     actuals.snapshot_is_first_day_of_fiscal_quarter_week,
     actuals.snapshot_days_until_last_day_of_month,
     actuals.snapshot_week_of_fiscal_quarter,
-    SUM(booked_net_arr_in_snapshot_quarter)     AS booked_net_arr_in_snapshot_quarter,
-    SUM(open_1plus_net_arr_in_snapshot_quarter) AS open_1plus_net_arr_in_snapshot_quarter,
-    SUM(open_3plus_net_arr_in_snapshot_quarter) AS open_3plus_net_arr_in_snapshot_quarter,
-    SUM(open_4plus_net_arr_in_snapshot_quarter) AS open_4plus_net_arr_in_snapshot_quarter
-  FROM actuals
-  GROUP BY ALL
-),
-
-quarterly_actuals AS (
-
-  SELECT
-    actuals.snapshot_fiscal_quarter_name,
-    actuals.snapshot_fiscal_quarter_date,
-    actuals.dim_crm_current_account_set_hierarchy_sk,
-    actuals.sales_qualified_source_name,
-    actuals.sales_qualified_source_grouped,
-    actuals.order_type,
-    actuals.order_type_grouped,
-    SUM(actuals.booked_net_arr_in_snapshot_quarter) AS total_booked_net_arr
-  FROM actuals
-  WHERE snapshot_date = snapshot_last_day_of_fiscal_quarter
-  GROUP BY ALL
-
-),
-
-combined_data AS (
-
-  SELECT
-    dim_crm_current_account_set_hierarchy_sk,
-    sales_qualified_source_name,
-    sales_qualified_source_grouped,
-    order_type,
-    order_type_grouped,
-    fiscal_quarter_name
-  FROM total_targets
-
-  UNION
-
-  SELECT
-    dim_crm_current_account_set_hierarchy_sk,
-    sales_qualified_source_name,
-    sales_qualified_source_grouped,
-    order_type,
-    order_type_grouped,
-    snapshot_fiscal_quarter_name
-  FROM quarterly_actuals
-
-  UNION
-
-  SELECT
-    dim_crm_current_account_set_hierarchy_sk,
-    sales_qualified_source_name,
-    sales_qualified_source_grouped,
-    order_type,
-    order_type_grouped,
-    snapshot_fiscal_quarter_name
-  FROM daily_actuals
-
-),
-
-base AS (
-
-  /*
-    Cross join all dimensions (hierarchy, qualified source, order type) and
-    the dates to create a comprehensive set of all possible combinations of these dimensions and dates.
-    This is essential for scenarios where we need to account for all possible configurations in our analysis.
-
-    When we eventually join this set of combinations with the quarterly actuals,
-    it ensures that even the newly introduced dimensions are accounted for.
-  */
-
-  SELECT
-    spine.date_id,
-    spine.day_7 AS date_actual,
-    combined_data.dim_crm_current_account_set_hierarchy_sk,
-    combined_data.sales_qualified_source_name,
-    combined_data.sales_qualified_source_grouped,
-    combined_data.order_type,
-    combined_data.order_type_grouped,
-    combined_data.fiscal_quarter_name
-  FROM combined_data
-  INNER JOIN spine
-    ON combined_data.fiscal_quarter_name = spine.fiscal_quarter_name
-
-),
-
-final AS (
-
-  SELECT
-    base.date_id,
-    base.date_actual,
-    base.dim_crm_current_account_set_hierarchy_sk,
-    base.sales_qualified_source_name,
-    base.sales_qualified_source_grouped,
-    base.order_type,
-    base.order_type_grouped,
-
-    -- hierarchy fields
-    daily_actuals.crm_current_account_set_sales_segment,
-    daily_actuals.crm_current_account_set_geo,
-    daily_actuals.crm_current_account_set_region,
-    daily_actuals.crm_current_account_set_area,
-    daily_actuals.crm_current_account_set_business_unit,
-    daily_actuals.crm_current_account_set_role_name,
-    daily_actuals.crm_current_account_set_role_level_1,
-    daily_actuals.crm_current_account_set_role_level_2,
-    daily_actuals.crm_current_account_set_role_level_3,
-    daily_actuals.crm_current_account_set_role_level_4,
-    daily_actuals.crm_current_account_set_role_level_5,
-
-    --Dates
-    daily_actuals.current_day_name,
-    daily_actuals.current_date_actual,
-    daily_actuals.current_fiscal_year,
-    daily_actuals.current_first_day_of_fiscal_year,
-    daily_actuals.current_fiscal_quarter_name_fy,
-    daily_actuals.current_first_day_of_month,
-    daily_actuals.current_first_day_of_fiscal_quarter,
-    daily_actuals.current_day_of_month,
-    daily_actuals.current_day_of_fiscal_quarter,
-    daily_actuals.current_day_of_fiscal_year,
-    daily_actuals.current_first_day_of_week,
-    daily_actuals.current_week_of_fiscal_quarter_normalised,
-    daily_actuals.current_week_of_fiscal_quarter,
-    daily_actuals.snapshot_day,
-    daily_actuals.snapshot_date,
-    daily_actuals.snapshot_month,
-    daily_actuals.snapshot_fiscal_year,
-    daily_actuals.snapshot_fiscal_quarter_name,
-    daily_actuals.snapshot_fiscal_quarter_date,
-    daily_actuals.snapshot_day_of_fiscal_quarter_normalised,
-    daily_actuals.snapshot_day_of_fiscal_year_normalised,
-    daily_actuals.snapshot_day_name, 
-    daily_actuals.snapshot_day_of_week,
-    daily_actuals.snapshot_first_day_of_week,
-    daily_actuals.snapshot_week_of_year,
-    daily_actuals.snapshot_day_of_month,
-    daily_actuals.snapshot_day_of_quarter,
-    daily_actuals.snapshot_day_of_year,
-    daily_actuals.snapshot_fiscal_quarter,
-    daily_actuals.snapshot_day_of_fiscal_quarter,
-    daily_actuals.snapshot_day_of_fiscal_year,
-    daily_actuals.snapshot_month_name,
-    daily_actuals.snapshot_first_day_of_month,
-    daily_actuals.snapshot_last_day_of_month,
-    daily_actuals.snapshot_first_day_of_year,
-    daily_actuals.snapshot_last_day_of_year,
-    daily_actuals.snapshot_first_day_of_quarter,
-    daily_actuals.snapshot_last_day_of_quarter,
-    daily_actuals.snapshot_first_day_of_fiscal_quarter,
-    daily_actuals.snapshot_last_day_of_fiscal_quarter,
-    daily_actuals.snapshot_first_day_of_fiscal_year,
-    daily_actuals.snapshot_last_day_of_fiscal_year,
-    daily_actuals.snapshot_week_of_fiscal_year,
-    daily_actuals.snapshot_month_of_fiscal_year,
-    daily_actuals.snapshot_last_day_of_week,
-    daily_actuals.snapshot_quarter_name,
-    daily_actuals.snapshot_fiscal_quarter_name_fy,
-    daily_actuals.snapshot_fiscal_quarter_number_absolute,
-    daily_actuals.snapshot_fiscal_month_name,
-    daily_actuals.snapshot_fiscal_month_name_fy,
-    daily_actuals.snapshot_holiday_desc,
-    daily_actuals.snapshot_is_holiday,
-    daily_actuals.snapshot_last_month_of_fiscal_quarter,
-    daily_actuals.snapshot_is_first_day_of_last_month_of_fiscal_quarter,
-    daily_actuals.snapshot_last_month_of_fiscal_year,
-    daily_actuals.snapshot_is_first_day_of_last_month_of_fiscal_year,
-    daily_actuals.snapshot_days_in_month_count,
-    daily_actuals.snapshot_week_of_month_normalised,
-    daily_actuals.snapshot_week_of_fiscal_quarter_normalised,
-    daily_actuals.snapshot_is_first_day_of_fiscal_quarter_week,
-    daily_actuals.snapshot_days_until_last_day_of_month,
-    daily_actuals.snapshot_week_of_fiscal_quarter,
     'targets_actuals'                                                       AS source,
     SUM(total_targets.pipeline_created_total_quarter_target)                AS pipeline_created_total_quarter_target,
     SUM(total_targets.net_arr_total_quarter_target)                         AS net_arr_total_quarter_target,
@@ -310,21 +228,40 @@ final AS (
     SUM(daily_actuals.open_4plus_net_arr_in_snapshot_quarter)               AS coverage_open_4plus_net_arr,
     SUM(quarterly_actuals.total_booked_net_arr)                             AS total_booked_net_arr
   FROM base
+  LEFT JOIN actuals
+    ON actuals.snapshot_date = base.date_actual
+      AND base.sales_qualified_source_name = actuals.sales_qualified_source_name
+      AND base.sales_qualified_source_grouped = actuals.sales_qualified_source_grouped
+      AND base.dim_crm_current_account_set_hierarchy_sk = actuals.dim_crm_current_account_set_hierarchy_sk
+      AND base.order_type = actuals.order_type 
+      AND base.order_type_grouped = actuals.order_type_grouped 
+  LEFT JOIN targets
+    ON targets.target_date = base.date_actual
+      AND base.sales_qualified_source_grouped = targets.sales_qualified_source_grouped
+      AND base.dim_crm_current_account_set_hierarchy_sk = targets.dim_crm_user_hierarchy_sk
+      AND base.order_type = targets.order_type_name
+      AND base.order_type_grouped = targets.order_type_grouped 
   LEFT JOIN total_targets
-    ON base.fiscal_quarter_name = total_targets.fiscal_quarter_name
+    ON base.snapshot_fiscal_quarter_name = total_targets.fiscal_quarter_name
       AND base.sales_qualified_source_name = total_targets.sales_qualified_source_name
-      AND base.dim_crm_current_account_set_hierarchy_sk = total_targets.dim_crm_current_account_set_hierarchy_sk
-      AND base.order_type = total_targets.order_type
+      AND base.sales_qualified_source_grouped = total_targets.sales_qualified_source_grouped
+      AND base.dim_crm_current_account_set_hierarchy_sk = total_targets.dim_crm_user_hierarchy_sk
+      AND base.order_type = total_targets.order_type_name
+      AND base.order_type_grouped = total_targets.order_type_grouped
   LEFT JOIN daily_actuals
     ON base.date_id = daily_actuals.snapshot_id
       AND base.sales_qualified_source_name = daily_actuals.sales_qualified_source_name
+      AND base.sales_qualified_source_grouped = daily_actuals.sales_qualified_source_grouped
       AND base.dim_crm_current_account_set_hierarchy_sk = daily_actuals.dim_crm_current_account_set_hierarchy_sk
       AND base.order_type = daily_actuals.order_type
+      AND base.order_type_grouped = daily_actuals.order_type_grouped
   LEFT JOIN quarterly_actuals
-    ON base.fiscal_quarter_name = quarterly_actuals.snapshot_fiscal_quarter_name
+    ON base.snapshot_fiscal_quarter_name = quarterly_actuals.snapshot_fiscal_quarter_name
       AND base.sales_qualified_source_name = quarterly_actuals.sales_qualified_source_name
+      AND base.sales_qualified_source_grouped = quarterly_actuals.sales_qualified_source_grouped
       AND base.dim_crm_current_account_set_hierarchy_sk = quarterly_actuals.dim_crm_current_account_set_hierarchy_sk
       AND base.order_type = quarterly_actuals.order_type
+      AND base.order_type_grouped = quarterly_actuals.order_type_grouped
   GROUP BY ALL
 
 )
