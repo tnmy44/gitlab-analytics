45e9a922a522dfc81dde34d1a733f20070e21d45
Jong Lee <jonglee@gitlab.com>
2024-06-26 22:56:12 +0000

    Update file rpt_product_usage_health_score_with_arr.sql

--- transform/snowflake-dbt/models/common_mart_product/restricted_safe/rpt_product_usage_health_score_with_arr.sql
+++ transform/snowflake-dbt/models/common_mart_product/restricted_safe/rpt_product_usage_health_score_with_arr.sql
@@ -4,10 +4,15 @@ WITH final as (
 
 SELECT
     rpt_product_usage_health_score.*,
-        CASE WHEN ci_pipeline_utilization > 0.33 THEN 88
-                  WHEN ci_pipeline_utilization >= 0.1 AND ci_pipeline_utilization <=0.33 THEN 63
-                  WHEN ci_pipeline_utilization < 0.1 THEN 25
-                  ELSE NULL END AS ci_pipeline_utilization_score
+        CASE WHEN weighted_ci_adoption_child_account > 0.33 THEN 88
+                  WHEN weighted_ci_adoption_child_account >= 0.1 AND weighted_ci_adoption_child_account <=0.33 THEN 63
+                  WHEN weighted_ci_adoption_child_account < 0.1 THEN 25
+                  ELSE NULL END AS ci_score_child_account
+        CASE WHEN weighted_ci_adoption_child_account > 0.33 THEN 'Green'
+                  WHEN weighted_ci_adoption_child_account >= 0.1 AND weighted_ci_adoption_child_account <=0.33 THEN 'Yellow'
+                  WHEN weighted_ci_adoption_child_account < 0.1 THEN 'Red'
+                  ELSE 'NO DATA AT ALL' END AS ci_color_child_account
+
 FROM
   {{ ref('mart_arr_with_zero_dollar_charges') }} AS mart_arr_all
   LEFT JOIN {{ ref('rpt_product_usage_health_score') }} AS rpt_product_usage_health_score
