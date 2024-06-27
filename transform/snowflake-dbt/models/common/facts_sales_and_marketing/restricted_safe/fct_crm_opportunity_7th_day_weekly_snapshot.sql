116b044361be1d50d3c34614a319ea2753b84457
Lis Vinueza <lvinueza@gitlab.com>
2024-06-27 14:01:55 +0200

    Add star macro

--- transform/snowflake-dbt/models/common/facts_sales_and_marketing/restricted_safe/fct_crm_opportunity_7th_day_weekly_snapshot.sql
+++ transform/snowflake-dbt/models/common/facts_sales_and_marketing/restricted_safe/fct_crm_opportunity_7th_day_weekly_snapshot.sql
@@ -16,7 +16,8 @@ day_7_list AS (
 final AS (
 
   SELECT 
-    actuals.*
+    {{ dbt_utils.star(from=ref('fct_crm_opportunity_daily_snapshot'), 
+                        except=['CREATED_BY', 'UPDATED_BY', 'MODEL_CREATED_DATE', 'MODEL_UPDATED_DATE', 'DBT_UPDATED_AT', 'DBT_CREATED_AT']) }},
   FROM actuals
   INNER JOIN day_7_list
     ON actuals.snapshot_date = day_7_list.day_7
@@ -28,5 +29,5 @@ final AS (
     created_by="@lisvinueza",
     updated_by="@lisvinueza",
     created_date="2024-05-09",
-    updated_date="2024-05-09"
+    updated_date="2024-06-27"
 ) }}
