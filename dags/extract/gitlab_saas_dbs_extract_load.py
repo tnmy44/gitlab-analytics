f0bd76cc96a43e401c9ab3f10169375e8cb16d8a
Snehansh Chaturvedi <schaturvedi@gitlab.com>
2024-06-28 14:42:10 +0530

    redact dates

--- dags/extract/gitlab_saas_dbs_extract_load.py
+++ dags/extract/gitlab_saas_dbs_extract_load.py
@@ -122,7 +122,7 @@ config_dict: Dict[Any, Any] = {
             GITLAB_METADATA_DB_USER,
             GITLAB_METADATA_SCHEMA,
         ],
-        "start_date": datetime(2024, 6, 28),
+        "start_date": datetime(2023, 10, 3),
         "task_name": "gitlab-com",
         "description": "This DAG does Incremental extract & load  of gitlab.com database(Postgres) to snowflake",
         "description_incremental": "This DAG does backfill of incremental table extract & load of gitlab.com database(Postgres) to snowflake",
