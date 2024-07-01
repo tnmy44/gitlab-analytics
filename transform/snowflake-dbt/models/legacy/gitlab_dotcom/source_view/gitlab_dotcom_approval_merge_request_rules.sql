84307a1e021cdb56680f8bc9ee6d6c0de2aee98d
michellecooper <mcooper@gitlab.com>
2024-07-01 15:21:18 -0700

    drop legacy models

--- transform/snowflake-dbt/models/legacy/gitlab_dotcom/source_view/gitlab_dotcom_approval_merge_request_rules.sql
+++
@@ -1,9 +0,0 @@
-WITH source AS (
-
-    SELECT *
-    FROM {{ ref('gitlab_dotcom_approval_merge_request_rules_source') }}
-
-)
-
-SELECT *
-FROM source
--- transform/snowflake-dbt/models/legacy/gitlab_dotcom/source_view/gitlab_dotcom_experiments.sql
+++
@@ -1,9 +0,0 @@
-WITH source AS (
-
-    SELECT *
-    FROM {{ ref('gitlab_dotcom_experiments_source') }}
-
-)
-
-SELECT *
-FROM source
