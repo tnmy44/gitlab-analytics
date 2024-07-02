0ce9875655723f2aaf51dc07bee48dce4d599360
Lis Vinueza <lvinueza@gitlab.com>
2024-07-02 19:02:38 +0200

    Add fields

--- transform/snowflake-dbt/models/common_mart_sales/reports/rpt_final_bookings.sql
+++ transform/snowflake-dbt/models/common_mart_sales/reports/rpt_final_bookings.sql
@@ -1,17 +1,40 @@
-WITH live_actuals AS (
+{{ simple_cte([
+    ('live_actuals', 'mart_crm_opportunity_stamped_hierarchy_hist'),
+    ('dim_date', 'dim_date')
+    ])
 
-  SELECT * 
-  FROM {{ ref('mart_crm_opportunity_stamped_hierarchy_hist')}}
+}},
 
-),
-
-distinct_quarters AS (
+base AS (
 
   SELECT DISTINCT
-    first_day_of_fiscal_quarter,
-    fiscal_quarter_name_fy,
-    days_in_fiscal_quarter_count
-  FROM {{ ref('dim_date')}}
+    current_day_name,
+    current_date_actual,
+    current_fiscal_year,
+    current_first_day_of_fiscal_year,
+    current_fiscal_quarter_name_fy,
+    current_first_day_of_month,
+    current_first_day_of_fiscal_quarter,
+    current_day_of_month,
+    current_day_of_fiscal_quarter,
+    current_day_of_fiscal_year,
+    current_first_day_of_week,
+    current_week_of_fiscal_quarter_normalised,
+    current_week_of_fiscal_quarter,
+    fiscal_quarter_name_fy                       AS snapshot_fiscal_quarter_name,
+    fiscal_quarter_name_fy                       AS snapshot_fiscal_quarter_name_fy,
+    fiscal_quarter                               AS snapshot_fiscal_quarter,
+    fiscal_year                                  AS snapshot_fiscal_year,
+    first_day_of_fiscal_quarter                  AS snapshot_first_day_of_fiscal_quarter,
+    last_day_of_fiscal_quarter                   AS snapshot_last_day_of_fiscal_quarter,
+    first_day_of_fiscal_year                     AS snapshot_first_day_of_fiscal_year,
+    last_day_of_fiscal_year                      AS snapshot_last_day_of_fiscal_year,
+    first_day_of_fiscal_quarter                  AS snapshot_fiscal_quarter_date,
+    fiscal_quarter_number_absolute               AS snapshot_fiscal_quarter_number_absolute,
+    last_month_of_fiscal_quarter                 AS snapshot_last_month_of_fiscal_quarter,
+    last_month_of_fiscal_year                    AS snapshot_last_month_of_fiscal_year,
+    days_in_fiscal_quarter_count                 AS snapshot_days_in_fiscal_quarter_count
+  FROM dim_date
 
 ),
 
@@ -19,9 +42,13 @@ booked_arr AS (
 
   SELECT 
     close_fiscal_quarter_date,
+    sales_qualified_source_name,
+    sales_qualified_source_grouped,
+    order_type,
+    order_type_grouped,
     SUM(booked_net_arr)     AS total_booked_arr
   FROM live_actuals
-  GROUP BY 1
+  GROUP BY 1,2,3,4,5
 
 ),
 
@@ -29,25 +56,57 @@ created_arr AS (
 
   SELECT
     arr_created_fiscal_quarter_date,
+    sales_qualified_source_name,
+    sales_qualified_source_grouped,
+    order_type,
+    order_type_grouped,
     SUM(created_arr)        AS total_created_arr
   FROM live_actuals
-  GROUP BY 1
+  GROUP BY 1,2,3,4,5
 ), 
 
 final AS (
 
   SELECT
-    distinct_quarters.fiscal_quarter_name_fy,
-    distinct_quarters.fiscal_quarter_name_fy AS fiscal_quarter_name,
-    distinct_quarters.days_in_fiscal_quarter_count + 1 AS final_day_of_fiscal_quarter,   -- to put the live value on the day after the final day of the quarter, i.e. live value is on day 93 if there are 92 days in the quarter
-    distinct_quarters.first_day_of_fiscal_quarter AS fiscal_quarter_date,
-    created_arr.total_created_arr,
-    booked_arr.total_booked_arr
-  FROM distinct_quarters 
+    base.current_day_name,
+    base.current_date_actual,
+    base.current_fiscal_year,
+    base.current_first_day_of_fiscal_year,
+    base.current_fiscal_quarter_name_fy,
+    base.current_first_day_of_month,
+    base.current_first_day_of_fiscal_quarter,
+    base.current_day_of_month,
+    base.current_day_of_fiscal_quarter,
+    base.current_day_of_fiscal_year,
+    base.current_first_day_of_week,
+    base.current_week_of_fiscal_quarter_normalised,
+    base.current_week_of_fiscal_quarter,
+    base.snapshot_fiscal_quarter,
+    base.snapshot_fiscal_year,
+    base.snapshot_first_day_of_fiscal_year,
+    base.snapshot_last_day_of_fiscal_year,
+    base.snapshot_fiscal_quarter_name,
+    base.snapshot_fiscal_quarter_name_fy,
+    base.snapshot_fiscal_quarter_date,
+    base.snapshot_fiscal_quarter_number_absolute,
+    base.snapshot_last_month_of_fiscal_quarter,
+    base.snapshot_last_month_of_fiscal_year,
+    base.snapshot_days_in_fiscal_quarter_count AS final_day_of_fiscal_quarter, 
+    created_arr.sales_qualified_source_name,
+    created_arr.sales_qualified_source_grouped,
+    created_arr.order_type,
+    created_arr.order_type_grouped,
+    booked_arr.total_booked_arr,
+    created_arr.total_created_arr
+  FROM base 
   INNER JOIN created_arr
-    ON distinct_quarters.first_day_of_fiscal_quarter = created_arr.arr_created_fiscal_quarter_date
+    ON base.snapshot_fiscal_quarter_date = created_arr.arr_created_fiscal_quarter_date
   INNER JOIN booked_arr
-    ON distinct_quarters.first_day_of_fiscal_quarter = booked_arr.close_fiscal_quarter_date
+    ON created_arr.arr_created_fiscal_quarter_date = booked_arr.close_fiscal_quarter_date
+      AND created_arr.sales_qualified_source_name = booked_arr.sales_qualified_source_name
+        AND created_arr.sales_qualified_source_grouped = booked_arr.sales_qualified_source_grouped 
+          AND created_arr.order_type = booked_arr.order_type
+            AND created_arr.order_type_grouped = booked_arr.order_type_grouped
 )
 
 SELECT * 
