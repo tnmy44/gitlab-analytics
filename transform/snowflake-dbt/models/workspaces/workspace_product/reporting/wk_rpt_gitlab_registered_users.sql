1310bc3c6352b37050bd31beb49571efd57cbc56
Carolyn Braza <cbraza@gitlab.com>
2024-07-10 16:53:00 -0700

    add file

---
+++ transform/snowflake-dbt/models/workspaces/workspace_product/reporting/wk_rpt_gitlab_registered_users.sql
@@ -0,0 +1,72 @@
+{{ config(
+    materialized='table',
+    tags=["mnpi_exception", "product"]
+) }}
+
+{{ simple_cte([
+    ('dim_ping_instance', 'dim_ping_instance'),
+    ('mart_arr', 'mart_arr'),
+    ('dim_date', 'dim_date')
+    ])
+}},
+
+user_counts AS (
+
+  SELECT
+    ping_created_date_month  AS reporting_month,
+    ping_delivery_type       AS delivery_type,
+    ping_deployment_type     AS deployment_type,
+    SUM(instance_user_count) AS total_user_count
+  FROM dim_ping_instance
+  WHERE ping_created_date_month BETWEEN '2022-01-01' AND DATE_TRUNC('month', CURRENT_DATE) --arbitrary date range, exclude current month
+    AND is_last_ping_of_month = TRUE
+  GROUP BY 1, 2, 3
+
+),
+
+paid_seats AS (
+
+  SELECT
+    arr_month,
+    product_delivery_type,
+    product_deployment_type,
+    SUM(quantity) AS seat_count
+  FROM mart_arr
+  WHERE arr_month BETWEEN '2022-01-01' AND DATE_TRUNC('month', CURRENT_DATE) - 1 --arbitrary date range, exclude current month
+    AND product_category = 'Base Products' --do not pull in add-ons, could cause double-counting of users
+    AND subscription_status IN ('Active', 'Cancelled')
+  GROUP BY 1, 2, 3
+
+),
+
+counts_combined AS (
+
+  SELECT
+    user_counts.reporting_month,
+    dim_date.is_first_day_of_last_month_of_fiscal_quarter,
+    dim_date.fiscal_quarter_name_fy,
+    user_counts.delivery_type,
+    user_counts.deployment_type,
+    user_counts.total_user_count,
+    CASE
+      WHEN paid_seats.seat_count > user_counts.total_user_count THEN user_counts.total_user_count --handle cases where more seats are sold than there are reported registered users (ex: Dedicated)
+      ELSE paid_seats.seat_count --using seat count to define paid 
+    END                                AS paid_user_count,
+    total_user_count - paid_user_count AS free_user_count --defining free as total-paid
+  FROM user_counts
+  INNER JOIN paid_seats
+    ON user_counts.reporting_month = paid_seats.arr_month
+      AND user_counts.delivery_type = paid_seats.product_delivery_type
+      AND user_counts.deployment_type = paid_seats.product_deployment_type
+  INNER JOIN dim_date
+    ON user_counts.reporting_month = dim_date.date_actual
+
+)
+
+{{ dbt_audit(
+    cte_ref="counts_combined",
+    created_by="@cbraza",
+    updated_by="@cbraza",
+    created_date="2024-07-11",
+    updated_date="2024-07-11"
+) }}
