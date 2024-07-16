5143af2bc0fa87e4b623a6936457c73a94a4b5a9
michellecooper <mcooper@gitlab.com>
2024-07-16 10:40:47 -0700

    move to single function for different resource types and update to use file under snowflake-dbt folder, added to gitignore

--- transform/snowflake-dbt/dbt_dependency_check.py
+++ transform/snowflake-dbt/dbt_dependency_check.py
@@ -1,11 +1,12 @@
 """Module returning a csv with downstream dbt dependencies and data science exposures for a list of model provided by the user."""
 
+import os
 import csv
 import pandas as pd
 from dbt.cli.main import dbtRunner, dbtRunnerResult
 
 
-def dbt_model_dependencies_list(model_name_list: list) -> list:
+def dbt_model_dependencies_list(model_name_list: list, resource_type: str) -> list:
     """Takes in a list of models, returns the model and its dependencies"""
 
     # create empty lists to hold models
@@ -16,10 +17,15 @@ def dbt_model_dependencies_list(model_name_list: list) -> list:
 
     # for each create CLI args as a list of strings
     for model in model_name_list:
-        cli_args_model = ["list", "--select", f"{model}+", "--resource-type=model"]
+        cli_args = [
+            "list",
+            "--select",
+            f"{model}+",
+            f"--resource-type={resource_type}"
+        ]
 
         # run the command for models
-        res_models: dbtRunnerResult = dbt.invoke(cli_args_model)
+        res_models: dbtRunnerResult = dbt.invoke(cli_args)
 
         # get counts
         if res_models.result is not None:
@@ -38,77 +44,43 @@ def dbt_model_dependencies_list(model_name_list: list) -> list:
 
     return model_dependencies
 
+# Find directory name
+dirname = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-def dbt_model_exposures_list(model_name_list: list) -> list:
-    """Takes in a list of models, returns the model and its data science exposures"""
+"""
+Navigate to the file models_to_check.csv and add the dbt model names (ex. fct_behavior_structured_event).
+The file should contain a list of model names to test with no header, with one model name per line.
 
-    # create empty lists to hold exposures
-    model_exposures = []
+example file list:
+gitlab_dotcom_audit_events
+dim_crm_opportunity
+"""
+fileToReadPath = f"{dirname}/snowflake-dbt/models_to_check.csv"
 
-    # initialize dbt invoke
-    dbt = dbtRunner()
+# Create empty set to append the cleaned model names to
+model_names = set()
 
-    # for each create CLI args as a list of strings
-    for model in model_name_list:
-        cli_args_exposure = [
-            "list",
-            "--select",
-            f"{model}+",
-            "--resource-type=exposure",
-        ]
-
-        # run the command for exposures
-        res_exposures: dbtRunnerResult = dbt.invoke(cli_args_exposure)
-
-        # get counts
-        if res_exposures.result is not None:
-            count_exposures = len(res_exposures.result)
-        else:
-            count_exposures = 0
-
-        # append results in table for exposures
-        model_exposures.append(
-            {
-                "model_name": f"{model}",
-                "exposures": res_exposures.result,
-                "count_exposures": count_exposures,
-            }
-        )
-
-    return model_exposures
-
-
-model_names = []
-
-# responses should be of the form
-# full/path/from/home/directory/to/file/my_read_and_write_files.csv
-# file should contain a comma-separated list of models to test with no header
-
-fileToReadPath = input("Provide the full path to your read file: ")
-
+# open file and add models to empty list for testing
 with open(fileToReadPath, "r", newline="", encoding="utf-8") as readFile:
-    reader = csv.reader(
-        readFile, skipinitialspace=True, delimiter=",", quoting=csv.QUOTE_NONE
-    )
-    for row in reader:
-        model_names.append(row)
-
-model_list = model_names[0]
+    lines = readFile.readlines()
+    for line in lines:
+        model_names.add(line.strip().lower())
 
 # generate results
-models_with_dependencies = dbt_model_dependencies_list(model_list)
-models_with_exposures = dbt_model_exposures_list(model_list)
+# Note: model dependencies count includes the model requested + all downstream, so the minimum number of dependencies is 1
+models_with_dependencies = dbt_model_dependencies_list(model_names, "model")
+models_with_exposures = dbt_model_dependencies_list(model_names, "exposure")
 
-# convert lists to dataframes in order to merge
+# convert lists to dataframes in order to merge the results into a single output
 models_with_dependencies_df = pd.DataFrame(models_with_dependencies)
 models_with_exposures_df = pd.DataFrame(models_with_exposures)
 
-# merge
+# merge the two results on model name to get a single output
 models_with_dependencies_and_exposures = pd.merge(
     models_with_dependencies_df, models_with_exposures_df, on="model_name"
 )
 
-# create csv
+# create csv of the output called models_with_dependencies.csv
 with open("models_with_dependencies.csv", "w", encoding="utf-8") as csvfile:
     models_with_dependencies_and_exposures.to_csv(
         "models_with_dependencies.csv", index=False
