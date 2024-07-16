c446d9f7637beefad51ec991b5e6fd71917de77c
Lis Vinueza <lvinueza@gitlab.com>
2024-07-16 16:22:43 +0000

    Fix

--- transform/snowflake-dbt/models/common_prep/prep_note.sql
+++ transform/snowflake-dbt/models/common_prep/prep_note.sql
@@ -3,9 +3,9 @@
 ) }}
 
 {{ config({
-    materialized="incremental",
-    unique_key="dim_note_id",
-    on_schema_change="sync_all_columns"
+    "materialized": "incremental",
+    "unique_key": "dim_note_id",
+    "on_schema_change": "sync_all_columns"
     })
 }}
 
