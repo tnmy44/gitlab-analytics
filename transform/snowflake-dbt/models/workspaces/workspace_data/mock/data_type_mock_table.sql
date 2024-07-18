0127f0740093a633ffd17b040ba1c7b52dcbd963
Peter Empey <pempey@gitlab.com>
2024-07-18 09:41:42 -0600

    ci test can vars quote

--- transform/snowflake-dbt/models/workspaces/workspace_data/mock/data_type_mock_table.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_data/mock/data_type_mock_table.sql
@@ -9,6 +9,6 @@ SELECT
   '2022-04-18 01:01:01'::TIMESTAMP AS the_timestamp,
   TRUE::BOOLEAN AS the_boolean
 
-  -- comment
+  -- comment "{{ var('key') }}"
 
   --  {{ source('mock_source','mock_source_table') }}
