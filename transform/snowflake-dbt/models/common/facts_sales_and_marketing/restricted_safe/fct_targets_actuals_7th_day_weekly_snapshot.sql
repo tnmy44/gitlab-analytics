0c073550c293c4246433cf736af5afad19c15c50
Lis Vinueza <lvinueza@gitlab.com>
2024-07-18 15:59:59 +0200

    Formatting

--- transform/snowflake-dbt/models/common/facts_sales_and_marketing/restricted_safe/fct_targets_actuals_7th_day_weekly_snapshot.sql
+++ transform/snowflake-dbt/models/common/facts_sales_and_marketing/restricted_safe/fct_targets_actuals_7th_day_weekly_snapshot.sql
@@ -21,13 +21,13 @@ prep_date AS (
 
 live_actuals AS (
 
-  SELECT  
+  SELECT
     fct_crm_opportunity.*,
-    close_date.first_day_of_fiscal_quarter AS close_fiscal_quarter_date,
-    close_date.fiscal_quarter_name_fy AS close_fiscal_quarter_name
+    close_date.first_day_of_fiscal_quarter                                                               AS close_fiscal_quarter_date,
+    close_date.fiscal_quarter_name_fy                                                                    AS close_fiscal_quarter_name
   FROM {{ ref('fct_crm_opportunity') }}
   INNER JOIN prep_date AS close_date
-    ON close_date.date_id = fct_crm_opportunity.close_date_id
+    ON fct_crm_opportunity.close_date_id = close_date.date_id
 
 ),
 
@@ -40,13 +40,13 @@ spine AS (
 total_targets AS (
 
   SELECT
-    prep_date.fiscal_quarter_name_fy                                                     AS fiscal_quarter_name,
-    prep_date.first_day_of_fiscal_quarter                                                AS fiscal_quarter_date,
-    targets.dim_crm_user_hierarchy_sk                                                    AS dim_crm_current_account_set_hierarchy_sk,
+    prep_date.fiscal_quarter_name_fy                                                                     AS fiscal_quarter_name,
+    prep_date.first_day_of_fiscal_quarter                                                                AS fiscal_quarter_date,
+    targets.dim_crm_user_hierarchy_sk                                                                    AS dim_crm_current_account_set_hierarchy_sk,
     targets.dim_sales_qualified_source_id,
     targets.dim_order_type_id,
-    SUM(CASE WHEN kpi_name = 'Net ARR' THEN daily_allocated_target END)                  AS net_arr_total_quarter_target,
-    SUM(CASE WHEN kpi_name = 'Net ARR Pipeline Created' THEN daily_allocated_target END) AS pipeline_created_total_quarter_target
+    SUM(CASE WHEN targets.kpi_name = 'Net ARR' THEN targets.daily_allocated_target END)                  AS net_arr_total_quarter_target,
+    SUM(CASE WHEN targets.kpi_name = 'Net ARR Pipeline Created' THEN targets.daily_allocated_target END) AS pipeline_created_total_quarter_target
   FROM targets
   LEFT JOIN prep_date
     ON targets.target_date_id = prep_date.date_id
@@ -64,10 +64,10 @@ daily_actuals AS (
     actuals.dim_crm_current_account_set_hierarchy_sk,
     actuals.dim_sales_qualified_source_id,
     actuals.dim_order_type_id,
-    SUM(booked_net_arr_in_snapshot_quarter)     AS booked_net_arr_in_snapshot_quarter,
-    SUM(open_1plus_net_arr_in_snapshot_quarter) AS open_1plus_net_arr_in_snapshot_quarter,
-    SUM(open_3plus_net_arr_in_snapshot_quarter) AS open_3plus_net_arr_in_snapshot_quarter,
-    SUM(open_4plus_net_arr_in_snapshot_quarter) AS open_4plus_net_arr_in_snapshot_quarter
+    SUM(booked_net_arr_in_snapshot_quarter)                                                              AS booked_net_arr_in_snapshot_quarter,
+    SUM(open_1plus_net_arr_in_snapshot_quarter)                                                          AS open_1plus_net_arr_in_snapshot_quarter,
+    SUM(open_3plus_net_arr_in_snapshot_quarter)                                                          AS open_3plus_net_arr_in_snapshot_quarter,
+    SUM(open_4plus_net_arr_in_snapshot_quarter)                                                          AS open_4plus_net_arr_in_snapshot_quarter
   FROM actuals
   {{ dbt_utils.group_by(n=7) }}
 ),
@@ -80,7 +80,7 @@ quarterly_actuals AS (
     live_actuals.dim_crm_current_account_set_hierarchy_sk,
     live_actuals.dim_sales_qualified_source_id,
     live_actuals.dim_order_type_id,
-    SUM(live_actuals.booked_net_arr)            AS total_booked_net_arr
+    SUM(live_actuals.booked_net_arr) AS total_booked_net_arr
   FROM live_actuals
   {{ dbt_utils.group_by(n=5) }}
 
@@ -158,18 +158,17 @@ final AS (
     base.dim_crm_current_account_set_hierarchy_sk,
     base.dim_order_type_id,
     base.dim_sales_qualified_source_id,
-    SUM(total_targets.pipeline_created_total_quarter_target)                AS pipeline_created_total_quarter_target,
-
-    SUM(total_targets.net_arr_total_quarter_target)                         AS net_arr_total_quarter_target,
+    SUM(total_targets.pipeline_created_total_quarter_target)                                             AS pipeline_created_total_quarter_target,
+    SUM(total_targets.net_arr_total_quarter_target)                                                      AS net_arr_total_quarter_target,
 
     CASE WHEN base.date_actual = base.last_day_of_fiscal_quarter
-      THEN SUM(quarterly_actuals.total_booked_net_arr)
-      ELSE SUM(daily_actuals.booked_net_arr_in_snapshot_quarter)                   
-    END AS coverage_booked_net_arr,
-    SUM(daily_actuals.open_1plus_net_arr_in_snapshot_quarter)               AS coverage_open_1plus_net_arr,
-    SUM(daily_actuals.open_3plus_net_arr_in_snapshot_quarter)               AS coverage_open_3plus_net_arr,
-    SUM(daily_actuals.open_4plus_net_arr_in_snapshot_quarter)               AS coverage_open_4plus_net_arr,
-    SUM(quarterly_actuals.total_booked_net_arr)                             AS total_booked_net_arr
+        THEN SUM(quarterly_actuals.total_booked_net_arr)
+      ELSE SUM(daily_actuals.booked_net_arr_in_snapshot_quarter)
+    END                                                                                                  AS coverage_booked_net_arr,
+    SUM(daily_actuals.open_1plus_net_arr_in_snapshot_quarter)                                            AS coverage_open_1plus_net_arr,
+    SUM(daily_actuals.open_3plus_net_arr_in_snapshot_quarter)                                            AS coverage_open_3plus_net_arr,
+    SUM(daily_actuals.open_4plus_net_arr_in_snapshot_quarter)                                            AS coverage_open_4plus_net_arr,
+    SUM(quarterly_actuals.total_booked_net_arr)                                                          AS total_booked_net_arr
   FROM base
   LEFT JOIN total_targets
     ON base.fiscal_quarter_name = total_targets.fiscal_quarter_name
--- transform/snowflake-dbt/models/common_mart_sales/reports/rpt_final_bookings.sql
+++ transform/snowflake-dbt/models/common_mart_sales/reports/rpt_final_bookings.sql
@@ -42,19 +42,19 @@ base AS (
     current_first_day_of_week,
     current_week_of_fiscal_quarter_normalised,
     current_week_of_fiscal_quarter,
-    fiscal_quarter_name_fy                       AS snapshot_fiscal_quarter_name,
-    fiscal_quarter_name_fy                       AS snapshot_fiscal_quarter_name_fy,
-    fiscal_quarter                               AS snapshot_fiscal_quarter,
-    fiscal_year                                  AS snapshot_fiscal_year,
-    first_day_of_fiscal_quarter                  AS snapshot_first_day_of_fiscal_quarter,
-    last_day_of_fiscal_quarter                   AS snapshot_last_day_of_fiscal_quarter,
-    first_day_of_fiscal_year                     AS snapshot_first_day_of_fiscal_year,
-    last_day_of_fiscal_year                      AS snapshot_last_day_of_fiscal_year,
-    first_day_of_fiscal_quarter                  AS snapshot_fiscal_quarter_date,
-    fiscal_quarter_number_absolute               AS snapshot_fiscal_quarter_number_absolute,
-    last_month_of_fiscal_quarter                 AS snapshot_last_month_of_fiscal_quarter,
-    last_month_of_fiscal_year                    AS snapshot_last_month_of_fiscal_year,
-    days_in_fiscal_quarter_count                 AS snapshot_days_in_fiscal_quarter_count,
+    fiscal_quarter_name_fy         AS snapshot_fiscal_quarter_name,
+    fiscal_quarter_name_fy         AS snapshot_fiscal_quarter_name_fy,
+    fiscal_quarter                 AS snapshot_fiscal_quarter,
+    fiscal_year                    AS snapshot_fiscal_year,
+    first_day_of_fiscal_quarter    AS snapshot_first_day_of_fiscal_quarter,
+    last_day_of_fiscal_quarter     AS snapshot_last_day_of_fiscal_quarter,
+    first_day_of_fiscal_year       AS snapshot_first_day_of_fiscal_year,
+    last_day_of_fiscal_year        AS snapshot_last_day_of_fiscal_year,
+    first_day_of_fiscal_quarter    AS snapshot_fiscal_quarter_date,
+    fiscal_quarter_number_absolute AS snapshot_fiscal_quarter_number_absolute,
+    last_month_of_fiscal_quarter   AS snapshot_last_month_of_fiscal_quarter,
+    last_month_of_fiscal_year      AS snapshot_last_month_of_fiscal_year,
+    days_in_fiscal_quarter_count   AS snapshot_days_in_fiscal_quarter_count,
     dim_sales_qualified_source.sales_qualified_source_name,
     dim_sales_qualified_source.sales_qualified_source_grouped,
     dim_order_type.order_type_name,
@@ -63,17 +63,17 @@ base AS (
     sales_type.sales_type,
     stage_name.stage_name
   FROM dim_date
-  JOIN dim_sales_qualified_source
-  JOIN dim_order_type
-  JOIN dim_deal_path
-  JOIN sales_type
-  JOIN stage_name
+  INNER JOIN dim_sales_qualified_source
+  INNER JOIN dim_order_type
+  INNER JOIN dim_deal_path
+  INNER JOIN sales_type
+  INNER JOIN stage_name
 
 ),
 
 booked_arr AS (
 
-  SELECT 
+  SELECT
     close_fiscal_quarter_date,
     sales_qualified_source_name,
     sales_qualified_source_grouped,
@@ -82,9 +82,9 @@ booked_arr AS (
     deal_path_name,
     sales_type,
     stage_name,
-    SUM(booked_net_arr)     AS total_booked_arr
+    SUM(booked_net_arr) AS total_booked_arr
   FROM live_actuals
-  {{ dbt_utils.group_by(n=8)}}
+  {{ dbt_utils.group_by(n=8) }}
 
 ),
 
@@ -99,10 +99,10 @@ created_arr AS (
     deal_path_name,
     sales_type,
     stage_name,
-    SUM(created_arr)        AS total_created_arr
+    SUM(created_arr) AS total_created_arr
   FROM live_actuals
-  {{ dbt_utils.group_by(n=8)}}
-), 
+  {{ dbt_utils.group_by(n=8) }}
+),
 
 final AS (
 
@@ -130,40 +130,40 @@ final AS (
     base.snapshot_fiscal_quarter_number_absolute,
     base.snapshot_last_month_of_fiscal_quarter,
     base.snapshot_last_month_of_fiscal_year,
-    base.snapshot_days_in_fiscal_quarter_count + 1 AS snapshot_day_of_fiscal_quarter, 
-    base.sales_qualified_source_name AS sales_qualified_source_name_live,
-    base.sales_qualified_source_grouped AS sales_qualified_source_grouped_live,
-    base.order_type_name AS order_type_live,
-    base.order_type_grouped AS order_type_grouped_live,
-    base.deal_path_name AS deal_path_live,
-    base.sales_type AS sales_type,
-    base.stage_name AS stage_name,
-    booked_arr.total_booked_arr,
-    created_arr.total_created_arr
-  FROM base 
+    base.snapshot_days_in_fiscal_quarter_count + 1 AS snapshot_day_of_fiscal_quarter,
+    base.sales_qualified_source_name               AS sales_qualified_source_name_live,
+    base.sales_qualified_source_grouped            AS sales_qualified_source_grouped_live,
+    base.order_type_name                           AS order_type_live,
+    base.order_type_grouped                        AS order_type_grouped_live,
+    base.deal_path_name                            AS deal_path_live,
+    base.sales_type,
+    base.stage_name,
+    booked_arr.total_booked_arr                    AS booked_net_arr_in_snapshot_quarter,
+    created_arr.total_created_arr                  AS created_arr_in_snapshot_quarter
+  FROM base
   LEFT JOIN created_arr
     ON base.snapshot_fiscal_quarter_date = created_arr.arr_created_fiscal_quarter_date
       AND base.sales_qualified_source_name = created_arr.sales_qualified_source_name
-        AND base.sales_qualified_source_grouped = created_arr.sales_qualified_source_grouped 
-          AND base.order_type_name = created_arr.order_type
-            AND base.order_type_grouped = created_arr.order_type_grouped
-              AND base.deal_path_name = created_arr.deal_path_name
-               AND base.sales_type = created_arr.sales_type
-               AND base.stage_name = created_arr.stage_name
+      AND base.sales_qualified_source_grouped = created_arr.sales_qualified_source_grouped
+      AND base.order_type_name = created_arr.order_type
+      AND base.order_type_grouped = created_arr.order_type_grouped
+      AND base.deal_path_name = created_arr.deal_path_name
+      AND base.sales_type = created_arr.sales_type
+      AND base.stage_name = created_arr.stage_name
   LEFT JOIN booked_arr
     ON base.snapshot_fiscal_quarter_date = booked_arr.close_fiscal_quarter_date
       AND base.sales_qualified_source_name = booked_arr.sales_qualified_source_name
-        AND base.sales_qualified_source_grouped = booked_arr.sales_qualified_source_grouped 
-          AND base.order_type_name = booked_arr.order_type
-            AND base.order_type_grouped = booked_arr.order_type_grouped
-              AND base.deal_path_name = booked_arr.deal_path_name
-                AND base.sales_type = booked_arr.sales_type
-                AND base.stage_name = booked_arr.stage_name
+      AND base.sales_qualified_source_grouped = booked_arr.sales_qualified_source_grouped
+      AND base.order_type_name = booked_arr.order_type
+      AND base.order_type_grouped = booked_arr.order_type_grouped
+      AND base.deal_path_name = booked_arr.deal_path_name
+      AND base.sales_type = booked_arr.sales_type
+      AND base.stage_name = booked_arr.stage_name
   WHERE base.current_first_day_of_fiscal_quarter > base.snapshot_fiscal_quarter_date
     AND base.snapshot_fiscal_quarter_date >= DATEADD(QUARTER, -9, CURRENT_DATE())
-      AND (total_booked_arr IS NOT NULL OR total_created_arr IS NOT NULL)
+    AND (total_booked_arr IS NOT NULL OR total_created_arr IS NOT NULL)
 
 )
 
-SELECT * 
+SELECT *
 FROM final
