2b57cbad6af3af59cb33a79be5d9156d11c7a767
Chris Sharp <csharp@gitlab.com>
2024-07-19 12:09:00 +0100

    apply jinja and formatting

--- transform/snowflake-dbt/models/common_mart_sales/restricted_safe/reports/rpt_gtm_crm_actuals.sql
+++ transform/snowflake-dbt/models/common_mart_sales/restricted_safe/reports/rpt_gtm_crm_actuals.sql
@@ -1,112 +1,125 @@
-with created_actuals AS (
-SELECT
-    DIM_CRM_OPPORTUNITY_ID,
-    DIM_ORDER_TYPE_ID,
-    DIM_SALES_QUALIFIED_SOURCE_ID,
-    DIM_CRM_CURRENT_ACCOUNT_SET_HIERARCHY_SK,
-    arr_created_date                as actual_date,
+{{ simple_cte([
+    ('fct_crm_opportunity','fct_crm_opportunity'),
+    ('dim_date','dim_date'),
+    ('mart_crm_opportunity', 'mart_crm_opportunity')
+]) }},
+
+created_actuals AS (
+  SELECT
+    dim_crm_opportunity_id,
+    dim_order_type_id,
+    dim_sales_qualified_source_id,
+    dim_crm_current_account_set_hierarchy_sk,
+    arr_created_date AS actual_date,
     CASE WHEN is_net_arr_pipeline_created = TRUE
-            AND arr_created_date IS NOT NULL
-        THEN net_arr END            AS net_arr_pipeline_created,
-    NULL                            AS net_arr_closed,
-    NULL                            AS new_logo_count_closed,
-    NULL                            AS deal_ids_count,
-    NULL                            AS sao_net_arr,
-    NULL                            AS sao_opportunity_id_count
-FROM PROD.restricted_safe_common.fct_crm_opportunity
-WHERE is_net_arr_pipeline_created = TRUE
+        AND arr_created_date IS NOT NULL
+        THEN net_arr
+    END              AS net_arr_pipeline_created,
+    NULL             AS net_arr_closed,
+    NULL             AS new_logo_count_closed,
+    NULL             AS deal_ids_count,
+    NULL             AS sao_net_arr,
+    NULL             AS sao_opportunity_id_count
+  FROM fct_crm_opportunity
+  WHERE is_net_arr_pipeline_created = TRUE
     AND arr_created_date IS NOT NULL
-)
+),
 
-, closed_actuals as (
-SELECT
-    DIM_CRM_OPPORTUNITY_ID,
-    DIM_ORDER_TYPE_ID,
-    DIM_SALES_QUALIFIED_SOURCE_ID,
-    DIM_CRM_CURRENT_ACCOUNT_SET_HIERARCHY_SK,
-    close_date                      as actual_date,
-    NULL                            as net_arr_pipeline_created,
+closed_actuals AS (
+  SELECT
+    dim_crm_opportunity_id,
+    dim_order_type_id,
+    dim_sales_qualified_source_id,
+    dim_crm_current_account_set_hierarchy_sk,
+    close_date AS actual_date,
+    NULL       AS net_arr_pipeline_created,
     CASE
-        WHEN FPA_MASTER_BOOKINGS_FLAG = TRUE
-            THEN BOOKED_NET_ARR END        AS net_arr_closed,
+      WHEN fpa_master_bookings_flag = TRUE
+        THEN booked_net_arr
+    END        AS net_arr_closed,
     CASE
-        WHEN is_new_logo_first_order = TRUE
-            THEN new_logo_count END AS new_logo_count_closed,
+      WHEN is_new_logo_first_order = TRUE
+        THEN new_logo_count
+    END        AS new_logo_count_closed,
     CASE
-        WHEN NEW_LOGO_COUNT >=0
-            THEN 1
-        WHEN NEW_LOGO_COUNT = -1 THEN -1
-        END                         AS deal_ids_count,
-    NULL                            AS sao_net_arr,
-    NULL                            AS sao_opportunity_id_count
-FROM PROD.restricted_safe_common.fct_crm_opportunity
-WHERE FPA_MASTER_BOOKINGS_FLAG = TRUE)
+      WHEN new_logo_count >= 0
+        THEN 1
+      WHEN new_logo_count = -1 THEN -1
+    END        AS deal_ids_count,
+    NULL       AS sao_net_arr,
+    NULL       AS sao_opportunity_id_count
+  FROM fct_crm_opportunity
+  WHERE fpa_master_bookings_flag = TRUE
+),
 
-, sao_actuals as (
-SELECT
-    DIM_CRM_OPPORTUNITY_ID,
-    DIM_ORDER_TYPE_ID,
-    DIM_SALES_QUALIFIED_SOURCE_ID,
-    DIM_CRM_CURRENT_ACCOUNT_SET_HIERARCHY_SK,
-    sales_accepted_date as actual_date,
-    NULL                            AS net_arr_pipeline_created,
-    NULL                            AS net_arr_closed,
-    NULL                            AS new_logo_count_closed,
-    NULL                            AS deal_ids_count,
-    CASE WHEN FPA_MASTER_BOOKINGS_FLAG = TRUE
-        THEN net_arr END            AS sao_net_arr,
+sao_actuals AS (
+  SELECT
+    dim_crm_opportunity_id,
+    dim_order_type_id,
+    dim_sales_qualified_source_id,
+    dim_crm_current_account_set_hierarchy_sk,
+    sales_accepted_date AS actual_date,
+    NULL                AS net_arr_pipeline_created,
+    NULL                AS net_arr_closed,
+    NULL                AS new_logo_count_closed,
+    NULL                AS deal_ids_count,
+    CASE WHEN fpa_master_bookings_flag = TRUE
+        THEN net_arr
+    END                 AS sao_net_arr,
     CASE WHEN is_sao = TRUE
-        THEN 1 END AS                  sao_opportunity_id_count
-FROM PROD.restricted_safe_common.fct_crm_opportunity
-)
+        THEN 1
+    END                 AS sao_opportunity_id_count
+  FROM fct_crm_opportunity
+),
 
 --union
-, unioned_actuals AS (
-SELECT *
-FROM created_actuals
+unioned_actuals AS (
+  SELECT *
+  FROM created_actuals
 
-UNION
+  UNION
 
-SELECT *
-FROM closed_actuals
+  SELECT *
+  FROM closed_actuals
 
-UNION
+  UNION
 
-SELECT *
-FROM sao_actuals
+  SELECT *
+  FROM sao_actuals
 
+),
+
+main_actuals AS (
+  SELECT
+    unioned_actuals.dim_crm_opportunity_id,
+    unioned_actuals.dim_order_type_id,
+    unioned_actuals.dim_sales_qualified_source_id,
+    unioned_actuals.dim_crm_current_account_set_hierarchy_sk,
+    unioned_actuals.actual_date,
+    unioned_actuals.net_arr_pipeline_created,
+    unioned_actuals.net_arr_closed,
+    unioned_actuals.new_logo_count_closed,
+    unioned_actuals.deal_ids_count,
+    unioned_actuals.sao_net_arr,
+    unioned_actuals.sao_opportunity_id_count AS sao_count,
+    dim_date.date_id                         AS actual_date_id,
+    mart_crm_opportunity.product_category,
+    mart_crm_opportunity.deal_size,
+    dim_date.fiscal_quarter_name_fy,
+    dim_date.fiscal_year
+  FROM unioned_actuals
+  LEFT JOIN dim_date
+    ON unioned_actuals.actual_date = dim_date.date_actual
+  LEFT JOIN mart_crm_opportunity
+    ON unioned_actuals.dim_crm_opportunity_id = mart_crm_opportunity.dim_crm_opportunity_id
+  WHERE fiscal_year > 2020
+  ORDER BY 1 DESC
 )
 
-, main_actuals AS (
-SELECT unioned_actuals.DIM_CRM_OPPORTUNITY_ID,
-       unioned_actuals.DIM_ORDER_TYPE_ID,
-       unioned_actuals.DIM_SALES_QUALIFIED_SOURCE_ID,
-       unioned_actuals.DIM_CRM_CURRENT_ACCOUNT_SET_HIERARCHY_SK,
-       unioned_actuals.actual_date,
-       unioned_actuals.net_arr_pipeline_created,
-       unioned_actuals.net_arr_closed,
-       unioned_actuals.new_logo_count_closed,
-       unioned_actuals.deal_ids_count,
-       unioned_actuals.sao_net_arr,
-       unioned_actuals.sao_opportunity_id_count as sao_count,
-       DIM_DATE.DATE_ID AS actual_date_id,
-       MART_CRM_OPPORTUNITY.PRODUCT_CATEGORY,
-       MART_CRM_OPPORTUNITY.DEAL_SIZE,
-       dim_date.FISCAL_QUARTER_NAME_FY,
-       dim_date.FISCAL_YEAR
-    --COUNT(DISTINCT sao_opportunity_id) AS sao_count,
-    --(DISTINCT positive_deal_ids) - COUNT(DISTINCT negative_deal_ids) AS deal_count
-FROM unioned_actuals
-LEFT JOIN COMMON.DIM_DATE
-ON unioned_actuals.actual_date = DIM_DATE.DATE_ACTUAL
-LEFT JOIN PROD.RESTRICTED_SAFE_COMMON_MART_SALES.MART_CRM_OPPORTUNITY
-ON unioned_actuals.DIM_CRM_OPPORTUNITY_ID = MART_CRM_OPPORTUNITY.DIM_CRM_OPPORTUNITY_ID
-WHERE FISCAL_YEAR > 2020
-ORDER BY 1 desc
-)
-
-SELECT FISCAL_QUARTER_NAME_FY, SUM(deal_ids_count)
+SELECT
+  fiscal_quarter_name_fy,
+  SUM(deal_ids_count) AS deal_count
 FROM main_actuals
-WHERE FISCAL_YEAR > 2019
+WHERE fiscal_year > 2019
 GROUP BY 1
-ORDER BY 1 desc
+ORDER BY 1 DESC
