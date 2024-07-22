b71d7fea51306792c188abc93d69bf6014b26589
Radovan Bacovic <rbacovic@gitlab.com>
2024-07-22 14:09:15 +0200

    pod_env_vars added in a better way.

--- dags/extract/data_classification.py
+++ dags/extract/data_classification.py
@@ -28,8 +28,11 @@ from kube_secrets import (
 from kubernetes_helpers import get_affinity, get_toleration
 
 env = os.environ.copy()
-pod_env_vars = {**gitlab_pod_env_vars}
-pod_env_vars["RUN_DATE"] = "{{ next_ds }}"
+pod_env_vars = {
+    "RUN_DATE": "{{ next_ds }}",
+}
+
+pod_env_vars = {**gitlab_pod_env_vars, **pod_env_vars}
 
 DAG_NAME = "data_classification"
 
@@ -64,7 +67,9 @@ def get_command(task: str):
         operation = "CLASSIFY"
 
     unset = Variable.get("DATA_CLASSIFICATION_UNSET", default_var="False")
-    tagging_type = Variable.get("DATA_CLASSIFICATION_TAGGING_TYPE", default_var="INCREMENTAL")
+    tagging_type = Variable.get(
+        "DATA_CLASSIFICATION_TAGGING_TYPE", default_var="INCREMENTAL"
+    )
 
     commands = {
         # "extract_classification": f"""{dbt_install_deps_cmd} && dbt --profiles-dir profile --target prod --quiet ls --models tag:mnpi+ --exclude tag:mnpi_exception config.database:$SNOWFLAKE_PREP_DATABASE config.schema:restricted_safe_common config.schema:restricted_safe_common_mapping config.schema:restricted_safe_common_mart_finance config.schema:restricted_safe_common_mart_sales config.schema:restricted_safe_common_mart_marketing config.schema:restricted_safe_common_mart_product config.schema:restricted_safe_common_prep config.schema:restricted_safe_legacy config.schema:restricted_safe_workspace_finance config.schema:restricted_safe_workspace_sales config.schema:restricted_safe_workspace_marketing config.schema:restricted_safe_workspace_engineering --output json > safe_models.json; ret=$?;""",
