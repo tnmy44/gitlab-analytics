0639a9d91d3445ce70400c7e2d44892f535fd00e
Radovan Bacovic <rbacovic@gitlab.com>
2024-07-22 13:54:37 +0200

    Add variables.

--- dags/extract/data_classification.py
+++ dags/extract/data_classification.py
@@ -4,7 +4,7 @@ Unit to run data classification tasks
 
 import os
 from datetime import datetime, timedelta
-
+from airflow.models import Variable
 from airflow import DAG
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 from airflow_utils import (
@@ -28,7 +28,9 @@ from kube_secrets import (
 from kubernetes_helpers import get_affinity, get_toleration
 
 env = os.environ.copy()
-pod_env_vars = {**gitlab_pod_env_vars, **{"RUN_DATE": "{{ ds }}"}}
+pod_env_vars = {**gitlab_pod_env_vars}
+pod_env_vars["RUN_DATE"]= "{ds}"
+
 DAG_NAME = "data_classification"
 
 DAG_DESCRIPTION = "This DAG run to identify data classification for MNPI and PII data."
@@ -61,8 +63,8 @@ def get_command(task: str):
     else:
         operation = "CLASSIFY"
 
-    unset = "False"
-    tagging_type = "INCREMENTAL"
+    unset = Variable.get("DATA_CLASSIFICATION_UNSET", default_var="False")
+    tagging_type = Variable.get("DATA_CLASSIFICATION_TAGGING_TYPE", default_var="INCREMENTAL")
 
     commands = {
         # "extract_classification": f"""{dbt_install_deps_cmd} && dbt --profiles-dir profile --target prod --quiet ls --models tag:mnpi+ --exclude tag:mnpi_exception config.database:$SNOWFLAKE_PREP_DATABASE config.schema:restricted_safe_common config.schema:restricted_safe_common_mapping config.schema:restricted_safe_common_mart_finance config.schema:restricted_safe_common_mart_sales config.schema:restricted_safe_common_mart_marketing config.schema:restricted_safe_common_mart_product config.schema:restricted_safe_common_prep config.schema:restricted_safe_legacy config.schema:restricted_safe_workspace_finance config.schema:restricted_safe_workspace_sales config.schema:restricted_safe_workspace_marketing config.schema:restricted_safe_workspace_engineering --output json > safe_models.json; ret=$?;""",
