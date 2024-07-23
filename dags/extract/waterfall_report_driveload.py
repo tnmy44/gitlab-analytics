996d47a24970f70453258a284fd658dc3df72fe3
jjstark <jstark@gitlab.com>
2024-07-23 08:22:47 -0700

    add DAG & migrate drives

---
+++ dags/extract/waterfall_report_driveload.py
@@ -0,0 +1,93 @@
+import os
+from datetime import datetime, timedelta
+from yaml import safe_load, YAMLError
+
+from airflow import DAG
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow_utils import (
+    DATA_IMAGE,
+    clone_and_setup_extraction_cmd,
+    gitlab_defaults,
+    gitlab_pod_env_vars,
+    slack_failed_task,
+    REPO_BASE_PATH,
+)
+from kube_secrets import (
+    GCP_SERVICE_CREDS,
+    SNOWFLAKE_ACCOUNT,
+    SNOWFLAKE_LOAD_PASSWORD,
+    SNOWFLAKE_LOAD_ROLE,
+    SNOWFLAKE_LOAD_USER,
+    SNOWFLAKE_LOAD_WAREHOUSE,
+)
+from kubernetes_helpers import get_affinity, get_toleration
+
+# Load the env vars into a dict and set Secrets
+env = os.environ.copy()
+GIT_BRANCH = env["GIT_BRANCH"]
+# Change the DATABASE If the branch if not MASTER
+pod_env_vars = {**gitlab_pod_env_vars, **{}}
+
+# Default arguments for the DAG
+default_args = {
+    "depends_on_past": False,
+    "on_failure_callback": slack_failed_task,
+    "owner": "airflow",
+    "retries": 0,
+    "retry_delay": timedelta(minutes=1),
+    "start_date": datetime(2019, 1, 1),
+    "dagrun_timeout": timedelta(hours=2),
+}
+
+config_path = "extract/sheetload/zuora_waterfall/drives.yml"
+
+with open(f"{REPO_BASE_PATH}/{config_path}", "r") as file:
+    try:
+        stream = safe_load(file)
+    except YAMLError as exc:
+        print(exc)
+
+    folders = [folder for folder in stream["folders"]]
+
+# Create the DAG
+dag = DAG(
+    "waterfall_report_driveload",
+    default_args=default_args,
+    schedule_interval="15 */4 1-5 * 1-5", 
+    concurrency=1,
+    catchup=False,
+)
+
+for folder in folders:
+    folder_name = folder.get("folder_name")
+    table_name = folder.get("table_name")
+
+    # Set the command for the container
+    container_cmd = f"""
+        {clone_and_setup_extraction_cmd} &&
+        cd sheetload/ &&
+        python3 sheetload.py drive --drive_file zuora_waterfall/drives.yml --table_name {table_name}
+    """
+
+    cleaned_folder_name = folder_name.replace("_", "-")
+
+    # Task 1
+    folder_run = KubernetesPodOperator(
+        **gitlab_defaults,
+        image=DATA_IMAGE,
+        task_id=f"{cleaned_folder_name}-driveload",
+        name=f"{cleaned_folder_name}-driveload",
+        secrets=[
+            GCP_SERVICE_CREDS,
+            SNOWFLAKE_ACCOUNT,
+            SNOWFLAKE_LOAD_ROLE,
+            SNOWFLAKE_LOAD_USER,
+            SNOWFLAKE_LOAD_WAREHOUSE,
+            SNOWFLAKE_LOAD_PASSWORD,
+        ],
+        env_vars=pod_env_vars,
+        affinity=get_affinity("extraction"),
+        tolerations=get_toleration("extraction"),
+        arguments=[container_cmd],
+        dag=dag,
+    )
