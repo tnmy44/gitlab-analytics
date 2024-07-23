69809a64bee1eaf4505f4bfe83b1dcab09366465
Radovan Bacovic <rbacovic@gitlab.com>
2024-07-23 12:25:40 +0200

    Test log

--- extract/data_classification/data_classification_utils.py
+++ extract/data_classification/data_classification_utils.py
@@ -60,6 +60,7 @@ class DataClassification:
         Upload dataframe to Snowflake
         """
         try:
+            info(self.identify_mnpi_data.head())
             dataframe_uploader(
                 dataframe=self.identify_mnpi_data,
                 engine=self.loader_engine,
@@ -336,8 +337,6 @@ class DataClassification:
         Entry point to identify MNPI data
         """
         mnpi_list = self.load_mnpi_list()
-        for mnpi in mnpi_list:
-            info(str(mnpi))
         mnpi_data = self.transform_mnpi_list(mnpi_list=mnpi_list)
         mnpi_data_filtered = self.filter_data(mnpi_data=mnpi_data, section="MNPI")
         columns = [
