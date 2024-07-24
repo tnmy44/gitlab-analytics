7b48a1e15ea2aff9ca09c43d2465478ac184ce27
Radovan Bacovic <rbacovic@gitlab.com>
2024-07-24 14:42:42 +0200

    test case added.

--- extract/test/test_data_classification.py
+++ extract/test/test_data_classification.py
@@ -19,58 +19,11 @@ def test_quoted(data_classification):
 
 def test_double_quoted(data_classification):
     assert data_classification.double_quoted("test") == '"test"'
-#
-#
-# @patch("os.path.exists")
-# @patch("builtins.open", new_callable=MagicMock)
-# def test_load_mnpi_list(mock_open, mock_exists, data_classification):
-#     mock_exists.return_value = True
-#     mock_open.return_value.__enter__.return_value.readlines.return_value = [
-#         '{"config": {"database": "db", "schema": "schema"}, "alias": "table"}\n'
-#     ]
-#     result = data_classification.load_mnpi_list()
-#     assert result == [{"config": {"database": "db", "schema": "schema"}, "alias": "table"}]
-
 
 def test_transform_mnpi_list(data_classification):
     input_list = [{"config": {"database": "db", "schema": "schema"}, "alias": "table"}]
     result = data_classification.transform_mnpi_list(input_list)
     assert result == [["DB", "SCHEMA", "TABLE"]]
-
-#
-# @patch.object(DataClassification, "scope", new_callable=MagicMock)
-# def test_get_pii_scope(mock_scope, data_classification):
-#     mock_scope.get.return_value = {
-#         "PII": {
-#             "include": {
-#                 "databases": ["DB1", "DB2"],
-#                 "schemas": ["DB1.SCHEMA1", "DB2.*"],
-#                 "tables": ["DB1.SCHEMA1.TABLE1", "DB2.*.TABLE2"]
-#             }
-#         }
-#     }
-#     result = data_classification.get_pii_scope("PII", "include")
-#     assert "table_catalog IN ('DB1', 'DB2')" in result
-#     assert "table_schema = 'SCHEMA1'" in result
-#     assert "table_schema ILIKE '%'" in result
-#     assert "table_name = 'TABLE1'" in result
-#     assert "table_name = 'TABLE2'" in result
-
-
-# @patch.object(DataClassification, "scope", new_callable=MagicMock)
-# def test_pii_query(mock_scope, data_classification):
-#     mock_scope.get.return_value = {
-#         "PII": {
-#             "include": {"databases": ["DB1"]},
-#             "exclude": {"schemas": ["DB1.EXCLUDE_SCHEMA"]}
-#         }
-#     }
-#     query = data_classification.pii_query
-#     assert "INSERT INTO data_classification.sensitive_objects_classification" in query
-#     assert "WHERE 1=1" in query
-#     assert "AND (table_catalog IN ('DB1'))" in query
-#     assert "AND NOT (table_catalog = 'DB1' AND table_schema = 'EXCLUDE_SCHEMA')" in query
-
 
 @patch.object(DataClassification, "scope", new_callable=MagicMock)
 def test_get_mnpi_scope(mock_scope, data_classification):
@@ -118,3 +71,59 @@ def test_identify_mnpi_data(mock_dataframe, data_classification):
                     "classification_type", "created", "last_altered", "last_ddl",
                     "database_name", "schema_name", "table_name", "table_type"
                 ]
+
+def test_check_pii_query(data_classification):
+    """
+    Check PII query
+    """
+    query = data_classification.pii_query
+    print(query)
+
+# @patch("os.path.exists")
+# @patch("builtins.open", new_callable=MagicMock)
+# def test_load_mnpi_list(mock_open, mock_exists, data_classification):
+#     mock_exists.return_value = True
+#     mock_open.return_value.__enter__.return_value.readlines.return_value = [
+#         '{"config": {"database": "db", "schema": "schema"}, "alias": "table"}\n'
+#     ]
+#     result = data_classification.load_mnpi_list()
+#     assert result == [{"config": {"database": "db", "schema": "schema"}, "alias": "table"}]
+
+
+
+
+#
+# @patch.object(DataClassification, "scope", new_callable=MagicMock)
+# def test_get_pii_scope(mock_scope, data_classification):
+#     mock_scope.get.return_value = {
+#         "PII": {
+#             "include": {
+#                 "databases": ["DB1", "DB2"],
+#                 "schemas": ["DB1.SCHEMA1", "DB2.*"],
+#                 "tables": ["DB1.SCHEMA1.TABLE1", "DB2.*.TABLE2"]
+#             }
+#         }
+#     }
+#     result = data_classification.get_pii_scope("PII", "include")
+#     assert "table_catalog IN ('DB1', 'DB2')" in result
+#     assert "table_schema = 'SCHEMA1'" in result
+#     assert "table_schema ILIKE '%'" in result
+#     assert "table_name = 'TABLE1'" in result
+#     assert "table_name = 'TABLE2'" in result
+
+
+# @patch.object(DataClassification, "scope", new_callable=MagicMock)
+# def test_pii_query(mock_scope, data_classification):
+#     mock_scope.get.return_value = {
+#         "PII": {
+#             "include": {"databases": ["DB1"]},
+#             "exclude": {"schemas": ["DB1.EXCLUDE_SCHEMA"]}
+#         }
+#     }
+#     query = data_classification.pii_query
+#     assert "INSERT INTO data_classification.sensitive_objects_classification" in query
+#     assert "WHERE 1=1" in query
+#     assert "AND (table_catalog IN ('DB1'))" in query
+#     assert "AND NOT (table_catalog = 'DB1' AND table_schema = 'EXCLUDE_SCHEMA')" in query
+
+
