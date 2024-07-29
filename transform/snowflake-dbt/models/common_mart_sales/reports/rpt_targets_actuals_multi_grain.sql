257afd0c1d7f3b17f264eef3c207908648b527b7
Lis Vinueza <lvinueza@gitlab.com>
2024-07-29 19:39:12 +0200

    Linting

--- transform/snowflake-dbt/models/common_mart_sales/reports/rpt_targets_actuals_multi_grain.sql
+++ transform/snowflake-dbt/models/common_mart_sales/reports/rpt_targets_actuals_multi_grain.sql
@@ -21,16 +21,18 @@ WITH unioned AS (
 ),
 
 final AS (
-SELECT 
-  unioned.*,
-  MAX(CASE 
-        WHEN source != 'targets_actuals' AND snapshot_date < CURRENT_DATE() THEN snapshot_date 
-        ELSE NULL 
+
+  SELECT 
+    unioned.*,
+    MAX(CASE 
+      WHEN source != 'targets_actuals' AND snapshot_date < CURRENT_DATE() THEN snapshot_date 
+      ELSE NULL 
     END) OVER ()                                                        AS max_snapshot_date, -- We want to ensure we have the max_snapshot_date that comes from the actuals in every row but excluding the future dates we have in the targets data 
-  FLOOR((DATEDIFF(day, current_first_day_of_fiscal_quarter, max_snapshot_date) / 7)) 
+    FLOOR((DATEDIFF(day, current_first_day_of_fiscal_quarter, max_snapshot_date) / 7)) 
                                                                         AS most_recent_snapshot_week
-FROM unioned 
-WHERE snapshot_fiscal_quarter_date >= DATEADD(QUARTER, -9, CURRENT_DATE())
+  FROM unioned 
+  WHERE snapshot_fiscal_quarter_date >= DATEADD(QUARTER, -9, CURRENT_DATE())
+
 )
 
 SELECT final.*,
@@ -40,4 +42,3 @@ SELECT final.*,
 FROM final
 LEFT JOIN {{ ref('dim_date') }} dim_date
 ON final.max_snapshot_date = dim_date.date_actual
- 
