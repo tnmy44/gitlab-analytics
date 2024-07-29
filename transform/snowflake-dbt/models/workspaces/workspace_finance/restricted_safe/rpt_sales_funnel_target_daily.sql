6cf78ea58a994c2041ba934a8134b8b15a0427fb
Lis Vinueza <lvinueza@gitlab.com>
2024-07-29 17:58:19 +0200

    Remove duplicate column

--- transform/snowflake-dbt/models/workspaces/workspace_finance/restricted_safe/rpt_sales_funnel_target_daily.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_finance/restricted_safe/rpt_sales_funnel_target_daily.sql
@@ -5,7 +5,6 @@ SELECT
   DATE_TRUNC(month, dim_date.date_actual)  AS date_range_month,
   dim_date.date_id                         AS date_range_id,
   dim_date.fiscal_month_name_fy,
-  dim_date.fiscal_quarter_name_fy,
   dim_date.first_day_of_fiscal_quarter
 FROM {{ ref('mart_sales_funnel_target_daily') }}
 LEFT JOIN {{ ref('dim_date') }}
