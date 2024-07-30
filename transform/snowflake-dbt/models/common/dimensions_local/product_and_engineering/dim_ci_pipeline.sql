bd6d2a5f88bcd2b667a9714880c97a233d899540
Nicole Hervas <ngalang@gitlab.com>
2024-07-30 16:45:17 +0000

    Update 2 files
    - /transform/snowflake-dbt/models/common/dimensions_local/product_and_engineering/dim_ci_pipeline.sql
    - /transform/snowflake-dbt/models/common_prep/prep_ci_pipeline.sql

--- transform/snowflake-dbt/models/common/dimensions_local/product_and_engineering/dim_ci_pipeline.sql
+++ transform/snowflake-dbt/models/common/dimensions_local/product_and_engineering/dim_ci_pipeline.sql
@@ -29,26 +29,7 @@ WITh prep AS (
       lock_version, 
       auto_canceled_by_id, 
       pipeline_schedule_id, 
-      --- add enum from pipeline.rb
-      CASE 
-        WHEN ci_pipeline_source = 1 THEN 'push'
-        WHEN ci_pipeline_source = 2 THEN 'web'
-        WHEN ci_pipeline_source = 3 THEN 'trigger'
-        WHEN ci_pipeline_source = 4 THEN 'schedule'
-        WHEN ci_pipeline_source = 5 THEN 'api'
-        WHEN ci_pipeline_source = 6 THEN 'external'
-        WHEN ci_pipeline_source = 7 THEN 'pipeline'
-        WHEN ci_pipeline_source = 8 THEN 'chat'
-        WHEN ci_pipeline_source = 9 THEN 'webide'
-        WHEN ci_pipeline_source = 10 THEN 'merge_request_event'
-        WHEN ci_pipeline_source = 11 THEN 'external_pull_request_event'
-        WHEN ci_pipeline_source = 12 THEN 'parent_pipeline'
-        WHEN ci_pipeline_source = 13 THEN 'ondemand_dast_scan'
-        WHEN ci_pipeline_source = 14 THEN 'ondemande_dast_validation'
-        WHEN ci_pipeline_source = 15 THEN 'security_orchestration_policy'
-        WHEN ci_pipeline_source = 16 THEN 'container_registry_push'
-        ELSE NULL 
-        END as ci_pipeline_source,
+      ci_pipeline_source,
       config_source, 
       is_protected, 
       failure_reason_id,
