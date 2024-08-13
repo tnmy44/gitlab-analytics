ce67b57f073b6706363ff2481611bdd717ff97bb
Utkarsh <usinghal@gitlab.com>
2024-08-13 22:45:20 +0530

    fix invalid identifier error

--- transform/snowflake-dbt/models/common_mapping/map_namespace_subscription.sql
+++ transform/snowflake-dbt/models/common_mapping/map_namespace_subscription.sql
@@ -14,19 +14,16 @@
 
   SELECT
     dim_date.date_actual,
-    subscriptions.term_start_month,
-    subscriptions.term_end_month,
     subscriptions.dim_subscription_id,
     subscriptions.dim_subscription_id_original,
-    subscriptions.namespace_id                      AS dim_namespace_id,
+    subscriptions.namespace_id                     AS dim_namespace_id,
     subscriptions.dim_crm_account_id,
     subscriptions.subscription_version,
     subscriptions.subscription_created_date,
-    product_detail.dim_product_detail_id,
     product_detail.product_rate_plan_charge_name,
     product_detail.product_deployment_type,
     charges.charge_type,
-    product_tier.is_paid_tier
+    IFF(product_tier.product_ranking > 0, 1, 0)    AS is_paid_tier
   FROM subscriptions
   LEFT JOIN charges 
     ON charges.dim_subscription_id = subscriptions.dim_subscription_id
@@ -41,7 +38,6 @@
 
 )
 
--- map_namespace_subscription
 SELECT
   date_actual,
   dim_subscription_id,
@@ -52,8 +48,8 @@ SELECT
   product_rate_plan_charge_name,
   charge_type
 FROM joined
-WHERE is_paid_tier = 1 
-AND product_deployment_type = 'GitLab.com'
+WHERE product_deployment_type = 'GitLab.com'
+AND is_paid_tier = 1
 AND product_rate_plan_charge_name NOT IN (
   '1,000 CI Minutes',
   'Gitlab Storage 10GB - 1 Year',
@@ -64,7 +60,6 @@ AND charge_type != 'OneTime'
 -- picking most recent subscription version
 QUALIFY
   ROW_NUMBER() OVER(
-    PARTITION BY
-      dim_namespace_id, date_actual
+    PARTITION BY dim_namespace_id, date_actual
     ORDER BY subscription_created_date DESC, subscription_version DESC
   ) = 1
