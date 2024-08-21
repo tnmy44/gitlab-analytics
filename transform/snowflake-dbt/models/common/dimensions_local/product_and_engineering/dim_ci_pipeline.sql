a0f7f6202428b841c90a9c28ae72c2b019f4590e
Lis Vinueza <lvinueza@gitlab.com>
2024-08-21 15:13:25 +0200

    Lint

--- transform/snowflake-dbt/models/common/dimensions_local/product_and_engineering/dim_ci_pipeline.sql
+++ transform/snowflake-dbt/models/common/dimensions_local/product_and_engineering/dim_ci_pipeline.sql
@@ -1,47 +1,50 @@
-WITh prep AS (
+WITH prep AS (
 
-    SELECT *  FROM {{ ref('prep_ci_pipeline')}}
+  SELECT * 
+  FROM {{ ref('prep_ci_pipeline') }}
 
-), final AS (
+),
 
-    SELECT 
-      dim_ci_pipeline_id, 
+final AS (
 
-      --NATURAL KEY
-      ci_pipeline_id,
+  SELECT
+    dim_ci_pipeline_id,
 
-      -- FOREIGN KEYS
-      dim_project_id,
-      dim_namespace_id,
-      ultimate_parent_namespace_id,
-      dim_user_id,
-      created_date_id,
-      dim_plan_id,
-      merge_request_id,
+    --NATURAL KEY
+    ci_pipeline_id,
 
-      created_at, 
-      started_at, 
-      updated_at,
-      committed_at,
-      finished_at, 
-      ci_pipeline_duration_in_s, 
+    -- FOREIGN KEYS
+    dim_project_id,
+    dim_namespace_id,
+    ultimate_parent_namespace_id,
+    dim_user_id,
+    created_date_id,
+    dim_plan_id,
+    merge_request_id,
 
-      status, 
-      ref,
-      has_tag, 
-      yaml_errors, 
-      lock_version, 
-      auto_canceled_by_id, 
-      pipeline_schedule_id, 
-      ci_pipeline_source,
-      config_source, 
-      is_protected, 
-      failure_reason_id,
-      failure_reason,
-      ci_pipeline_internal_id,
-      is_deleted,
-      is_deleted_updated_at
-    FROM prep
+    created_at,
+    started_at,
+    updated_at,
+    committed_at,
+    finished_at,
+    ci_pipeline_duration_in_s,
+
+    status,
+    ref,
+    has_tag,
+    yaml_errors,
+    lock_version,
+    auto_canceled_by_id,
+    pipeline_schedule_id,
+    ci_pipeline_source,
+    config_source,
+    is_protected,
+    failure_reason_id,
+    failure_reason,
+    ci_pipeline_internal_id,
+    is_deleted,
+    is_deleted_updated_at
+  FROM prep
 
 )
 
