26eb97a3185d858d5a36cf541a6380d42acb22ce
michellecooper <mcooper@gitlab.com>
2024-08-22 08:29:04 -0700

    adjust sk

--- transform/snowflake-dbt/models/common/facts_sales_and_marketing/fct_behavior_website_page_view.sql
+++ transform/snowflake-dbt/models/common/facts_sales_and_marketing/fct_behavior_website_page_view.sql
@@ -67,8 +67,8 @@
       {{ dbt_utils.generate_surrogate_key(['event_id','page_view_end_at']) }}                               AS fct_behavior_website_page_view_sk,
 
       -- Foreign Keys
-      {{ dbt_utils.generate_surrogate_key(['page_url_host_path', 'app_id', 'page_url_scheme']) }}           AS dim_behavior_website_page_sk,
-      {{ dbt_utils.generate_surrogate_key(['referrer_url_host_path', 'app_id', 'referer_url_scheme']) }}    AS dim_behavior_referrer_page_sk,
+      {{ dbt_utils.generate_surrogate_key(['page_url', 'app_id', 'page_url_scheme']) }}                     AS dim_behavior_website_page_sk,
+      {{ dbt_utils.generate_surrogate_key(['referer_url', 'app_id', 'referer_url_scheme']) }}               AS dim_behavior_referrer_page_sk,
       page_views_w_clean_url.dim_namespace_id,
       page_views_w_clean_url.dim_project_id,
 
