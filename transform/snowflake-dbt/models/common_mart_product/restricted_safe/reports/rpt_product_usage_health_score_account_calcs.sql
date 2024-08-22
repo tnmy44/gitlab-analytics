9f7887279d7b5bfc74c3592cb44f8f6dc02adcb2
Jong Lee <jonglee@gitlab.com>
2024-08-22 18:05:22 +0000

    Update file rpt_product_usage_health_score_account_calcs.sql

--- transform/snowflake-dbt/models/common_mart_product/restricted_safe/reports/rpt_product_usage_health_score_account_calcs.sql
+++ transform/snowflake-dbt/models/common_mart_product/restricted_safe/reports/rpt_product_usage_health_score_account_calcs.sql
@@ -206,7 +206,7 @@ final AS (
   SELECT
     rpt_product_usage_health_score.* EXCLUDE (created_by, updated_by, model_created_date, model_updated_date, dbt_updated_at, dbt_created_at, primary_key),
     account_rollup_calculations.arr_month,
-    account_rollup_calculations.dim_crm_account_id as dim_crm_account_id_mart_arr_all,
+    account_rollup_calculations.dim_crm_account_id AS dim_crm_account_id_mart_arr_all,
     account_rollup_calculations.crm_account_name AS mart_arr_all_account_name,
     account_rollup_calculations.fy25_account_rank,
     account_rollup_calculations.is_fy25_top_100_account,
