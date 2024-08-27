2740552913a56c372313e6b03396eb09739960bf
Sushma  N <snalamaru@gitlab.com>
2024-08-27 17:23:18 +0000

    Update group by

--- transform/snowflake-dbt/models/workspaces/workspace_finance/restricted_safe/wk_fct_available_to_renew_subscription_linkage.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_finance/restricted_safe/wk_fct_available_to_renew_subscription_linkage.sql
@@ -32,7 +32,7 @@
       SUM(ARR) AS ARR, 
       SUM(Quantity) AS Quantity
     FROM  {{ ref('wk_fct_available_to_renew') }} 
-    {{ dbt_utils.group_by(n=19) }}
+    GROUP BY 1,2,3,4,5,6,8,9,10,11,12,13,14,15,16,17,18,19
 )
 
 {{ dbt_audit(
