e6e464265cc63d7a06e4071e66f7b9cdb0ae92b1
Paul Armstrong <parmstrong@gitlab.com>
2024-08-28 14:50:11 +0200

    Add better comments.

--- transform/snowflake-dbt/models/workspaces/workspace_data/dbt/dbt_gdpr_logs.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_data/dbt/dbt_gdpr_logs.sql
@@ -6,12 +6,16 @@ WITH gdpr_delete_logs AS (
 
 ), max_time_stamps AS (
 
+    -- Retrieve max timestamp by invocationID
     SELECT invocation_id, MAX(time_stamp) max_time_stamp
     FROM gdpr_delete_logs
     GROUP BY 1
 
 ), partition_times as (
 
+    -- Use specific log line to retrieve start of e-mail processing,
+    -- Then partition by emails,
+    -- Max times retrieved above to partition for the last email in the batch.
     SELECT
       logs.time_stamp,
       logs.info_msg,
@@ -23,15 +27,17 @@ WITH gdpr_delete_logs AS (
 
 ), final AS (
 
-SELECT
-    logs.time_stamp,
-    logs.info_msg,
-    RTRIM(REPLACE(REPLACE(REPLACE(partition_times.info_msg, '        SET email_sha = ''', ''), ''';', ''), '\n', '')) AS email_sha,
-    logs.invocation_id
-FROM gdpr_delete_logs logs
-JOIN partition_times
-    ON partition_times.invocation_id = logs.invocation_id
-    AND logs.time_stamp >= partition_times.time_stamp AND logs.time_stamp < next_time_stamp
+    -- Cleans up the log line line and joins by partition times to apply the e-mail SHA to all relevant rows
+    SELECT
+      logs.time_stamp,
+      logs.info_msg,
+      RTRIM(REPLACE(REPLACE(REPLACE(partition_times.info_msg, '        SET email_sha = ''', ''), ''';', ''), '\n', '')) AS email_sha,
+      logs.invocation_id
+    FROM gdpr_delete_logs logs
+    JOIN partition_times
+        ON partition_times.invocation_id = logs.invocation_id
+        AND logs.time_stamp >= partition_times.time_stamp AND logs.time_stamp < next_time_stamp
+
 )
 
 SELECT *
