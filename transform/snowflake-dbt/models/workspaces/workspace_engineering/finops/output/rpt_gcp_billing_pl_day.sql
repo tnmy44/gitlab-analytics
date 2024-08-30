3ad7d1514f39c1b1a1913c645c7e909a12765588
Clement Leroux <cleroux@gitlab.com>
2024-08-30 11:42:38 +0200

    add full path to input

--- transform/snowflake-dbt/models/workspaces/workspace_engineering/finops/output/rpt_gcp_billing_pl_day.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_engineering/finops/output/rpt_gcp_billing_pl_day.sql
@@ -116,22 +116,9 @@ grouping AS (
     combined_pk
 )
 
-,
-
-add_path AS (
-
-  SELECT
-    grouping.*,
-    project_full_path.full_path
-  FROM grouping
-  LEFT JOIN project_full_path ON (grouping.gcp_project_id = project_full_path.gcp_project_id 
-  AND DATE_TRUNC('day', grouping.date_day) >= DATE_TRUNC('day', project_full_path.first_created_at) 
-  AND DATE_TRUNC('day', grouping.date_day) <= DATEADD('day', -1, DATE_TRUNC('day', project_full_path.last_updated_at)))
-
-)
 
 SELECT
   *,
   {{ dbt_utils.generate_surrogate_key([ 'date_day', 'gcp_project_id', 'gcp_service_description', 'gcp_sku_description', 'infra_label', 'env_label', 'runner_label', 'folder_label', 'pl_category', 'from_mapping']) }} AS pl_pk
-FROM add_path
+FROM grouping
 
