48a8f0d2b2a046d9e36ebf20c739d2c25706e791
Justin Wong <juwong@gitlab.com>
2024-09-10 18:04:26 -0700

    init source models

---
+++ transform/snowflake-dbt/models/sources/level_up/level_up_assessment_attempt_answers_source.sql
@@ -0,0 +1,30 @@
+{{ config(
+    materialized='incremental',
+    unique_key='answer_id'
+) }}
+
+{{ level_up_incremental('assessment_attempts') }}
+
+parsed AS (
+  SELECT
+    intermediate.value['id']::VARCHAR  AS assessment_attempt_id,
+    answers.value['answer']::VARCHAR   AS answer,
+    answers.value['correct']::VARCHAR  AS is_correct,
+    answers.value['id']::VARCHAR       AS answer_id,
+    answers.value['question']::VARCHAR AS question,
+
+    uploaded_at
+  FROM intermediate
+  INNER JOIN LATERAL FLATTEN(input => intermediate.value['answers']) AS answers
+
+  -- remove dups in case 'raw' is reloaded
+  QUALIFY
+    ROW_NUMBER() OVER (
+      PARTITION BY
+        answer_id
+      ORDER BY
+        uploaded_at DESC
+    ) = 1
+)
+
+SELECT * FROM parsed
---
+++ transform/snowflake-dbt/models/sources/level_up/level_up_meetings_source.sql
@@ -0,0 +1,33 @@
+{{ config(
+    materialized='incremental',
+    unique_key='meeting_id'
+) }}
+
+{{ level_up_incremental('meetings') }}
+
+parsed AS (
+  SELECT
+    value['attendeeInfo']::VARCHAR    AS attendee_info,
+    value['course']['id']::VARCHAR    AS course_id,
+    value['course']['title']::VARCHAR AS course_title,
+    value['endDate']::TIMESTAMP       AS meeting_end_date,
+    value['id']::VARCHAR              AS meeting_id,
+    value['instructors']::VARIANT     AS instructors,
+    value['location']::VARCHAR        AS location,
+    value['startDate']::TIMESTAMP     AS meeting_start_date,
+    value['title']::VARCHAR           AS meeting_title,
+
+    uploaded_at
+  FROM intermediate
+
+  -- remove dups in case 'raw' is reloaded
+  QUALIFY
+    ROW_NUMBER() OVER (
+      PARTITION BY
+        meeting_id
+      ORDER BY
+        uploaded_at DESC
+    ) = 1
+)
+
+SELECT * FROM parsed
