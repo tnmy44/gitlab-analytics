3a63941662cb468c9bba5a6a7b25ef0cc1ff7843
Miles Russell <mdrussell@gitlab.com>
2024-09-11 13:18:58 -0500

    adding in mistakenly deleted field

--- transform/snowflake-dbt/models/common_prep/prep_ping_instance.sql
+++ transform/snowflake-dbt/models/common_prep/prep_ping_instance.sql
@@ -63,10 +63,15 @@
         WHEN original_edition = 'EEP'                                    THEN 'Premium'
         WHEN original_edition = 'EEU'                                    THEN 'Ultimate'
         ELSE NULL END                                                                                                                             AS product_tier,
- NULLIF(COALESCE(
-  raw_usage_data.raw_usage_data_payload,
-  usage_data.raw_usage_data_payload_reconstructed
-)['gitlab_dedicated'], -1)::BOOLEAN AS is_dedicated_metric,
+      NULLIF(
+        COALESCE(
+          raw_usage_data.raw_usage_data_payload,
+          usage_data.raw_usage_data_payload_reconstructed
+        )['gitlab_dedicated'], -1
+      )::BOOLEAN AS is_dedicated_metric,
+      IFF(hostname LIKE ANY ('%gitlab-dedicated.us%', '%gitlab-dedicated.com%', -- Production instances
+                              '%gitlab-dedicated.systems%', '%testpony.net%', '%gitlab-private.org%') -- beta, sandbox, test
+          , TRUE, FALSE)                                                                                                                          AS is_dedicated_hostname,
       IFF(is_dedicated_metric = TRUE OR is_dedicated_hostname = TRUE, TRUE, FALSE)                                                                AS is_saas_dedicated,
       CASE
         WHEN uuid = 'ea8bf810-1d6f-4a6a-b4fd-93e8cbd8b57f' THEN 'SaaS'
