4d747e18ab398b37dfcf414961004fc61680f703
michellecooper <mcooper@gitlab.com>
2024-09-18 13:21:17 -0700

    update valid to dates

--- transform/snowflake-dbt/models/common_mapping/map_installation_subscription_product.sql
+++ transform/snowflake-dbt/models/common_mapping/map_installation_subscription_product.sql
@@ -24,16 +24,22 @@ Do the following to begin mapping an installation to a subscription:
   SELECT
     prep_ping_instance.ping_created_at,
     prep_ping_instance.dim_installation_id,
-    COALESCE(sha256.dim_subscription_id, md5.dim_subscription_id)        AS dim_subscription_id,
+    COALESCE(
+      COALESCE(sha256.dim_subscription_id, md5.dim_subscription_id), 
+      prep_ping_instance.raw_usage_data_payload:license_subscription_id::TEXT
+      )                                                                                   AS dim_subscription_id,
     LEAD(ping_created_at) OVER (
       PARTITION BY dim_installation_id ORDER BY ping_created_at ASC
-      )                                                                  AS next_ping_date
+      )                                                                                   AS next_ping_date
   FROM prep_ping_instance
   LEFT JOIN prep_license AS md5
     ON prep_ping_instance.license_md5 = md5.license_md5
   LEFT JOIN prep_license AS sha256
     ON prep_ping_instance.license_sha256 = sha256.license_sha256
-  WHERE COALESCE(sha256.dim_subscription_id, md5.dim_subscription_id) IS NOT NULL
+  WHERE COALESCE(
+      COALESCE(sha256.dim_subscription_id, md5.dim_subscription_id), 
+      prep_ping_instance.raw_usage_data_payload:license_subscription_id::TEXT
+      ) IS NOT NULL
   
 ), ping_daily_mapping AS (
 
@@ -42,7 +48,7 @@ Expand the Sevice Ping records to the day grain.
 
 Assumptions:
 1. The dim_installation_id <> dim_subscription_id mapping is valid between the ping_created_at date and the next received ping date for that installation
-2. If no other pings have been received for this installation, this mapping is valid until the current date.
+2. If no other pings have been received for this installation, this mapping is valid until one week the last report created date since this data is received daily.
 */
 
   SELECT 
@@ -52,7 +58,7 @@ Assumptions:
   FROM base
   INNER JOIN prep_date
     ON base.ping_created_at <= prep_date.date_actual
-      AND COALESCE(base.next_ping_date, CURRENT_DATE()) > prep_date.date_actual
+      AND COALESCE(base.next_ping_date, DATEADD('day', 7, base.ping_created_at)) > prep_date.date_actual
 
 ), seat_link AS (
 
@@ -75,7 +81,7 @@ Expand the Seat Link data to the daily grain for each installation - subscriptio
 
 Assumptions:
 1. The dim_installation_id <> dim_subscription_id mapping is valid between the report_date and the next received report_date for that installation
-2. If no other Seat Link records have been received for this installation, this mapping is valid until the current date.
+2. If no other Seat Link records have been received for this installation, this mapping is valid until one day after the report date since this data is received daily.
 */
 
   SELECT
@@ -85,7 +91,7 @@ Assumptions:
   FROM seat_link
   INNER JOIN prep_date
     ON seat_link.report_date <= prep_date.date_actual
-    AND COALESCE(seat_link.next_report_date, CURRENT_DATE) > prep_date.date_actual
+    AND COALESCE(seat_link.next_report_date, DATEADD('day', 1, seat_link.report_date)) > prep_date.date_actual
 
 ), joined AS (
 
@@ -161,7 +167,7 @@ between the subscription_created_datetime (adjusted for the first version of a s
   LEFT JOIN prep_subscription AS subscriptions_ping
     ON joined.dim_subscription_id = subscriptions_ping.dim_subscription_id
   LEFT JOIN prep_subscription AS subscriptions_charge
-    ON prep_charge_mrr_daily_latest.subscription_name = subscriptions_charge.subscription_name
+    ON prep_charge_mrr_daily_latest.dim_subscription_id_original = subscriptions_charge.dim_subscription_id_original
       AND prep_charge_mrr_daily_latest.date_actual BETWEEN subscriptions_charge.subscription_created_datetime_adjusted AND subscriptions_charge.next_subscription_created_datetime
   LEFT JOIN prep_product_detail
     ON prep_charge_mrr_daily_latest.dim_product_detail_id = prep_product_detail.dim_product_detail_id
