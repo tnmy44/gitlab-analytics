fdab4a3dd8f69019ea8d8052ec11b4d0ea21d975
Emma Neuberger <eneuberger@gitlab.com>
2024-09-20 17:19:08 +0000

    Replace all duo_pro with duo - includes all duo subscription types

--- transform/snowflake-dbt/models/workspaces/workspace_product/reporting/rpt_duo_license_utilization_weekly.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_product/reporting/rpt_duo_license_utilization_weekly.sql
@@ -18,7 +18,7 @@
     ])
 }},
 
-all_duo_pro_weekly_seats AS (
+all_duo_weekly_seats AS (
 
 SELECT 
   arr_week
@@ -45,20 +45,20 @@ GROUP BY ALL
 
 ), 
 
-duo_pro_and_paired_tier AS ( --tier occurring concurrently with paid duo pro subscription
+duo_and_paired_tier AS ( --tier occurring concurrently with paid duo pro subscription
 
 SELECT 
-  duo_pro.*,
+  duo.*,
   detail.is_oss_or_edu_rate_plan,
   ARRAY_TO_STRING(ARRAY_AGG(DISTINCT SPLIT_PART(tier.product_tier_name, ' - ', 2)), ', ') -- multiple product tiers can show up within the same ARR reporting week
     AS paired_tier,
   IFF(paired_tier IN ('Premium, Ultimate', 'Ultimate, Premium'), 'Premium & Ultimate', paired_tier)
     AS clean_paired_tier, -- not able to sort within group while using SPLIT_PART function - using this method for standard results
-FROM all_duo_pro_weekly_seats AS duo_pro
+FROM all_duo_weekly_seats AS duo
 LEFT JOIN mart_arr_all_weekly AS tier -- joining to get tier occuring within same week as add on
-  ON duo_pro.reporting_week = tier.arr_week
-  AND duo_pro.dim_crm_account_id = tier.dim_crm_account_id
-  AND duo_pro.dim_subscription_id = tier.dim_subscription_id -- add on will be on the same subscription as the tier
+  ON duo.reporting_week = tier.arr_week
+  AND duo.dim_crm_account_id = tier.dim_crm_account_id
+  AND duo.dim_subscription_id = tier.dim_subscription_id -- add on will be on the same subscription as the tier
   AND tier.product_category = 'Base Products' --tiers only - not add ons or other charges
 LEFT JOIN dim_product_detail AS detail
   ON detail.dim_product_detail_id = tier.dim_product_detail_id
@@ -66,11 +66,11 @@ GROUP BY ALL
 
 ), 
 
-sm_dedicated_duo_pro_weekly_seats AS ( -- duo pro weekly seats associated entities -- dedicated and SM in one CTE due to the same type of product entity identifier used - dim_installation_id
+sm_dedicated_duo_weekly_seats AS ( -- duo pro weekly seats associated entities -- dedicated and SM in one CTE due to the same type of product entity identifier used - dim_installation_id
 --WEEK STARTING ON SUNDAY
 
   SELECT DISTINCT
-    duo_pro.*,
+    duo.*,
     m.dim_installation_id
       AS product_entity_id,
     'dim_installation_id'
@@ -79,22 +79,22 @@ sm_dedicated_duo_pro_weekly_seats AS ( -- duo pro weekly seats associated entiti
       AS is_product_entity_associated_w_subscription,
     MAX(m.major_minor_version_id)
       AS major_minor_version_id, --max major minor version within week
-    MAX(duo_pro.dp_seats)
-      AS duo_pro_seats -- max because left join can result in duplicate records
-  FROM duo_pro_and_paired_tier AS duo_pro
+    MAX(duo.dp_seats)
+      AS duo_seats -- max because left join can result in duplicate records
+  FROM duo_and_paired_tier AS duo
   LEFT JOIN mart_ping_instance_metric_weekly AS m -- joining to get installation id because that identifier is not in mart_arr
-    ON duo_pro.dim_subscription_id = m.latest_subscription_id
+    ON duo.dim_subscription_id = m.latest_subscription_id
     AND m.ping_created_date_week = reporting_week
-  WHERE duo_pro.product_deployment IN ('Self-Managed', 'Dedicated')
+  WHERE duo.product_deployment IN ('Self-Managed', 'Dedicated')
     AND reporting_week BETWEEN '2024-02-18' AND DATEADD(day, -8, DATE_TRUNC(WEEK, current_date)) -- last week sunday (not including current week)
   GROUP BY ALL
 
 ), 
 
-dotcom_duo_pro_weekly_seats AS ( -- duo pro weekly seats and associated entities 
+dotcom_duo_weekly_seats AS ( -- duo pro weekly seats and associated entities 
 
   SELECT DISTINCT
-    duo_pro.*,
+    duo.*,
     s.namespace_id
       AS product_entity_id,
     'ultimate_parent_namespace_id'
@@ -103,11 +103,11 @@ dotcom_duo_pro_weekly_seats AS ( -- duo pro weekly seats and associated entities
       AS is_product_entity_associated_w_subscription,
     MAX(m.major_minor_version_id)
       AS major_minor_version_id, --max major minor version within week
-    MAX(duo_pro.dp_seats)
-      AS duo_pro_seats -- max because left join can result in duplicate records
-  FROM duo_pro_and_paired_tier AS duo_pro
+    MAX(duo.dp_seats)
+      AS duo_seats -- max because left join can result in duplicate records
+  FROM duo_and_paired_tier AS duo
   INNER JOIN dim_subscription AS s -- joining to get namespace id because that identifier is not in mart_arr
-    ON duo_pro.dim_subscription_id = s.dim_subscription_id
+    ON duo.dim_subscription_id = s.dim_subscription_id
   LEFT JOIN mart_ping_instance_metric_weekly AS m-- for latest version
     ON m.ping_created_date_week = reporting_week
     AND m.dim_installation_id = '8b52effca410f0a380b0fcffaa1260e7' -- installation id for Gitlab.com
@@ -117,7 +117,7 @@ dotcom_duo_pro_weekly_seats AS ( -- duo pro weekly seats and associated entities
 
 ),
 
-duo_pro_seat_assignments AS ( -- CTE to get number of seats assigned per namepsace ID
+duo_seat_assignments AS ( -- CTE to get number of seats assigned per namepsace ID
 -- methodology used in:
 -- https://gitlab.com/gitlab-data/product-analytics/-/issues/1677
 -- AND https://10az.online.tableau.com/#/site/gitlab/workbooks/2252465/views
@@ -145,8 +145,8 @@ dotcom_chat_users AS ( -- gitlab.com chat weekly users with subacriptions
   SELECT
     DATEADD(day, -1, DATE_TRUNC(week, behavior_date))  --WEEK STARTING ON SUNDAY                 
       AS reporting_week,
-    duo_pro.product_entity_id,
-    duo_pro.product_entity_type,
+    duo.product_entity_id,
+    duo.product_entity_type,
     'duo'                                               
       AS unit_primitive_group,
     'chat'                                                  
@@ -154,8 +154,8 @@ dotcom_chat_users AS ( -- gitlab.com chat weekly users with subacriptions
     ZEROIFNULL(COUNT(DISTINCT gsc_pseudonymized_user_id))
       AS count_active_users
   FROM mart_behavior_structured_event, lateral flatten(input => contexts:data[0]:data:feature_enabled_by_namespace_ids) AS f  --GSC_FEATURE_ENABLED_BY_NAMESPACE_IDS is a VARCHAR datatype, so I need to use the value from contexts here
-  INNER JOIN dotcom_duo_pro_weekly_seats AS duo_pro
-    ON duo_pro.product_entity_id = f.value
+  INNER JOIN dotcom_duo_weekly_seats AS duo
+    ON duo.product_entity_id = f.value
   WHERE event_action = 'request_duo_chat_response'
     AND behavior_at >= '2024-04-11' -- event was implemented in production
     AND f.value IS NOT NULL
@@ -169,17 +169,17 @@ sm_dedicated_chat_users AS ( -- sm & dedicated chat 28d count unique users with 
   SELECT DISTINCT
     p.ping_created_date_week  
       AS reporting_week,
-    duo_pro.product_entity_id,
-    duo_pro.product_entity_type,
+    duo.product_entity_id,
+    duo.product_entity_type,
     'duo'                  
       AS unit_primitive_group,
     'chat'                     
       AS primitive,
     ZEROIFNULL(p.metric_value) 
       AS count_active_users
-  FROM sm_dedicated_duo_pro_weekly_seats AS duo_pro
+  FROM sm_dedicated_duo_weekly_seats AS duo
   INNER JOIN mart_ping_instance_metric_weekly AS p
-    ON duo_pro.product_entity_id = p.dim_installation_id
+    ON duo.product_entity_id = p.dim_installation_id
   WHERE p.metrics_path = 'redis_hll_counters.count_distinct_user_id_from_request_duo_chat_response_weekly'
     AND p.major_minor_version_id >= 1611 --metric instrumented for 1611
     AND p.metric_value > 0
@@ -192,8 +192,8 @@ dotcom_cs_users AS ( -- gitlab.com code_suggestions weekly users with subscripti
   SELECT
     DATEADD(day, -1, DATE_TRUNC(week, behavior_date))  --WEEK STARTING ON SUNDAY                 
       AS reporting_week,    
-    duo_pro.product_entity_id,
-    duo_pro.product_entity_type,
+    duo.product_entity_id,
+    duo.product_entity_type,
     'duo'                             
       AS unit_primitive_group,
     'code suggestions'                    
@@ -201,8 +201,8 @@ dotcom_cs_users AS ( -- gitlab.com code_suggestions weekly users with subscripti
     COUNT(DISTINCT gitlab_global_user_id) 
       AS count_active_users
   FROM rpt_behavior_code_suggestion_gateway_request, LATERAL FLATTEN(input => ultimate_parent_namespace_ids) AS f
-  INNER JOIN dotcom_duo_pro_weekly_seats AS duo_pro
-    ON duo_pro.product_entity_id = f.value::VARCHAR
+  INNER JOIN dotcom_duo_weekly_seats AS duo
+    ON duo.product_entity_id = f.value::VARCHAR
   WHERE event_action = 'suggestion_requested'
     AND app_id = 'gitlab_ai_gateway'
     AND behavior_at >= '2024-01-01' --first charge month
@@ -216,8 +216,8 @@ sm_dedicated_cs_users AS ( -- sm & dedicated chat code suggestions users with su
   SELECT
     DATEADD(day, -1, DATE_TRUNC(week, behavior_date))  --WEEK STARTING ON SUNDAY                 
       AS reporting_week, 
-    duo_pro.product_entity_id,
-    duo_pro.product_entity_type,
+    duo.product_entity_id,
+    duo.product_entity_type,
     'duo'                             
       AS unit_primitive_group,
     'code suggestions'                    
@@ -225,8 +225,8 @@ sm_dedicated_cs_users AS ( -- sm & dedicated chat code suggestions users with su
     COUNT(DISTINCT gitlab_global_user_id)  
       AS count_active_users
   FROM rpt_behavior_code_suggestion_gateway_request, LATERAL FLATTEN(input => dim_installation_ids) AS f
-  INNER JOIN sm_dedicated_duo_pro_weekly_seats AS duo_pro
-    ON duo_pro.product_entity_id = f.value::VARCHAR
+  INNER JOIN sm_dedicated_duo_weekly_seats AS duo
+    ON duo.product_entity_id = f.value::VARCHAR
   WHERE event_action = 'suggestion_requested'
     AND app_id = 'gitlab_ai_gateway'
     AND behavior_at >= '2024-01-01' -- first charge month
@@ -255,13 +255,13 @@ unit_primitive_group_product_usage AS ( --long format to accomodate more unit pr
 
 ),
 
-all_weekly_duo_pro_seats AS (
+all_weekly_duo_seats AS (
 
-  SELECT * FROM sm_dedicated_duo_pro_weekly_seats
+  SELECT * FROM sm_dedicated_duo_weekly_seats
 
   UNION ALL
 
-  SELECT * FROM dotcom_duo_pro_weekly_seats
+  SELECT * FROM dotcom_duo_weekly_seats
 
 ),
 
@@ -286,8 +286,8 @@ final AS (
     a.is_dp_subscription_paid,
     MAX(a.major_minor_version_id)                                                                  
       AS major_minor_version_id,
-    ZEROIFNULL(MAX(a.duo_pro_seats))                                                               
-      AS paid_duo_pro_seats,
+    ZEROIFNULL(MAX(a.duo_seats))                                                               
+      AS paid_duo_seats,
     MAX(CASE WHEN a.product_deployment = 'GitLab.com' THEN ZEROIFNULL(s.number_of_seats_assigned)
            ELSE null END)            
       AS count_seats_assigned,  -- only available for dotcom data - all SM/Dedicated deployments will show null  
@@ -296,23 +296,23 @@ final AS (
     ZEROIFNULL(MAX(IFF(u.primitive = 'code suggestions', ZEROIFNULL(u.count_active_users), NULL)))
       AS code_suggestions_active_users,
     ZEROIFNULL(MAX(count_active_users))
-      AS max_duo_pro_active_users,
-    ZEROIFNULL(max_duo_pro_active_users / paid_duo_pro_seats)
+      AS max_duo_active_users,
+    ZEROIFNULL(max_duo_active_users / paid_duo_seats)
       AS pct_usage_seat_utilization,
     IFF(pct_usage_seat_utilization > 1, 1, pct_usage_seat_utilization)
       AS standard_pct_usage_seat_utilization,
-    count_seats_assigned / paid_duo_pro_seats
+    count_seats_assigned / paid_duo_seats
       AS pct_assignment_seat_utilization,  -- only available for dotcom data - all SM/Dedicated deployments will show null     
     IFF(pct_assignment_seat_utilization > 1, 1,  pct_assignment_seat_utilization)
       AS standard_pct_assignment_seat_utilization,
     COALESCE(a.is_oss_or_edu_rate_plan, FALSE) 
       AS is_oss_or_edu_rate_plan
-  FROM all_weekly_duo_pro_seats AS a
+  FROM all_weekly_duo_seats AS a
   LEFT JOIN unit_primitive_group_product_usage AS u
     ON a.reporting_week = u.reporting_week
       AND TO_CHAR(u.product_entity_id) = TO_CHAR(a.product_entity_id) --installation id contains letters, namespace id only numbers - TO_CHAR prevents datatype errors
       AND a.product_entity_type = u.product_entity_type
-  LEFT JOIN duo_pro_seat_assignments AS s
+  LEFT JOIN duo_seat_assignments AS s
     ON TO_CHAR(s.namespace_id) = TO_CHAR(a.product_entity_id)
       AND a.product_entity_type = 'ultimate_parent_namespace_id'
       AND a.subscription_name = s.purchase_xid -- # .com seats assigned associated with a subscription
