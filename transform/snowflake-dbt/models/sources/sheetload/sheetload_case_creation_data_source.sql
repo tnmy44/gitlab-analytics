ca4f098e8de3e0531506d9a554330d4381af8101
Dennis van Rooijen <dvanrooijen@gitlab.com>
2024-09-24 13:24:12 +0000

    Update file sheetload_case_creation_data_source.sql

--- transform/snowflake-dbt/models/sources/sheetload/sheetload_case_creation_data_source.sql
+++ transform/snowflake-dbt/models/sources/sheetload/sheetload_case_creation_data_source.sql
@@ -8,18 +8,18 @@ WITH source AS (
 renamed AS (
 
   SELECT
- 	  TRY_TO_NUMBER(case_trigger_id)                   AS case_trigger_id
-	  case_trigger ::VARCHAR                           AS case_trigger
-	  owner_id::VARCHAR                                AS owner_id
-	  status::VARCHAR                                  AS status
-	  case_origin::VARCHAR                             AS case_origin
-	  type::VARCHAR                                    AS type
-    case_subject::VARCHAR                            AS case_subject
-	  case_reason::VARCHAR                             AS case_reason
-  	record_type_id::VARCHAR                          AS record_type_id
-    priority::VARCHAR                                AS priority
-    live::VARCHAR                                    AS live
-    case_cta::VARCHAR                                AS case_cta
+ 	  TRY_TO_NUMBER(case_trigger_id)                   AS case_trigger_id,
+	  case_trigger ::VARCHAR                           AS case_trigger,
+	  owner_id::VARCHAR                                AS owner_id,
+	  status::VARCHAR                                  AS status,
+	  case_origin::VARCHAR                             AS case_origin,
+	  type::VARCHAR                                    AS type,
+    case_subject::VARCHAR                            AS case_subject,
+	  case_reason::VARCHAR                             AS case_reason,
+  	record_type_id::VARCHAR                          AS record_type_id,
+    priority::VARCHAR                                AS priority,
+    live::VARCHAR                                    AS live,
+    case_cta::VARCHAR                                AS case_cta,
     case_context::VARCHAR                            AS case_context
   FROM source
 
