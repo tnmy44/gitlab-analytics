e44e186c48236a1920b39bbac1ab336982039253
Paul Armstrong <parmstrong@gitlab.com>
2024-09-27 08:38:04 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- transform/snowflake-dbt/models/sources/sheetload/sheetload_case_creation_data_source.sql
+++ transform/snowflake-dbt/models/sources/sheetload/sheetload_case_creation_data_source.sql
@@ -8,15 +8,15 @@ WITH source AS (
 renamed AS (
 
   SELECT
- 	  TRY_TO_NUMBER(case_trigger_id)                   AS case_trigger_id,
-	  case_trigger ::VARCHAR                           AS case_trigger,
-	  owner_id::VARCHAR                                AS owner_id,
-	  status::VARCHAR                                  AS status,
-	  case_origin::VARCHAR                             AS case_origin,
-	  type::VARCHAR                                    AS type,
+    TRY_TO_NUMBER(case_trigger_id)                   AS case_trigger_id,
+    case_trigger ::VARCHAR                           AS case_trigger,
+    owner_id::VARCHAR                                AS owner_id,
+    status::VARCHAR                                  AS status,
+    case_origin::VARCHAR                             AS case_origin,
+    type::VARCHAR                                    AS type,
     case_subject::VARCHAR                            AS case_subject,
-	  case_reason::VARCHAR                             AS case_reason,
-  	record_type_id::VARCHAR                          AS record_type_id,
+    case_reason::VARCHAR                             AS case_reason,
+    record_type_id::VARCHAR                          AS record_type_id,
     priority::VARCHAR                                AS priority,
     live::VARCHAR                                    AS live,
     case_cta::VARCHAR                                AS case_cta,
